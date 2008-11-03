2f65b39ee4f46b437a3c1afde42f78f572fab219
Tijs van der Storm <T.van.der.Storm@cwi.nl>
2008-11-03 14:01:35 +0000

    Regenerated with the forgotten labels in it.

--- src/org/meta_environment/rascal/ast/Assignment.java
+++ src/org/meta_environment/rascal/ast/Assignment.java
@@ -1,79 +1,112 @@
 package org.meta_environment.rascal.ast;
-
 import org.eclipse.imp.pdb.facts.ITree;
-
-public abstract class Assignment extends AbstractAST {
-	static public class Addition extends Assignment {
-		/* package */Addition(ITree tree) {
-			this.tree = tree;
-		}
-
-		public IVisitable accept(IASTVisitor visitor) {
-			return visitor.visitAssignmentAddition(this);
-		}
-	}
-
-	static public class Ambiguity extends Assignment {
-		private final java.util.List<org.meta_environment.rascal.ast.Assignment> alternatives;
-
-		public Ambiguity(
-				java.util.List<org.meta_environment.rascal.ast.Assignment> alternatives) {
-			this.alternatives = java.util.Collections
-					.unmodifiableList(alternatives);
-		}
-
-		public java.util.List<org.meta_environment.rascal.ast.Assignment> getAlternatives() {
-			return alternatives;
-		}
-	}
-
-	static public class Default extends Assignment {
-		/* package */Default(ITree tree) {
-			this.tree = tree;
-		}
-
-		public IVisitable accept(IASTVisitor visitor) {
-			return visitor.visitAssignmentDefault(this);
-		}
-	}
-
-	static public class Division extends Assignment {
-		/* package */Division(ITree tree) {
-			this.tree = tree;
-		}
-
-		public IVisitable accept(IASTVisitor visitor) {
-			return visitor.visitAssignmentDivision(this);
-		}
-	}
-
-	static public class Interesection extends Assignment {
-		/* package */Interesection(ITree tree) {
-			this.tree = tree;
-		}
-
-		public IVisitable accept(IASTVisitor visitor) {
-			return visitor.visitAssignmentInteresection(this);
-		}
-	}
-
-	static public class Product extends Assignment {
-		/* package */Product(ITree tree) {
-			this.tree = tree;
-		}
-
-		public IVisitable accept(IASTVisitor visitor) {
-			return visitor.visitAssignmentProduct(this);
-		}
-	}
-
-	static public class Substraction extends Assignment {
-		/* package */Substraction(ITree tree) {
-			this.tree = tree;
-		}
-
-		public IVisitable accept(IASTVisitor visitor) {
-			return visitor.visitAssignmentSubstraction(this);
-		}
-	}
+public abstract class Assignment extends AbstractAST
+{
+  static public class Default extends Assignment
+  {
+/* "=" -> Assignment {cons("Default")} */
+    private Default ()
+    {
+    }
+    /*package */ Default (ITree tree)
+    {
+      this.tree = tree;
+    }
+    public IVisitable accept (IASTVisitor visitor)
+    {
+      return visitor.visitAssignmentDefault (this);
+    }
+  }
+  static public class Ambiguity extends Assignment
+  {
+    private final java.util.List <
+      org.meta_environment.rascal.ast.Assignment > alternatives;
+    public Ambiguity (java.util.List <
+		      org.meta_environment.rascal.ast.Assignment >
+		      alternatives)
+    {
+      this.alternatives =
+	java.util.Collections.unmodifiableList (alternatives);
+    }
+    public java.util.List < org.meta_environment.rascal.ast.Assignment >
+      getAlternatives ()
+    {
+      return alternatives;
+    }
+  }
+  static public class Addition extends Assignment
+  {
+/* "+=" -> Assignment {cons("Addition")} */
+    private Addition ()
+    {
+    }
+    /*package */ Addition (ITree tree)
+    {
+      this.tree = tree;
+    }
+    public IVisitable accept (IASTVisitor visitor)
+    {
+      return visitor.visitAssignmentAddition (this);
+    }
+  }
+  static public class Substraction extends Assignment
+  {
+/* "-=" -> Assignment {cons("Substraction")} */
+    private Substraction ()
+    {
+    }
+    /*package */ Substraction (ITree tree)
+    {
+      this.tree = tree;
+    }
+    public IVisitable accept (IASTVisitor visitor)
+    {
+      return visitor.visitAssignmentSubstraction (this);
+    }
+  }
+  static public class Product extends Assignment
+  {
+/* "*=" -> Assignment {cons("Product")} */
+    private Product ()
+    {
+    }
+    /*package */ Product (ITree tree)
+    {
+      this.tree = tree;
+    }
+    public IVisitable accept (IASTVisitor visitor)
+    {
+      return visitor.visitAssignmentProduct (this);
+    }
+  }
+  static public class Division extends Assignment
+  {
+/* "/=" -> Assignment {cons("Division")} */
+    private Division ()
+    {
+    }
+    /*package */ Division (ITree tree)
+    {
+      this.tree = tree;
+    }
+    public IVisitable accept (IASTVisitor visitor)
+    {
+      return visitor.visitAssignmentDivision (this);
+    }
+  }
+  static public class Interesection extends Assignment
+  {
+/* "&=" -> Assignment {cons("Interesection")} */
+    private Interesection ()
+    {
+    }
+    /*package */ Interesection (ITree tree)
+    {
+      this.tree = tree;
+    }
+    public IVisitable accept (IASTVisitor visitor)
+    {
+      return visitor.visitAssignmentInteresection (this);
+    }
+  }
 }
--- src/org/meta_environment/rascal/ast/BasicType.java
+++ src/org/meta_environment/rascal/ast/BasicType.java
@@ -1,99 +1,142 @@
 package org.meta_environment.rascal.ast;
-
 import org.eclipse.imp.pdb.facts.ITree;
-
-public abstract class BasicType extends AbstractAST {
-	static public class Ambiguity extends BasicType {
-		private final java.util.List<org.meta_environment.rascal.ast.BasicType> alternatives;
-
-		public Ambiguity(
-				java.util.List<org.meta_environment.rascal.ast.BasicType> alternatives) {
-			this.alternatives = java.util.Collections
-					.unmodifiableList(alternatives);
-		}
-
-		public java.util.List<org.meta_environment.rascal.ast.BasicType> getAlternatives() {
-			return alternatives;
-		}
-	}
-
-	static public class Bool extends BasicType {
-		/* package */Bool(ITree tree) {
-			this.tree = tree;
-		}
-
-		public IVisitable accept(IASTVisitor visitor) {
-			return visitor.visitBasicTypeBool(this);
-		}
-	}
-
-	static public class Double extends BasicType {
-		/* package */Double(ITree tree) {
-			this.tree = tree;
-		}
-
-		public IVisitable accept(IASTVisitor visitor) {
-			return visitor.visitBasicTypeDouble(this);
-		}
-	}
-
-	static public class Int extends BasicType {
-		/* package */Int(ITree tree) {
-			this.tree = tree;
-		}
-
-		public IVisitable accept(IASTVisitor visitor) {
-			return visitor.visitBasicTypeInt(this);
-		}
-	}
-
-	static public class Loc extends BasicType {
-		/* package */Loc(ITree tree) {
-			this.tree = tree;
-		}
-
-		public IVisitable accept(IASTVisitor visitor) {
-			return visitor.visitBasicTypeLoc(this);
-		}
-	}
-
-	static public class String extends BasicType {
-		/* package */String(ITree tree) {
-			this.tree = tree;
-		}
-
-		public IVisitable accept(IASTVisitor visitor) {
-			return visitor.visitBasicTypeString(this);
-		}
-	}
-
-	static public class Term extends BasicType {
-		/* package */Term(ITree tree) {
-			this.tree = tree;
-		}
-
-		public IVisitable accept(IASTVisitor visitor) {
-			return visitor.visitBasicTypeTerm(this);
-		}
-	}
-
-	static public class Value extends BasicType {
-		/* package */Value(ITree tree) {
-			this.tree = tree;
-		}
-
-		public IVisitable accept(IASTVisitor visitor) {
-			return visitor.visitBasicTypeValue(this);
-		}
-	}
-
-	static public class Void extends BasicType {
-		/* package */Void(ITree tree) {
-			this.tree = tree;
-		}
-
-		public IVisitable accept(IASTVisitor visitor) {
-			return visitor.visitBasicTypeVoid(this);
-		}
-	}
+public abstract class BasicType extends AbstractAST
+{
+  static public class Bool extends BasicType
+  {
+/* "bool" -> BasicType {cons("Bool")} */
+    private Bool ()
+    {
+    }
+    /*package */ Bool (ITree tree)
+    {
+      this.tree = tree;
+    }
+    public IVisitable accept (IASTVisitor visitor)
+    {
+      return visitor.visitBasicTypeBool (this);
+    }
+  }
+  static public class Ambiguity extends BasicType
+  {
+    private final java.util.List < org.meta_environment.rascal.ast.BasicType >
+      alternatives;
+    public Ambiguity (java.util.List <
+		      org.meta_environment.rascal.ast.BasicType >
+		      alternatives)
+    {
+      this.alternatives =
+	java.util.Collections.unmodifiableList (alternatives);
+    }
+    public java.util.List < org.meta_environment.rascal.ast.BasicType >
+      getAlternatives ()
+    {
+      return alternatives;
+    }
+  }
+  static public class Int extends BasicType
+  {
+/* "int" -> BasicType {cons("Int")} */
+    private Int ()
+    {
+    }
+    /*package */ Int (ITree tree)
+    {
+      this.tree = tree;
+    }
+    public IVisitable accept (IASTVisitor visitor)
+    {
+      return visitor.visitBasicTypeInt (this);
+    }
+  }
+  static public class Double extends BasicType
+  {
+/* "double" -> BasicType {cons("Double")} */
+    private Double ()
+    {
+    }
+    /*package */ Double (ITree tree)
+    {
+      this.tree = tree;
+    }
+    public IVisitable accept (IASTVisitor visitor)
+    {
+      return visitor.visitBasicTypeDouble (this);
+    }
+  }
+  static public class String extends BasicType
+  {
+/* "str" -> BasicType {cons("String")} */
+    private String ()
+    {
+    }
+    /*package */ String (ITree tree)
+    {
+      this.tree = tree;
+    }
+    public IVisitable accept (IASTVisitor visitor)
+    {
+      return visitor.visitBasicTypeString (this);
+    }
+  }
+  static public class Value extends BasicType
+  {
+/* "value" -> BasicType {cons("Value")} */
+    private Value ()
+    {
+    }
+    /*package */ Value (ITree tree)
+    {
+      this.tree = tree;
+    }
+    public IVisitable accept (IASTVisitor visitor)
+    {
+      return visitor.visitBasicTypeValue (this);
+    }
+  }
+  static public class Term extends BasicType
+  {
+/* "term" -> BasicType {cons("Term")} */
+    private Term ()
+    {
+    }
+    /*package */ Term (ITree tree)
+    {
+      this.tree = tree;
+    }
+    public IVisitable accept (IASTVisitor visitor)
+    {
+      return visitor.visitBasicTypeTerm (this);
+    }
+  }
+  static public class Void extends BasicType
+  {
+/* "void" -> BasicType {cons("Void")} */
+    private Void ()
+    {
+    }
+    /*package */ Void (ITree tree)
+    {
+      this.tree = tree;
+    }
+    public IVisitable accept (IASTVisitor visitor)
+    {
+      return visitor.visitBasicTypeVoid (this);
+    }
+  }
+  static public class Loc extends BasicType
+  {
+/* "loc" -> BasicType {cons("Loc")} */
+    private Loc ()
+    {
+    }
+    /*package */ Loc (ITree tree)
+    {
+      this.tree = tree;
+    }
+    public IVisitable accept (IASTVisitor visitor)
+    {
+      return visitor.visitBasicTypeLoc (this);
+    }
+  }
 }
