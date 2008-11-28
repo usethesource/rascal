29c187f7b5ade7bb40badb5ac64dbc9a8f6d484a
Paul Klint <Paul.Klint@cwi.nl>
2008-11-28 11:16:37 +0000

    Added first batch of test cases; now working to a green light ...

--- src/test/RascalTests.java
+++ src/test/RascalTests.java
@@ -3,6 +3,8 @@ package test;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
+import junit.framework.TestCase;
+
 import org.eclipse.imp.pdb.facts.INode;
 import org.eclipse.imp.pdb.facts.IValue;
 import org.eclipse.imp.pdb.facts.impl.hash.ValueFactory;
@@ -13,7 +15,7 @@ import org.meta_environment.rascal.interpreter.Evaluator;
 import org.meta_environment.rascal.parser.ASTBuilder;
 import org.meta_environment.rascal.parser.Parser;
 
-public class RascalTests {
+public class RascalTests extends TestCase{
 	
 	private boolean runTest(String statement) throws IOException {
 		Parser parser = Parser.getInstance();
@@ -34,8 +36,74 @@ public class RascalTests {
 	//	}
 	}
 	
-	private void testAddition() throws IOException 
+	public void testBool() throws IOException
 	{
-		runTest("assert \"1\": 1 + 1 == 2");
+		assertTrue(runTest("true == true;"));
+		assertFalse(runTest("true == false;"));
+		assertTrue(runTest("true != false;"));	
+		
+		assertTrue(runTest("(!true) == false;"));
+		assertTrue(runTest("(!false) == true;"));
+		
+		assertTrue(runTest("true && true == true;"));	
+		assertTrue(runTest("true && false == false;"));	
+		assertTrue(runTest("false && true == false;"));	
+		assertTrue(runTest("false && false == false;"));	
+		
+		assertTrue(runTest("true || true == true;"));	
+		assertTrue(runTest("true || false == true;"));	
+		assertTrue(runTest("false || true == true;"));	
+		assertTrue(runTest("false || false == false;"));	
+		
+
 	}
+	
+	public void testInt() throws IOException 
+	{
+		assertTrue(runTest("1 == 1;"));
+		assertTrue(runTest("1 != 2;"));
+		
+		assertTrue(runTest("-1 == -1;"));
+		assertTrue(runTest("-1 != 1;"));
+		
+		assertTrue(runTest("1 + 1 == 2;"));
+//		assertTrue(runTest("-1 + 2 == 1;"));
+		assertTrue(runTest("1 + -12 == -1;"));
+		
+		assertTrue(runTest("1 <= 2;"));
+		assertTrue(runTest("2 <= 2;"));
+		assertFalse(runTest("2 <= 1;"));
+		
+		assertTrue(runTest("1 < 2;"));
+		assertFalse(runTest("2 < 2;"));
+		
+		assertTrue(runTest("2 >= 1;"));
+		assertTrue(runTest("2 >= 2;"));
+		assertFalse(runTest("1 >= 2;"));
+		
+		assertTrue(runTest("2 > 1;"));
+		assertFalse(runTest("2 > 2;"));
+		assertFalse(runTest("1 > 2;"));
+		
+		
+		
+	}
+	
+	public void testDouble() throws IOException 
+	{
+		assertTrue(runTest("1.5 == 1.5;"));
+		assertFalse(runTest("1.5 == 2.5;"));
+	}
+	
+	public void testList() throws IOException 
+	{
+		assertTrue(runTest("[1] == [1];"));
+		assertFalse(runTest("[1] == [2];"));
+		
+		assertTrue(runTest("[1, 2] == [1, 2];"));
+		assertFalse(runTest("[1, 2] == [2, 1];"));
+		assertTrue(runTest("[1, 2] + [3, 4, 5] == [1, 2, 3, 4, 5];"));	
+		
+	}
+	
 }
