0fa70f08f58fa8a7fdc47d16c086a34ba8fad187
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2008-12-01 14:04:53 +0000

    fixed parse errors in tests activated all commented out tests

--- src/test/RascalTests.java
+++ src/test/RascalTests.java
@@ -49,8 +49,8 @@ public class RascalTests extends TestCase{
 		
 		assertTrue(runTest("true && true == true;"));	
 		assertTrue(runTest("true && false == false;"));	
-//		assertTrue(runTest("false && true == false;"));	 //TODO ok in shell
-//		assertTrue(runTest("false && false == false;"));	
+		assertTrue(runTest("false && true == false;"));	 
+		assertTrue(runTest("false && false == false;"));	
 		
 		assertTrue(runTest("true || true == true;"));	
 		assertTrue(runTest("true || false == true;"));	
@@ -66,9 +66,6 @@ public class RascalTests extends TestCase{
 		assertTrue(runTest("true <==> false == false;"));	
 		assertTrue(runTest("false <==> true == false;"));	
 		assertTrue(runTest("false <==> false == true;"));
-		
-		
-
 	}
 	
 	public void testInt() throws IOException 
@@ -81,34 +78,34 @@ public class RascalTests extends TestCase{
 		
 		assertTrue(runTest("1 + 1 == 2;"));
 		assertTrue(runTest("-1 + 2 == 1;"));
-//		assertTrue(runTest("1 + -2 == -1;"));
+		assertTrue(runTest("1 + -2 == -1;"));
 		
 		assertTrue(runTest("2 - 1 == 1;"));	
 		assertTrue(runTest("2 - 3 == -1;"));	
-//		assertTrue(runTest("2 - -1 == 3;"));	
-//		assertTrue(runTest("-2 - 1 == -3;"));	
+		assertTrue(runTest("2 - -1 == 3;"));	
+		assertTrue(runTest("-2 - 1 == -3;"));	
 		
 		assertTrue(runTest("2 * 3 == 6;"));	
-//		assertTrue(runTest("-2 * 3 == -6;"));	
-//		assertTrue(runTest("2 * -3 == -6;"));
-//		assertTrue(runTest("-2 * -3 == 6;"));	
+		assertTrue(runTest("-2 * 3 == -6;"));	
+		assertTrue(runTest("2 * -3 == -6;"));
+		assertTrue(runTest("-2 * -3 == 6;"));	
 		
 		assertTrue(runTest("8 / 4 == 2;"));	
-//		assertTrue(runTest("-8 / 4 == -2;"));
-//		assertTrue(runTest("8 / -4 == -2;"));	
-//		assertTrue(runTest("-8 / -4 == 2;"));
+		assertTrue(runTest("-8 / 4 == -2;"));
+		assertTrue(runTest("8 / -4 == -2;"));	
+		assertTrue(runTest("-8 / -4 == 2;"));
 		
 		assertTrue(runTest("7 / 2 == 3;"));	
-//		assertTrue(runTest("-7 / 2 == -3;"));
-//		assertTrue(runTest("7 / -2 == -3;"));	
-//		assertTrue(runTest("-7 / -2 == 3;"));	
+		assertTrue(runTest("-7 / 2 == -3;"));
+		assertTrue(runTest("7 / -2 == -3;"));	
+		assertTrue(runTest("-7 / -2 == 3;"));	
 		
 		assertTrue(runTest("0 / 5 == 0;"));	
 		assertTrue(runTest("5 / 1 == 5;"));	
 		
 		assertTrue(runTest("5 % 2 == 1;"));	
-//		assertTrue(runTest("-5 % 2 == -1;"));
-//		assertTrue(runTest("5 % -2 == -1;"));		
+		assertTrue(runTest("-5 % 2 == -1;"));
+		assertTrue(runTest("5 % -2 == 1;"));		
 		
 		assertTrue(runTest("-2 <= -1;"));
 		assertTrue(runTest("-2 <= 1;"));
@@ -133,7 +130,7 @@ public class RascalTests extends TestCase{
 		assertFalse(runTest("2 > 2;"));
 		assertFalse(runTest("1 > 2;"));
 		
-//		assertTrue(runTest("(3 > 2 ? 3 : 2) == 3;"));
+		assertTrue(runTest("(3 > 2 ? 3 : 2) == 3;"));
 		
 	}
 	
@@ -147,27 +144,27 @@ public class RascalTests extends TestCase{
 		
 		assertTrue(runTest("1.0 + 1.0 == 2.0;"));
 		assertTrue(runTest("-1.0 + 2.0 == 1.0;"));
-//		assertTrue(runTest("1.0 + -2.0 == -1.0;"));
+		assertTrue(runTest("1.0 + -2.0 == -1.0;"));
 		
 		assertTrue(runTest("2.0 - 1.0 == 1.0;"));	
-//		assertTrue(runTest("2.0 - 3 == -1.0;"));	
-//		assertTrue(runTest("2.0 - -1.0 == 3;"));	
-//		assertTrue(runTest("-2.0 - 1.0 == -3;"));	
+		assertTrue(runTest("2.0 - 3 == -1.0;"));	
+		assertTrue(runTest("2.0 - -1.0 == 3.0;"));	
+		assertTrue(runTest("-2.0 - 1.0 == -3.0;"));	
 		
 		assertTrue(runTest("2.0 * 3.0 == 6.0;"));	
 		assertTrue(runTest("-2.0 * 3.0 == -6.0;"));	
-//		assertTrue(runTest("2.0 * -3.0 == -6.0;"));
-//		assertTrue(runTest("-2.0 * -3.0 == 6.0;"));	
+		assertTrue(runTest("2.0 * -3.0 == -6.0;"));
+		assertTrue(runTest("-2.0 * -3.0 == 6.0;"));	
 		
 		assertTrue(runTest("8.0 / 4.0 == 2.0;"));	
-//		assertTrue(runTest("-8.0 / 4.0 == -2.0;"));
-//		assertTrue(runTest("8.0 / -4.0 == -2.0;"));	
-//		assertTrue(runTest("-8.0 / -4.0 == 2.0;"));
+		assertTrue(runTest("-8.0 / 4.0 == -2.0;"));
+		assertTrue(runTest("8.0 / -4.0 == -2.0;"));	
+		assertTrue(runTest("-8.0 / -4.0 == 2.0;"));
 		
 		assertTrue(runTest("7.0 / 2.0 == 3.5;"));	
-//		assertTrue(runTest("-7.0 / 2.0 == -3.5;"));
-//		assertTrue(runTest("7.0 / -2.0 == -3.5;"));	
-//		assertTrue(runTest("-7.0 / -2.0 == 3.5;"));	
+		assertTrue(runTest("-7.0 / 2.0 == -3.5;"));
+		assertTrue(runTest("7.0 / -2.0 == -3.5;"));	
+		assertTrue(runTest("-7.0 / -2.0 == 3.5;"));	
 		
 		assertTrue(runTest("0.0 / 5.0 == 0.0;"));	
 		assertTrue(runTest("5.0 / 1.0 == 5.0;"));	
@@ -195,7 +192,7 @@ public class RascalTests extends TestCase{
 		assertFalse(runTest("2.0 > 2.0;"));
 		assertFalse(runTest("1.0 > 2.0;"));
 		
-//		assertTrue(runTest("3.5 > 2.5 ? 3.5 : 2.5 == 3.5;"));
+		assertTrue(runTest("3.5 > 2.5 ? 3.5 : 2.5 == 3.5;"));
 	}
 	
 	public void testString() throws IOException {
@@ -273,7 +270,7 @@ public class RascalTests extends TestCase{
 		assertTrue(runTest("2 in [1, 2, 3];"));
 		assertTrue(runTest("3 notin [2, 4, 6];"));
 		
-//		assertTrue(runTest("2 > 3 ? [1,2] : [1,2,3] == [1,2,3];"));
+		assertTrue(runTest("2 > 3 ? [1,2] : [1,2,3] == [1,2,3];"));
 	}
 	
 	public void testSet() throws IOException {
@@ -296,24 +293,24 @@ public class RascalTests extends TestCase{
 		assertTrue(runTest("{{}, {}} == {{}};"));
 		assertTrue(runTest("{{}, {}, {}} == {{}};"));
 		
-//		assertTrue(runTest("{{1, 2}, {3,4}} == {{2,1}, {4,3}};"));	
+		assertTrue(runTest("{{1, 2}, {3,4}} == {{2,1}, {4,3}};"));	
 	
 		assertTrue(runTest("{} + {} == {};"));
 		assertTrue(runTest("{1, 2, 3} + {} == {1, 2, 3};"));
 		assertTrue(runTest("{} + {1, 2, 3} == {1, 2, 3};"));
 		assertTrue(runTest("{1, 2} + {3, 4, 5} == {1, 2, 3, 4, 5};"));	
 		assertTrue(runTest("{1, 2, 3, 4} + {3, 4, 5} == {1, 2, 3, 4, 5};"));
-//		assertTrue(runTest("{{1, 2}, {3,4}} + {{5,6}} == {{1,2},{3,4},{5,6};"));	
+		assertTrue(runTest("{{1, 2}, {3,4}} + {{5,6}} == {{1,2},{3,4},{5,6}};"));	
 		
 		assertTrue(runTest("{} - {} == {};"));
 		assertTrue(runTest("{1, 2, 3} - {} == {1, 2, 3};"));
 		assertTrue(runTest("{} - {1, 2, 3} == {};"));
 		assertTrue(runTest("{1, 2, 3} - {3, 4, 5} == {1, 2};"));	
 		assertTrue(runTest("{1, 2, 3, 4} - {1, 2, 3, 4, 5} == {};"));
-//		assertTrue(runTest("{{1, 2}, {3,4}, {5,6}} {{3,4}} == {{1,2}, {5,6};"));
+		assertTrue(runTest("{{1, 2}, {3,4}, {5,6}} - {{3,4}} == {{1,2}, {5,6}};"));
 		
 		assertTrue(runTest("{} & {} == {};"));
-//		assertTrue(runTest("{1, 2, 3} & {} == {};")); //TODO: ok in shell but wrong here
+		assertTrue(runTest("{1, 2, 3} & {} == {};"));
 		assertTrue(runTest("{} & {1, 2, 3} == {};"));
 		assertTrue(runTest("{1, 2, 3} & {3, 4, 5} == {3};"));	
 		assertTrue(runTest("{1, 2, 3, 4} & {3, 4, 5} == {3, 4};"));	
@@ -325,23 +322,23 @@ public class RascalTests extends TestCase{
 		assertTrue(runTest("{2, 1} <= {1, 2, 3};"));
 		assertTrue(runTest("{2, 1} <= {2, 1, 0};"));
 	
-//		assertTrue(runTest("{} < {1};"));
-//		assertTrue(runTest("{2, 1} < {2, 1, 3};"));
+		assertTrue(runTest("{} < {1};"));
+		assertTrue(runTest("{2, 1} < {2, 1, 3};"));
 	
 		assertTrue(runTest("{} >= {};"));
 		assertTrue(runTest("{1} >= {};"));
 		assertTrue(runTest("{2, 3} >= {2};"));
 	
-//		assertTrue(runTest("{1} > {};"));
-//		assertTrue(runTest("{2, 1, 3} > {2, 3};"));
+		assertTrue(runTest("{1} > {};"));
+		assertTrue(runTest("{2, 1, 3} > {2, 3};"));
 		
 		assertTrue(runTest("2 in {1, 2, 3};"));
-//		assertTrue(runTest("{4,3} in {{1, 2}, {3,4}, {5,6}};"));
+		assertTrue(runTest("{4,3} in {{1, 2}, {3,4}, {5,6}};"));
 		
 		assertTrue(runTest("5 notin {1, 2, 3};"));
-//		assertTrue(runTest("{7,8} notin {{1, 2}, {3,4}, {5,6}};"));
+		assertTrue(runTest("{7,8} notin {{1, 2}, {3,4}, {5,6}};"));
 		
-//		assertTrue(runTest("3 > 2 ? {1,2} : {1,2,3} == {1,2,3};"));
+		assertTrue(runTest("3 > 2 ? {1,2} : {1,2,3} == {1,2};"));
 	}
 	
 	public void testMap() throws IOException {
@@ -371,9 +368,9 @@ public class RascalTests extends TestCase{
 		assertTrue(runTest("(1:10) <= (1:10, 2:20);"));
 		
 		assertFalse(runTest("() < ();"));
-//		assertTrue(runTest("() < (1:10);"));
+		assertTrue(runTest("() < (1:10);"));
 		assertFalse(runTest("(1:10) < (1:10);"));
-//		assertTrue(runTest("(1:10) < (1:10, 2:20);"));
+		assertTrue(runTest("(1:10) < (1:10, 2:20);"));
 		
 		assertTrue(runTest("() >= ();"));
 		assertTrue(runTest("(1:10) >= ();"));
@@ -381,9 +378,9 @@ public class RascalTests extends TestCase{
 		assertTrue(runTest("(1:10, 2:20) >= (1:10);"));
 		
 		assertFalse(runTest("() > ();"));
-//		assertTrue(runTest("(1:10) > ();"));
+		assertTrue(runTest("(1:10) > ();"));
 		assertFalse(runTest("(1:10) > (1:10);"));
-//		assertTrue(runTest("(1:10, 2:20) > (1:10);"));
+		assertTrue(runTest("(1:10, 2:20) > (1:10);"));
 		
 		
 		assertTrue(runTest("20 in (1:10, 2:20);"));
@@ -409,7 +406,7 @@ public class RascalTests extends TestCase{
 		assertTrue(runTest("{<1,10>, <2,20>} == {<1,10>, <2,20>};"));
 		assertTrue(runTest("{<1,10>, <2,20>, <3,30>} == {<1,10>, <2,20>, <3,30>};"));
 		assertTrue(runTest("{<1,2,3>, <4,5,6>} == {<4,5,6>, <1,2,3>};"));
-//		assertTrue(runTest("{<1,2,3,4>, <4,5,6,7>} == {<4,5,6,7>, <1,2,3,4>}};"));
+		assertTrue(runTest("{<1,2,3,4>, <4,5,6,7>} == {<4,5,6,7>, <1,2,3,4>};"));
 		
 		assertTrue(runTest("{} != {<1,2>, <3,4>};"));
 		assertFalse(runTest("{<1,2>, <3,4>} == {};"));
@@ -428,7 +425,7 @@ public class RascalTests extends TestCase{
 		assertTrue(runTest("{} - {<1,10>}  == {};"));
 		assertTrue(runTest("{<1,10>, <2,20>} - {<2,20>, <3,30>} == {<1,10>};"));
 		
-//		assertTrue(runTest("{<1,10>} & {} == {};"));
+		assertTrue(runTest("{<1,10>} & {} == {};"));
 		assertTrue(runTest("{} & {<1,10>}  == {};"));
 		assertTrue(runTest("{<1,10>, <2,20>} & {<2,20>, <3,30>} == {<2,20>};"));
 		assertTrue(runTest("{<1,2,3,4>, <2,3,4,5>} & {<2,3,4,5>,<3,4,5,6>} == {<2,3,4,5>};"));
@@ -439,9 +436,9 @@ public class RascalTests extends TestCase{
 		assertTrue(runTest("<4,40> notin {<1,10>, <2,20>, <3,30>};"));
 		assertTrue(runTest("<1,2,4> notin {<1,2,3>, <4,5,6>};"));
 		
-//		assertTrue(runTest("{} o {} == {};"));
-//		assertTrue(runTest("{<1,10>,<2,20>} o {} == {};"));
-//		assertTrue(runTest("{} o {<10,100>, <20,200>} == {};"));
+		assertTrue(runTest("{} o {} == {};"));
+		assertTrue(runTest("{<1,10>,<2,20>} o {} == {};"));
+		assertTrue(runTest("{} o {<10,100>, <20,200>} == {};"));
 		assertTrue(runTest("{<1,10>,<2,20>} o {<10,100>, <20,200>} == {<1,100>, <2,200>};"));
 		
 		assertTrue(runTest("{<1,2>, <2,3>, <3,4>} + == {<1,2>, <2,3>, <3,4>, <1, 3>, <2, 4>, <1, 4>};"));
@@ -454,8 +451,8 @@ public class RascalTests extends TestCase{
 	}
 	
 	public void testOther() throws IOException {
-		assertTrue(runTest("1 ? 13 == 1;"));
-		assertTrue(runTest("x ? 13 == 13;"));
-		assertTrue(runTest("x = 3; x ? 13 == 13;"));
+		assertTrue(runTest("1 =? 13 == 1;"));
+		assertTrue(runTest("x =? 13 == 13;"));
+		assertTrue(runTest("x = 3; x =? 13 == 13;"));
 	}
 }
