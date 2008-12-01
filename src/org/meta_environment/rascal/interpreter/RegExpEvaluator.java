91a817902e4d23e184dbad9589f899bd19c6a6d1
Paul Klint <Paul.Klint@cwi.nl>
2008-12-01 15:01:47 +0000

    Implemented RegExps.

--- src/org/meta_environment/rascal/interpreter/RegExpEvaluator.java
+++ src/org/meta_environment/rascal/interpreter/RegExpEvaluator.java
@@ -1,6 +1,12 @@
 package org.meta_environment.rascal.interpreter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.eclipse.imp.pdb.facts.IValueFactory;
 import org.meta_environment.rascal.ast.NullASTVisitor;
@@ -10,25 +16,43 @@ import org.meta_environment.rascal.ast.RegExp.Lexical;
 
 class RegExpResult {
 	String RegExpAsString;
-	String modifier = "";
+	Character modifier = null;
 	List<String> patternVars;
+	Matcher matcher = null;
 	
 	RegExpResult(String s){
 		RegExpAsString = s;
+		modifier = null;
+		patternVars = null;
+	}
+	
+	RegExpResult(String s, Character mod, List<String> names){
+		RegExpAsString = s;
+		modifier = mod;
+		patternVars = names;
 	}
 	
 	public String toString(){
-		return "RegExpResult(" + RegExpAsString + ")";
+		return "RegExpResult(" + RegExpAsString + ", " + modifier + ", " + patternVars + ")";
+	}
+	
+	public boolean matches(String s){
+		Pattern pat = Pattern.compile(RegExpAsString);
+		matcher = pat.matcher(s);
+		return matcher.matches();
+	}
+	public Map<String,String> getBindings(){
+		Map<String,String> map = new HashMap<String,String>();
+		int k = 1;
+		for(String nm : patternVars){
+			map.put(nm, matcher.group(k));
+			k++;
+		}
+		return map;
 	}
 }
 
 public class RegExpEvaluator extends NullASTVisitor<RegExpResult> {
-	
-	private int varCount = 0;
-	
-	RegExpEvaluator(){
-		
-	}
 	
 	
 	public RegExpResult visitExpressionLiteral(Literal x) {
@@ -50,8 +74,33 @@ public class RegExpEvaluator extends NullASTVisitor<RegExpResult> {
 	@Override
 	public RegExpResult visitRegExpLiteralLexical(
 			org.meta_environment.rascal.ast.RegExpLiteral.Lexical x) {
-		System.err.println("visitRegExpLiteralLexical: " + x.getString() + "; tree: " + x.getTree());
-		return new RegExpResult(x.getString());
+		System.err.println("visitRegExpLiteralLexical: " + x.getString()
+				);
+
+		String subject = x.getString();
+		Character modifier = null;
+		
+		int start = 1;
+		int end = subject.length()-1;
+		if(subject.charAt(end) != '/'){
+			modifier = subject.charAt(end);
+			end--;
+		}
+		
+		Pattern replacePat = Pattern.compile("<([a-zA-Z0-9]+):([^>]*)>");
+		Matcher m = replacePat.matcher(subject);
+		
+		String resultRegExp = "";
+		List<String> names = new ArrayList<String>();
+
+		while(m.find()){
+			String varName = m.group(1);
+			names.add(varName);
+			resultRegExp += subject.substring(start, m.start(0)) + "(" + m.group(2) + ")";
+			start = m.end(0);
+		}
+		resultRegExp += subject.substring(start, end);
+		return new RegExpResult(resultRegExp, modifier, names);
 	}
 	
 	@Override
@@ -72,7 +121,6 @@ public class RegExpEvaluator extends NullASTVisitor<RegExpResult> {
 	public RegExpResult visitNamedBackslashLexical(
 			org.meta_environment.rascal.ast.NamedBackslash.Lexical x) {
 		System.err.println("visitNamedBackslashLexical: " + x.getString());
-
 		return new RegExpResult(x.getString());
 	}
 	
