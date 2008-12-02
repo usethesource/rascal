36e955e42b3968386c7ce3529a10ffd97f943b95
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2008-12-02 18:32:07 +0000

    introduced RascalBug exception for marking bugs introduced new implementation of AssignableEvaluator added visit methods for Assignables to compute the current value of an assignable (needed in AssignableEvaluator)

---
+++ src/org/meta_environment/rascal/interpreter/AssignableEvaluator.java
@@ -0,0 +1,145 @@
+package org.meta_environment.rascal.interpreter;
+
+import org.eclipse.imp.pdb.facts.IBool;
+import org.eclipse.imp.pdb.facts.IInteger;
+import org.eclipse.imp.pdb.facts.IList;
+import org.eclipse.imp.pdb.facts.IMap;
+import org.eclipse.imp.pdb.facts.INode;
+import org.eclipse.imp.pdb.facts.ITuple;
+import org.eclipse.imp.pdb.facts.IValue;
+import org.eclipse.imp.pdb.facts.type.MapType;
+import org.eclipse.imp.pdb.facts.type.TupleType;
+import org.eclipse.imp.pdb.facts.type.Type;
+import org.meta_environment.rascal.ast.Assignable;
+import org.meta_environment.rascal.ast.NullASTVisitor;
+import org.meta_environment.rascal.ast.Assignable.Annotation;
+import org.meta_environment.rascal.ast.Assignable.Constructor;
+import org.meta_environment.rascal.ast.Assignable.FieldAccess;
+import org.meta_environment.rascal.ast.Assignable.IfDefined;
+import org.meta_environment.rascal.ast.Assignable.Subscript;
+import org.meta_environment.rascal.ast.Assignable.Tuple;
+import org.meta_environment.rascal.ast.Assignable.Variable;
+
+/**
+ * Implements assignments in their different shapes, using value lookup
+ * implemented by Evaluator.
+ * TODO: does not implement type checking completely
+ */
+public class AssignableEvaluator extends NullASTVisitor<EvalResult> {
+    private EvalResult value;
+    private final EnvironmentStack env;
+    private final Evaluator eval;
+    
+	public AssignableEvaluator(EnvironmentStack env, EvalResult value, Evaluator eval) {
+		this.value = value;
+		this.env = env;
+		this.eval = eval;
+	}  
+	
+	@Override
+	public EvalResult visitAssignableVariable(Variable x) {
+		env.storeVariable(x.getQualifiedName().toString(), value);
+		return value;
+	}
+	
+	@Override
+	public EvalResult visitAssignableAnnotation(Annotation x) {
+		String label = x.getAnnotation().toString();
+		EvalResult result = x.getReceiver().accept(eval);
+		
+		result.value = result.value.setAnnotation(label, result.value);
+		
+		this.value = result;
+		return x.getReceiver().accept(this);
+	}
+	
+	@Override
+	public EvalResult visitAssignableSubscript(Subscript x) {
+		EvalResult rec = x.getReceiver().accept(eval);
+		EvalResult subscript = x.getSubscript().accept(eval);
+		EvalResult result;
+		
+		if (rec.type.getBaseType().isListType() && subscript.type.getBaseType().isIntegerType()) {
+			IList list = (IList) rec.value;
+			int index = ((IInteger) subscript.value).getValue();
+			list = list.put(index, value.value);
+			result = eval.result(rec.type, list);
+		}
+		else if (rec.type.getBaseType().isMapType()) {
+			Type keyType = ((MapType) rec.type).getKeyType();
+			
+			if (subscript.type.isSubtypeOf(keyType)) {
+				IMap map = ((IMap) rec.value).put(subscript.value, value.value);
+				result = eval.result(rec.type, map);
+			}
+			else {
+				throw new RascalTypeError("Key type " + keyType + " of map is not compatible with " + subscript.type);
+			}
+		} else {
+			throw new RascalTypeError("Receiver " + rec.type + " is incompatible with subscript " + subscript.type);
+			// TODO implement other subscripts
+		}
+		
+		return x.getReceiver().accept(new AssignableEvaluator(env, result, eval));
+		
+	}
+	
+	@Override
+	public EvalResult visitAssignableIfDefined(IfDefined x) {
+		EvalResult cond = x.getCondition().accept(eval);
+		
+		if (((IBool) cond.value).getValue()) {
+			return x.getReceiver().accept(this);
+		}
+		else {
+			return x.getReceiver().accept(eval);
+		}
+	}
+	
+	@Override
+	public EvalResult visitAssignableFieldAccess(FieldAccess x) {
+		EvalResult receiver = x.getReceiver().accept(eval);
+		String label = x.getField().toString();
+		
+		if (receiver.type.isTupleType()) {
+			IValue result = ((ITuple) receiver.value).set(label, value.value);
+			return eval.result(receiver.type, result);
+		}
+		else if (receiver.type.isTreeNodeType() || receiver.type.isNamedTreeType()) {
+			IValue result = ((INode) receiver.value).set(label, value.value);
+			return eval.result(receiver.type, result);
+		}
+		else {
+			throw new RascalTypeError(x.getReceiver() + " has no field named " + label);
+		}
+		
+	}
+	
+	@Override
+	public EvalResult visitAssignableTuple(Tuple x) {
+		java.util.List<Assignable> arguments = x.getElements();
+		
+		if (!value.type.getBaseType().isTupleType()) {
+			throw new RascalTypeError("Receiver is a tuple, but the assigned value is not: " + value.type); 
+		}
+		
+		TupleType tupleType = (TupleType) value.type;
+		ITuple tuple = (ITuple) value.value;
+		EvalResult[] results = new EvalResult[arguments.size()];
+		
+		for (int i = 0; i < arguments.size(); i++) {
+			Type argType = tupleType.getFieldType(i);
+			IValue arg = tuple.get(i);
+			EvalResult result = eval.result(argType, arg);
+			AssignableEvaluator ae = new AssignableEvaluator(env,result, eval);
+			results[i] = arguments.get(i).accept(ae);
+		}
+		
+		return eval.result(tupleType, tupleType.make(eval.vf, results));
+	}
+	
+	@Override
+	public EvalResult visitAssignableConstructor(Constructor x) {
+		throw new RascalBug("Constructor assignables not yet implemented");
+	}
+}
---
+++ src/org/meta_environment/rascal/interpreter/RascalBug.java
@@ -0,0 +1,20 @@
+package org.meta_environment.rascal.interpreter;
+
+/**
+ * Exception used for when the implementation detects that it has a bug.
+ */
+public class RascalBug extends RuntimeException {
+	private static final long serialVersionUID = -8740312542969306482L;
+
+	public RascalBug(String message) {
+		super(message);
+	}
+	
+	public RascalBug(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+	public boolean hasCause() {
+		return getCause() != null;
+	}
+}
