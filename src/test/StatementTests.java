d8d45ac75544c7c95ec93fac6681e7007a62abe6
Paul Klint <Paul.Klint@cwi.nl>
2008-12-03 11:33:34 +0000

    Added test cases

--- src/test/StatementTests.java
+++ src/test/StatementTests.java
@@ -46,7 +46,29 @@ public class StatementTests extends TestCase {
 	}
 	
 	public void testAssignmnment() throws IOException {
+		assertTrue(runTest("{int x = 3; assert \"a\": x == 3;};"));
+		assertTrue(runTest("{int x = 3; x = 4; assert \"a\": x == 4;};"));
+		assertTrue(runTest("{<x, y> = <3, 4>; assert \"a\": (x == 3) && (y == 4);};"));
+		assertTrue(runTest("{<x, y, z> = <3, 4, 5>; assert \"a\": (x == 3) && (y == 4) && (z == 5);};"));
+		assertTrue(runTest("{<x, y> = <3, 4>; x = 5; assert \"a\": (x == 5) && (y == 4);};"));
 		
+//		assertTrue(runTest("{int x = 3; x += 2; assert \"a\": x == 5;};"));	
+//		assertTrue(runTest("{int x = 3; x -= 2; assert \"a\": x == 1;};"));	
+//		assertTrue(runTest("{int x = 3; x *= 2; assert \"a\": x == 6;};"));	
+//		assertTrue(runTest("{int x = 3; x /= 2; assert \"a\": x == 1;};"));	
+//		assertTrue(runTest("{int x = 3; x %= 2; assert \"a\": x == 1;};"));	
+		
+		assertTrue(runTest("{list[int] x = [0,1,2]; assert \"a\": x == [0,1,2];};"));
+		assertTrue(runTest("{list[int] x = [0,1,2]; assert \"a\": x[0] == 0;};"));
+		assertTrue(runTest("{list[int] x = [0,1,2]; assert \"a\": x[1] == 1;};"));
+		assertTrue(runTest("{list[int] x = [0,1,2]; assert \"a\": x[2] == 2;};"));
+		assertTrue(runTest("{list[int] x = [0,1,2]; x[1] = 10; assert \"a\": (x[0] == 0) && (x[1] == 10) && (x[2] == 2);};"));
+		
+		assertTrue(runTest("{map[int,int] x = (0:0,1:10,2:20); assert \"a\": x == (0:0,1:10,2:20);};"));
+//		assertTrue(runTest("{map[int,int] x = (0:0,1:10,2:20); x[1] = 15; assert \"a\": (x[0] == 0) && (x[1] == 15) && (x[2] == 20);};"));
+		
+		assertTrue(runTest("{set[int] x = {0,1,2}; assert \"a\": x == {0,1,2};};"));
+		assertTrue(runTest("{set[int] x = {0,1,2}; x = x + {3,4}; assert \"a\": x == {0,1,2, 3,4};};"));
 	}
 	
 	public void testBlock() throws IOException {
@@ -59,6 +81,8 @@ public class StatementTests extends TestCase {
 	}
 	
 	public void testDoWhile()throws IOException {
+		assertTrue(runTest("{int n = 0; m = 2; do {m = m * m; n = n + 1;} while (n < 1); assert \"a\": m == 4;};"));
+		assertTrue(runTest("{int n = 0; m = 2; do {m = m * m; n = n + 1;} while (n < 3); assert \"a\": m == 16;};"));
 	}
 	
 	public void testFail() throws IOException {
@@ -67,10 +91,16 @@ public class StatementTests extends TestCase {
 	public void testFirst() throws IOException {
 	}
 	public void testFor() throws IOException {
+		assertTrue(runTest("int n == 0; for(int i:[1,2,3,4]){ n = n + i;}; assert \"a\": n == 10;};"));
+		assertTrue(runTest("int n == 0; for(int i:[1,2,3,4], n <= 3){ n = n + i;}; assert \"a\": n == 6;};"));
 	}
 	public void testIfThen() throws IOException {
+		assertTrue(runTest("int n == 10; if(n < 10){n = n - 4;}; assert \"a\": n == 6;};"));
 	}
+	
 	public void testIfThenElse() throws IOException {
+		assertTrue(runTest("int n == 10; if(n < 10){n = n - 4;} else { n = n + 4};; assert \"a\": n == 6;};"));
+		assertTrue(runTest("int n == 12; if(n < 10){n = n - 4;} else { n = n + 4};; assert \"a\": n == 16;};"));
 	}
 	public void testInsert() throws IOException {
 	}
@@ -79,6 +109,11 @@ public class StatementTests extends TestCase {
 	public void testSolve() throws IOException {
 	}
 	public void testSwitch() throws IOException {
+		assertTrue(runTest("{int n = 0; switch(2){ case 2: n = 2; case 4: n = 4; case 6: n = 6; default: n = 10;} assert \"a\": n == 2;};"));
+		assertTrue(runTest("{int n = 0; switch(4){ case 2: n = 2; case 4: n = 4; case 6: n = 6; default: n = 10;} assert \"a\": n == 4;};"));
+		assertTrue(runTest("{int n = 0; switch(6){ case 2: n = 2; case 4: n = 4; case 6: n = 6; default: n = 10;} assert \"a\": n == 6;};"));
+		assertTrue(runTest("{int n = 0; switch(8){ case 2: n = 2; case 4: n = 4; case 6: n = 6; default: n = 10;} assert \"a\": n == 10;};"));
+		
 	}
 	public void testThrow() throws IOException {
 	}
@@ -87,6 +122,7 @@ public class StatementTests extends TestCase {
 	public void testVisit() throws IOException {
 	}
 	public void testWhile() throws IOException {
+		assertTrue(runTest("int n = 0; m = 2; while(n < 3){ m = m * m;}; assert \"a\": m == 8;};"));
 	}
 	
 	public void testTry() throws IOException {
