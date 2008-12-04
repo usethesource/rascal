d48df256aa4f5f26f479393e8f5219dd947dbb5c
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2008-12-04 12:50:08 +0000

    small bugfixes added RascalException

--- src/org/meta_environment/rascal/interpreter/JavaFunctionCaller.java
+++ src/org/meta_environment/rascal/interpreter/JavaFunctionCaller.java
@@ -134,6 +134,7 @@ public class JavaFunctionCaller {
 
 		compilation.addSource(fullClassName).addLine(
 				"package org.meta_environment.rascal.java;").
+				addLine("import org.meta_environment.rascal.interpreter.RascalException;").
 				addLine("import org.eclipse.imp.pdb.facts.type.*;").
 				addLine("import org.eclipse.imp.pdb.facts.*;").
 				addLine("import " + VALUE_FACTORY + ";").
