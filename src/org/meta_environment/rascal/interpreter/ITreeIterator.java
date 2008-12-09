f67c0c90415f2397905040671da88921afccc92c
Paul Klint <Paul.Klint@cwi.nl>
2008-12-09 07:03:22 +0000

    Extended ITreeIterator with cases for embeddded lists, sets and maps.

--- src/org/meta_environment/rascal/interpreter/ITreeIterator.java
+++ src/org/meta_environment/rascal/interpreter/ITreeIterator.java
@@ -4,6 +4,9 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.eclipse.imp.pdb.facts.IList;
+import org.eclipse.imp.pdb.facts.IMap;
+import org.eclipse.imp.pdb.facts.ISet;
 import org.eclipse.imp.pdb.facts.ITree;
 import org.eclipse.imp.pdb.facts.IValue;
 
@@ -37,6 +40,33 @@ public class ITreeIterator implements Iterator<IValue> {
 		}		
 		return spine.size() > 0;
 	}
+	
+	private IValue insertAndNext(IValue v, Iterator<IValue> children){
+		if(topdown){
+			spine.add(0, children);
+			spine.add(0, v);
+		} else {
+			spine.add(0, v);
+			spine.add(0, children);
+		}
+		return next();
+	}
+	
+	private IValue expand(IValue v){
+		if(v.getType().isTreeType()){
+			return insertAndNext(v,  ((ITree) v).getChildren().iterator());
+		}
+		if(v.getType().isListType()){
+			return insertAndNext(v, ((IList) v).iterator());
+		}
+		if(v.getType().isSetType()){
+			return insertAndNext(v, ((ISet) v).iterator());
+		}
+		if(v.getType().isMapType()){
+			return insertAndNext(v, ((IMap) v).iterator());
+		}
+		return v;
+	}
 
 	public IValue next() {
 		if(spine.get(0) instanceof Iterator){
@@ -45,20 +75,7 @@ public class ITreeIterator implements Iterator<IValue> {
 				spine.remove(0);
 				return next();
 			}
-			IValue res = (IValue) iter.next();
-			if(res instanceof ITree){
-				Iterator<IValue> children = ((ITree) res).getChildren().iterator();
-				if(topdown){
-					spine.add(0, children);
-					spine.add(0, res);
-				} else {
-					spine.add(0, res);
-					spine.add(0, children);
-				}
-				res = next();
-			}
-			return res;
-			
+			return expand((IValue) iter.next());
 		} else {
 			IValue res = (IValue) spine.get(0);
 			spine.remove(0);
