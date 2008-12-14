55a58c566f839c0780da993bec9a992cea9368c4
Paul Klint <Paul.Klint@cwi.nl>
2008-12-14 09:44:47 +0000

    Minor improvements

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -5,14 +5,17 @@ import junit.framework.TestSuite;
 public class AllTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite();
+		suite.addTestSuite(TestIO.class);
+		suite.addTestSuite(TestParsing.class);
 		suite.addTestSuite(DataTypeTests.class);
+		suite.addTestSuite(DataDeclarationTests.class);
 		suite.addTestSuite(RegExpTests.class);
+		suite.addTestSuite(PatternTests.class);
 		suite.addTestSuite(ComprehensionTests.class);
 		suite.addTestSuite(StatementTests.class);
-		suite.addTestSuite(PatternTests.class);
 		suite.addTestSuite(CallTests.class);
 		suite.addTestSuite(ErrorTests.class);
-		suite.addTestSuite(DataDeclarationTests.class);
+
 		suite.addTestSuite(ImportTests.class);
 		suite.addTestSuite(StandardLibraryTests.class);
 		
