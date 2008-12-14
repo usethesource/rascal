5cc169f6eddf0c5102dda38fad8cd5dbd0fb30b6
Paul Klint <Paul.Klint@cwi.nl>
2008-12-14 08:28:19 +0000

    Added a new exception RascalRunTimeException to be used for runtime errors during the execution of a Rascal program, like subscript out of bound and the like.

---
+++ src/org/meta_environment/rascal/interpreter/RascalRunTimeError.java
@@ -0,0 +1,23 @@
+package org.meta_environment.rascal.interpreter;
+
+/*
+ * This class is for representing runtime errors during the execution of
+ * Rascal programs. Examples are subscript out of bounds and the like.
+ */
+
+public class RascalRunTimeError extends RuntimeException {
+	
+	private static final long serialVersionUID = 3715676299644311671L;
+	
+	public RascalRunTimeError(String message) {
+		super(message);
+	}
+	
+	public RascalRunTimeError(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+	public boolean hasCause() {
+		return getCause() != null;
+	}
+}
