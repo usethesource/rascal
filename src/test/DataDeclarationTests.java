77f104e92f94fae3c52ac422f5a9136afee76b40
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2008-12-16 19:56:03 +0000

    fixed small bug in test

--- src/test/DataDeclarationTests.java
+++ src/test/DataDeclarationTests.java
@@ -10,15 +10,15 @@ public class DataDeclarationTests extends TestCase{
 		
 		tf.prepare("data Bool btrue | bfalse | band(Bool left, Bool right) | bor(Bool left, Bool right);");
 		
-		assertTrue(tf.runTestInSameEvaluator("{Bool b = btrue; b == btrue;}"));
-		assertTrue(tf.runTestInSameEvaluator("{Bool b = bfalse; b == bfalse;}"));
-		assertTrue(tf.runTestInSameEvaluator("{Bool b = band(btrue,bfalse);  b == band(btrue,bfalse);}"));
+		assertTrue(tf.runTestInSameEvaluator("{Bool b = btrue; b == Bool::btrue;}"));
+		assertTrue(tf.runTestInSameEvaluator("{Bool b = bfalse; b == Bool::bfalse;}"));
+		assertTrue(tf.runTestInSameEvaluator("{Bool b = band(btrue,bfalse);  b == Bool::band(Bool::btrue,Bool::bfalse);}"));
 		assertTrue(tf.runTestInSameEvaluator("{Bool b = bor(btrue,bfalse); b == bor(btrue,bfalse);}"));
 		assertTrue(tf.runTestInSameEvaluator("band(btrue,bfalse).left == btrue;"));
 		assertTrue(tf.runTestInSameEvaluator("band(btrue,bfalse).right == bfalse;"));
 		assertTrue(tf.runTestInSameEvaluator("bor(btrue,bfalse).left == btrue;"));
 		assertTrue(tf.runTestInSameEvaluator("bor(btrue,bfalse).right == bfalse;"));
-		assertTrue(tf.runTestInSameEvaluator("{Bool b = band(btrue,bfalse).left; b.left == btrue;}"));
-		assertTrue(tf.runTestInSameEvaluator("{Bool b = band(btrue,bfalse).right; b.left == bfalse;}"));
+		assertTrue(tf.runTestInSameEvaluator("{Bool b = band(btrue,bfalse).left; b == btrue;}"));
+		assertTrue(tf.runTestInSameEvaluator("{Bool b = band(btrue,bfalse).right; b == bfalse;}"));
 	}
 }
