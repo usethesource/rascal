60be5ee7bbd78bf08483b15bda3e5f64a73ac0c5
Paul Klint <Paul.Klint@cwi.nl>
2008-12-19 12:08:27 +0000

    Some refactoring of tests

--- src/StandardLibrary/Integer.rsc
+++ src/StandardLibrary/Integer.rsc
@@ -1,29 +1,31 @@
 module Integer
 
 public int java arb(int limit)
-@doc{arb returns an arbitrary integer value in the interval [0, limit).}
+@doc{arb -- return an arbitrary integer value in the interval [0, limit).}
 {
    return values.integer(random.nextInt(limit.getValue()));
 }
 
 public int max(int n, int m)
+@doc{max -- largest of two integers}
 {
 	return n > m ? n : m;
 }
 
 public int min(int n, int m)
+@doc{max -- smallest of two integers}
 {
 	return n < m ? n : m;
 }
 
 public double java toDouble(int n)
-@doc{toDouble converts an integer value to a double value.}
+@doc{toDouble -- convert an integer value to a double value.}
 {
   return n.toDouble();
 }
 
 public str java toString(int n)
-@doc{toString converts an integer value to a string.}
+@doc{toString -- convert an integer value to a string.}
 {
   return values.string(n.toString());
 }
--- src/test/StandardLibraryStringTests.java
+++ src/test/StandardLibraryStringTests.java
@@ -5,56 +5,76 @@ import java.io.IOException;
 import junit.framework.TestCase;
 
 public class StandardLibraryStringTests extends TestCase {
-	
+
 	private static TestFramework tf = new TestFramework("import String;");
-	
-	public void testString() throws IOException {
-		
-		System.err.println("String::charAt");
-		
-		assertTrue(tf.runTestInSameEvaluator("String::charAt(\"abc\", 0) == 97;"));
-		assertTrue(tf.runTestInSameEvaluator("String::charAt(\"abc\", 1) == 98;"));
-		assertTrue(tf.runTestInSameEvaluator("String::charAt(\"abc\", 2) == 99;"));
+
+	public void testStringCharAt() throws IOException {
+
+		assertTrue(tf
+				.runTestInSameEvaluator("String::charAt(\"abc\", 0) == 97;"));
+		assertTrue(tf
+				.runTestInSameEvaluator("String::charAt(\"abc\", 1) == 98;"));
+		assertTrue(tf
+				.runTestInSameEvaluator("String::charAt(\"abc\", 2) == 99;"));
 		assertTrue(tf.runTestInSameEvaluator("charAt(\"abc\", 0) == 97;"));
-		
-		System.err.println("String::endsWith");
-		
-		assertTrue(tf.runTestInSameEvaluator("String::endsWith(\"abc\", \"abc\");"));
+	}
+
+	public void testStringEndsWith() throws IOException {
+
+		assertTrue(tf
+				.runTestInSameEvaluator("String::endsWith(\"abc\", \"abc\");"));
 		assertTrue(tf.runTestInSameEvaluator("endsWith(\"abc\", \"abc\");"));
-		assertTrue(tf.runTestInSameEvaluator("String::endsWith(\"abcdef\", \"def\");"));
-		assertFalse(tf.runTestInSameEvaluator("String::endsWith(\"abcdef\", \"abc\");"));
-		
-		System.err.println("String::reverse");
-		
+		assertTrue(tf
+				.runTestInSameEvaluator("String::endsWith(\"abcdef\", \"def\");"));
+		assertFalse(tf
+				.runTestInSameEvaluator("String::endsWith(\"abcdef\", \"abc\");"));
+	}
+
+	public void testStringReverse() throws IOException {
+
 		assertTrue(tf.runTestInSameEvaluator("String::reverse(\"\") == \"\";"));
 		assertTrue(tf.runTestInSameEvaluator("reverse(\"\") == \"\";"));
-		assertTrue(tf.runTestInSameEvaluator("String::reverse(\"abc\") == \"cba\";"));
-		
-		System.err.println("String::size");
-		
+		assertTrue(tf
+				.runTestInSameEvaluator("String::reverse(\"abc\") == \"cba\";"));
+	}
+
+	public void testStringSize() throws IOException {
+
 		assertTrue(tf.runTestInSameEvaluator("String::size(\"\") == 0;"));
 		assertTrue(tf.runTestInSameEvaluator("size(\"\") == 0;"));
 		assertTrue(tf.runTestInSameEvaluator("String::size(\"abc\") == 3;"));
-		
-		System.err.println("String::startsWith");
-		
-		assertTrue(tf.runTestInSameEvaluator("String::startsWith(\"abc\", \"abc\");"));
+	}
+
+	public void testStringStartsWith() throws IOException {
+
+		assertTrue(tf
+				.runTestInSameEvaluator("String::startsWith(\"abc\", \"abc\");"));
 		assertTrue(tf.runTestInSameEvaluator("startsWith(\"abc\", \"abc\");"));
-		assertTrue(tf.runTestInSameEvaluator("String::startsWith(\"abcdef\", \"abc\");"));
-		assertFalse(tf.runTestInSameEvaluator("String::startsWith(\"abcdef\", \"def\");"));
-		
-		System.err.println("String::toLowerCase");
-		
-		assertTrue(tf.runTestInSameEvaluator("String::toLowerCase(\"\") == \"\";"));
+		assertTrue(tf
+				.runTestInSameEvaluator("String::startsWith(\"abcdef\", \"abc\");"));
+		assertFalse(tf
+				.runTestInSameEvaluator("String::startsWith(\"abcdef\", \"def\");"));
+	}
+
+	public void testStringToLowerCase() throws IOException {
+
+		assertTrue(tf
+				.runTestInSameEvaluator("String::toLowerCase(\"\") == \"\";"));
 		assertTrue(tf.runTestInSameEvaluator("toLowerCase(\"\") ==  \"\";"));
-		assertTrue(tf.runTestInSameEvaluator("String::toLowerCase(\"ABC\") == \"abc\";"));
-		assertTrue(tf.runTestInSameEvaluator("String::toLowerCase(\"ABC123\") == \"abc123\";"));
-		
-		System.err.println("String::toUpperCase");
-		
-		assertTrue(tf.runTestInSameEvaluator("String::toUpperCase(\"\") == \"\";"));
+		assertTrue(tf
+				.runTestInSameEvaluator("String::toLowerCase(\"ABC\") == \"abc\";"));
+		assertTrue(tf
+				.runTestInSameEvaluator("String::toLowerCase(\"ABC123\") == \"abc123\";"));
+	}
+
+	public void testStringUpperCase() throws IOException {
+
+		assertTrue(tf
+				.runTestInSameEvaluator("String::toUpperCase(\"\") == \"\";"));
 		assertTrue(tf.runTestInSameEvaluator("toUpperCase(\"\") == \"\";"));
-		assertTrue(tf.runTestInSameEvaluator("String::toUpperCase(\"abc\") == \"ABC\";"));
-		assertTrue(tf.runTestInSameEvaluator("String::toUpperCase(\"abc123\") == \"ABC123\";"));
+		assertTrue(tf
+				.runTestInSameEvaluator("String::toUpperCase(\"abc\") == \"ABC\";"));
+		assertTrue(tf
+				.runTestInSameEvaluator("String::toUpperCase(\"abc123\") == \"ABC123\";"));
 	}
 }
