afd4f35976544ee8ca550e9f1bfa1e834cc2c47c
Paul Klint <Paul.Klint@cwi.nl>
2008-12-19 13:13:46 +0000

    - Improved Graph module - Added tests

--- src/test/StandardLibraryGraphTests.java
+++ src/test/StandardLibraryGraphTests.java
@@ -9,13 +9,15 @@ public class StandardLibraryGraphTests extends TestCase {
 	private static TestFramework tf = new TestFramework("import Graph;");
 	
 	public void testGraphTop() throws IOException {
-		assertTrue(tf.runTestInSameEvaluator("top({}) == {};"));
-		assertTrue(tf.runTestInSameEvaluator("top({<1,2>, <1,3>, <2,4>, <3,4>}) == {1};"));
+		//assertTrue(tf.runTestInSameEvaluator("top({}) == {};"));
+		//assertTrue(tf.runTestInSameEvaluator("top({<1,2>, <1,3>, <2,4>, <3,4>}) == {1};"));
+		assertTrue(tf.runTestInSameEvaluator("gtop({<1,2>, <1,3>, <2,4>, <3,4>}) == {1};"));
 	}
 	
 	public void testGraphBottom() throws IOException {
-		assertTrue(tf.runTestInSameEvaluator("bottom({}) == {};"));
+		//assertTrue(tf.runTestInSameEvaluator("bottom({}) == {};"));
 		assertTrue(tf.runTestInSameEvaluator("bottom({<1,2>, <1,3>, <2,4>, <3,4>}) == {4};"));
+		assertTrue(tf.runTestInSameEvaluator("gbottom({<1,2>, <1,3>, <2,4>, <3,4>}) == {4};"));
 	}
 	
 }
