5965482e5117a768ecebe714d1c0f6f0e528eb64
Paul Klint <Paul.Klint@cwi.nl>
2008-12-30 17:13:39 +0000

    - Added Tree.rsc, a library with Tree access and construction functions. - Added StandardLibraryTreeTests with corresponding tests.

---
+++ src/test/StandardLibraryTreeTests.java
@@ -0,0 +1,43 @@
+package test;
+
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+public class StandardLibraryTreeTests extends TestCase {
+	private static TestFramework tf = new TestFramework("import Tree;");
+
+	public void testTreeArity() throws IOException {
+		assertTrue(tf.runTestInSameEvaluator("arity(f()) == 0;"));
+		assertTrue(tf.runTestInSameEvaluator("arity(f(1)) == 1;"));
+		assertTrue(tf.runTestInSameEvaluator("arity(f(1,2)) == 2;"));
+	}
+	
+	public void testTreeGetChild() throws IOException {
+		assertTrue(tf.runTestInSameEvaluator("getChild(f(1),0) == 1;"));
+		assertTrue(tf.runTestInSameEvaluator("getChild(f(1,2),0) == 1;"));
+		assertTrue(tf.runTestInSameEvaluator("getChild(f(1,2),1) == 2;"));
+	}
+	
+	public void testTreeGetChildren() throws IOException {
+		assertTrue(tf.runTestInSameEvaluator("getChildren(f()) == [];"));
+		assertTrue(tf.runTestInSameEvaluator("getChildren(f(1)) == [1];"));
+		assertTrue(tf.runTestInSameEvaluator("getChildren(f(1,2)) == [1,2];"));
+	}
+	
+	public void testTreeGetName() throws IOException {
+		assertTrue(tf.runTestInSameEvaluator("getName(f()) == \"f\";"));
+		assertTrue(tf.runTestInSameEvaluator("getName(f(1,2,3)) == \"f\";"));
+	}
+	
+	// makeTree
+	
+	public void testTreeSetChild() throws IOException {
+		assertTrue(tf.runTestInSameEvaluator("setChild(f(1,2,3), 0, 10) == f(10,2,3);"));
+		assertTrue(tf.runTestInSameEvaluator("setChild(f(1,2,3), 1, 20) == f(1,20,3);"));
+		assertTrue(tf.runTestInSameEvaluator("setChild(f(1,2,3), 2, 30) == f(1,2,30);"));
+	}
+	
+	
+		
+}
