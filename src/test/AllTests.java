10c73af9153c3e1da97b16f15a2dc3a290e5336c
Paul Klint <Paul.Klint@cwi.nl>
2009-01-04 09:56:01 +0000

    - Rerun all tests. - Reorganized AllTests. - Updated progress.

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -5,30 +5,33 @@ import junit.framework.TestSuite;
 public class AllTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite();
+		suite.addTestSuite(AssignmentTests.class);
+		suite.addTestSuite(CallTests.class);
+		suite.addTestSuite(ComprehensionTests.class);
+		suite.addTestSuite(DataDeclarationTests.class);
+		suite.addTestSuite(DataTypeTests.class);
+		suite.addTestSuite(ErrorTests.class);
+		suite.addTestSuite(ImportTests.class);
+		
 		suite.addTestSuite(IOTests.class);
 		suite.addTestSuite(ParsingTests.class);
-		suite.addTestSuite(DataTypeTests.class);
-		suite.addTestSuite(DataDeclarationTests.class);
-		suite.addTestSuite(AssignmentTests.class);
-		suite.addTestSuite(SubscriptTests.class);
-		suite.addTestSuite(RegExpTests.class);
 		suite.addTestSuite(PatternTests.class);
-		suite.addTestSuite(ComprehensionTests.class);
-		suite.addTestSuite(StatementTests.class);
-		suite.addTestSuite(CallTests.class);
-		suite.addTestSuite(ErrorTests.class);
-		suite.addTestSuite(VisitTests.class);
-		suite.addTestSuite(TryCatchTests.class);
-
-		suite.addTestSuite(ImportTests.class);
+		suite.addTestSuite(RegExpTests.class);
+		suite.addTestSuite(RuleTests.class);
 		suite.addTestSuite(StandardLibraryBooleanTests.class);
-		suite.addTestSuite(StandardLibraryIntegerTests.class);
 		suite.addTestSuite(StandardLibraryDoubleTests.class);
-		suite.addTestSuite(StandardLibraryStringTests.class);
-		suite.addTestSuite(StandardLibraryListTests.class);
-		suite.addTestSuite(StandardLibrarySetTests.class);
-		suite.addTestSuite(StandardLibraryRelationTests.class);
 		suite.addTestSuite(StandardLibraryGraphTests.class);
+		suite.addTestSuite(StandardLibraryIntegerTests.class);
+		suite.addTestSuite(StandardLibraryListTests.class);
+		suite.addTestSuite(StandardLibraryMapTests.class);
+		suite.addTestSuite(StandardLibraryRelationTests.class);
+		suite.addTestSuite(StandardLibrarySetTests.class);
+		suite.addTestSuite(StandardLibraryStringTests.class);
+		suite.addTestSuite(StandardLibraryTreeTests.class);
+		suite.addTestSuite(SubscriptTests.class);
+		suite.addTestSuite(StatementTests.class);
+		suite.addTestSuite(TryCatchTests.class);
+		suite.addTestSuite(VisitTests.class);
 		
 		return suite;
 	  }
