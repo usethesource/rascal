4db1a00d0922f6299f14ccb92abb78431746113f
Paul Klint <Paul.Klint@cwi.nl>
2009-01-22 23:01:59 +0000

    - Added left/center/right formatting functions to String. - Added test cases.

--- src/test/StandardLibraryStringTests.java
+++ src/test/StandardLibraryStringTests.java
@@ -7,7 +7,27 @@ import junit.framework.TestCase;
 public class StandardLibraryStringTests extends TestCase {
 
 	private static TestFramework tf = new TestFramework("import String;");
-
+	
+	public void testStringCenter() throws IOException {
+		tf = new TestFramework("import String;");
+		
+		assertTrue(tf.runTestInSameEvaluator("center(\"a\", 0) == \"a\";"));
+		assertTrue(tf.runTestInSameEvaluator("center(\"a\", 1) == \"a\";"));
+		assertTrue(tf.runTestInSameEvaluator("center(\"a\", 2) == \"a \";"));
+		assertTrue(tf.runTestInSameEvaluator("center(\"a\", 3) == \" a \";"));
+		
+		assertTrue(tf.runTestInSameEvaluator("center(\"ab\", 0, \"-\") == \"ab\";"));
+		assertTrue(tf.runTestInSameEvaluator("center(\"ab\", 1, \"-\") == \"ab\";"));
+		assertTrue(tf.runTestInSameEvaluator("center(\"ab\", 2, \"-\") == \"ab\";"));
+		assertTrue(tf.runTestInSameEvaluator("center(\"ab\", 3, \"-\") == \"ab-\";"));
+		assertTrue(tf.runTestInSameEvaluator("center(\"ab\", 4, \"-\") == \"-ab-\";"));
+		
+		assertTrue(tf.runTestInSameEvaluator("center(\"ab\", 3, \"-+\") == \"ab-\";"));
+		assertTrue(tf.runTestInSameEvaluator("center(\"ab\", 4, \"-+\") == \"-ab-\";"));
+		assertTrue(tf.runTestInSameEvaluator("center(\"ab\", 5, \"-+\") == \"-ab-+\";"));
+		assertTrue(tf.runTestInSameEvaluator("center(\"ab\", 6, \"-+\") == \"-+ab-+\";"));
+	}
+	
 	public void testStringCharAt() throws IOException {
 		
 		tf = new TestFramework("import String;");
@@ -20,7 +40,7 @@ public class StandardLibraryStringTests extends TestCase {
 				.runTestInSameEvaluator("String::charAt(\"abc\", 2) == 99;"));
 		assertTrue(tf.runTestInSameEvaluator("charAt(\"abc\", 0) == 97;"));
 	}
-
+	
 	public void testStringEndsWith() throws IOException {
 		
 		tf = new TestFramework("import String;");
@@ -34,6 +54,25 @@ public class StandardLibraryStringTests extends TestCase {
 				.runTestInSameEvaluator("String::endsWith(\"abcdef\", \"abc\");"));
 	}
 
+	public void testStringLeft() throws IOException {
+		tf = new TestFramework("import String;");
+		
+		assertTrue(tf.runTestInSameEvaluator("left(\"a\", 0) == \"a\";"));
+		assertTrue(tf.runTestInSameEvaluator("left(\"a\", 1) == \"a\";"));
+		assertTrue(tf.runTestInSameEvaluator("left(\"a\", 2) == \"a \";"));
+		
+		assertTrue(tf.runTestInSameEvaluator("left(\"ab\", 0, \"-\") == \"ab\";"));
+		assertTrue(tf.runTestInSameEvaluator("left(\"ab\", 1, \"-\") == \"ab\";"));
+		assertTrue(tf.runTestInSameEvaluator("left(\"ab\", 2, \"-\") == \"ab\";"));
+		assertTrue(tf.runTestInSameEvaluator("left(\"ab\", 3, \"-\") == \"ab-\";"));
+		assertTrue(tf.runTestInSameEvaluator("left(\"ab\", 4, \"-\") == \"ab--\";"));
+		
+		assertTrue(tf.runTestInSameEvaluator("left(\"ab\", 3, \"-+\") == \"ab-\";"));
+		assertTrue(tf.runTestInSameEvaluator("left(\"ab\", 4, \"-+\") == \"ab-+\";"));
+		assertTrue(tf.runTestInSameEvaluator("left(\"ab\", 5, \"-+\") == \"ab-+-\";"));
+		assertTrue(tf.runTestInSameEvaluator("left(\"ab\", 6, \"-+\") == \"ab-+-+\";"));
+	}
+
 	public void testStringReverse() throws IOException {
 		
 		tf = new TestFramework("import String;");
@@ -42,6 +81,25 @@ public class StandardLibraryStringTests extends TestCase {
 		assertTrue(tf.runTestInSameEvaluator("reverse(\"\") == \"\";"));
 		assertTrue(tf
 				.runTestInSameEvaluator("String::reverse(\"abc\") == \"cba\";"));
+	}
+
+	public void testStringRight() throws IOException {
+		tf = new TestFramework("import String;");
+		
+		assertTrue(tf.runTestInSameEvaluator("right(\"a\", 0) == \"a\";"));
+		assertTrue(tf.runTestInSameEvaluator("right(\"a\", 1) == \"a\";"));
+		assertTrue(tf.runTestInSameEvaluator("right(\"a\", 2) == \" a\";"));
+		
+		assertTrue(tf.runTestInSameEvaluator("right(\"ab\", 0, \"-\") == \"ab\";"));
+		assertTrue(tf.runTestInSameEvaluator("right(\"ab\", 1, \"-\") == \"ab\";"));
+		assertTrue(tf.runTestInSameEvaluator("right(\"ab\", 2, \"-\") == \"ab\";"));
+		assertTrue(tf.runTestInSameEvaluator("right(\"ab\", 3, \"-\") == \"-ab\";"));
+		assertTrue(tf.runTestInSameEvaluator("right(\"ab\", 4, \"-\") == \"--ab\";"));
+		
+		assertTrue(tf.runTestInSameEvaluator("right(\"ab\", 3, \"-+\") == \"-ab\";"));
+		assertTrue(tf.runTestInSameEvaluator("right(\"ab\", 4, \"-+\") == \"-+ab\";"));
+		assertTrue(tf.runTestInSameEvaluator("right(\"ab\", 5, \"-+\") == \"-+-ab\";"));
+		assertTrue(tf.runTestInSameEvaluator("right(\"ab\", 6, \"-+\") == \"-+-+ab\";"));
 	}
 
 	public void testStringSize() throws IOException {
@@ -79,7 +137,7 @@ public class StandardLibraryStringTests extends TestCase {
 				.runTestInSameEvaluator("String::toLowerCase(\"ABC123\") == \"abc123\";"));
 	}
 
-	public void testStringUpperCase() throws IOException {
+	public void testStringToUpperCase() throws IOException {
 		
 		tf = new TestFramework("import String;");
 
