b01f57259a959ce3bbaf72d50e9290bec269946d
Paul Klint <Paul.Klint@cwi.nl>
2009-01-22 14:36:16 +0000

    Continued fixing/testing. Updated progress.

--- src/test/AllDemoTests.java
+++ src/test/AllDemoTests.java
@@ -7,6 +7,11 @@ import junit.framework.TestCase;
 public class AllDemoTests extends TestCase {
 private static TestFramework tf = new TestFramework();
 	
+	public void testAckermann() throws IOException {
+		tf = new TestFramework("import Ackermann;");
+		assertTrue(tf.runTestInSameEvaluator("Ackermann::test();"));
+	}
+	
 	public void testBoolAbstractRules() throws IOException {
 		tf = new TestFramework("import Bool-abstract-rules;");
 		assertTrue(tf.runTestInSameEvaluator("Bool-abstract-rules::test();"));
@@ -15,11 +20,6 @@ private static TestFramework tf = new TestFramework();
 	public void testBoolAbstractVisit() throws IOException {
 		tf = new TestFramework("import Bool-abstract-visit;");
 		assertTrue(tf.runTestInSameEvaluator("Bool-abstract-visit::test();"));
-	}
-	
-	public void testIntegerAbstractRules() throws IOException {
-		tf = new TestFramework("import Integer-abstract-rules;");
-		assertTrue(tf.runTestInSameEvaluator("Integer-abstract-rules::test();"));
 	}
 	
 	public void testBubble() throws IOException {
@@ -42,6 +42,11 @@ private static TestFramework tf = new TestFramework();
 		assertTrue(tf.runTestInSameEvaluator("Dominators::test();"));
 	}
 	
+	public void testFunAbstract() throws IOException {
+		tf = new TestFramework("import Fun-abstract;");
+		assertTrue(tf.runTestInSameEvaluator("Fun-abstract::test();"));
+	}
+	
 	public void testGraphDataType() throws IOException {
 		tf = new TestFramework("import GraphDataType;");
 		assertTrue(tf.runTestInSameEvaluator("GraphDataType::test();"));
@@ -52,29 +57,14 @@ private static TestFramework tf = new TestFramework();
 		assertTrue(tf.runTestInSameEvaluator("Innerproduct::test();"));
 	}
 	
+	public void testIntegerAbstractRules() throws IOException {
+		tf = new TestFramework("import Integer-abstract-rules;");
+		assertTrue(tf.runTestInSameEvaluator("Integer-abstract-rules::test();"));
+	}
+	
 	public void testLift() throws IOException {
 		tf = new TestFramework("import Lift;");
 		assertTrue(tf.runTestInSameEvaluator("Lift::test();"));
-	}
-	
-	public void testTrans() throws IOException {
-		tf = new TestFramework("import Trans;");
-		assertTrue(tf.runTestInSameEvaluator("Trans::test();"));
-	}
-	
-	public void testAckermann() throws IOException {
-		tf = new TestFramework("import Ackermann;");
-		assertTrue(tf.runTestInSameEvaluator("Ackermann::test();"));
-	}
-	
-	public void testWordcount() throws IOException {
-		tf = new TestFramework("import WordCount;");
-		assertTrue(tf.runTestInSameEvaluator("WordCount::test();"));
-	}
-	
-	public void testFunAbstract() throws IOException {
-		tf = new TestFramework("import Fun-abstract;");
-		assertTrue(tf.runTestInSameEvaluator("Fun-abstract::test();"));
 	}
 	
 	public void testMEPT() throws IOException {
@@ -82,5 +72,13 @@ private static TestFramework tf = new TestFramework();
 		assertTrue(tf.runTestInSameEvaluator("MEPT::test();"));
 	}
 	
+	public void testTrans() throws IOException {
+		tf = new TestFramework("import Trans;");
+		assertTrue(tf.runTestInSameEvaluator("Trans::test();"));
+	}
 	
+	public void testWordcount() throws IOException {
+		tf = new TestFramework("import WordCount;");
+		assertTrue(tf.runTestInSameEvaluator("WordCount::test();"));
+	}
 }
