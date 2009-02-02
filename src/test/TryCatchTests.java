94afee7567636512f3793ac0d27e84bdd4f14b7d
Paul Klint <Paul.Klint@cwi.nl>
2009-02-02 15:00:08 +0000

    - Renamed various NODE declarations in test cases. - Updated progress.

--- src/test/TryCatchTests.java
+++ src/test/TryCatchTests.java
@@ -26,7 +26,7 @@ public class TryCatchTests extends TestCase {
 		"	}" +
 		"}";
 		
-		tf = new TestFramework("data NODE = f(int N);");
+		tf = new TestFramework("data NODEA = f(int N);");
 		
 		assertTrue(tf.runTestInSameEvaluator("{" + classify + "classify(3) == 1;}"));
 		assertTrue(tf.runTestInSameEvaluator("{" + classify + "classify(f(3)) == 2;}"));
@@ -53,7 +53,7 @@ public class TryCatchTests extends TestCase {
 		"	}" +
 		"}";
 		
-		tf = new TestFramework("data NODE = f(int N) | d(NODE a, NODE b);");
+		tf = new TestFramework("data NODEB = f(int N) | d(NODEB a, NODEB b);");
 		
 		assertTrue(tf.runTestInSameEvaluator("{" + duplicate + "duplicate(3) == 6;}"));
 		assertTrue(tf.runTestInSameEvaluator("{" + duplicate + "duplicate(f(3)) == d(f(3),f(3));}"));
@@ -84,7 +84,7 @@ public class TryCatchTests extends TestCase {
 		"	}" +
 		"}";
 		
-		tf = new TestFramework("data NODE = f(int N) | fin(value V) | d(NODE a) | d(NODE a, NODE b);");
+		tf = new TestFramework("data NODEC = f(int N) | fin(value V) | d(NODEC a) | d(NODEC a, NODEC b);");
 		
 		assertTrue(tf.runTestInSameEvaluator("{" + dfin + "dfin(3) == fin(6);}"));
 		assertTrue(tf.runTestInSameEvaluator("{" + dfin + "dfin(f(3)) == fin(d(f(3),f(3)));}"));
