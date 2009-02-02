a7775a7a941ee39dc4353c31cdf5ed960fcdaf90
Paul Klint <Paul.Klint@cwi.nl>
2009-02-02 15:00:08 +0000

    - Renamed various NODE declarations in test cases. - Updated progress.

--- src/test/VisitTests.java
+++ src/test/VisitTests.java
@@ -9,7 +9,7 @@ public class VisitTests extends TestCase {
 	
 	public void testCnt() throws IOException {
 		String cnt =
-		"int cnt(NODE T) {" +
+		"int cnt(NODE1 T) {" +
 		"   int C = 0;" +
 		"   visit(T) {" +
 		"      case int N: C = C + 1;" +
@@ -17,7 +17,7 @@ public class VisitTests extends TestCase {
 		"    return C;" +
 		"}";
 		
-		tf = new TestFramework("data NODE = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2);");
+		tf = new TestFramework("data NODE1 = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2);");
 		
 		assertTrue(tf.runTestInSameEvaluator("{" + cnt + "cnt(f(3)) == 1;}"));
 		assertTrue(tf.runTestInSameEvaluator("{" + cnt + "cnt(f(1,2,3)) == 3;}"));
@@ -29,25 +29,24 @@ public class VisitTests extends TestCase {
 		assertTrue(tf.runTestInSameEvaluator("{" + cnt + "cnt(f(1,g(2,(1:10,2:20)))) == 6;}"));
 	}
 	
-	public void testInc() throws IOException {
-		String inc =
-		"	NODE inc(NODE T) {" +
-		"    return visit(T) {" +
-		"      case int N: insert N + 1;" +
-		"    };" + 
+	public void testDrepl() throws IOException {
+		String drepl =
+			
+		// Deep replacement of g by h
+			
+		"NODE2 drepl(NODE2 T) {" +
+		"    return bottom-up-break visit (T) {" +
+		"      case g(value T1, value T2) =>  h(T1, T2)" +
+		"    };" +
 		"}";
 		
-		tf = new TestFramework("data NODE = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2);");
+		tf = new TestFramework("data NODE2 = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2);");
 
 		
-		assertTrue(tf.runTestInSameEvaluator("{" + inc + "inc(f(3)) == f(4);}"));
-		assertTrue(tf.runTestInSameEvaluator("{" + inc + "inc(f(1,2,3)) == f(2,3,4);}"));
-		assertTrue(tf.runTestInSameEvaluator("{" + inc + "inc(f(1,g(2,3))) == f(2,g(3,4));}"));
-		assertTrue(tf.runTestInSameEvaluator("{" + inc + "inc(f(1,g(2,[3,4,5]))) == f(2,g(3,[4,5,6]));}"));
-		assertTrue(tf.runTestInSameEvaluator("{" + inc + "inc(f(1,g(2,{3,4,5}))) == f(2,g(3,{4,5,6}));}"));
-		assertTrue(tf.runTestInSameEvaluator("{" + inc + "inc(f(1,g(2,<3,4,5>))) == f(2,g(3,<4,5,6>));}"));
-		assertTrue(tf.runTestInSameEvaluator("{" + inc + "inc(f(1,g(2,{<1,10>,<2,20>}))) == f(2,g(3,{<2,11>,<3,21>}));}"));
-		assertTrue(tf.runTestInSameEvaluator("{" + inc + "inc(f(1,g(2,(1:10,2:20)))) == f(2,g(3,(2:11,3:21)));}"));
+		assertTrue(tf.runTestInSameEvaluator("{" + drepl + "drepl(f(3)) == f(3);}"));
+		assertTrue(tf.runTestInSameEvaluator("{" + drepl + "drepl(g(1,2)) == h(1,2);}"));
+		assertTrue(tf.runTestInSameEvaluator("{" + drepl + "drepl(g(1,f(g(2,3)))) == g(1,f(h(2,3)));}"));
+		assertTrue(tf.runTestInSameEvaluator("{" + drepl + "drepl(g(1,f([g(2,3),4,5]))) == g(1,f([h(2,3),4,5]));}"));
 	}
 	
 	public void testFrepA() throws IOException {
@@ -55,14 +54,14 @@ public class VisitTests extends TestCase {
 		// Replace all nodes g(_,_) by h(_,_)
 		// Using insert
 
-		"NODE frepa(NODE T) { " +
+		"NODE3 frepa(NODE3 T) { " +
 		"    return visit (T) {" +
 		"      case g(value T1, value T2):" +
 		"           insert h(T1, T2);" +
 		"    };" +
 		"}";
 		
-		tf = new TestFramework("data NODE = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2);");
+		tf = new TestFramework("data NODE3 = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2);");
 
 		
 		assertTrue(tf.runTestInSameEvaluator("{" + frepa + "frepa(f(3)) == f(3);}"));
@@ -80,13 +79,13 @@ public class VisitTests extends TestCase {
 		// Replace all nodes g(_,_) by h(_,_)
 		// Using replacement rule
 
-		"NODE frepb(NODE T) {" +
+		"NODE4 frepb(NODE4 T) {" +
 		"    return visit (T) {" +
 		"      case g(value T1, value T2) => h(T1, T2)" +
 		"    };" +
 		"}";
 		
-		tf = new TestFramework("data NODE = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2);");
+		tf = new TestFramework("data NODE4 = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2);");
 
 		
 		assertTrue(tf.runTestInSameEvaluator("{" + frepb + "frepb(f(3)) == f(3);}"));
@@ -105,14 +104,14 @@ public class VisitTests extends TestCase {
 		// Replace all nodes g(_,_) by h(_,_,_)
 		// Using insert
 
-		"NODE frepG2H3a(NODE T) {" +
+		"NODE5 frepG2H3a(NODE5 T) {" +
 		"    return visit (T) {" +
 		"      case g(value T1, value T2):" +
 		"           insert h(T1, T2, 0);" +
 		"    };" +
 		"}";
 		
-		tf = new TestFramework("data NODE = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2) | h(value V1, value V2, value V3);");
+		tf = new TestFramework("data NODE5 = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2) | h(value V1, value V2, value V3);");
 
 		
 		assertTrue(tf.runTestInSameEvaluator("{" + frepG2H3a + "frepG2H3a(f(3)) == f(3);}"));
@@ -130,13 +129,13 @@ public class VisitTests extends TestCase {
 		// Replace all nodes g(_,_) by h(_,_,_)
 		// Using replacement rule
 			
-		"NODE frepG2H3b(NODE T) {" +
+		"NODE6 frepG2H3b(NODE6 T) {" +
 		"   return visit (T) {" +
 		"      case g(value T1, value T2) => h(T1, T2, 0)" +
 		"    };" +
 		"}";
 		
-		tf = new TestFramework("data NODE = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2) | h(value V1, value V2, value V3);");
+		tf = new TestFramework("data NODE6 = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2) | h(value V1, value V2, value V3);");
 
 		
 		assertTrue(tf.runTestInSameEvaluator("{" + frepG2H3b + "frepG2H3b(f(3)) == f(3);}"));
@@ -149,12 +148,33 @@ public class VisitTests extends TestCase {
 		assertTrue(tf.runTestInSameEvaluator("{" + frepG2H3b + "frepG2H3b(f(1,g(2,(1:10,2:20)))) == f(1,h(2,(1:10,2:20), 0));}"));
 	}
 	
+	public void testInc() throws IOException {
+		String inc =
+		"	NODE7 inc(NODE7 T) {" +
+		"    return visit(T) {" +
+		"      case int N: insert N + 1;" +
+		"    };" + 
+		"}";
+		
+		tf = new TestFramework("data NODE7 = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2);");
+
+		
+		assertTrue(tf.runTestInSameEvaluator("{" + inc + "inc(f(3)) == f(4);}"));
+		assertTrue(tf.runTestInSameEvaluator("{" + inc + "inc(f(1,2,3)) == f(2,3,4);}"));
+		assertTrue(tf.runTestInSameEvaluator("{" + inc + "inc(f(1,g(2,3))) == f(2,g(3,4));}"));
+		assertTrue(tf.runTestInSameEvaluator("{" + inc + "inc(f(1,g(2,[3,4,5]))) == f(2,g(3,[4,5,6]));}"));
+		assertTrue(tf.runTestInSameEvaluator("{" + inc + "inc(f(1,g(2,{3,4,5}))) == f(2,g(3,{4,5,6}));}"));
+		assertTrue(tf.runTestInSameEvaluator("{" + inc + "inc(f(1,g(2,<3,4,5>))) == f(2,g(3,<4,5,6>));}"));
+		assertTrue(tf.runTestInSameEvaluator("{" + inc + "inc(f(1,g(2,{<1,10>,<2,20>}))) == f(2,g(3,{<2,11>,<3,21>}));}"));
+		assertTrue(tf.runTestInSameEvaluator("{" + inc + "inc(f(1,g(2,(1:10,2:20)))) == f(2,g(3,(2:11,3:21)));}"));
+	}
+	
 	public void testIncAndCount() throws IOException {
 		String inc_and_count =
 		// Accumulating transformer that increments integer leaves with 
 		// amount D and counts them as well.
 
-		"tuple[int, NODE] inc_and_count(NODE T, int D) {" +
+		"tuple[int, NODE8] inc_and_count(NODE8 T, int D) {" +
 		"    int C = 0;" +  
 		"    T = visit (T) {" +
 		"        case int N: { C = C + 1; " +
@@ -164,7 +184,7 @@ public class VisitTests extends TestCase {
 		"    return <C, T>;" +
 		"}";
 		
-		tf = new TestFramework("data NODE = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2);");
+		tf = new TestFramework("data NODE8 = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2);");
 
 		
 		assertTrue(tf.runTestInSameEvaluator("{" + inc_and_count + "inc_and_count(f(3),10)                       == <1,f(13)>;}"));
@@ -177,38 +197,18 @@ public class VisitTests extends TestCase {
 		assertTrue(tf.runTestInSameEvaluator("{" + inc_and_count + "inc_and_count(f(1,g(2,(1:10,2:20))),10)      == <6, f(11,g(12,(11:20,12:30)))>;}"));
 	}
 	
-	public void testDrepl() throws IOException {
-		String drepl =
-			
-		// Deep replacement of g by h
-			
-		"NODE drepl(NODE T) {" +
-		"    return bottom-up-break visit (T) {" +
-		"      case g(value T1, value T2) =>  h(T1, T2)" +
-		"    };" +
-		"}";
-		
-		tf = new TestFramework("data NODE = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2);");
-
-		
-		assertTrue(tf.runTestInSameEvaluator("{" + drepl + "drepl(f(3)) == f(3);}"));
-		assertTrue(tf.runTestInSameEvaluator("{" + drepl + "drepl(g(1,2)) == h(1,2);}"));
-		assertTrue(tf.runTestInSameEvaluator("{" + drepl + "drepl(g(1,f(g(2,3)))) == g(1,f(h(2,3)));}"));
-		assertTrue(tf.runTestInSameEvaluator("{" + drepl + "drepl(g(1,f([g(2,3),4,5]))) == g(1,f([h(2,3),4,5]));}"));
-	}
-	
 	public void testSrepl() throws IOException {
 		String srepl =
 		// Ex6: shallow replacement of g by h (i.e. only outermost 
 		// g's are replaced); 
 
-		"NODE srepl(NODE T) {" +
+		"NODE9 srepl(NODE9 T) {" +
 		"    return top-down-break visit (T) {" +
 		"       case g(value T1, value T2) =>  h(T1, T2)" +
 		"    };" +
 		"}";
 		
-		tf = new TestFramework("data NODE = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2);");
+		tf = new TestFramework("data NODE9 = f(value V) | f(value V1, value V2) | f(value V1, value V2, value V3) | g(value V1, value V2) | h(value V1, value V2);");
 	
 		assertTrue(tf.runTestInSameEvaluator("{" + srepl + "srepl(f(3)) == f(3);}"));
 		assertTrue(tf.runTestInSameEvaluator("{" + srepl + "srepl(g(1,2)) == h(1,2);}"));
