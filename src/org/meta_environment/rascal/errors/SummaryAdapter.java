786bef2f217e3e765e8d91e98031cac4dd8ad073
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2009-02-03 20:21:23 +0000

    added basic support for getting location info from parse errors

---
+++ src/org/meta_environment/rascal/errors/SummaryAdapter.java
@@ -0,0 +1,44 @@
+package org.meta_environment.rascal.errors;
+
+import java.util.Iterator;
+
+import org.eclipse.imp.pdb.facts.IConstructor;
+import org.eclipse.imp.pdb.facts.IList;
+import org.eclipse.imp.pdb.facts.IValue;
+
+public class SummaryAdapter implements Iterable<ErrorAdapter> {
+	private IConstructor summary;
+	
+	public SummaryAdapter(IValue summary) {
+		this.summary = (IConstructor) summary;
+	}
+	
+	public IValue getProducer() {
+		return summary.get("producer");
+	}
+	
+	public IValue getId() {
+		return summary.get("id");
+	}
+
+	/**
+	 * Iterates over errors in summary
+	 */
+	public Iterator<ErrorAdapter> iterator() {
+		return new Iterator<ErrorAdapter>() {
+			Iterator<IValue> errors = ((IList) summary.get("errors")).iterator();
+
+			public boolean hasNext() {
+				return errors.hasNext();
+			}
+
+			public ErrorAdapter next() {
+				return new ErrorAdapter(errors.next());
+			}
+
+			public void remove() {
+				errors.remove();
+			}
+		};
+	}
+}
