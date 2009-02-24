c60acbbc998fc5ae4a9f33e8600b96ea3981f362
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2009-02-24 15:46:39 +0000

    separated Module Loading from Interpreter and introduced abstraction such that different module loaders can be given to the interpreter (i.e. one that knows about the MODULE_PATH, another one that integrates with Eclipse Bundles, etc, one that reads from jar files, etc)

---
+++ src/org/meta_environment/rascal/interpreter/errors/ModuleLoadException.java
@@ -0,0 +1,14 @@
+package org.meta_environment.rascal.interpreter.errors;
+
+
+public class ModuleLoadException extends Exception {
+	private static final long serialVersionUID = 4541540663859196204L;
+
+	public ModuleLoadException(String message) {
+		super(message);
+	}
+	
+	public ModuleLoadException(String string, Throwable e) {
+		super(string, e);
+	}
+}
