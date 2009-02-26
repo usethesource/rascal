2ec96649d417bf516d2fa730511ec37c422cd427
Paul Klint <Paul.Klint@cwi.nl>
2009-02-26 15:55:45 +0000

    Added tests for new semantics if annotations

--- src/test/AnnotationTests.java
+++ src/test/AnnotationTests.java
@@ -11,7 +11,7 @@ public class AnnotationTests extends TestFramework{
 	@Test(expected=NoSuchAnnotationError.class)
 	public void annotationNotAllowed(){
 		prepare("data POS = pos(int n);");
-		runTestInSameEvaluator("1 @ (pos:3);");
+		runTestInSameEvaluator("1 [@pos=3];");
 	}
 	
 	@Test(expected=NoSuchAnnotationError.class)
@@ -22,32 +22,48 @@ public class AnnotationTests extends TestFramework{
 	@Test(expected=TypeError.class)
 	public void annotationNotAllowed3(){
 		prepare("data F = f | f(int n) | g(int n) | deep(F f);");
-		prepareMore("anno int pos on F;");
-		runTestInSameEvaluator("f @ (pos:true);");
+		prepareMore("anno int F @ pos;");
+		runTestInSameEvaluator("f[@pos=true];");
 	}
 	
 	@Test(expected=NoSuchAnnotationError.class)
 	public void annotationNotAllowed4(){
 		prepare("data F = f | f(int n) | g(int n) | deep(F f);");
-		prepareMore("anno int pos on F;");
-		runTestInSameEvaluator("f @ (wrongpos:true);");
+		prepareMore("anno int F @ pos;");
+		runTestInSameEvaluator("f [@wrongpos:true];");
 	}
 	
 	@Test
 	public void annotations(){
 		prepare("data F = f | f(int n) | g(int n) | deep(F f);");
-		prepareMore("anno int pos on F;");
+		prepareMore("anno int F @ pos;");
 		
-		assertTrue(runTestInSameEvaluator("f @ (pos:1) == f;"));
-		assertTrue(runTestInSameEvaluator("f @ (pos:1) @ pos == 1;"));
-		assertTrue(runTestInSameEvaluator("f @ (pos:1) @(pos:2) @ pos == 2;"));
+		assertTrue(runTestInSameEvaluator("f [@pos=1] == f;"));
+		assertTrue(runTestInSameEvaluator("f [@pos=1] @ pos == 1;"));
+		assertTrue(runTestInSameEvaluator("f [@pos=1] [@pos=2] @ pos == 2;"));
 		
-		assertTrue(runTestInSameEvaluator("f(5) @ (pos:1) == f(5);"));
-		assertTrue(runTestInSameEvaluator("f(5) @ (pos:1) @ pos == 1;"));
-		assertTrue(runTestInSameEvaluator("f(5) @ (pos:1) @(pos:2) @ pos == 2;"));
+		assertTrue(runTestInSameEvaluator("f(5) [@pos=1] == f(5);"));
+		assertTrue(runTestInSameEvaluator("f(5) [@pos=1] @ pos == 1;"));
+		assertTrue(runTestInSameEvaluator("f(5) [@pos=1] [@pos=2] @ pos == 2;"));
 		
-		assertTrue(runTestInSameEvaluator("deep(f(5) @ (pos:1)) == deep(f(5));"));
-		assertTrue(runTestInSameEvaluator("f(5) @ (pos:1) == f(5) @ (pos:2);"));	
+		assertTrue(runTestInSameEvaluator("deep(f(5) [@pos=1]) == deep(f(5));"));
+		assertTrue(runTestInSameEvaluator("f(5) [@pos=1] == f(5) [@pos=2);"));	
+	}
+	
+	@Test
+	public void annotationsInSets(){
+		prepare("data F = f | f(int n) | g(int n) | deep(F f);");
+		prepareMore("anno int F @ pos;");
+		
+		assertTrue(runTestInSameEvaluator("{f [@pos=1]} == {f};"));
+		assertTrue(runTestInSameEvaluator("{f [@pos=1], g(2) [@pos=2]} == {f, g(2)};"));
+		assertTrue(runTestInSameEvaluator("{f [@pos=1], g(2)} == {f, g(2)[@pos=2]};"));		
+		assertTrue(runTestInSameEvaluator("{deep(f(5) [@pos=1])} == {deep(f(5))};"));
+		
+		assertTrue(runTestInSameEvaluator("{f [@pos=1]} + {g(2) [@pos=2]} == {f, g(2)};"));
+		
+		assertTrue(runTestInSameEvaluator("X = {f [@pos=1]} + {f [@pos=2]}; {F elem} := X; elem@pos == 2;"));
+		
 	}
 }
 
