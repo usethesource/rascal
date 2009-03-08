1b7b4295bb4a1c0a48d5822fe1bbed8b26bbecc2
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2009-03-08 08:15:04 +0000

    added (badly tested) support for out of order alias declarations in modules

--- src/org/meta_environment/rascal/interpreter/exceptions/UndeclaredTypeException.java
+++ src/org/meta_environment/rascal/interpreter/exceptions/UndeclaredTypeException.java
@@ -4,8 +4,14 @@ import org.meta_environment.rascal.ast.AbstractAST;
 
 public class UndeclaredTypeException extends TypeErrorException {
 	private static final long serialVersionUID = -2394719759439179575L;
+	private String name;
 
 	public UndeclaredTypeException(String name, AbstractAST ast) {
 		super("Undeclared type " + name, ast);
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
 	}
 }
