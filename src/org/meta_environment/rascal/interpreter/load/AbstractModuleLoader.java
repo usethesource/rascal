dc612cb2734ed6f80d750e1001e42db85b1fa587
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2009-03-15 07:22:55 +0000

    fixed import tests. AbstractModuleLoader should only throw IOExceptions.

--- src/org/meta_environment/rascal/interpreter/load/AbstractModuleLoader.java
+++ src/org/meta_environment/rascal/interpreter/load/AbstractModuleLoader.java
@@ -5,13 +5,11 @@ import java.io.InputStream;
 
 import org.eclipse.imp.pdb.facts.IConstructor;
 import org.eclipse.imp.pdb.facts.exceptions.FactTypeUseException;
-import org.meta_environment.ValueFactoryFactory;
 import org.meta_environment.errors.SubjectAdapter;
 import org.meta_environment.errors.SummaryAdapter;
 import org.meta_environment.rascal.ast.ASTFactory;
 import org.meta_environment.rascal.ast.Module;
 import org.meta_environment.rascal.interpreter.Names;
-import org.meta_environment.rascal.interpreter.RuntimeExceptionFactory;
 import org.meta_environment.rascal.interpreter.asserts.ImplementationError;
 import org.meta_environment.rascal.interpreter.staticErrors.SyntaxError;
 import org.meta_environment.rascal.parser.ASTBuilder;
@@ -39,15 +37,9 @@ public abstract class AbstractModuleLoader implements IModuleLoader {
 			return BUILDER.buildModule(tree);
 		} catch (FactTypeUseException e) {
 			throw new ImplementationError("Unexpected PDB typecheck exception", e);
-		} catch (IOException e) {
-			throw RuntimeExceptionFactory.io(ValueFactoryFactory.getValueFactory().string(e.getMessage()));
 		} finally {
 			if (stream != null) {
-				try {
-					stream.close();
-				} catch (IOException e) {
-					throw RuntimeExceptionFactory.io(ValueFactoryFactory.getValueFactory().string(e.getMessage()));
-				}
+				stream.close();
 			}
 		}
 	}
