17a40bc8d5466d071d774ba8d54c1cbaa91b9eeb
Paul Klint <Paul.Klint@cwi.nl>
2009-04-02 06:11:21 +0000

    Added double declaration tests

--- src/test/DeclarationTests.java
+++ src/test/DeclarationTests.java
@@ -6,6 +6,10 @@ import org.junit.Test;
 import org.meta_environment.rascal.interpreter.staticErrors.*;
 
 public class DeclarationTests extends TestFramework {
+	@Test(expected=UndeclaredTypeError.class)
+	public void undeclaredType1(){
+		runTest("X N;");
+	}
 	
 	@Test(expected=RedeclaredVariableError.class)
 	public void doubleDeclaration1(){
@@ -21,4 +25,6 @@ public class DeclarationTests extends TestFramework {
 	public void doubleDeclaration3(){
 		assertTrue(runTest("{int f(int N){int N = 1; return N;} f(3) == 1;}"));
 	}
+	
+	
 }
