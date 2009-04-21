adf1f822fbeda28d4bbde9195e907241d2f835d3
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2009-04-21 12:34:04 +0000

    added some tests concerning declarations with inferred types

--- src/test/DeclarationTests.java
+++ src/test/DeclarationTests.java
@@ -6,6 +6,28 @@ import org.junit.Test;
 import org.meta_environment.rascal.interpreter.staticErrors.*;
 
 public class DeclarationTests extends TestFramework {
+	@Test
+	public void localTypeInferenceBottomScope() {
+		runTest("x = 1;");
+		runTestInSameEvaluator("x == 1;");
+	}
+	
+	@Test
+	public void localTypeInferenceNestedScope() {
+		runTest("{ x = 1; x == 1; }");
+	}
+	
+	public void localTypeInferenceNoEscape() {
+		runTest("{ x = 1; x == 1; }");
+		runTestInSameEvaluator("{ x = \"1\"; x == \"1\";}");
+	}
+	
+	@Test(expected=StaticError.class)
+	public void localTypeInferenceNoEscape2() {
+		runTest("{ x = 1; x == 1; }");
+		runTestInSameEvaluator("x;");
+	}
+	
 	@Test(expected=UndeclaredTypeError.class)
 	public void undeclaredType1(){
 		runTest("X N;");
