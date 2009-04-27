5871b868cb70ffbad23e54cc1a714d8c5e7e4f85
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2009-04-27 11:35:44 +0000

    added all kinds of tests that don't trigger my bug

--- src/test/RuleTests.java
+++ src/test/RuleTests.java
@@ -14,5 +14,33 @@ public class RuleTests extends TestFramework{
 		runTestInSameEvaluator("i(0) == i(1);");
 	}
 	
+	@Test
+	public void useOfGlobalVariableInRuleLhs(){
+		prepareModule("module A public int j = 0;\n" + 
+				      "data Int = i(int i);\n" +
+		              "rule test i(j) => i(1);"
+				      );
+		prepareMore("import A;");
+		runTestInSameEvaluator("i(0) == i(1);");
+	}
+	
+	@Test
+	public void useOfGlobalVariableInRuleLhsNested(){
+		prepareModule("module A public int j = 0;\n" + 
+				      "data Int = i(int i) | i(Int j);\n" +
+				      "public Int Example = i(0);\n" +
+		              "rule test i(Example) => i(1);"
+				      );
+		prepareMore("import A;");
+		runTestInSameEvaluator("i(i(0)) == i(1);");
+	}
+	
+	@Test
+	public void useOfGlobalimportedVariableInRuleLhs(){
+		prepare("import Mbase;");
+		prepareMore("data Int = i(int i);");
+		prepareMore("rule test i(n) => i(1);");
+		runTestInSameEvaluator("i(2) == i(1);");
+	}
 }
 
