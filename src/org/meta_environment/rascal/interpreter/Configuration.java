7fc9f545ec43c98d87899b9f2b5e124c89a2c7f8
Arnold Lankamp <arnold.lankamp@gmail.com>
2009-05-20 08:23:58 +0000

    -Fixed a problem with the Configuration (path properties).
    NOTE: I'm gonna shoot the next person that uses relative paths somewhere.

--- src/org/meta_environment/rascal/interpreter/Configuration.java
+++ src/org/meta_environment/rascal/interpreter/Configuration.java
@@ -16,41 +16,105 @@ public class Configuration {
 	private final static String RASCAL2TABLE_PROPERTY = "rascal.rascal2table.command";
 	private final static String RASCAL2TABLE_BINDIR_PROPERTY = "rascal.rascal2table.dir";
 	private final static String SDF_LIBRARY_PATH_PROPERTY = "rascal.sdf.library.dir";
+	
+	private volatile static String basePath = null;
+	
+	public static void setBasePath(String path){
+		if(path == null){
+			basePath = null;
+			return;
+		}
+			
+		if(path.endsWith("/")) basePath = path;
+		else basePath = path+"/";
+	}
 
-	public static String getDefaultParsetableProperty() {
-		try {
-			return System.getProperty(DEFAULT_PARSETABLE_PROPERTY, new File("../rascal-grammar/spec/rascal.tbl").getCanonicalPath());
-		} catch (IOException e) {
-			throw new ImplementationError("unexpected error in impl");
+	public static String getDefaultParsetableProperty(){
+		String defaultParsetableProperty = System.getProperty(DEFAULT_PARSETABLE_PROPERTY);
+		if(defaultParsetableProperty != null){
+			return defaultParsetableProperty;
 		}
+		
+		try{
+			if(basePath != null){
+				defaultParsetableProperty = new File(basePath+"rascal-grammar/spec/rascal.tbl").getCanonicalPath();
+			}else{
+				defaultParsetableProperty = new File("../rascal-grammar/spec/rascal.tbl").getCanonicalPath();
+			}
+			
+			System.setProperty(DEFAULT_PARSETABLE_PROPERTY, defaultParsetableProperty);
+		}catch(IOException ioex){
+			throw new ImplementationError("unexpected error in impl", ioex);
+		}
+		
+		return defaultParsetableProperty;
 	}
 	
-	public static String getHeaderParsetableProperty() {
-		try {
-			return System.getProperty(HEADER_PARSETABLE_PROPERTY, new File("../rascal-grammar/spec/rascal-header.tbl").getCanonicalPath());
-		} catch (IOException e) {
-			throw new ImplementationError("unexpected error in impl");
+	public static String getHeaderParsetableProperty(){
+		String headerParsetableProperty = System.getProperty(HEADER_PARSETABLE_PROPERTY);
+		if(headerParsetableProperty != null){
+			return headerParsetableProperty;
 		}
+		
+		try{
+			if(basePath != null){
+				headerParsetableProperty = new File(basePath+"rascal-grammar/spec/rascal-header.tbl").getCanonicalPath();
+			}else{
+				headerParsetableProperty = new File("../rascal-grammar/spec/rascal-header.tbl").getCanonicalPath();
+			}
+			
+			System.setProperty(HEADER_PARSETABLE_PROPERTY, headerParsetableProperty);
+		}catch(IOException ioex){
+			throw new ImplementationError("unexpected error in impl", ioex);
+		}
+		
+		return headerParsetableProperty;
 	}
 	
-	public static String getTableCacheDirectoryProperty() {
+	public static String getTableCacheDirectoryProperty(){
 		return System.getProperty(CACHE_PATH_PROPERTY, System.getProperty("java.io.tmpdir"));
 	}
 	
-	public static String getRascal2TableCommandProperty() {
-		try {
-			return System.getProperty(RASCAL2TABLE_PROPERTY, new File("../rascal-grammar/src/rascal2table").getCanonicalPath());
-		} catch (IOException e) {
-			throw new ImplementationError("unexpected error in impl");
+	public static String getRascal2TableCommandProperty(){
+		String rascal2TableCommandProperty = System.getProperty(RASCAL2TABLE_PROPERTY);
+		if(rascal2TableCommandProperty != null){
+			return rascal2TableCommandProperty;
 		}
+		
+		try{
+			if(basePath != null){
+				rascal2TableCommandProperty = new File(basePath+"rascal-grammar/src/rascal2table").getCanonicalPath();
+			}else{
+				rascal2TableCommandProperty = new File("../rascal-grammar/src/rascal2table").getCanonicalPath();
+			}
+			
+			System.setProperty(RASCAL2TABLE_PROPERTY, rascal2TableCommandProperty);
+		}catch(IOException ioex){
+			throw new ImplementationError("unexpected error in impl", ioex);
+		}
+		
+		return rascal2TableCommandProperty;
 	}
 	
-	public static String getRascal2TableBinDirProperty() {
-		try {
-			return System.getProperty(RASCAL2TABLE_BINDIR_PROPERTY, new File("../rascal-grammar/src").getCanonicalPath());
-		} catch (IOException e) {
-			throw new ImplementationError("unexpected error in impl");
+	public static String getRascal2TableBinDirProperty(){
+		String rascal2TableBinDirProperty = System.getProperty(RASCAL2TABLE_BINDIR_PROPERTY);
+		if(rascal2TableBinDirProperty != null){
+			return rascal2TableBinDirProperty;
 		}
+		
+		try{
+			if(basePath != null){
+				rascal2TableBinDirProperty = new File(basePath+"rascal-grammar/src").getCanonicalPath();
+			}else{
+				rascal2TableBinDirProperty = new File("../rascal-grammar/src").getCanonicalPath();
+			}
+			
+			System.setProperty(RASCAL2TABLE_BINDIR_PROPERTY, rascal2TableBinDirProperty);
+		}catch(IOException ioex){
+			throw new ImplementationError("unexpected error in impl", ioex);
+		}
+		
+		return rascal2TableBinDirProperty;
 	}
 
 	public static String getSDFExtensionProperty() {
@@ -58,10 +122,23 @@ public class Configuration {
 	}
 
 	public static String getSdfLibraryPathProperty() {
-		try {
-			return System.getProperty(SDF_LIBRARY_PATH_PROPERTY,new File("../sdf-library/library").getCanonicalPath());
-		} catch (IOException e) {
-			throw new ImplementationError("unexpected error in impl");
+		String sdfLibraryPathProperty = System.getProperty(SDF_LIBRARY_PATH_PROPERTY);
+		if(sdfLibraryPathProperty != null){
+			return sdfLibraryPathProperty;
 		}
+		
+		try{
+			if(basePath != null){
+				sdfLibraryPathProperty = new File(basePath+"sdf-library/library").getCanonicalPath();
+			}else{
+				sdfLibraryPathProperty = new File("../sdf-library/library").getCanonicalPath();
+			}
+			
+			System.setProperty(SDF_LIBRARY_PATH_PROPERTY, sdfLibraryPathProperty);
+		}catch(IOException ioex){
+			throw new ImplementationError("unexpected error in impl", ioex);
+		}
+		
+		return sdfLibraryPathProperty;
 	}
 }
