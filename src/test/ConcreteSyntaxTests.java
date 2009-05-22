7d6ef72a5203a3928a011cc7a0facd4a546b9e32
Paul Klint <Paul.Klint@cwi.nl>
2009-05-22 13:29:15 +0000

    Started with ConcreteSyntaxTest (all fail!)

---
+++ src/test/ConcreteSyntaxTests.java
@@ -0,0 +1,23 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ConcreteSyntaxTests extends TestFramework {
+	
+	@Test
+	public void Pico(){
+		
+		prepareModule("module M\n" +
+				 "import languages::pico::syntax::Pico;\n" +
+				 "public Tree t1 = [| begin declare x: natural; x := 10 end |];\n" +
+				 "public Tree t2 = begin declare x: natural; x := 10 end;\n" +
+		         "public bool match1() { return [| begin <decls>; <stats> end |] := t1; }\n" +
+		         "public bool match2() { return begin <decls>; <stats> end := t2; }");
+		
+		assertTrue(runTestInSameEvaluator("match1();"));
+		assertTrue(runTestInSameEvaluator("match2();"));
+	}
+
+}
