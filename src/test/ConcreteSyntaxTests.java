f57823878662ec3defb419238dd0077c5c8de143
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2009-05-25 09:35:17 +0000

    fixed syntax errors

--- src/test/ConcreteSyntaxTests.java
+++ src/test/ConcreteSyntaxTests.java
@@ -11,10 +11,11 @@ public class ConcreteSyntaxTests extends TestFramework {
 		
 		prepareModule("module M\n" +
 				 "import languages::pico::syntax::Pico;\n" +
+				 "import basic::Whitespace;\n" +
 				 "public Tree t1 = [| begin declare x: natural; x := 10 end |];\n" +
 				 "public Tree t2 = begin declare x: natural; x := 10 end;\n" +
-		         "public bool match1() { return [| begin <decls>; <stats> end |] := t1; }\n" +
-		         "public bool match2() { return begin <decls>; <stats> end := t2; }");
+		         "public bool match1() { return [| begin <decls> <stats> end |] := t1; }\n" +
+		         "public bool match2() { return begin <decls> <stats> end := t2; }");
 		
 		assertTrue(runTestInSameEvaluator("match1();"));
 		assertTrue(runTestInSameEvaluator("match2();"));
