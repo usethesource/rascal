f94441d87097ba81001c042a156af94e9f7eaa0b
Paul Klint <Paul.Klint@cwi.nl>
2009-05-29 14:48:12 +0000

    Extended tests.

--- src/test/ConcreteSyntaxTests.java
+++ src/test/ConcreteSyntaxTests.java
@@ -5,19 +5,95 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class ConcreteSyntaxTests extends TestFramework {
+	private String QmoduleM = "module M\n" +
+	                         "import languages::pico::syntax::Pico;\n" +
+	                         "public Tree t1 = [| begin declare x: natural; x := 10 end |];\n";
+	
+	private String UQmoduleM = "module M\n" +
+                              "import languages::pico::syntax::Pico;\n" +
+                              "public Tree t1 = begin declare x: natural; x := 10 end;\n";
 	
 	@Test
-	public void Pico(){
-		
-		prepareModule("module M\n" +
-				 "import languages::pico::syntax::Pico;\n" +
-				 "public Tree t1 = [| begin declare x: natural; x := 10 end |];\n" +
-				 "public Tree t2 = begin declare x: natural; x := 10 end;\n" +
-		         "public bool match1() { return [| begin <decls> <stats> end |] := t1; }\n" +
-		         "public bool match2() { return begin <decls> <stats> end := t2; }");
+	public void PicoQuoted1(){
+		prepareModule(QmoduleM + "public bool match1() { return [| <PROGRAM program> |] := t1; }\n");
 		prepareMore("import M;");
 		assertTrue(runTestInSameEvaluator("match1();"));
+	}
+	
+	@Test
+	public void PicoQuoted2(){
+		prepareModule(QmoduleM + "public bool match2() { return PROGRAM program := t1; }\n");
+		prepareMore("import M;");
 		assertTrue(runTestInSameEvaluator("match2();"));
 	}
-
+	
+	@Test
+	public void PicoQuoted3(){
+		prepareModule(QmoduleM + "public bool match3() { return [| begin <decls> <stats> end |] := t1; }\n");
+		prepareMore("import M;");
+		assertTrue(runTestInSameEvaluator("match3();"));
+	}
+	
+	@Test
+	public void PicoQuoted4(){
+		prepareModule(QmoduleM + "public bool match4() { return [| begin <DECLS decls> <stats> end |] := t1; }");
+		prepareMore("import M;");
+		assertTrue(runTestInSameEvaluator("match4();"));
+	}
+	
+	@Test
+	public void PicoQuoted5(){
+		prepareModule(QmoduleM + "public bool match5() { return [| begin <decls> <{STATEMENT \";\"}* stats> end |] := t1; }");
+		prepareMore("import M;");
+		assertTrue(runTestInSameEvaluator("match5();"));
+	}
+	
+	@Test
+	public void PicoQuoted6(){
+		prepareModule(QmoduleM + "public bool match6() { return [| begin <DECLS decls> <{STATEMENT \";\"}* stats> end |] := t1; }");
+		prepareMore("import M;");
+		assertTrue(runTestInSameEvaluator("match6();"));
+	}
+	
+	@Test
+	public void PicoUnQuoted1(){
+		prepareModule(UQmoduleM + "public bool match1() { return <PROGRAM program> := t1; }\n");
+		prepareMore("import M;");
+		assertTrue(runTestInSameEvaluator("match1();"));
+	}
+	
+	@Test
+	public void PicoUnQuoted2(){
+		prepareModule(UQmoduleM + "public bool match2() { return PROGRAM program := t1; }\n");
+		prepareMore("import M;");
+		assertTrue(runTestInSameEvaluator("match2();"));
+	}
+	
+	@Test
+	public void PicoUnQuoted3(){
+		prepareModule(UQmoduleM + "public bool match3() { return begin <decls> <stats> end := t1; }\n");
+		prepareMore("import M;");
+		assertTrue(runTestInSameEvaluator("match3();"));
+	}
+	
+	@Test
+	public void PicoUnQuoted4(){
+		prepareModule(UQmoduleM + "public bool match4() { return begin <DECLS decls> <stats> end := t1; }");
+		prepareMore("import M;");
+		assertTrue(runTestInSameEvaluator("match4();"));
+	}
+	
+	@Test
+	public void PicoUnQuoted5(){
+		prepareModule(UQmoduleM + "public bool match5() { return begin <decls> <{STATEMENT \";\"}* stats> end := t1; }");
+		prepareMore("import M;");
+		assertTrue(runTestInSameEvaluator("match5();"));
+	}
+	
+	@Test
+	public void PicoUnQuoted6(){
+		prepareModule(UQmoduleM + "public bool match6() { return begin <DECLS decls> <{STATEMENT \";\"}* stats> end := t1; }");
+		prepareMore("import M;");
+		assertTrue(runTestInSameEvaluator("match6();"));
+	}
 }
