6468072458d727ad77fceb70c74f3870f5108324
Emilie Balland <emilie.balland@gmail.com>
2009-06-09 12:01:05 +0000

    * improve the code for call stack * fix a bug in the call stack view's line numbers * improve the code relative to the interruption of the interpreter thread (in particular, when the debugger target terminates)

--- src/org/meta_environment/rascal/interpreter/IDebugger.java
+++ src/org/meta_environment/rascal/interpreter/IDebugger.java
@@ -2,12 +2,15 @@ package org.meta_environment.rascal.interpreter;
 
 //TODO: need to use an other way to identify a location that does not depend of eclipse
 import org.eclipse.imp.pdb.facts.ISourceLocation;
+import org.meta_environment.rascal.interpreter.control_exceptions.QuitException;
 
 public interface IDebugger {
 	
 	public boolean isStepping();
 
-	public void notifySuspend();
+	public boolean isTerminated();
+
+	public void notifySuspend() throws QuitException;
 	
 	public boolean hasEnabledBreakpoint(ISourceLocation sourceLocation);
 
