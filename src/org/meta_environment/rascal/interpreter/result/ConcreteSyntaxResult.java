132e66ca8dbd44c0f444fbab8ed55e1d5e1f650a
Tijs van der Storm <T.van.der.Storm@cwi.nl>
2009-06-23 14:11:05 +0000

    Fixed equality on syntax trees.

--- src/org/meta_environment/rascal/interpreter/result/ConcreteSyntaxResult.java
+++ src/org/meta_environment/rascal/interpreter/result/ConcreteSyntaxResult.java
@@ -1,17 +1,24 @@
 package org.meta_environment.rascal.interpreter.result;
 
+import org.eclipse.imp.pdb.facts.IBool;
 import org.eclipse.imp.pdb.facts.IConstructor;
+import org.eclipse.imp.pdb.facts.IList;
+import org.eclipse.imp.pdb.facts.ISet;
 import org.eclipse.imp.pdb.facts.IValue;
 import org.eclipse.imp.pdb.facts.type.Type;
 import org.meta_environment.rascal.interpreter.EvaluatorContext;
 import org.meta_environment.rascal.interpreter.asserts.ImplementationError;
+import org.meta_environment.uptr.ProductionAdapter;
+import org.meta_environment.uptr.TreeAdapter;
+
+import static org.meta_environment.rascal.interpreter.result.ResultFactory.makeResult;
+import static org.meta_environment.rascal.interpreter.result.ResultFactory.bool;
 
 public class ConcreteSyntaxResult extends ConstructorResult {
 
 	public ConcreteSyntaxResult(Type type, IConstructor cons,
 			EvaluatorContext ctx) {
 		super(type, cons, ctx);
-		// TODO Auto-generated constructor stub
 	}
 	
 	@Override
@@ -32,24 +39,118 @@ public class ConcreteSyntaxResult extends ConstructorResult {
 		return false;
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Override
 	protected <U extends IValue> Result<U> equalToConcreteSyntax(
 			ConcreteSyntaxResult that, EvaluatorContext ctx) {
 		IConstructor left = this.getValue();
-		IConstructor right = this.getValue();
-		if(!left.getName().equals(right.getName())){
-			throw new ImplementationError("names unequal");
+		IConstructor right = that.getValue();
+		
+		TreeAdapter t1 = new TreeAdapter(left);
+		TreeAdapter t2 = new TreeAdapter(right);
+		
+		if (t1.isLayout() && t2.isLayout()) {
+			return bool(true);
 		}
-		int i = 0;
-		for (IValue leftKid: left.getChildren()) {
-			IValue rightKid = right.get(i);
-			i++;
-			if(isLayout(leftKid) && isLayout(rightKid))
-				continue;
-			if(!leftKid.equals(rightKid))
-				return (Result<U>) new BoolResult(false, ctx);
+		
+		if (t1.isAppl() && t2.isAppl()) {
+			ProductionAdapter p1 = t1.getProduction();
+			ProductionAdapter p2 = t2.getProduction();
+			
+			// NB: using ordinary equals here...
+			if (!p1.getRhs().getTree().equals(p2.getRhs().getTree())) {
+				return bool(false);
+			}
+			
+			if (!t1.isList() && !t2.isList()) {
+				if (!p1.getLhs().equals(p2.getLhs())) {
+					return bool(false);
+				}
+			}
+			
+			if (t1.isList() && !t2.isList()) {
+				return bool(false);
+			}
+			
+			if (t2.isList() && !t1.isList()) {
+				return bool(false);
+			}
+			
+			if (!p1.getAttributes().equals(p2.getAttributes())) {
+				// TODO: attrs are unordered, no?
+				return bool(false);
+			}
+		
+			IList l1 = t1.getArgs();
+			IList l2 = t2.getArgs();
+			
+			if (t1.isList() && t2.isList()) {
+				if (l1.length() != l2.length()) {
+					return bool(false);
+				}
+			}
+			
+			for (int i = 0; i < l1.length(); i++) {
+				IValue kid1 = l1.get(i);
+				IValue kid2 = l2.get(i);
+				// Recurse here on kids to reuse layout handling etc.
+				Result<IBool> result = makeResult(kid1.getType(), kid1, ctx)
+					.equals(makeResult(kid2.getType(), kid2, ctx), ctx);
+				if (!result.getValue().getValue()) {
+					return bool(false);
+				}
+				if (t1.isContextFree()) {
+					i++; // skip layout
+				}
+			}
+			return bool(true);
 		}
-		return (Result<U>) new BoolResult(true, ctx);
+		
+		
+		if (t1.isChar() && t2.isChar()) {
+			return bool(t1.getCharacter() == t2.getCharacter());
+		}
+		
+		if (t1.isAmb() && t2.isAmb()) {
+			ISet alts1 = t1.getAlternatives();
+			ISet alts2 = t2.getAlternatives();
+
+			// TODO: this is very inefficient
+			again: for (IValue alt1: alts1) {
+				for (IValue alt2: alts2) {
+					Result<IBool> result = makeResult(alt1.getType(), alt1, ctx)
+						.equals(makeResult(alt2.getType(), alt2, ctx), ctx);
+					if (result.getValue().getValue()) {
+						// As soon an alt1 is equal to an alt2
+						// continue the outer loop.
+						continue again;
+					}
+				}
+				// If an alt1 is not equal to any of the the alt2's return false;
+				return bool(false);
+			}
+			return bool(true);
+		}
+
+		if (left.getType().equals(right.getType())) {
+			throw new ImplementationError("Missing case in equals of concrete syntax");
+		}
+		
+		return bool(false);
+		
+//		if(!left.getName().equals(right.getName()) || !left.getName().equals("appl")) {
+//			throw new ImplementationError("names unequal or not 'appl'");
+//		}
+//		int i = 0;
+//		for (IValue leftKid: left.getChildren()) {
+//			IValue rightKid = right.get(i);
+//			i++;
+//			if(isLayout(leftKid) && isLayout(rightKid))
+//				continue;
+//			if(!leftKid.equals(rightKid))
+//				return (Result<U>) new BoolResult(false, ctx);
+//		}
+//		return (Result<U>) new BoolResult(true, ctx);
 	}
 
 }
