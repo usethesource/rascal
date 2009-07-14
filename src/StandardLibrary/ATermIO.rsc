079582954ac5143841a276d1755760a653fb9a0f
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2009-07-14 11:20:00 +0000

    added naieve implementation of ATerm reader for library

---
+++ src/StandardLibrary/ATermIO.rsc
@@ -0,0 +1,6 @@
+module ATermIO
+
+public value java readATermFromTextFile(str name)
+@doc{readValueFromTextFile -- read an ATerm from a text file}
+@javaClass{org.meta_environment.rascal.std.ATermIO};
+	
---
+++ src/org/meta_environment/rascal/std/ATermIO.java
@@ -0,0 +1,34 @@
+package org.meta_environment.rascal.std;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.eclipse.imp.pdb.facts.IString;
+import org.eclipse.imp.pdb.facts.IValue;
+import org.eclipse.imp.pdb.facts.IValueFactory;
+import org.eclipse.imp.pdb.facts.io.ATermReader;
+import org.meta_environment.ValueFactoryFactory;
+import org.meta_environment.rascal.interpreter.RuntimeExceptionFactory;
+
+public class ATermIO {
+	private static final IValueFactory values = ValueFactoryFactory.getValueFactory();
+	
+	public static IValue readATermFromTextFile(IString sfileName)
+	//@doc{readValueFromTextFile -- read a value from a text file}
+	{
+			java.lang.String fileName = sfileName.getValue();
+			
+			try {
+				File file = new File(fileName);
+				// TODO: We need to use the TypeStore from the current environment,
+				// but how can we get access to it?
+				return new ATermReader().read(values, new FileInputStream(file));
+			} catch (IOException e) {
+				throw RuntimeExceptionFactory.io(values.string(e.getMessage()), null, null);
+			} catch (Exception e){
+				e.printStackTrace();
+				throw RuntimeExceptionFactory.io(values.string(e.getMessage()), null, null);
+			}
+	}
+}
