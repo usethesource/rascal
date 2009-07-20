770478787b7e839a7e7cec60815f537dc13c3271
Arnold Lankamp <arnold.lankamp@gmail.com>
2009-07-20 13:56:49 +0000

    -This probably fixes the binary generation problem.

--- src/org/meta_environment/rascal/interpreter/load/FromResourceLoader.java
+++ src/org/meta_environment/rascal/interpreter/load/FromResourceLoader.java
@@ -1,10 +1,13 @@
 package org.meta_environment.rascal.interpreter.load;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
 import org.eclipse.imp.pdb.facts.IConstructor;
+import org.eclipse.imp.pdb.facts.io.PBFWriter;
 
 public class FromResourceLoader implements IModuleFileLoader{
 	private final String sourceFolder;
@@ -66,7 +69,27 @@ public class FromResourceLoader implements IModuleFileLoader{
 	}
 	
 	public boolean tryWriteBinary(String filename, String binaryName, IConstructor tree){
-		// Unsupported operation.
+		File binFile = new File(binaryName);
+		if(!binFile.canWrite()) return false;
+		
+		FileOutputStream outputStream = null;
+		
+		PBFWriter pbfWriter = new PBFWriter();
+		try{
+			outputStream = new FileOutputStream(binFile);
+			pbfWriter.write(tree, outputStream);
+			return true;
+		}catch(IOException ioex){
+			ioex.printStackTrace();
+		}finally{
+			if(outputStream != null){
+				try{
+					outputStream.close();
+				}catch(IOException ioex){
+					ioex.printStackTrace();
+				}
+			}
+		}
 		return false;
 	}
 }
