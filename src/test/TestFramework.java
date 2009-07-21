518e6d41de951378965aa47ff0619235198faf88
Arnold Lankamp <arnold.lankamp@gmail.com>
2009-07-21 12:57:10 +0000

    -I want stacktraces.

--- src/test/TestFramework.java
+++ src/test/TestFramework.java
@@ -105,6 +105,7 @@ public class TestFramework {
 		} catch (Exception e) {
 			System.err
 					.println("Unhandled exception while preparing test: " + e);
+			e.printStackTrace();
 		}
 		return this;
 	}
