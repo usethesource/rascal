eb2abf03990e473f1b19bd2844b2010e9e448bfa
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2009-07-24 07:35:02 +0000

    Fixed small bugs in pattern matching code

--- src/org/meta_environment/rascal/interpreter/matching/MatchResult.java
+++ src/org/meta_environment/rascal/interpreter/matching/MatchResult.java
@@ -12,6 +12,7 @@ public class MatchResult extends AbstractBooleanResult {
 	private boolean positive;
 	private IMatchingResult mp;
 	private Expression expression;
+	private boolean firstTime;
 	
 	public MatchResult(IValueFactory vf, EvaluatorContext ctx, IMatchingResult pat, boolean positive, Expression expression) {
 		super(vf, ctx);
@@ -23,33 +24,32 @@ public class MatchResult extends AbstractBooleanResult {
     @Override
     public void init() {
     	super.init();
-    	mp.initMatch(expression.accept(ctx.getEvaluator()));
+//    	mp.initMatch(expression.accept(ctx.getEvaluator()));
+    	firstTime = true;
     }
 
     @Override
 	public boolean hasNext() {
-		if(hasNext){
-			boolean hn = mp.hasNext();
-			if(!hn){
-				hasNext = false;
-			}
-			return hn;
-		}
-		
-		return false;
+    	if (firstTime) {
+    		return true;
+    	}
+    	
+    	return mp.hasNext();
 	}
 
     @Override
 	public boolean next() {
-		Result<IValue> result = expression.accept(ctx.getEvaluator());
-		subject = result.getValue();
-    	Type subjectType = result.getType();
-    	
-    	if(!mp.mayMatch(subjectType, ctx.getCurrentEnvt())) {
-    		throw new UnexpectedTypeError(mp.getType(ctx.getCurrentEnvt()), subjectType, ctx.getCurrentAST());
+    	if (firstTime) {
+    		firstTime = false;
+    		Result<IValue> result = expression.accept(ctx.getEvaluator());
+    		Type subjectType = result.getType();
+        	
+        	mp.initMatch(result);
+        	
+        	if(!mp.mayMatch(subjectType, ctx.getCurrentEnvt())) {
+        		throw new UnexpectedTypeError(mp.getType(ctx.getCurrentEnvt()), subjectType, ctx.getCurrentAST());
+        	}
     	}
-    
-    	mp.initMatch(result);
     	
 		// TODO: should manage escape variable from negative matches!!!
 		if(hasNext()){	
