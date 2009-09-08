0be024fb89f769b3177dcd1c2474b17b0d0ececa
Emilie Balland <emilie.balland@gmail.com>
2009-09-08 11:44:20 +0000

    * return a closure for the All * check if the function given in argument is type-preserving

--- src/org/meta_environment/rascal/interpreter/strategy/All.java
+++ src/org/meta_environment/rascal/interpreter/strategy/All.java
@@ -1,31 +1,43 @@
 package org.meta_environment.rascal.interpreter.strategy;
 
 import org.eclipse.imp.pdb.facts.IValue;
+import org.eclipse.imp.pdb.facts.type.Type;
+import org.meta_environment.rascal.interpreter.IEvaluatorContext;
 import org.meta_environment.rascal.interpreter.result.AbstractFunction;
+import org.meta_environment.rascal.interpreter.result.ElementResult;
+import org.meta_environment.rascal.interpreter.result.OverloadedFunctionResult;
+import org.meta_environment.rascal.interpreter.result.Result;
 
-public class All implements Strategy {
+public class All extends StrategyFunction {
 
-	private Strategy s;
-
-	public All(Strategy s) {
-		this.s = s;
+	public All(AbstractFunction function) {
+		super(function);
 	}
 
-	public Visitable apply(Visitable v) {
-		Visitable result = v;
-		for (int i = 0; i < v.arity(); i++) {
-			result = result.set(i, s.apply(v.get(i)));
+	@Override
+	public Result<?> call(Type[] argTypes, IValue[] argValues,
+			IEvaluatorContext ctx) {
+		Visitable result = VisitableFactory.make(argValues[0]);
+		for (int i = 0; i < result.arity(); i++) {
+			result = result.set(i, VisitableFactory.make(function.call(argTypes, new IValue[]{result.get(i).getValue()}, ctx).getValue()));
 		}
-		return result;
+		return new ElementResult(result.getValue().getType(), result.getValue(), ctx);
 	}
 
-	public static IValue applyAll(IValue arg, IValue i) {
+	public static IValue makeAll(IValue arg) {
 		if (arg instanceof AbstractFunction) {
 			AbstractFunction function = (AbstractFunction) arg;
-			Strategy s = new All(new StrategyFunction(function));
-			Visitable v = s.apply(VisitableFactory.make(i));
-			return v.getValue();
-		}	
+			if (function.isTypePreserving()) {
+				return new All(new StrategyFunction(function));	
+			}
+		} else if (arg instanceof OverloadedFunctionResult) {
+			OverloadedFunctionResult res = (OverloadedFunctionResult) arg;
+			for (AbstractFunction function: res.iterable()) {
+				if (function.isTypePreserving()) {
+					return new All(new StrategyFunction(function));	
+				}
+			}
+		}
 		throw new RuntimeException("Unexpected strategy argument "+arg);
 	}
 
