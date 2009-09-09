b8bd78c3e0733c1e5bc95e1dfad48ce2e94a30dc
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2009-09-09 12:43:05 +0000

    fixed error message

--- src/org/meta_environment/rascal/interpreter/staticErrors/ModuleLoadError.java
+++ src/org/meta_environment/rascal/interpreter/staticErrors/ModuleLoadError.java
@@ -4,7 +4,7 @@ import org.meta_environment.rascal.ast.AbstractAST;
 
 public class ModuleLoadError extends StaticError {
 	public ModuleLoadError(String name, String cause, AbstractAST ast) {
-		super("Could not load module " + name + ": " + cause, ast);
+		super("Could not load module " + name + (cause != null ? (": " + cause) : ""), ast);
 	}
 
 	private static final long serialVersionUID = -2382848293435609203L;
