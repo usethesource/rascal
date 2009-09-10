eeeca361595b4dce3207103144aeba02a06f5684
Arnold Lankamp <arnold.lankamp@gmail.com>
2009-09-10 11:16:17 +0000

    -Simplified VisiableList#set(int, Visitable) code.

--- src/org/meta_environment/rascal/interpreter/strategy/VisitableList.java
+++ src/org/meta_environment/rascal/interpreter/strategy/VisitableList.java
@@ -4,10 +4,11 @@ import org.eclipse.imp.pdb.facts.IList;
 import org.eclipse.imp.pdb.facts.IValue;
 
 public class VisitableList implements Visitable {
-
-	private IList list;
+	private final IList list;
 
 	public VisitableList(IList list) {
+		super();
+		
 		this.list = list;
 	}
 
@@ -19,26 +20,11 @@ public class VisitableList implements Visitable {
 		return VisitableFactory.make(list.get(i));
 	}
 
-	public Visitable set(int i, Visitable newChild)
-	throws IndexOutOfBoundsException {
-		if (i<0 || i>=arity()) throw new IndexOutOfBoundsException();
-		if (i==0) {
-			IList suffix = list.sublist(1, arity()-1);
-			return new VisitableList(suffix.insert(newChild.getValue()));
-		} else {
-			IList newlist = list.sublist(0, i);
-			if (i<arity()-1) {
-				IList suffix = list.sublist(i+1, arity()-i-1);
-				newlist = newlist.append(newChild.getValue()).concat(suffix);
-			} else {
-				newlist = newlist.append(newChild.getValue());
-			}
-			return new VisitableList(newlist);
-		} 
+	public Visitable set(int i, Visitable newChild) throws IndexOutOfBoundsException {
+		return new VisitableList(list.put(i, newChild.getValue()));
 	}
 
 	public IValue getValue() {
 		return list;
 	}
-
 }
