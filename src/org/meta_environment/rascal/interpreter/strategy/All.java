953b55532ea0708f52a5ce141e2eca56b131a6d5
Emilie Balland <emilie.balland@gmail.com>
2009-09-17 09:14:47 +0000

    * improvements

--- src/org/meta_environment/rascal/interpreter/strategy/All.java
+++ src/org/meta_environment/rascal/interpreter/strategy/All.java
@@ -5,7 +5,6 @@ import org.eclipse.imp.pdb.facts.type.Type;
 import org.meta_environment.rascal.interpreter.IEvaluatorContext;
 import org.meta_environment.rascal.interpreter.result.AbstractFunction;
 import org.meta_environment.rascal.interpreter.result.ElementResult;
-import org.meta_environment.rascal.interpreter.result.OverloadedFunctionResult;
 import org.meta_environment.rascal.interpreter.result.Result;
 
 public class All extends Strategy {
@@ -26,20 +25,8 @@ public class All extends Strategy {
 	}
 
 	public static IValue makeAll(IValue arg) {
-		if (arg instanceof AbstractFunction) {
-			AbstractFunction function = (AbstractFunction) arg;
-			if (function.isStrategy()) {
-				return new All(function);	
-			}
-		} else if (arg instanceof OverloadedFunctionResult) {
-			OverloadedFunctionResult res = (OverloadedFunctionResult) arg;
-			for (AbstractFunction function: res.iterable()) {
-				if (function.isStrategy()) {
-					return new All(function);	
-				}
-			}
-		}
-		throw new RuntimeException("Unexpected strategy argument "+arg);
+		if (! Strategy.checkType(arg)) throw new RuntimeException("Unexpected strategy argument "+arg);
+		return new All((AbstractFunction) arg);
 	}
 
 }
--- src/org/meta_environment/rascal/interpreter/strategy/Strategy.java
+++ src/org/meta_environment/rascal/interpreter/strategy/Strategy.java
@@ -1,6 +1,8 @@
 package org.meta_environment.rascal.interpreter.strategy;
 
+import org.eclipse.imp.pdb.facts.IValue;
 import org.meta_environment.rascal.interpreter.result.AbstractFunction;
+import org.meta_environment.rascal.interpreter.result.OverloadedFunctionResult;
 
 public class Strategy extends AbstractFunction {
 
@@ -9,6 +11,23 @@ public class Strategy extends AbstractFunction {
 	public Strategy(AbstractFunction function) {
 		super(function.getAst(), function.getEval(), function.getFunctionType(), function.hasVarArgs(), function.getEnv());
 		this.function = function;
+	}
+	
+	public static boolean checkType(IValue arg) {
+		if (arg instanceof AbstractFunction) {
+			AbstractFunction function = (AbstractFunction) arg;
+			if (function.isStrategy()) {
+				return true;	
+			}
+		} else if (arg instanceof OverloadedFunctionResult) {
+			OverloadedFunctionResult res = (OverloadedFunctionResult) arg;
+			for (AbstractFunction function: res.iterable()) {
+				if (function.isStrategy()) {
+					return true;	
+				}
+			}
+		}
+		return false;
 	}
 
 }
