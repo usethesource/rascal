3361ec9d9ce7c4c3c103c9331fb380acae139855
Emilie Balland <emilie.balland@gmail.com>
2009-09-18 07:59:19 +0000

    * continue TopologicalAll

--- src/org/meta_environment/rascal/interpreter/strategy/TopologicalAll.java
+++ src/org/meta_environment/rascal/interpreter/strategy/TopologicalAll.java
@@ -1,6 +1,11 @@
 package org.meta_environment.rascal.interpreter.strategy;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 import org.eclipse.imp.pdb.facts.IRelation;
+import org.eclipse.imp.pdb.facts.ITuple;
 import org.eclipse.imp.pdb.facts.IValue;
 import org.eclipse.imp.pdb.facts.type.Type;
 import org.meta_environment.rascal.interpreter.IEvaluatorContext;
@@ -14,10 +19,28 @@ public class TopologicalAll extends All {
 		super(function);
 	}
 
+
+	private HashMap<IValue, LinkedList<IValue>> computeAdjacencies(IRelation relation) {
+		HashMap<IValue, LinkedList<IValue>> adjacencies = new HashMap<IValue, LinkedList<IValue>> ();
+		for(IValue v : relation){
+			ITuple tup = (ITuple) v;
+			IValue from = tup.get(0);
+			IValue to = tup.get(1);
+			LinkedList<IValue> children = adjacencies.get(from);
+			if(children == null)
+				children = new LinkedList<IValue>();
+			children.add(to);
+			adjacencies.put(from, children);
+		}  
+		return adjacencies;
+	}
+
 	@Override
 	public Result<IValue> call(Type[] argTypes, IValue[] argValues,
 			IEvaluatorContext ctx) {
 		if (argValues[0] instanceof IRelation) {
+			IRelation relation = ((IRelation) argValues[0]);
+			HashMap<IValue, LinkedList<IValue>> adjacencies = computeAdjacencies(relation);
 			Visitable result = VisitableFactory.make(argValues[0]);
 			for (int i = 0; i < result.arity(); i++) {
 				IValue child = result.get(i).getValue();
