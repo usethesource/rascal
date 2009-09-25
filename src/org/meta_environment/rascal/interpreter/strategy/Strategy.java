626fa65794c575ded5ce914d233e18ef7f8b2a6c
Emilie Balland <emilie.balland@gmail.com>
2009-09-25 08:40:05 +0000

    * should be like for topological implementations (should always remove the encapsulation before calling a rascal function)

--- src/org/meta_environment/rascal/interpreter/strategy/Strategy.java
+++ src/org/meta_environment/rascal/interpreter/strategy/Strategy.java
@@ -1,8 +1,14 @@
 package org.meta_environment.rascal.interpreter.strategy;
 
+import org.eclipse.imp.pdb.facts.IValue;
+import org.eclipse.imp.pdb.facts.type.Type;
+import org.meta_environment.rascal.interpreter.IEvaluatorContext;
 import org.meta_environment.rascal.interpreter.result.AbstractFunction;
+import org.meta_environment.rascal.interpreter.result.OverloadedFunctionResult;
+import org.meta_environment.rascal.interpreter.result.Result;
+import org.meta_environment.rascal.interpreter.result.ResultFactory;
 
-public abstract class Strategy extends AbstractFunction {
+public class Strategy extends AbstractFunction {
 
     protected final AbstractFunction function;
 	
@@ -14,5 +20,37 @@ public abstract class Strategy extends AbstractFunction {
 	public AbstractFunction getFunction() {
 		return function;
 	}
+	
+	
+	@Override
+	public Result<IValue> call(Type[] argTypes, IValue[] argValues, IEvaluatorContext ctx) {
+		if (argValues[0] instanceof IVisitable) {
+			IVisitable visitable = (IVisitable) argValues[0];
+			IValue v = visitable.getValue();	
+			Result<IValue> res = function.call(argTypes, new IValue[]{v}, ctx);
+			return ResultFactory.makeResult(res.getType(), VisitableFactory.makeVisitable(res.getValue()), ctx);
+		} else {
+			return function.call(argTypes, argValues, ctx);
+		}
+	}
+
+	public static IValue makeStrategy(IValue arg) {
+		if (arg instanceof AbstractFunction) {
+			AbstractFunction function = (AbstractFunction) arg;
+			if (function.isStrategy()) {
+				return new Strategy(function);	
+			} 
+		} else if (arg instanceof OverloadedFunctionResult) {
+			OverloadedFunctionResult res = (OverloadedFunctionResult) arg;
+			for (AbstractFunction function: res.iterable()) {
+				if (function.isStrategy()) {
+					return new Strategy(function);
+				}
+			}
+		}
+		throw new RuntimeException("Unexpected strategy argument "+arg);
+	}
+	
+	
 
 }
