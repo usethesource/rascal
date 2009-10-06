7caf3d543ceffba5ff8f4baad4101b7bfb1b9986
Emilie Balland <emilie.balland@gmail.com>
2009-10-06 08:39:38 +0000

    * fix cycle management

--- src/org/meta_environment/rascal/interpreter/strategy/topological/TopologicalContext.java
+++ src/org/meta_environment/rascal/interpreter/strategy/topological/TopologicalContext.java
@@ -2,9 +2,7 @@ package org.meta_environment.rascal.interpreter.strategy.topological;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.eclipse.imp.pdb.facts.IRelation;
 import org.eclipse.imp.pdb.facts.IRelationWriter;
@@ -103,7 +101,7 @@ public class TopologicalContext implements IStrategyContext {
 		if (visits.containsKey(v)) {
 			visits.put(v, visits.get(v)+1);	
 		} else {
-			visits.put(v, 0);
+			visits.put(v, 1);
 		}
 	}
 
