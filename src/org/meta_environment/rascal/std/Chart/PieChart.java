a636944ee0d2a3685d5f75c1e620fa644ca96121
Paul Klint <Paul.Klint@cwi.nl>
2009-10-07 22:08:08 +0000

    Added a datatype for defining Chart settings

--- src/org/meta_environment/rascal/std/Chart/PieChart.java
+++ src/org/meta_environment/rascal/std/Chart/PieChart.java
@@ -5,27 +5,28 @@ import java.util.Iterator;
 import java.util.Map.Entry;
 
 import org.eclipse.imp.pdb.facts.IInteger;
+import org.eclipse.imp.pdb.facts.IList;
 import org.eclipse.imp.pdb.facts.IMap;
 import org.eclipse.imp.pdb.facts.IReal;
 import org.eclipse.imp.pdb.facts.IString;
 import org.eclipse.imp.pdb.facts.IValue;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot;
 import org.jfree.chart.plot.PiePlot3D;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.PieDataset;
-import org.jfree.experimental.chart.swt.ChartComposite;
 import org.meta_environment.rascal.interpreter.utils.RuntimeExceptionFactory;
 
 /**
  * PieChart functions for Rascal library
  */
 public class PieChart {
-    
+	private static String[] provides = {
+		  "noSectionLabels", "noLegend", "noToolTips", "dim3", "circular"
+	};
+
+		
     /**
      * Converts a Rascal Map to a PieDataset.
      * 
@@ -60,21 +61,37 @@ public class PieChart {
      * @return A chart.
      */
     private static JFreeChart createChart(java.lang.String title, PieDataset dataset) {
-        
-        JFreeChart chart = ChartFactory.createPieChart3D(
-            title,  	// chart title
-            dataset,    // data
-            true,      // include legend
-            true,      // tooltips
-            false
-        );
-
-        PiePlot3D plot = (PiePlot3D) chart.getPlot();
-        plot.setSectionOutlinesVisible(false);
+    	JFreeChart chart;
+    	 PiePlot plot;
+    	
+    	if(Settings.has("dim3")){
+            chart = ChartFactory.createPieChart3D(
+            			title,  						// chart title
+            			dataset,    					// data
+            			!Settings.has("noLegend"),      // include legend
+            			!Settings.has("noToolTips"),    // tooltips
+            			false);
+            plot = (PiePlot3D) chart.getPlot();
+    	} else {
+        	 chart = ChartFactory.createPieChart(
+         				title,  						// chart title
+         				dataset,    					// data
+         				!Settings.has("noLegend"),      // include legend
+         				!Settings.has("noToolTips"),   	// tooltips
+         				false);
+        	 plot = (PiePlot) chart.getPlot();
+    	}
+     
         plot.setLabelFont(new Font("SansSerif", Font.PLAIN, 12));
         plot.setNoDataMessage("No data available");
-        plot.setCircular(false);
-        plot.setLabelGap(0.02);
+        plot.setCircular(Settings.has("circular"));
+        plot.setSectionOutlinesVisible(false);
+        if(Settings.has("noSectionLabels")){
+        	 plot.setInteriorGap(0.0);
+             plot.setLabelGenerator(null);
+        } else {
+        	plot.setLabelGap(0.02);
+        }
         plot.setForegroundAlpha(0.5f);
         return chart;
     }
@@ -83,44 +100,22 @@ public class PieChart {
      * makePiechart: a reusable function to a create a piechart
      */
     
-    public static JFreeChart makePiechart(IString title, IMap facts){
+    public static JFreeChart makePiechart(IString title, IMap facts, IValue settings){
     	String titleString = title.getValue();
+    	Settings.validate(provides, (IList)settings);
     	return createChart(titleString, createDataset(facts));
     }
     
     /**
-     * The piechart library function
+     * The pieChart library function
      *
      * @param title title of the chart
      * @param facts the data (a map)
      */
-    public static void piechart(IString title, IMap facts)
+    public static void pieChart(IString title, IMap facts, IValue settings)
     {
-    	DisplayChart dc = new DisplayChart(title.getValue(), makePiechart(title, facts));
+    	DisplayChart dc = new DisplayChart(title.getValue(), makePiechart(title, facts, settings));
     	dc.run();
-    }
-    
-    /**
-     * Display a titled chart
-     *
-     * @param title	   title of the chart
-     * @param chart   the actual chart
-     */
-    static void display(String title, JFreeChart chart)
-    {
-        Display display = new Display();
-        Shell shell = new Shell(display);
-        shell.setSize(600, 400);
-        shell.setLayout(new FillLayout());
-        shell.setText(title);
-        final ChartComposite frame = new ChartComposite(shell, SWT.NONE, chart, true);
-        //frame.setDisplayToolTips(false);
-        frame.pack();
-        shell.open();
-        while (!shell.isDisposed()) {
-            if (!display.readAndDispatch())
-                display.sleep();
-        }
     }
 
 }
---
+++ src/org/meta_environment/rascal/std/Chart/Settings.java
@@ -0,0 +1,67 @@
+package org.meta_environment.rascal.std.Chart;
+
+import org.eclipse.imp.pdb.facts.IInteger;
+import org.eclipse.imp.pdb.facts.IList;
+import org.eclipse.imp.pdb.facts.IReal;
+import org.eclipse.imp.pdb.facts.IString;
+import org.eclipse.imp.pdb.facts.IValue;
+import org.eclipse.imp.pdb.facts.impl.fast.Constructor;
+import org.meta_environment.rascal.interpreter.asserts.ImplementationError;
+import org.meta_environment.rascal.interpreter.utils.RuntimeExceptionFactory;
+
+public class Settings {
+	static IList settings;
+	private static IValue lastSetting;
+	
+	static void supported(IValue v, String s, String[] provided){
+		for(int i = 0; i < provided.length; i ++){
+			if(provided[i].	equals(s))
+				return;
+		}
+		throw RuntimeExceptionFactory.illegalArgument(v, null, null);
+	}
+	
+	static void validate(String[] provided, IList settings){
+		Settings.settings = settings;
+		for(IValue s : settings){
+			Constructor scons = (Constructor) s;
+			supported(s, scons.getName(), provided);
+		}
+	}
+    
+	static boolean has(String name){
+		for(IValue s : settings){
+			Constructor scons = (Constructor) s;
+			if(scons.getName().equals(name)){
+				if(scons.arity() > 0){
+					lastSetting = scons.get(0);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	@SuppressWarnings("unused")
+	private static int lastInteger(){
+		if(lastSetting != null && lastSetting.getType().isIntegerType()){
+			return ((IInteger) lastSetting).intValue();
+		} else
+			throw new ImplementationError("Last used setting not an integer");
+	}
+	
+	@SuppressWarnings("unused")
+	private static float lastReal(){
+		if(lastSetting != null && lastSetting.getType().isRealType()){
+			return ((IReal) lastSetting).floatValue();
+		} else
+			throw new ImplementationError("Last used setting not a real");
+	}
+	@SuppressWarnings("unused")
+	private static String lastString(){
+		if(lastSetting != null && lastSetting.getType().isStringType()){
+			return ((IString) lastSetting).getValue();
+		} else
+			throw new ImplementationError("Last used setting not a string");
+	}
+}
