ce0b286f736b1bc15dff9f86db3f7480752d4bb1
Arnold Lankamp <arnold.lankamp@gmail.com>
2009-10-26 10:32:05 +0000

    -Updated strategy tests, so they are also executed in separate evaluators, in addition to being executed in the same. This way we can test for unexpected side effects on the environment (to a degree).

--- src/org/meta_environment/rascal/test/StrategyTests.java
+++ src/org/meta_environment/rascal/test/StrategyTests.java
@@ -37,6 +37,65 @@ public class StrategyTests extends TestFramework{
 	}
 	
 	@Test
+	public void testStrategySeperately(){
+		TestFramework tf = new TestFramework();
+		tf.prepare("import StrategyTests;");
+		tf.prepareMore("import Strategy;");
+		tf.prepareMore("A t = f(g(g(b())),g(g(b())));");
+		assertTrue(tf.runTestInSameEvaluator("top_down(rules)(t) == f(g(b()),g(b()))"));
+		tf = new TestFramework();
+		tf.prepare("import StrategyTests;");
+		tf.prepareMore("import Strategy;");
+		tf.prepareMore("A t = f(g(g(b())),g(g(b())));");
+		assertTrue(tf.runTestInSameEvaluator("bottom_up(rules)(t) == f(b(),b())"));
+
+		tf = new TestFramework();
+		tf.prepare("import StrategyTests;");
+		tf.prepareMore("import Strategy;");
+		tf.prepareMore("B t2 = g(c());");
+		assertTrue(tf.runTestInSameEvaluator("once_top_down(rules2)(t2) == c()"));
+		tf = new TestFramework();
+		tf.prepare("import StrategyTests;");
+		tf.prepareMore("import Strategy;");
+		tf.prepareMore("B t2 = g(c());");
+		assertTrue(tf.runTestInSameEvaluator("once_bottom_up(rules2)(t2) == g(b())"));
+
+		tf = new TestFramework();
+		tf.prepare("import StrategyTests;");
+		tf.prepareMore("import Strategy;");
+		tf.prepareMore("list[B] l = [g(c()),c()];");
+		assertTrue(tf.runTestInSameEvaluator("makeAll(rules2)(l) == [c(),b()]"));
+
+		tf = new TestFramework();
+		tf.prepare("import StrategyTests;");
+		tf.prepareMore("import Strategy;");
+		tf.prepareMore("tuple[A,B] t3 = <a(),c()>;");
+		assertTrue(tf.runTestInSameEvaluator("top_down(rules2)(t3) == <a(),b()>"));
+
+		tf = new TestFramework();
+		tf.prepare("import StrategyTests;");
+		tf.prepareMore("import Strategy;");
+		tf.prepareMore("rel[A, B] r = {<a(), b()>, <f(b(),b()), c()>};");
+		assertTrue(tf.runTestInSameEvaluator("top_down(rules3)(r) == {<a(), c()>, <f(c(),c()), c()>}"));
+
+		tf = new TestFramework();
+		tf.prepare("import StrategyTests;");
+		tf.prepareMore("import Strategy;");
+		tf.prepareMore("A t4 = f(g(b()),g(b()));");
+		assertTrue(tf.runTestInSameEvaluator("top_down(functionToStrategy(rules4))(t4) == f(g(c()),g(c()))"));
+		tf = new TestFramework();
+		tf.prepare("import StrategyTests;");
+		tf.prepareMore("import Strategy;");
+		tf.prepareMore("A t = f(g(g(b())),g(g(b())));");
+		assertTrue(tf.runTestInSameEvaluator("innermost(rules)(t) == f(b(),b())"));
+		tf = new TestFramework();
+		tf.prepare("import StrategyTests;");
+		tf.prepareMore("import Strategy;");
+		tf.prepareMore("A t = f(g(g(b())),g(g(b())));");
+		assertTrue(tf.runTestInSameEvaluator("outermost(rules)(t) == f(b(),b())"));
+	}
+	
+	@Test
 	public void testTopologicalStrategy(){
 		prepare("import StrategyTests;");
 		prepareMore("import TopologicalStrategy;");
@@ -51,6 +110,43 @@ public class StrategyTests extends TestFramework{
 		assertTrue(runTestInSameEvaluator("topological_innermost(rules6)(r2) == {<h(aa()), h(dd())>, <h(aa()), h(ee())>, <h(dd()),h(ee())>}"));
 		
 		assertTrue(runTestInSameEvaluator("topological_outermost(rules6)(r2) == {<h(aa()), h(dd())>, <h(aa()), h(ee())>, <h(dd()),h(ee())>}"));
+	}
+	
+	@Test
+	public void testTopologicalStrategySeperately(){
+		TestFramework tf = new TestFramework();
+		tf.prepare("import StrategyTests;");
+		tf.prepareMore("import TopologicalStrategy;");
+		tf.prepareMore("rel[A, A] r2 = {<a(), d()>, <a(), e()>, <d(), e()>};");
+		assertTrue(tf.runTestInSameEvaluator("topological_once_bottom_up(rules5)(r2) == {<a(),ee()>,<a(),d()>,<d(),ee()>}"));
+		tf = new TestFramework();
+		tf.prepare("import StrategyTests;");
+		tf.prepareMore("import TopologicalStrategy;");
+		tf.prepareMore("rel[A, A] r2 = {<a(), d()>, <a(), e()>, <d(), e()>};");
+		assertTrue(tf.runTestInSameEvaluator("topological_once_top_down(rules5)(r2) == {<aa(),e()>,<aa(),d()>,<d(),e()>}"));
+		
+		tf = new TestFramework();
+		tf.prepare("import StrategyTests;");
+		tf.prepareMore("import TopologicalStrategy;");
+		tf.prepareMore("rel[A, A] r2 = {<a(), d()>, <a(), e()>, <d(), e()>};");
+		assertTrue(tf.runTestInSameEvaluator("topological_top_down(rules5)(r2) == {<aa(), dd()>, <aa(), ee()>, <dd(),ee()>}"));
+		
+		tf = new TestFramework();
+		tf.prepare("import StrategyTests;");
+		tf.prepareMore("import TopologicalStrategy;");
+		tf.prepareMore("rel[A, A] r2 = {<a(), d()>, <a(), e()>, <d(), e()>};");
+		assertTrue(tf.runTestInSameEvaluator("topological_bottom_up(rules5)(r2) == {<aa(), dd()>, <aa(), ee()>, <dd(),ee()>}"));
+		tf = new TestFramework();
+		tf.prepare("import StrategyTests;");
+		tf.prepareMore("import TopologicalStrategy;");
+		tf.prepareMore("rel[A, A] r2 = {<a(), d()>, <a(), e()>, <d(), e()>};");
+		assertTrue(tf.runTestInSameEvaluator("topological_innermost(rules6)(r2) == {<h(aa()), h(dd())>, <h(aa()), h(ee())>, <h(dd()),h(ee())>}"));
+		
+		tf = new TestFramework();
+		tf.prepare("import StrategyTests;");
+		tf.prepareMore("import TopologicalStrategy;");
+		tf.prepareMore("rel[A, A] r2 = {<a(), d()>, <a(), e()>, <d(), e()>};");
+		assertTrue(tf.runTestInSameEvaluator("topological_outermost(rules6)(r2) == {<h(aa()), h(dd())>, <h(aa()), h(ee())>, <h(dd()),h(ee())>}"));
 	}
 	
 	@Test
