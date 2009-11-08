39fa7471e883debad84f9ce5e09f8505b16545cb
Paul Klint <Paul.Klint@cwi.nl>
2009-11-08 22:45:07 +0000

    Playing with another approach to visualization

---
+++ src/org/meta_environment/rascal/library/experiments/VL/VL.java
@@ -0,0 +1,17 @@
+package org.meta_environment.rascal.library.experiments.VL;
+
+import org.eclipse.imp.pdb.facts.IConstructor;
+import org.meta_environment.rascal.interpreter.IEvaluatorContext;
+import org.meta_environment.rascal.library.experiments.Processing.SketchSWT;
+
+import processing.core.PApplet;
+
+public class VL  {
+
+	public static void render(IConstructor panel, IEvaluatorContext ctx){
+		PApplet pa = new VLPApplet(panel, ctx);
+		SketchSWT mySketch = new SketchSWT(pa);
+	}
+}
+
+
---
+++ src/org/meta_environment/rascal/library/experiments/VL/VLPApplet.java
@@ -0,0 +1,69 @@
+package org.meta_environment.rascal.library.experiments.VL;
+
+import java.util.ArrayList;
+
+import org.eclipse.imp.pdb.facts.IConstructor;
+import org.eclipse.imp.pdb.facts.IInteger;
+import org.eclipse.imp.pdb.facts.IList;
+import org.eclipse.imp.pdb.facts.IValue;
+import org.meta_environment.rascal.interpreter.IEvaluatorContext;
+import org.meta_environment.rascal.interpreter.utils.RuntimeExceptionFactory;
+
+import processing.core.PApplet;
+
+
+public class VLPApplet extends PApplet {
+	private  IEvaluatorContext ctx;
+	private int width = 200;
+	private int height = 200;
+	private ArrayList<VELEM> velems;
+
+	VLPApplet(IConstructor panel, IEvaluatorContext ctx){
+		this.ctx = ctx;
+		IList props = (IList) panel.get(0);
+		IList elms = (IList) panel.get(1);
+		getProps(props);
+		getElems(elms);
+	}
+
+	private void getProps(IList props){
+		for(IValue v : props){
+			IConstructor c = (IConstructor) v;
+			String pname = c.getName();
+			System.err.println("pname = " + pname);
+			if(pname.equals("width")){
+				width = ((IInteger) c.get(0)).intValue();
+			} else if(pname.equals("height")){
+				height = ((IInteger) c.get(0)).intValue();
+			} else {
+				throw RuntimeExceptionFactory.illegalArgument(v, ctx.getCurrentAST(), ctx.getStackTrace());
+			}
+		}
+	}
+
+	private void getElems(IList elms){
+		velems = new ArrayList<VELEM>();
+		for(IValue v : elms){
+			IConstructor c = (IConstructor) v;
+			String ename = c.getName();
+			System.err.println("ename = " + ename);
+			if(ename.equals("bar")){
+				IList props = (IList) c.get(0);
+				VELEM ve = new Bar(props, ctx);
+				velems.add(ve);
+			} else {
+				throw RuntimeExceptionFactory.illegalArgument(v, ctx.getCurrentAST(), ctx.getStackTrace());
+			}
+		}
+	}
+
+	@Override
+	public void setup(){
+		size(width, height);
+	}
+
+	@Override
+	public void draw(){
+
+	}
+}
