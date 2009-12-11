3a8367580e77bcb5f67adcb89bd74360820cc76b
Paul Klint <Paul.Klint@cwi.nl>
2009-12-11 12:32:57 +0000

    Destroy display and Processing applet when visualization is finished.

--- src/org/meta_environment/rascal/library/experiments/Processing/SketchSWT.java
+++ src/org/meta_environment/rascal/library/experiments/Processing/SketchSWT.java
@@ -14,6 +14,7 @@ import processing.core.PApplet;
 public class SketchSWT   {
 	
 	private PApplet applet;
+	//private static Display display = new Display();
 
 	public SketchSWT (final PApplet pa){
 		this. applet = pa;
@@ -38,6 +39,8 @@ public class SketchSWT   {
 			if (!display.readAndDispatch())
 				display.sleep();
 		}
+		display.dispose();
+		pa.destroy();
 	}
 	
 	public PApplet getApplet(){
