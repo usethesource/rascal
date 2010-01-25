3223b033a2b48b2f0ceaccbc24da8527713022c3
Paul Klint <Paul.Klint@cwi.nl>
2010-01-25 12:26:18 +0000

    Factored out common code in Container class.

--- src/org/meta_environment/rascal/library/experiments/VL/Ellipse.java
+++ src/org/meta_environment/rascal/library/experiments/VL/Ellipse.java
@@ -7,50 +7,15 @@ import org.meta_environment.rascal.interpreter.IEvaluatorContext;
 import processing.core.PConstants;
 
 
-public class Ellipse extends VELEM {
-	private VELEM inside;
-	private static boolean debug = true;
+public class Ellipse extends Container {
 
 	public Ellipse(VLPApplet vlp, PropertyManager inheritedProps, IList props, IConstructor inside, IEvaluatorContext ctx) {
-		super(vlp, inheritedProps, props, ctx);
-		if(inside != null)
-			this.inside = VELEMFactory.make(vlp, inside, this.properties, ctx);
-	}
-
-	@Override
-	void bbox(float left, float top){
-		this.left = left;
-		this.top = top;
-		int lw = getLineWidthProperty();
-		width = getWidthProperty();
-		height = getHeightProperty();
-		if(inside != null){
-			int hgap = getHGapProperty();
-			int vgap = getVGapProperty();
-			inside.bbox();
-			if(width == 0 && height == 0){
-				width = inside.width + 2 * hgap;
-				height = inside.height + 2 * vgap;
-			}
-		}
-		width += 2*lw;
-		height += 2*lw;
-		if(debug)System.err.printf("bbox.ellipse: %f, %f)\n", width, height);
+		super(vlp, inheritedProps, props, inside, ctx);
 	}
 	
 	@Override
-	void draw() {
-		if(debug)System.err.printf("ellipse.draw: %d, %d\n", left, top);
-		
-		applyProperties();
+	void drawContainer(){
 		vlp.ellipseMode(PConstants.CORNERS);
 		vlp.ellipse(left, top, left + width, top + height);
-		if(inside != null){
-			int hgap = getHGapProperty();
-			int vgap = getVGapProperty();
-			inside.draw(left + hgap + properties.hanchor*(width - inside.width - 2 * hgap),
-				        top  + vgap + properties.vanchor*(height - inside.height - 2 * vgap));
-		}
 	}
-
 }
