947794337b8cd1099daa9641a3c154110493a64e
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2010-03-16 16:27:13 +0000

    fixed bugs in guarded pattern

--- src/org/rascalmpl/interpreter/matching/GuardedPattern.java
+++ src/org/rascalmpl/interpreter/matching/GuardedPattern.java
@@ -9,6 +9,7 @@ import org.rascalmpl.interpreter.result.Result;
 import org.rascalmpl.interpreter.result.ResultFactory;
 import org.rascalmpl.interpreter.staticErrors.UnexpectedTypeError;
 import org.rascalmpl.interpreter.types.NonTerminalType;
+import org.rascalmpl.values.uptr.Factory;
 import org.rascalmpl.values.uptr.TreeAdapter;
 
 public class GuardedPattern extends AbstractMatchingResult {
@@ -27,22 +28,35 @@ public class GuardedPattern extends AbstractMatchingResult {
 	}
 	
 	@Override
+	public boolean hasNext() {
+		return pat.hasNext();
+	}
+	
+	@Override
 	public void initMatch(Result<IValue> subject){
 		super.initMatch(subject);
 		
 		Environment env = ctx.getCurrentEnvt();
 		
-		if (type instanceof NonTerminalType && pat.getType(env).isSubtypeOf(tf.stringType())) {
-			subject = ResultFactory.makeResult(tf.stringType(), ctx.getValueFactory().string(TreeAdapter.yield((IConstructor) subject.getValue())), ctx);
-			pat.initMatch(subject);
+		if (type instanceof NonTerminalType && pat.getType(env).isSubtypeOf(tf.stringType()) && subject.getValue().getType().isSubtypeOf(Factory.Tree)) {
+			if (subject.getValue().getType().isSubtypeOf(type)) {
+				subject = ResultFactory.makeResult(tf.stringType(), ctx.getValueFactory().string(TreeAdapter.yield((IConstructor) subject.getValue())), ctx);
+				pat.initMatch(subject);
+				this.hasNext = pat.hasNext();
+			}
+			else {
+				this.hasNext = false;
+			}
 		}
 		else {
 			pat.initMatch(subject);
-			if (!mayMatch(pat.getType(env), type)) {
-				throw new UnexpectedTypeError(pat.getType(env), type, ctx.getCurrentAST());
-			}
+			// this code triggers during a visit which might encounter other stuff that would never match
+//			if (!mayMatch(pat.getType(env), type)) {
+//				throw new UnexpectedTypeError(pat.getType(env), type, ctx.getCurrentAST());
+//			}
+			this.hasNext = pat.getType(env).equivalent(type);
 		}
-		this.hasNext = pat.getType(env).equivalent(type);
+		
 	}
 
 	@Override
