1ec1f4d62994c5f004b25dbe6d7e6fb7adb4478c
Tijs van der Storm <T.van.der.Storm@cwi.nl>
2010-03-31 08:26:31 +0000

    Fixed ranges from int to real.

--- src/org/rascalmpl/interpreter/result/RealResult.java
+++ src/org/rascalmpl/interpreter/result/RealResult.java
@@ -314,10 +314,10 @@ public class RealResult extends ElementResult<IReal> {
 	
 	private <U extends IValue, V extends INumber> Result<U> makeRangeWithDefaultStep(Result<V> from) {
 		if (from.getValue().less(getValue()).getValue()) {
-			return makeStepRangeFromToWithSecond(from, this, new NumberResult(getTypeFactory().realType(),
+			return makeStepRangeFromToWithSecond(from, this, new NumberResult(getTypeFactory().numberType(),
 					from.getValue().add(getValueFactory().real(1.0)), ctx), getValueFactory(), getTypeFactory(), ctx);
 		}
-		return makeStepRangeFromToWithSecond(from, this, new NumberResult(getTypeFactory().realType(),
+		return makeStepRangeFromToWithSecond(from, this, new NumberResult(getTypeFactory().numberType(),
 				from.getValue().subtract(getValueFactory().real(1.0)), ctx), getValueFactory(), getTypeFactory(), ctx);
 
 	}
