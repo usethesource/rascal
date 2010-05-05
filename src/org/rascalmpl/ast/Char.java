353c4e58c59de7b1dfed1a5c9923e9daebb19506
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2010-05-05 14:44:26 +0000

    debugged AST construction for the new syntax definition part of rascal

--- src/org/rascalmpl/ast/Char.java
+++ src/org/rascalmpl/ast/Char.java
@@ -1,21 +1,23 @@
 package org.rascalmpl.ast; 
 import org.eclipse.imp.pdb.facts.INode; 
 public abstract class Char extends AbstractAST { 
-  public boolean isEscape() { return false; }
-static public class Escape extends Char {
-/** EscapeSequence -> Char {cons("Escape"), category("Constant")} */
-	public Escape(INode node) {
-		this.node = node;
-	}
-	@Override
-	public <T> T accept(IASTVisitor<T> visitor) {
-		return visitor.visitCharEscape(this);
+	static public class Lexical extends Char {
+		private final String string;
+	         public Lexical(INode node, String string) {
+			this.node = node;
+			this.string = string;
+		}
+		public String getString() {
+			return string;
+		}
+
+	 	@Override
+		public <T> T accept(IASTVisitor<T> v) {
+	     		return v.visitCharLexical(this);
+	  	}
 	}
 
-	@Override
-	public boolean isEscape() { return true; }	
-}
-static public class Ambiguity extends Char {
+	static public class Ambiguity extends Char {
   private final java.util.List<org.rascalmpl.ast.Char> alternatives;
   public Ambiguity(INode node, java.util.List<org.rascalmpl.ast.Char> alternatives) {
 	this.alternatives = java.util.Collections.unmodifiableList(alternatives);
@@ -30,46 +32,4 @@ public <T> T accept(IASTVisitor<T> v) {
      return v.visitCharAmbiguity(this);
   }
 } 
-public boolean isUnicode() { return false; }
-static public class Unicode extends Char {
-/** UnicodeEscape -> Char {cons("Unicode"), category("Constant")} */
-	public Unicode(INode node) {
-		this.node = node;
-	}
-	@Override
-	public <T> T accept(IASTVisitor<T> visitor) {
-		return visitor.visitCharUnicode(this);
-	}
-
-	@Override
-	public boolean isUnicode() { return true; }	
-} @Override
-public abstract <T> T accept(IASTVisitor<T> visitor); public boolean isNormal() { return false; }
-static public class Normal extends Char {
-/** ~[\"\'\\\-\[\]\ ] -> Char {cons("Normal"), category("Constant")} */
-	public Normal(INode node) {
-		this.node = node;
-	}
-	@Override
-	public <T> T accept(IASTVisitor<T> visitor) {
-		return visitor.visitCharNormal(this);
-	}
-
-	@Override
-	public boolean isNormal() { return true; }	
-} 
-public boolean isExtraEscaped() { return false; }
-static public class ExtraEscaped extends Char {
-/** "\\" [\-\[\]\ ] -> Char {cons("ExtraEscaped"), category("Constant")} */
-	public ExtraEscaped(INode node) {
-		this.node = node;
-	}
-	@Override
-	public <T> T accept(IASTVisitor<T> visitor) {
-		return visitor.visitCharExtraEscaped(this);
-	}
-
-	@Override
-	public boolean isExtraEscaped() { return true; }	
-}
 }
