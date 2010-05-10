5c2ed2133f65bb289db005142c85f431339c93ab
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-05-10 12:08:08 +0000

    -And it isn't even 15:00 yet ....

--- src/org/rascalmpl/parser/sgll/stack/LiteralStackNode.java
+++ src/org/rascalmpl/parser/sgll/stack/LiteralStackNode.java
@@ -74,7 +74,7 @@ public final class LiteralStackNode extends StackNode{
 		throw new UnsupportedOperationException();
 	}
 	
-	public void addResult(INode[] children){
+	public void addResult(IConstructor production, INode[] children){
 		throw new UnsupportedOperationException();
 	}
 	
