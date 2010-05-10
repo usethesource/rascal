56b2e42a458807ce9f863ab2c3070f021c3c738c
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-05-10 11:54:11 +0000

    -Removed the production stuff for now, since it was wrong (sharing is in the way).

--- src/org/rascalmpl/parser/sgll/stack/LiteralStackNode.java
+++ src/org/rascalmpl/parser/sgll/stack/LiteralStackNode.java
@@ -74,7 +74,7 @@ public final class LiteralStackNode extends StackNode{
 		throw new UnsupportedOperationException();
 	}
 	
-	public void addResult(IConstructor production, INode[] children){
+	public void addResult(INode[] children){
 		throw new UnsupportedOperationException();
 	}
 	
