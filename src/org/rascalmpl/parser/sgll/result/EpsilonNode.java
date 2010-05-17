858dd8209148bf730be76da2545fd2c4a33e3c2e
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-05-17 09:45:26 +0000

    -Sort of fixed epsilon output.

--- src/org/rascalmpl/parser/sgll/result/EpsilonNode.java
+++ src/org/rascalmpl/parser/sgll/result/EpsilonNode.java
@@ -9,7 +9,7 @@ import org.rascalmpl.values.uptr.Factory;
 public class EpsilonNode implements INode{
 	private final static IValueFactory vf = ValueFactoryFactory.getValueFactory();
 	
-	private final static String EPSILON_STRING = "";
+	private final static String EPSILON_STRING = "appl(prod([],empty()))";
 	
 	public EpsilonNode(){
 		super();
@@ -29,7 +29,7 @@ public class EpsilonNode implements INode{
 	
 	public IValue toTerm(){
 		IConstructor empty = vf.constructor(Factory.Symbol_Empty);
-		IConstructor production = vf.constructor(Factory.Production, empty);
+		IConstructor production = vf.constructor(Factory.Production_Default, vf.list(Factory.Symbol), empty);
 		return vf.constructor(Factory.Tree_Appl, production);
 	}
 }
