bd77298a7168d14bbc03f8281b1e493b75ca0ada
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-07-09 14:10:28 +0000

    -Next.

--- src/org/rascalmpl/test/parser/Simple2.java
+++ src/org/rascalmpl/test/parser/Simple2.java
@@ -8,43 +8,52 @@ import java.io.Reader;
 import org.eclipse.imp.pdb.facts.IConstructor;
 import org.eclipse.imp.pdb.facts.IValue;
 import org.rascalmpl.parser.sgll.SGLL;
+import org.rascalmpl.parser.sgll.result.INode;
 import org.rascalmpl.parser.sgll.stack.AbstractStackNode;
 import org.rascalmpl.parser.sgll.stack.LiteralStackNode;
 import org.rascalmpl.parser.sgll.stack.NonTerminalStackNode;
 import org.rascalmpl.values.uptr.Factory;
 
 /*
-S ::= Ab
-A ::= aa
+S ::= AB
+A ::= a
+B ::= b
 */
-public class Simple1 extends SGLL{
+public class Simple2 extends SGLL{
 	private final static IConstructor SYMBOL_START_S = vf.constructor(Factory.Symbol_Sort, vf.string("S"));
 	private final static IConstructor SYMBOL_A0 = vf.constructor(Factory.Symbol_Sort, vf.string("A"));
-	private final static IConstructor SYMBOL_aa1 = vf.constructor(Factory.Symbol_Lit, vf.string("aa"));
-	private final static IConstructor SYMBOL_b2 = vf.constructor(Factory.Symbol_Lit, vf.string("b"));
+	private final static IConstructor SYMBOL_B1 = vf.constructor(Factory.Symbol_Sort, vf.string("B"));
+	private final static IConstructor SYMBOL_a2 = vf.constructor(Factory.Symbol_Lit, vf.string("a"));
+	private final static IConstructor SYMBOL_b3 = vf.constructor(Factory.Symbol_Lit, vf.string("b"));
 	private final static IConstructor SYMBOL_a = vf.constructor(Factory.Symbol_CharClass, vf.list(vf.constructor(Factory.CharRange_Single, vf.integer(97))));
 	private final static IConstructor SYMBOL_b = vf.constructor(Factory.Symbol_CharClass, vf.list(vf.constructor(Factory.CharRange_Single, vf.integer(98))));
 	
-	private final static IConstructor PROD_S_Ab = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_A0, SYMBOL_b2), SYMBOL_START_S, vf.list(Factory.Attributes));
-	private final static IConstructor PROD_A_aa = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_aa1), SYMBOL_A0, vf.list(Factory.Attributes));
-	private final static IConstructor PROD_aa_a = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_a, SYMBOL_a), SYMBOL_aa1, vf.list(Factory.Attributes));
-	private final static IConstructor PROD_b_b = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_b), SYMBOL_b2, vf.list(Factory.Attributes));
+	private final static IConstructor PROD_S_AB = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_A0, SYMBOL_B1), SYMBOL_START_S, vf.list(Factory.Attributes));
+	private final static IConstructor PROD_A_a = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_a2), SYMBOL_A0, vf.list(Factory.Attributes));
+	private final static IConstructor PROD_B_b = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_b3), SYMBOL_B1, vf.list(Factory.Attributes));
+	private final static IConstructor PROD_a_a = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_a), SYMBOL_a2, vf.list(Factory.Attributes));
+	private final static IConstructor PROD_b_b = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_b), SYMBOL_b3, vf.list(Factory.Attributes));
 	
 	private final static AbstractStackNode NONTERMINAL_START_S = new NonTerminalStackNode(START_SYMBOL_ID, "S");
 	private final static AbstractStackNode NONTERMINAL_A0 = new NonTerminalStackNode(0, "A");
-	private final static AbstractStackNode LITERAL_aa1 = new LiteralStackNode(1, PROD_aa_a, new char[]{'a','a'});
-	private final static AbstractStackNode LITERAL_b2 = new LiteralStackNode(2, PROD_b_b, new char[]{'b'});
+	private final static AbstractStackNode NONTERMINAL_B1 = new NonTerminalStackNode(1, "B");
+	private final static AbstractStackNode LITERAL_a2 = new LiteralStackNode(2, PROD_a_a, new char[]{'a'});
+	private final static AbstractStackNode LITERAL_b3 = new LiteralStackNode(3, PROD_b_b, new char[]{'b'});
 	
-	public Simple1(){
+	public Simple2(){
 		super();
 	}
 	
 	public void S(){
-		expect(PROD_S_Ab, NONTERMINAL_A0, LITERAL_b2);
+		expect(PROD_S_AB, NONTERMINAL_A0, NONTERMINAL_B1);
 	}
 	
 	public void A(){
-		expect(PROD_A_aa, LITERAL_aa1);
+		expect(PROD_A_a, LITERAL_a2);
+	}
+	
+	public void B(){
+		expect(PROD_B_b, LITERAL_b3);
 	}
 	
 	public IValue parse(IConstructor start, char[] input){
@@ -66,12 +75,12 @@ public class Simple1 extends SGLL{
 	public IValue parse(IConstructor start, String input){
 		throw new UnsupportedOperationException();
 	}
-
+	
 	public static void main(String[] args){
-		Simple1 s1 = new Simple1();
-		IValue result = s1.parse(NONTERMINAL_START_S, "aab".toCharArray());
+		Simple2 s2 = new Simple2();
+		IValue result = s2.parse(NONTERMINAL_START_S, "ab".toCharArray());
 		System.out.println(result);
 		
-		System.out.println("S(A(aa),b) <- good");
+		System.out.println("S(A(a),B(b)) <- good");
 	}
 }
