96dc5a3ee5d57c61e7f3b648022556b27e18d622
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-07-12 07:52:06 +0000

    -Next.

---
+++ src/org/rascalmpl/test/parser/Ambiguous1.java
@@ -0,0 +1,76 @@
+package org.rascalmpl.test.parser;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
+import org.eclipse.imp.pdb.facts.IConstructor;
+import org.eclipse.imp.pdb.facts.IValue;
+import org.rascalmpl.parser.sgll.SGLL;
+import org.rascalmpl.parser.sgll.stack.AbstractStackNode;
+import org.rascalmpl.parser.sgll.stack.LiteralStackNode;
+import org.rascalmpl.parser.sgll.stack.NonTerminalStackNode;
+import org.rascalmpl.values.uptr.Factory;
+
+/*
+S ::= A | a
+A ::= a
+*/
+public class Ambiguous1 extends SGLL{
+	private final static IConstructor SYMBOL_START_S = vf.constructor(Factory.Symbol_Sort, vf.string("S"));
+	private final static IConstructor SYMBOL_A0 = vf.constructor(Factory.Symbol_Sort, vf.string("A"));
+	private final static IConstructor SYMBOL_a1 = vf.constructor(Factory.Symbol_Lit, vf.string("a"));
+	private final static IConstructor SYMBOL_a = vf.constructor(Factory.Symbol_CharClass, vf.list(vf.constructor(Factory.CharRange_Single, vf.integer(97))));
+	
+	private final static IConstructor PROD_S_A = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_A0), SYMBOL_START_S, vf.list(Factory.Attributes));
+	private final static IConstructor PROD_S_a = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_a1), SYMBOL_START_S, vf.list(Factory.Attributes));
+	private final static IConstructor PROD_A_a = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_a1), SYMBOL_A0, vf.list(Factory.Attributes));
+	private final static IConstructor PROD_a_a = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_a), SYMBOL_a1, vf.list(Factory.Attributes));
+	
+	private final static AbstractStackNode NONTERMINAL_START_S = new NonTerminalStackNode(START_SYMBOL_ID, "S");
+	private final static AbstractStackNode NONTERMINAL_A0 = new NonTerminalStackNode(0, "A");
+	private final static AbstractStackNode LITERAL_a1 = new LiteralStackNode(1, PROD_a_a, new char[]{'a'});
+	
+	public Ambiguous1(){
+		super();
+	}
+	
+	public void S(){
+		expect(PROD_S_A, NONTERMINAL_A0);
+		
+		expect(PROD_S_a, LITERAL_a1);
+	}
+	
+	public void A(){
+		expect(PROD_A_a, LITERAL_a1);
+	}
+	
+	public IValue parse(IConstructor start, char[] input){
+		throw new UnsupportedOperationException();
+	}
+	
+	public IValue parse(IConstructor start, File inputFile) throws IOException{
+		throw new UnsupportedOperationException();
+	}
+	
+	public IValue parse(IConstructor start, InputStream in) throws IOException{
+		throw new UnsupportedOperationException();
+	}
+	
+	public IValue parse(IConstructor start, Reader in) throws IOException{
+		throw new UnsupportedOperationException();
+	}
+	
+	public IValue parse(IConstructor start, String input){
+		throw new UnsupportedOperationException();
+	}
+	
+	public static void main(String[] args){
+		Ambiguous1 a1 = new Ambiguous1();
+		IValue result = a1.parse(NONTERMINAL_START_S, "a".toCharArray());
+		System.out.println(result);
+		
+		System.out.println("[S(A(a)),S(a)] <- good");
+	}
+}
