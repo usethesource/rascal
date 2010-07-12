dfb39ed407ed6ea7c9b71587940ef9a879471052
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-07-12 11:43:52 +0000

    -More.

---
+++ src/org/rascalmpl/test/parser/Epsilon.java
@@ -0,0 +1,76 @@
+package org.rascalmpl.test.parser;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
+import org.eclipse.imp.pdb.facts.IConstructor;
+import org.eclipse.imp.pdb.facts.IValue;
+import org.rascalmpl.parser.sgll.SGLL;
+import org.rascalmpl.parser.sgll.stack.AbstractStackNode;
+import org.rascalmpl.parser.sgll.stack.EpsilonStackNode;
+import org.rascalmpl.parser.sgll.stack.LiteralStackNode;
+import org.rascalmpl.parser.sgll.stack.NonTerminalStackNode;
+import org.rascalmpl.values.uptr.Factory;
+
+/*
+S ::= Aepsilon
+A ::= a
+*/
+public class Epsilon extends SGLL{
+	private final static IConstructor SYMBOL_START_S = vf.constructor(Factory.Symbol_Sort, vf.string("S"));
+	private final static IConstructor SYMBOL_A = vf.constructor(Factory.Symbol_Sort, vf.string("A"));
+	private final static IConstructor SYMBOL_a = vf.constructor(Factory.Symbol_Lit, vf.string("a"));
+	private final static IConstructor SYMBOL_char_a = vf.constructor(Factory.Symbol_CharClass, vf.list(vf.constructor(Factory.CharRange_Single, vf.integer(97))));
+	private final static IConstructor SYMBOL_epsilon = vf.constructor(Factory.Symbol_Empty);
+	
+	private final static IConstructor PROD_S_Aepsilon = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_A, SYMBOL_epsilon), SYMBOL_START_S, vf.list(Factory.Attributes));
+	private final static IConstructor PROD_S_a = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_a), SYMBOL_START_S, vf.list(Factory.Attributes));
+	private final static IConstructor PROD_a_a = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_char_a), SYMBOL_a, vf.list(Factory.Attributes));
+	
+	private final static AbstractStackNode NONTERMINAL_START_S = new NonTerminalStackNode(START_SYMBOL_ID, "S");
+	private final static AbstractStackNode NONTERMINAL_A0 = new NonTerminalStackNode(0, "A");
+	private final static AbstractStackNode EPSILON_1 = new EpsilonStackNode(1);
+	private final static AbstractStackNode NONTERMINAL_a2 = new LiteralStackNode(2, PROD_a_a, new char[]{'a'});
+	
+	public Epsilon(){
+		super();
+	}
+	
+	public void S(){
+		expect(PROD_S_Aepsilon, NONTERMINAL_A0, EPSILON_1);
+	}
+	
+	public void A(){
+		expect(PROD_S_a, NONTERMINAL_a2);
+	}
+	
+	public IValue parse(IConstructor start, char[] input){
+		throw new UnsupportedOperationException();
+	}
+	
+	public IValue parse(IConstructor start, File inputFile) throws IOException{
+		throw new UnsupportedOperationException();
+	}
+	
+	public IValue parse(IConstructor start, InputStream in) throws IOException{
+		throw new UnsupportedOperationException();
+	}
+	
+	public IValue parse(IConstructor start, Reader in) throws IOException{
+		throw new UnsupportedOperationException();
+	}
+	
+	public IValue parse(IConstructor start, String input){
+		throw new UnsupportedOperationException();
+	}
+	
+	public static void main(String[] args){
+		Epsilon e = new Epsilon();
+		IValue result = e.parse(NONTERMINAL_START_S, "a".toCharArray());
+		System.out.println(result);
+		
+		System.out.println("S(A(a),) <- good");
+	}
+}
