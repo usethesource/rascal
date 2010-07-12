dd67dc66aa546566ef8b3169f6322906b7d9dae1
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-07-12 09:35:28 +0000

    -More.

---
+++ src/org/rascalmpl/test/parser/AmbiguousRecursive.java
@@ -0,0 +1,77 @@
+package org.rascalmpl.test.parser;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
+import org.eclipse.imp.pdb.facts.IConstructor;
+import org.eclipse.imp.pdb.facts.IValue;
+import org.rascalmpl.parser.sgll.SGLL;
+import org.rascalmpl.parser.sgll.stack.AbstractStackNode;
+import org.rascalmpl.parser.sgll.stack.LiteralStackNode;
+import org.rascalmpl.parser.sgll.stack.NonTerminalStackNode;
+import org.rascalmpl.values.uptr.Factory;
+
+/*
+S ::= SSS | SS | a
+*/
+public class AmbiguousRecursive extends SGLL{
+	private final static IConstructor SYMBOL_START_S = vf.constructor(Factory.Symbol_Sort, vf.string("S"));
+	private final static IConstructor SYMBOL_S = vf.constructor(Factory.Symbol_Sort, vf.string("S"));
+	private final static IConstructor SYMBOL_a = vf.constructor(Factory.Symbol_Lit, vf.string("a"));
+	private final static IConstructor SYMBOL_char_a = vf.constructor(Factory.Symbol_CharClass, vf.list(vf.constructor(Factory.CharRange_Single, vf.integer(97))));
+	
+	private final static IConstructor PROD_S_SSS = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_S, SYMBOL_S, SYMBOL_S), SYMBOL_START_S, vf.list(Factory.Attributes));
+	private final static IConstructor PROD_S_SS = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_S, SYMBOL_S), SYMBOL_START_S, vf.list(Factory.Attributes));
+	private final static IConstructor PROD_S_a = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_a), SYMBOL_START_S, vf.list(Factory.Attributes));
+	private final static IConstructor PROD_a_a = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_char_a), SYMBOL_a, vf.list(Factory.Attributes));
+	
+	private final static AbstractStackNode NONTERMINAL_START_S = new NonTerminalStackNode(START_SYMBOL_ID, "S");
+	private final static AbstractStackNode NONTERMINAL_S0 = new NonTerminalStackNode(0, "S");
+	private final static AbstractStackNode NONTERMINAL_S1 = new NonTerminalStackNode(1, "S");
+	private final static AbstractStackNode NONTERMINAL_S2 = new NonTerminalStackNode(2, "S");
+	private final static AbstractStackNode NONTERMINAL_S3 = new NonTerminalStackNode(3, "S");
+	private final static AbstractStackNode NONTERMINAL_S4 = new NonTerminalStackNode(4, "S");
+	private final static AbstractStackNode LITERAL_a5 = new LiteralStackNode(5, PROD_a_a, new char[]{'a'});
+	
+	public AmbiguousRecursive(){
+		super();
+	}
+	
+	public void S(){
+		expect(PROD_S_SSS, NONTERMINAL_S0, NONTERMINAL_S1, NONTERMINAL_S2);
+		
+		expect(PROD_S_SS, NONTERMINAL_S3, NONTERMINAL_S4);
+		
+		expect(PROD_S_a, LITERAL_a5);
+	}
+	
+	public IValue parse(IConstructor start, char[] input){
+		throw new UnsupportedOperationException();
+	}
+	
+	public IValue parse(IConstructor start, File inputFile) throws IOException{
+		throw new UnsupportedOperationException();
+	}
+	
+	public IValue parse(IConstructor start, InputStream in) throws IOException{
+		throw new UnsupportedOperationException();
+	}
+	
+	public IValue parse(IConstructor start, Reader in) throws IOException{
+		throw new UnsupportedOperationException();
+	}
+	
+	public IValue parse(IConstructor start, String input){
+		throw new UnsupportedOperationException();
+	}
+	
+	public static void main(String[] args){
+		AmbiguousRecursive ar = new AmbiguousRecursive();
+		IValue result = ar.parse(NONTERMINAL_START_S, "aaa".toCharArray());
+		System.out.println(result);
+		
+		System.out.println("[S(S(a),S(a),S(a)),S(S(a),S(S(a),S(a))),S(S(S(a),S(a)),S(a))] <- good");
+	}
+}
