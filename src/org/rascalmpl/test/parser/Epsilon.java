ef22770f4e0531c266f2d4d9dfa2017c20879931
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-07-14 08:02:05 +0000

    -Changed the epsilon stuff.

--- src/org/rascalmpl/test/parser/Epsilon.java
+++ src/org/rascalmpl/test/parser/Epsilon.java
@@ -12,41 +12,28 @@ import org.eclipse.imp.pdb.facts.io.StandardTextReader;
 import org.rascalmpl.parser.sgll.SGLL;
 import org.rascalmpl.parser.sgll.stack.AbstractStackNode;
 import org.rascalmpl.parser.sgll.stack.EpsilonStackNode;
-import org.rascalmpl.parser.sgll.stack.LiteralStackNode;
 import org.rascalmpl.parser.sgll.stack.NonTerminalStackNode;
 import org.rascalmpl.values.ValueFactoryFactory;
 import org.rascalmpl.values.uptr.Factory;
 
 /*
-S ::= Aepsilon
-A ::= a
+S ::= epsilon
 */
 public class Epsilon extends SGLL implements IParserTest{
 	private final static IConstructor SYMBOL_START_S = vf.constructor(Factory.Symbol_Sort, vf.string("S"));
-	private final static IConstructor SYMBOL_A = vf.constructor(Factory.Symbol_Sort, vf.string("A"));
-	private final static IConstructor SYMBOL_a = vf.constructor(Factory.Symbol_Lit, vf.string("a"));
-	private final static IConstructor SYMBOL_char_a = vf.constructor(Factory.Symbol_CharClass, vf.list(vf.constructor(Factory.CharRange_Single, vf.integer(97))));
 	private final static IConstructor SYMBOL_epsilon = vf.constructor(Factory.Symbol_Empty);
 	
-	private final static IConstructor PROD_S_Aepsilon = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_A, SYMBOL_epsilon), SYMBOL_START_S, vf.constructor(Factory.Attributes_NoAttrs));
-	private final static IConstructor PROD_S_a = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_a), SYMBOL_START_S, vf.constructor(Factory.Attributes_NoAttrs));
-	private final static IConstructor PROD_a_a = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_char_a), SYMBOL_a, vf.constructor(Factory.Attributes_NoAttrs));
+	private final static IConstructor PROD_S_epsilon = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_epsilon), SYMBOL_START_S, vf.constructor(Factory.Attributes_NoAttrs));
 	
 	private final static AbstractStackNode NONTERMINAL_START_S = new NonTerminalStackNode(START_SYMBOL_ID, "S");
-	private final static AbstractStackNode NONTERMINAL_A0 = new NonTerminalStackNode(0, "A");
 	private final static AbstractStackNode EPSILON_1 = new EpsilonStackNode(1);
-	private final static AbstractStackNode NONTERMINAL_a2 = new LiteralStackNode(2, PROD_a_a, new char[]{'a'});
 	
 	public Epsilon(){
 		super();
 	}
 	
 	public void S(){
-		expect(PROD_S_Aepsilon, NONTERMINAL_A0, EPSILON_1);
-	}
-	
-	public void A(){
-		expect(PROD_S_a, NONTERMINAL_a2);
+		expect(PROD_S_epsilon, EPSILON_1);
 	}
 	
 	public IValue parse(IConstructor start, char[] input){
@@ -71,17 +58,17 @@ public class Epsilon extends SGLL implements IParserTest{
 	
 	public boolean executeTest() throws IOException{
 		Epsilon e = new Epsilon();
-		IValue result = e.parse(NONTERMINAL_START_S, "a".toCharArray());
+		IValue result = e.parse(NONTERMINAL_START_S, new char[]{});
 
-		String expectedInput = "parsetree(appl(prod([sort(\"A\"),empty()],sort(\"S\"),\\no-attrs()),[appl(prod([lit(\"a\")],sort(\"S\"),\\no-attrs()),[appl(prod([\\char-class([single(97)])],lit(\"a\"),\\no-attrs()),[char(97)])]),empty()]),-1)";
+		String expectedInput = "parsetree(appl(prod([empty()],sort(\"S\"),\\no-attrs()),[]),-1)";
 		return result.isEqual(new StandardTextReader().read(ValueFactoryFactory.getValueFactory(), Factory.uptr, Factory.ParseTree, new ByteArrayInputStream(expectedInput.getBytes())));
 	}
 
 	public static void main(String[] args){
 		Epsilon e = new Epsilon();
-		IValue result = e.parse(NONTERMINAL_START_S, "a".toCharArray());
+		IValue result = e.parse(NONTERMINAL_START_S, new char[]{});
 		System.out.println(result);
 		
-		System.out.println("S(A(a),) <- good");
+		System.out.println("S() <- good");
 	}
 }
