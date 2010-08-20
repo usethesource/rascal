3a35ebc3ef0cef660e3be0e6038b1b40af291ac1
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2010-08-20 18:49:23 +0000

    added static methods to retrieve user time and system time per thread to be able to monitor performance more precisely

---
+++ src/org/rascalmpl/interpreter/utils/Timing.java
@@ -0,0 +1,26 @@
+package org.rascalmpl.interpreter.utils;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+
+public final class Timing {
+	public static long getCpuTime( ) {
+	    ThreadMXBean bean = ManagementFactory.getThreadMXBean( );
+	    return bean.isCurrentThreadCpuTimeSupported( ) ?
+	        bean.getCurrentThreadCpuTime( ) : 0L;
+	}
+	 
+	/** Get user time in nanoseconds. */
+	public static long getUserTime( ) {
+	    ThreadMXBean bean = ManagementFactory.getThreadMXBean( );
+	    return bean.isCurrentThreadCpuTimeSupported( ) ?
+	        bean.getCurrentThreadUserTime( ) : 0L;
+	}
+
+	/** Get system time in nanoseconds. */
+	public static long getSystemTime( ) {
+	    ThreadMXBean bean = ManagementFactory.getThreadMXBean( );
+	    return bean.isCurrentThreadCpuTimeSupported( ) ?
+	        (bean.getCurrentThreadCpuTime( ) - bean.getCurrentThreadUserTime( )) : 0L;
+	}
+}
