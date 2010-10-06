a71bf3f4866b4a71b43069bd4837a039e0caa21d
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-10-06 12:46:54 +0000

    -Cleanup.

--- src/org/rascalmpl/parser/sgll/stack/AbstractStackNode.java
+++ src/org/rascalmpl/parser/sgll/stack/AbstractStackNode.java
@@ -10,7 +10,6 @@ import org.rascalmpl.parser.sgll.util.specific.PositionStore;
 
 public abstract class AbstractStackNode{
 	public final static int START_SYMBOL_ID = -1;
-	public final static int DEFAULT_LEVEL_ID = 0;
 	protected final static int DEFAULT_LIST_EPSILON_ID = -2; // (0xeffffffe | 0x80000000)
 	
 	protected AbstractStackNode[] next;
