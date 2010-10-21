008e09dc56a4a44613916c11b0c8c6428bd22fb1
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2010-10-21 07:41:53 +0000

    copying trunk back to branch

---
+++ src/org/rascalmpl/library/vis/TreeMapNode.java
@@ -0,0 +1,184 @@
+
+package org.rascalmpl.library.vis;
+
+import java.util.ArrayList;
+
+import org.eclipse.imp.pdb.facts.IList;
+import org.rascalmpl.interpreter.IEvaluatorContext;
+
+/**
+ * A TreeMapNode is created for each "node" constructor that occurs in the TreeMap.
+ * 
+ * @author paulk
+ *
+ */
+public class TreeMapNode extends Figure {
+	
+	Figure rootFigure;
+	TreeMap treemap;
+	private ArrayList<TreeMapNode> children;
+	private float[] childLeft;
+	private float[] childTop;
+	private static boolean debug = true;
+	
+	public TreeMapNode(FigurePApplet fpa, TreeMap treeMap, PropertyManager inheritedProps,
+			IList props, Figure fig, IEvaluatorContext ctx) {
+		super(fpa, inheritedProps, props, ctx);
+		this.treemap = treeMap;
+		rootFigure = fig;
+		children = new ArrayList<TreeMapNode>();
+	}
+	
+	public void addChild(PropertyManager inheritedProps, IList props,
+			TreeMapNode toNode, IEvaluatorContext ctx) {
+		children.add(toNode);
+	}
+	
+	public void place(float width, float height, boolean hor) {
+		this.width = width;
+		this.height = height;
+		
+		float hgap = getHGapProperty();
+		float vgap = getHGapProperty();
+		
+		String id = rootFigure.getIdProperty();
+		if(debug)System.err.printf("%s: %f,%f,%s\n", id, width,height, hor? "hor":"vert");
+		
+		int n = children.size();
+		
+		childLeft = new float[n];
+		childTop = new float[n];
+		float ratio[] = new float[n];
+		float chsurf = 0;
+		float awidth = width - (n+1) * hgap;
+		float aheight = height - (n+1) * vgap;
+		for(int i = 0; i < n; i++){
+			TreeMapNode child = children.get(i);
+			child.bbox();
+			chsurf += child.width * child.height;
+		}
+		for(int i = 0; i < n; i++){
+			TreeMapNode child = children.get(i);
+			ratio[i] = (child.width * child.height) / chsurf;
+			if(debug)System.err.printf("%s: ratio = %f\n", child.rootFigure.getIdProperty(), ratio[i]);
+		}
+		if(hor){
+			float x = hgap;
+			for(int i = 0; i < n; i++){	
+				TreeMapNode child = children.get(i);
+				float dw = ratio[i] * awidth;
+				child.place(dw, height - 2* vgap, !hor);
+				childLeft[i] = x;
+				childTop[i] = vgap;
+				x += dw + hgap;
+			}
+		} else {
+			float y = vgap;
+			for(int i = 0; i < n; i++){	
+				TreeMapNode child = children.get(i);
+				float dh =  ratio[i] * aheight;
+				child.place(width - 2 * hgap, dh, !hor);
+				childLeft[i] = hgap;
+				childTop[i] = y;
+				y += dh + vgap;
+			}
+		}
+       return;
+	}
+	
+	@Override
+	void bbox() {
+		rootFigure.bbox();
+		width = rootFigure.width;
+		height = rootFigure.height;
+	}
+	
+	@Override
+	void draw(float left, float top){
+		this.left = left;
+		this.top = top;
+		if(debug)System.err.printf("draw: %s at %f, %f (%s)\n", 
+				          rootFigure.getIdProperty(), left,  top,
+				          isVisible() ? "visible" : "invisible");
+		if(!isVisible())
+			return;
+		
+		rootFigure.applyProperties();
+		fpa.rect(left, top, width, height);
+		
+		if(isNextVisible()){
+			fpa.incDepth();
+			int n = children.size();
+			for(int i = 0; i < n; i++){
+				TreeMapNode child = children.get(i);
+				child.draw(left + childLeft[i], top + childTop[i]);
+			}
+			fpa.decDepth();
+		}
+	}
+	
+//	@Override
+//	public boolean mouseInside(int mousex, int mousey){
+//		float l = left + leftDragged;
+//		float t = top + topDragged;
+//	
+//		return mousex > l && mousex < l + width &&
+//			   mousey > t && mousey < t + height;
+//	}
+	
+	@Override
+	public void drawFocus(){
+		if(debug)System.err.printf("TreeMapNode.drawFocus: %s, %f, %f\n", rootFigure.getIdProperty(), left, top);
+		fpa.stroke(255, 0,0);
+		fpa.noFill();
+		fpa.rect(left, top, width, height);
+	}
+	
+	@Override
+	public boolean mouseOver(int mousex, int mousey){
+		if(debug)System.err.printf("TreeMapNode.mouseover: %s, %d, %d\n", rootFigure.getIdProperty(), mousex, mousey);
+		if(debug)System.err.printf("TreeMapNode.mouseover: left=%f, top=%f\n", left, top);
+		if(!isVisible())
+			return false;
+		if(rootFigure.mouseOver(mousex, mousey))
+			return true;
+		if(isNextVisible()){
+			for(TreeMapNode child : children)
+				if(child.mouseOver(mousex, mousey))
+					return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public boolean mousePressed(int mousex, int mousey){
+		if(debug)System.err.printf("TreeMapNode.mousePressed: %s, %d, %d\n", rootFigure.getIdProperty(), mousex, mousey);
+		if(!isVisible())
+			return false;
+		if(isNextVisible()){
+			for(TreeMapNode child : children)
+				if(child.mousePressed(mousex, mousey))
+					return true;
+		}
+		if(mouseInside(mousex, mousey)){
+			fpa.registerFocus(this);
+			return true;
+		}
+		return false;
+	}
+	
+//	@Override
+//	public boolean mouseDragged(int mousex, int mousey){
+//		if(debug)System.err.printf("TreeMapNode.mouseDragged: %d, %d\n", mousex, mousey);
+//		for(TreeMapNode child : children)
+//			if(child.mouseDragged(mousex, mousey))
+//				return true;
+//		if(debug)System.err.println("TreeMapNode.mouseDragged: children do not match\n");
+//		if(mouseInside(mousex, mousey)){
+//			fpa.registerFocus(this);
+//			drag(mousex, mousey);
+//			return true;
+//		}
+//		return false;
+//	}
+}
