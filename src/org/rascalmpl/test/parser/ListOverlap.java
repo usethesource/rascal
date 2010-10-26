e201f6a7388690edfc52b31d7f3cb0526c68558e
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-10-26 13:01:21 +0000

    -Fixed list overlap test.

--- src/org/rascalmpl/test/parser/ListOverlap.java
+++ src/org/rascalmpl/test/parser/ListOverlap.java
@@ -5,66 +5,79 @@ import java.io.IOException;
 
 import org.eclipse.imp.pdb.facts.IConstructor;
 import org.eclipse.imp.pdb.facts.IValue;
-import org.eclipse.imp.pdb.facts.exceptions.FactTypeUseException;
 import org.eclipse.imp.pdb.facts.io.StandardTextReader;
-import org.eclipse.imp.pdb.facts.type.Type;
 import org.rascalmpl.parser.sgll.SGLL;
-import org.rascalmpl.parser.sgll.stack.CharStackNode;
+import org.rascalmpl.parser.sgll.stack.AbstractStackNode;
 import org.rascalmpl.parser.sgll.stack.ListStackNode;
+import org.rascalmpl.parser.sgll.stack.LiteralStackNode;
 import org.rascalmpl.parser.sgll.stack.NonTerminalStackNode;
 import org.rascalmpl.values.ValueFactoryFactory;
 import org.rascalmpl.values.uptr.Factory;
 
+/*
+* S ::= A*B*A*
+* A ::= a
+* B ::= b
+*/
 public class ListOverlap extends SGLL implements IParserTest{
-	private static IConstructor read(java.lang.String s, Type type){
-		try{
-			return (IConstructor) new StandardTextReader().read(vf, org.rascalmpl.values.uptr.Factory.uptr, type, new ByteArrayInputStream(s.getBytes()));
-		}catch(FactTypeUseException e){
-			throw new RuntimeException("unexpected exception in generated parser", e);
-		}catch(IOException e){
-			throw new RuntimeException("unexpected exception in generated parser", e);
-		}
-	}
+	private final static IConstructor SYMBOL_START_S = vf.constructor(Factory.Symbol_Sort, vf.string("S"));
+	private final static IConstructor SYMBOL_A = vf.constructor(Factory.Symbol_Sort, vf.string("A"));
+	private final static IConstructor SYMBOL_B = vf.constructor(Factory.Symbol_Sort, vf.string("B"));
+	private final static IConstructor SYMBOL_STAR_LIST_A = vf.constructor(Factory.Symbol_IterStar, SYMBOL_A);
+	private final static IConstructor SYMBOL_STAR_LIST_B = vf.constructor(Factory.Symbol_IterStar, SYMBOL_B);
+	private final static IConstructor SYMBOL_b = vf.constructor(Factory.Symbol_Lit, vf.string("b"));
+	private final static IConstructor SYMBOL_a = vf.constructor(Factory.Symbol_Lit, vf.string("a"));
+	private final static IConstructor SYMBOL_char_a = vf.constructor(Factory.Symbol_CharClass, vf.list(vf.constructor(Factory.CharRange_Single, vf.integer(97))));
+	private final static IConstructor SYMBOL_char_b = vf.constructor(Factory.Symbol_CharClass, vf.list(vf.constructor(Factory.CharRange_Single, vf.integer(98))));
 	
-	private static final IConstructor iter_star_Id = read("regular(\\iter-star(sort(\"Id\")),\\no-attrs())", Factory.Production);
-	private static final IConstructor iter_star_layout_elems_iter_star_seps_Id_iter_star_layout_iter_star_layout_Decl = read("prod([\\iter-star(layout()),label(\"elems\",\\iter-star-seps(sort(\"Id\"),[\\iter-star(layout())])),\\iter-star(layout())],sort(\"Decl\"),\\no-attrs())", Factory.Production);
-	private static final IConstructor iter_star_layout = read("regular(\\iter-star(layout()),\\no-attrs())", Factory.Production);
-	private static final IConstructor char32_Whitespace = read("prod([\\char-class([range(32,32)])],sort(\"Whitespace\"),\\no-attrs())", Factory.Production);
-	private static final IConstructor char97_Id = read("prod([\\char-class([range(97,97)])],sort(\"Id\"),\\no-attrs())", Factory.Production);
+	private final static IConstructor PROD_S_STARLISTASTARLISTBSTARLISTA = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_STAR_LIST_A, SYMBOL_STAR_LIST_B, SYMBOL_STAR_LIST_A), SYMBOL_START_S, vf.constructor(Factory.Attributes_NoAttrs));
+	private final static IConstructor PROD_STARLISTA = vf.constructor(Factory.Production_Regular, SYMBOL_STAR_LIST_A, vf.constructor(Factory.Attributes_NoAttrs));
+	private final static IConstructor PROD_STARLISTB = vf.constructor(Factory.Production_Regular, SYMBOL_STAR_LIST_B, vf.constructor(Factory.Attributes_NoAttrs));
+	private final static IConstructor PROD_A_a = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_a), SYMBOL_A, vf.constructor(Factory.Attributes_NoAttrs));
+	private final static IConstructor PROD_B_b = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_b), SYMBOL_B, vf.constructor(Factory.Attributes_NoAttrs));
+	private final static IConstructor PROD_a_a = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_char_a), SYMBOL_a, vf.constructor(Factory.Attributes_NoAttrs));
+	private final static IConstructor PROD_b_b = vf.constructor(Factory.Production_Default, vf.list(SYMBOL_char_b), SYMBOL_b, vf.constructor(Factory.Attributes_NoAttrs));
+	
+	private final static AbstractStackNode NONTERMINAL_START_S = new NonTerminalStackNode(AbstractStackNode.START_SYMBOL_ID, 0, "S");
+	private final static AbstractStackNode NONTERMINAL_A0 = new NonTerminalStackNode(0, 0, "A");
+	private final static AbstractStackNode NONTERMINAL_B1 = new NonTerminalStackNode(1, 0, "B");
+	private final static AbstractStackNode NONTERMINAL_A2 = new NonTerminalStackNode(2, 0, "A");
+	private final static AbstractStackNode LIST3 = new ListStackNode(3, 0, PROD_STARLISTA, NONTERMINAL_A0, false);
+	private final static AbstractStackNode LIST4 = new ListStackNode(4, 1, PROD_STARLISTB, NONTERMINAL_B1, false);
+	private final static AbstractStackNode LIST5 = new ListStackNode(5, 2, PROD_STARLISTA, NONTERMINAL_A2, false);
+	private final static AbstractStackNode LITERAL_a6 = new LiteralStackNode(6, 0, PROD_a_a, new char[]{'a'});
+	private final static AbstractStackNode LITERAL_b7 = new LiteralStackNode(7, 0, PROD_b_b, new char[]{'b'});
 	
 	public ListOverlap(){
 		super();
 	}
 	
-	public void Whitespace(){
-		expect(char32_Whitespace, new CharStackNode(1, 0, new char[][]{{32, 32}}));
+	public void S(){
+		expect(PROD_S_STARLISTASTARLISTBSTARLISTA, LIST3, LIST4, LIST5);
 	}
 	
-	public void Decl(){
-		expect(iter_star_layout_elems_iter_star_seps_Id_iter_star_layout_iter_star_layout_Decl,
-				new ListStackNode(3, 0, iter_star_layout, new NonTerminalStackNode(4, 0, "Whitespace"), false),
-				new ListStackNode(5, 1, iter_star_Id, new NonTerminalStackNode(6, 0, "Id"), false),
-				new ListStackNode(7, 2, iter_star_layout, new NonTerminalStackNode(8, 0, "Whitespace"), false));
+	public void A(){
+		expect(PROD_A_a, LITERAL_a6);
 	}
 	
-	public void Id(){
-		expect(char97_Id, new CharStackNode(10, 0, new char[][]{{97, 97}}));
+	public void B(){
+		expect(PROD_B_b, LITERAL_b7);
 	}
 	
 	public IConstructor executeParser(){
-		return parse("Decl", null, "  a".toCharArray());
+		return parse(NONTERMINAL_START_S, null, "aab".toCharArray());
 	}
 	
 	public IValue getExpectedResult() throws IOException{
-		String expectedInput = "appl(prod([\\iter-star(layout()),label(\"elems\",\\iter-star-seps(sort(\"Id\"),[\\iter-star(layout())])),\\iter-star(layout())],sort(\"Decl\"),\\no-attrs()),[appl(regular(\\iter-star(layout()),\\no-attrs()),[appl(prod([\\char-class([range(32,32)])],sort(\"Whitespace\"),\\no-attrs()),[char(32)]),appl(prod([\\char-class([range(32,32)])],sort(\"Whitespace\"),\\no-attrs()),[char(32)])]),appl(regular(\\iter-star(sort(\"Id\")),\\no-attrs()),[appl(prod([\\char-class([range(97,97)])],sort(\"Id\"),\\no-attrs()),[char(97)])]),appl(regular(\\iter-star(layout()),\\no-attrs()),[])])";
+		String expectedInput = "appl(prod([\\iter-star(sort(\"A\")),\\iter-star(sort(\"B\")),\\iter-star(sort(\"A\"))],sort(\"S\"),\\no-attrs()),[appl(regular(\\iter-star(sort(\"A\")),\\no-attrs()),[appl(prod([lit(\"a\")],sort(\"A\"),\\no-attrs()),[appl(prod([\\char-class([single(97)])],lit(\"a\"),\\no-attrs()),[char(97)])]),appl(prod([lit(\"a\")],sort(\"A\"),\\no-attrs()),[appl(prod([\\char-class([single(97)])],lit(\"a\"),\\no-attrs()),[char(97)])])]),appl(regular(\\iter-star(sort(\"B\")),\\no-attrs()),[appl(prod([lit(\"b\")],sort(\"B\"),\\no-attrs()),[appl(prod([\\char-class([single(98)])],lit(\"b\"),\\no-attrs()),[char(98)])])]),appl(regular(\\iter-star(sort(\"A\")),\\no-attrs()),[])])";
 		return new StandardTextReader().read(ValueFactoryFactory.getValueFactory(), Factory.uptr, Factory.Tree, new ByteArrayInputStream(expectedInput.getBytes()));
 	}
 	
 	public static void main(String[] args){
 		ListOverlap lo = new ListOverlap();
-		IConstructor result = lo.parse("Decl", null, "  a".toCharArray());
+		IConstructor result = lo.parse(NONTERMINAL_START_S, null, "aab".toCharArray());
 		System.out.println(result);
 		
-		System.out.println("? <- good");
+		System.out.println("S(A*(A(a),A(a)),B*(B(b)),A*()) <- good");
 	}
 }
