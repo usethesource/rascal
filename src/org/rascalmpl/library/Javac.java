596aeb4fb60136406df9af60675e75ea605ceb5f
Arnold Lankamp <arnold.lankamp@gmail.com>
2011-01-10 09:51:41 +0000

    -Removed com.sun.tools dependency to resolve potential deployment issues (and to get in to compile again). The JavaC library module now uses the same JavaCompiler that the parser generator does. I had to change the interface slightly to accomodate this change: 'compile(list[str] args)' changed to 'compile(str className, str sourceFile, list[str] opts)'.

--- src/org/rascalmpl/library/Javac.java
+++ src/org/rascalmpl/library/Javac.java
@@ -1,42 +1,54 @@
 package org.rascalmpl.library;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 import org.eclipse.imp.pdb.facts.IList;
 import org.eclipse.imp.pdb.facts.IListWriter;
-import org.eclipse.imp.pdb.facts.IValue;
 import org.eclipse.imp.pdb.facts.IString;
+import org.eclipse.imp.pdb.facts.IValue;
 import org.eclipse.imp.pdb.facts.IValueFactory;
+import org.rascalmpl.interpreter.utils.JavaCompiler;
 import org.rascalmpl.interpreter.utils.RuntimeExceptionFactory;
 
-public class Javac {
+public class Javac{
 	private final IValueFactory values;
 	
 	public Javac(IValueFactory values){
 		super();
+		
 		this.values = values;
 	}
 	
-	public IList compile(IList args) throws Exception{
-		if (!args.getType().getElementType().isStringType()) {
-			throw RuntimeExceptionFactory.illegalArgument(args, null, null);
+	public IList compile(IString className, IString sourceFile, IList opts) throws Exception{
+		if(!opts.getType().getElementType().isStringType()){
+			throw RuntimeExceptionFactory.illegalArgument(opts, null, null);
 		}
-		IList list = (IList)args;
-		java.lang.String jargs[] = new java.lang.String[list.length()];
-		int i = 0;
-		for (IValue arg: list) {
-			jargs[i++] = ((IString)arg).getValue();
+		
+		IList list = opts;
+		ArrayList<java.lang.String> jargs = new ArrayList<java.lang.String>(list.length());
+		for(IValue arg: list){
+			jargs.add(((IString)arg).getValue());
 		}
-		StringWriter str = new StringWriter();
-		PrintWriter out = new PrintWriter(str);
-        com.sun.tools.javac.Main compiler = new com.sun.tools.javac.Main();
-        compiler.compile(jargs, out);
+		
+		JavaCompiler<?> javaCompiler = new JavaCompiler(this.getClass().getClassLoader(), null, jargs);
+		Class<?> result = javaCompiler.compile(className.getValue(), sourceFile.getValue(), null, Object.class);
+		
+		InputStream classStream = result.getResourceAsStream(result.getName().replace('.', '/') + ".class");
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		byte[] buffer = new byte[8192];
+		
+		int bytesRead;
+		while((bytesRead = classStream.read(buffer)) != -1){
+			baos.write(buffer, 0, bytesRead);
+		}
+		
         IListWriter writer = values.listWriter();	
-        for (java.lang.String line: str.toString().split("\n")) {
+        for(java.lang.String line: baos.toString().split("\n")){
         	writer.append(values.string(line));
         }
         return writer.done();
     }
-
 }
