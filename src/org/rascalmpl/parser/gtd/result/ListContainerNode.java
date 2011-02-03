07596de3f91523b411ab0144bb2283f5215e7e0b
Arnold Lankamp <arnold.lankamp@gmail.com>
2011-02-03 11:08:44 +0000

    -Code consolidation.

--- src/org/rascalmpl/parser/gtd/result/ListContainerNode.java
+++ src/org/rascalmpl/parser/gtd/result/ListContainerNode.java
@@ -3,10 +3,7 @@ package org.rascalmpl.parser.gtd.result;
 import java.net.URI;
 
 import org.eclipse.imp.pdb.facts.IConstructor;
-import org.eclipse.imp.pdb.facts.IListWriter;
 import org.eclipse.imp.pdb.facts.ISetWriter;
-import org.eclipse.imp.pdb.facts.ISourceLocation;
-import org.eclipse.imp.pdb.facts.IValue;
 import org.rascalmpl.parser.IActionExecutor;
 import org.rascalmpl.parser.gtd.result.struct.Link;
 import org.rascalmpl.parser.gtd.util.ArrayList;
@@ -18,13 +15,14 @@ import org.rascalmpl.values.uptr.Factory;
 import org.rascalmpl.values.uptr.ProductionAdapter;
 
 public class ListContainerNode extends AbstractContainerNode{
-	private IConstructor cachedResult;
 	
 	public ListContainerNode(URI input, int offset, int endOffset, boolean isNullable, boolean isSeparator, boolean isLayout){
 		super(input, offset, endOffset, isNullable, isSeparator, isLayout);
 	}
 	
-	private void gatherAlternatives(Link child, DoubleArrayList<IConstructor[], IConstructor> gatheredAlternatives, IConstructor production, IndexedStack<AbstractNode> stack, int depth, CycleMark cycleMark, HashMap<ArrayList<Link>, IConstructor[]> sharedPrefixCache, PositionStore positionStore, FilteringTracker filteringTracker, IActionExecutor actionExecutor){
+	protected void gatherAlternatives(Link child, DoubleArrayList<IConstructor[], IConstructor> gatheredAlternatives, IConstructor production, IndexedStack<AbstractNode> stack, int depth, CycleMark cycleMark, PositionStore positionStore, FilteringTracker filteringTracker, IActionExecutor actionExecutor){
+		HashMap<ArrayList<Link>, IConstructor[]> sharedPrefixCache = new HashMap<ArrayList<Link>, IConstructor[]>();
+		
 		AbstractNode childNode = child.node;
 		
 		if(!(childNode.isEpsilon() && child.prefixes == null)){
@@ -301,105 +299,5 @@ public class ListContainerNode extends AbstractContainerNode{
 		}while(true);
 		
 		return null;
-	}
-	
-	private IConstructor buildAlternative(IConstructor production, IValue[] children){
-		IListWriter childrenListWriter = vf.listWriter(Factory.Tree);
-		for(int i = children.length - 1; i >= 0; --i){
-			childrenListWriter.insert(children[i]);
-		}
-		
-		return vf.constructor(Factory.Tree_Appl, production, childrenListWriter.done());
-	}
-	
-	public IConstructor toTerm(IndexedStack<AbstractNode> stack, int depth, CycleMark cycleMark, PositionStore positionStore, FilteringTracker filteringTracker, IActionExecutor actionExecutor){
-		if(cachedResult != null && (depth <= cycleMark.depth)){
-			if(depth == cycleMark.depth){
-				cycleMark.reset();
-			}
-			return cachedResult;
-		}
-		
-		if(rejected){
-			filteringTracker.setLastFilered(offset, endOffset);
-			return null;
-		}
-		
-		ISourceLocation sourceLocation = null;
-		if(!(isLayout || input == null)){
-			int beginLine = positionStore.findLine(offset);
-			int endLine = positionStore.findLine(endOffset);
-			sourceLocation = vf.sourceLocation(input, offset, endOffset - offset, beginLine + 1, endLine + 1, positionStore.getColumn(offset, beginLine), positionStore.getColumn(endOffset, endLine));
-		}
-		
-		int index = stack.contains(this);
-		if(index != -1){ // Cycle found.
-			IConstructor cycle = vf.constructor(Factory.Tree_Cycle, ProductionAdapter.getRhs(firstProduction), vf.integer(depth - index));
-			cycle = actionExecutor.filterAppl(cycle);
-			if(cycle != null && sourceLocation != null) cycle = cycle.setAnnotation(Factory.Location, sourceLocation);
-			
-			cycleMark.setMark(index);
-			
-			return cycle;
-		}
-		
-		int childDepth = depth + 1;
-		
-		stack.push(this, depth); // Push.
-		
-		// Gather
-		HashMap<ArrayList<Link>, IConstructor[]> sharedPrefixCache = new HashMap<ArrayList<Link>, IConstructor[]>();
-		DoubleArrayList<IConstructor[], IConstructor> gatheredAlternatives = new DoubleArrayList<IConstructor[], IConstructor>();
-		gatherAlternatives(firstAlternative, gatheredAlternatives, firstProduction, stack, childDepth, cycleMark, sharedPrefixCache, positionStore, filteringTracker, actionExecutor);
-		if(alternatives != null){
-			for(int i = alternatives.size() - 1; i >= 0; --i){
-				gatherAlternatives(alternatives.get(i), gatheredAlternatives, productions.get(i), stack, childDepth, cycleMark, sharedPrefixCache, positionStore, filteringTracker, actionExecutor);
-			}
-		}
-		
-		// Output.
-		IConstructor result = null;
-		
-		int nrOfAlternatives = gatheredAlternatives.size();
-		if(nrOfAlternatives == 1){ // Not ambiguous.
-			IConstructor production = gatheredAlternatives.getSecond(0);
-			IValue[] alternative = gatheredAlternatives.getFirst(0);
-			result = buildAlternative(production, alternative);
-			result = actionExecutor.filterAppl(result);
-			if(result != null){
-				if(sourceLocation != null) result = result.setAnnotation(Factory.Location, sourceLocation);
-			}else{
-				filteringTracker.setLastFilered(offset, endOffset);
-			}
-		}else if(nrOfAlternatives > 0){// Ambiguous.
-			ISetWriter ambSetWriter = vf.setWriter(Factory.Tree);
-			IConstructor lastAlternative = null;
-			
-			for(int i = nrOfAlternatives - 1; i >= 0; --i){
-				IConstructor production = gatheredAlternatives.getSecond(i);
-				IValue[] alternative = gatheredAlternatives.getFirst(i);
-				
-				IConstructor alt = buildAlternative(production, alternative);
-				alt = actionExecutor.filterAppl(alt);
-				if(alt != null){
-					if(sourceLocation != null) alt = alt.setAnnotation(Factory.Location, sourceLocation);
-					lastAlternative = alt;
-					ambSetWriter.insert(alt);
-				}
-			}
-			
-			if(ambSetWriter.size() == 1){
-				result = lastAlternative;
-			}else if(ambSetWriter.size() == 0){
-				filteringTracker.setLastFilered(offset, endOffset);
-			}else{
-				result = vf.constructor(Factory.Tree_Amb, ambSetWriter.done());
-				if(sourceLocation != null) result = result.setAnnotation(Factory.Location, sourceLocation);
-			}
-		}
-		
-		stack.dirtyPurge(); // Pop.
-		
-		return (depth <= cycleMark.depth) ? (cachedResult = result) : result;
 	}
 }
