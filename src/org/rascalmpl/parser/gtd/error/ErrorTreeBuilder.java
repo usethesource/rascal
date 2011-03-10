60d71470d99401f5c356e251d5a37d16adeddea1
Arnold Lankamp <arnold.lankamp@gmail.com>
2011-03-10 14:38:05 +0000

    -Updated tree stuff.

--- src/org/rascalmpl/parser/gtd/error/ErrorTreeBuilder.java
+++ src/org/rascalmpl/parser/gtd/error/ErrorTreeBuilder.java
@@ -2,34 +2,72 @@ package org.rascalmpl.parser.gtd.error;
 
 import org.eclipse.imp.pdb.facts.IConstructor;
 import org.rascalmpl.parser.gtd.result.AbstractContainerNode;
+import org.rascalmpl.parser.gtd.result.AbstractNode;
 import org.rascalmpl.parser.gtd.stack.AbstractStackNode;
+import org.rascalmpl.parser.gtd.util.DoubleStack;
 import org.rascalmpl.parser.gtd.util.IntegerKeyedHashMap;
 import org.rascalmpl.parser.gtd.util.ObjectIntegerKeyedHashMap;
 import org.rascalmpl.parser.gtd.util.Stack;
 
 public class ErrorTreeBuilder{
+	private final DoubleStack<AbstractStackNode, AbstractNode> errorNodes;
+	private final IntegerKeyedHashMap<ObjectIntegerKeyedHashMap<String, AbstractContainerNode>> errorResultStoreCache;
 	
-	private ErrorTreeBuilder(){
+	public ErrorTreeBuilder(){
 		super();
+		
+		errorNodes = new DoubleStack<AbstractStackNode, AbstractNode>();
+		errorResultStoreCache = new IntegerKeyedHashMap<ObjectIntegerKeyedHashMap<String,AbstractContainerNode>>();
 	}
 	
-	static IConstructor buildErrorTree(Stack<AbstractStackNode>[] errorStackStacks){
-		Stack<AbstractStackNode> errorNodes = new Stack<AbstractStackNode>();
-		IntegerKeyedHashMap<ObjectIntegerKeyedHashMap<String, AbstractContainerNode>> errorResultStoreCache = new IntegerKeyedHashMap<ObjectIntegerKeyedHashMap<String,AbstractContainerNode>>();
+	private void followEdges(AbstractStackNode node, AbstractNode result){
 		
-		for(int i = errorStackStacks.length - 1; i >= 0; --i){
-			Stack<AbstractStackNode> errorStackStack = errorStackStacks[i];
-			while(!errorStackStack.isEmpty()){
-				AbstractStackNode errorStackNode = errorStackStack.pop();
+	}
 	
-				// TODO Implement.
+	private void moveToNext(AbstractStackNode node, AbstractNode result){
+		
+	}
+	
+	private void move(AbstractStackNode node, AbstractNode result){
+		if(node.isEndNode()){
+			if(!result.isRejected()){
+				if(!node.isReject()){
+					followEdges(node, result);
+				}else{
+					// Ignore rejects for now.
+				}
 			}
 		}
 		
-		while(!errorNodes.isEmpty()){
-			AbstractStackNode errorStackNode = errorNodes.pop();
+		if(node.hasNext()){
+			moveToNext(node, result);
+		}
+	}
+	
+	IConstructor buildErrorTree(Stack<AbstractStackNode> unexpandableNodes, Stack<AbstractStackNode> unmatchableNodes, Stack<AbstractStackNode> filteredNodes){
+		while(!unexpandableNodes.isEmpty()){
+			AbstractStackNode unexpandableNode = unexpandableNodes.pop();
 
 			// TODO Implement.
+		}
+		
+		while(!unmatchableNodes.isEmpty()){
+			AbstractStackNode unmatchableNode = unmatchableNodes.pop();
+
+			// TODO Implement.
+		}
+		
+		while(!filteredNodes.isEmpty()){
+			AbstractStackNode filteredNode = filteredNodes.pop();
+
+			// TODO Implement.
+		}
+		
+		while(!errorNodes.isEmpty()){
+			AbstractStackNode errorStackNode = errorNodes.peekFirst();
+			AbstractNode result = errorNodes.popSecond();
+			
+			move(errorStackNode, result);
 		}
 		
 		return null; // Temp.
