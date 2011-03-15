11f3ad4cd5948ffe4ebf275f6baf47b74336e0a3
Arnold Lankamp <arnold.lankamp@gmail.com>
2011-03-15 09:04:44 +0000

    -Continued error tree builder.

--- src/org/rascalmpl/parser/gtd/error/ErrorTreeBuilder.java
+++ src/org/rascalmpl/parser/gtd/error/ErrorTreeBuilder.java
@@ -1,20 +1,41 @@
 package org.rascalmpl.parser.gtd.error;
 
+import java.net.URI;
+
 import org.eclipse.imp.pdb.facts.IConstructor;
+import org.eclipse.imp.pdb.facts.IList;
+import org.eclipse.imp.pdb.facts.IValueFactory;
 import org.rascalmpl.parser.gtd.result.AbstractContainerNode;
 import org.rascalmpl.parser.gtd.result.AbstractNode;
+import org.rascalmpl.parser.gtd.result.CharNode;
+import org.rascalmpl.parser.gtd.result.error.ErrorListContainerNode;
+import org.rascalmpl.parser.gtd.result.error.ErrorSortContainerNode;
+import org.rascalmpl.parser.gtd.result.error.ExpectedNode;
 import org.rascalmpl.parser.gtd.stack.AbstractStackNode;
+import org.rascalmpl.parser.gtd.stack.CharStackNode;
 import org.rascalmpl.parser.gtd.util.DoubleStack;
 import org.rascalmpl.parser.gtd.util.IntegerKeyedHashMap;
 import org.rascalmpl.parser.gtd.util.ObjectIntegerKeyedHashMap;
 import org.rascalmpl.parser.gtd.util.Stack;
+import org.rascalmpl.values.ValueFactoryFactory;
 
 public class ErrorTreeBuilder{
+	private final static IValueFactory VF = ValueFactoryFactory.getValueFactory();
+	private final static IList EMPTY_LIST = VF.list();
+	
+	private final char[] input;
+	private final int location;
+	private final URI inputURI;
+	
 	private final DoubleStack<AbstractStackNode, AbstractNode> errorNodes;
 	private final IntegerKeyedHashMap<ObjectIntegerKeyedHashMap<String, AbstractContainerNode>> errorResultStoreCache;
 	
-	public ErrorTreeBuilder(){
+	public ErrorTreeBuilder(char[] input, int location, URI inputURI){
 		super();
+		
+		this.input = input;
+		this.location = location;
+		this.inputURI = inputURI;
 		
 		errorNodes = new DoubleStack<AbstractStackNode, AbstractNode>();
 		errorResultStoreCache = new IntegerKeyedHashMap<ObjectIntegerKeyedHashMap<String,AbstractContainerNode>>();
@@ -47,20 +68,36 @@ public class ErrorTreeBuilder{
 	IConstructor buildErrorTree(Stack<AbstractStackNode> unexpandableNodes, Stack<AbstractStackNode> unmatchableNodes, Stack<AbstractStackNode> filteredNodes){
 		while(!unexpandableNodes.isEmpty()){
 			AbstractStackNode unexpandableNode = unexpandableNodes.pop();
-
-			// TODO Implement.
+			
+			// TODO Get the symbol.
+			AbstractNode resultStore = new ExpectedNode(null, inputURI, location, location, unexpandableNode.isSeparator(), unexpandableNode.isLayout());
+			
+			errorNodes.push(unexpandableNode, resultStore);
 		}
 		
 		while(!unmatchableNodes.isEmpty()){
 			AbstractStackNode unmatchableNode = unmatchableNodes.pop();
-
-			// TODO Implement.
+			
+			int startLocation = unmatchableNode.getStartLocation();
+			
+			AbstractNode[] children = new AbstractNode[location - startLocation];
+			for(int i = children.length - 1; i >= 0; --i){
+				children[i] = new CharNode(input[startLocation - i]);
+			}
+			
+			// TODO Construct proper node.
+			
+			errorNodes.push(unmatchableNode, null); // TODO Use the proper result store.
 		}
 		
 		while(!filteredNodes.isEmpty()){
 			AbstractStackNode filteredNode = filteredNodes.pop();
-
-			// TODO Implement.
+			
+			AbstractNode resultStore = (!filteredNode.isList()) ? new ErrorSortContainerNode(EMPTY_LIST, inputURI, filteredNode.getStartLocation(), location, filteredNode.isSeparator(), filteredNode.isLayout()) : new ErrorListContainerNode(EMPTY_LIST, inputURI, filteredNode.getStartLocation(), location, filteredNode.isSeparator(), filteredNode.isLayout());
+			
+			// TODO Set children.
+			
+			errorNodes.push(filteredNode, resultStore);
 		}
 		
 		while(!errorNodes.isEmpty()){
