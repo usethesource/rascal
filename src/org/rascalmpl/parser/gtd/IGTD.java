085b07d8f17076e74bbf56e4f24e6adee96212b7
Arnold Lankamp <arnold.lankamp@gmail.com>
2011-03-16 11:00:51 +0000

    -Updated interface. -Fixed problem related to regulars.

--- src/org/rascalmpl/parser/gtd/IGTD.java
+++ src/org/rascalmpl/parser/gtd/IGTD.java
@@ -9,10 +9,12 @@ import java.net.URI;
 import org.eclipse.imp.pdb.facts.IConstructor;
 import org.rascalmpl.parser.gtd.result.action.IActionExecutor;
 
-public interface IGTD {
-	public IConstructor parse(String nonterminal, URI inputURI, char[] input, IActionExecutor actionExecutor);
-	public IConstructor parse(String nonterminal, URI inputURI, String input, IActionExecutor actionExecutor);
-	public IConstructor parse(String nonterminal, URI inputURI, InputStream in, IActionExecutor actionExecutor) throws IOException;
-	public IConstructor parse(String nonterminal, URI inputURI, Reader in, IActionExecutor actionExecutor) throws IOException;
-	public IConstructor parse(String nonterminal, URI inputURI, File inputFile, IActionExecutor actionExecutor) throws IOException;
+public interface IGTD{
+	IConstructor parse(String nonterminal, URI inputURI, char[] input, IActionExecutor actionExecutor);
+	IConstructor parse(String nonterminal, URI inputURI, String input, IActionExecutor actionExecutor);
+	IConstructor parse(String nonterminal, URI inputURI, InputStream in, IActionExecutor actionExecutor) throws IOException;
+	IConstructor parse(String nonterminal, URI inputURI, Reader in, IActionExecutor actionExecutor) throws IOException;
+	IConstructor parse(String nonterminal, URI inputURI, File inputFile, IActionExecutor actionExecutor) throws IOException;
+	
+	IConstructor buildErrorTree();
 }
