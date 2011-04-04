fca101133085ae4cfdc4a881f4c1f6bab47bb612
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2011-04-04 15:14:37 +0000

    * added license banners, using svn log info to retrieve the contributors of each file.

--- src/org/rascalmpl/library/lang/jvm/transform/Rascalify.java
+++ src/org/rascalmpl/library/lang/jvm/transform/Rascalify.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2009-2011 CWI
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+
+ *   * Jeroen van den Bos - Jeroen.van.den.Bos@cwi.nl (CWI)
+ *   * Arnold Lankamp - Arnold.Lankamp@cwi.nl
+*******************************************************************************/
 package org.rascalmpl.library.lang.jvm.transform;
 
 import java.io.FileNotFoundException;
--- src/org/rascalmpl/library/util/tasks/Manager.rsc
+++ src/org/rascalmpl/library/util/tasks/Manager.rsc
@@ -1,3 +1,11 @@
+@license{
+  Copyright (c) 2009-2011 CWI
+  All rights reserved. This program and the accompanying materials
+  are made available under the terms of the Eclipse Public License v1.0
+  which accompanies this distribution, and is available at
+  http://www.eclipse.org/legal/epl-v10.html
+}
+@contributor{Anya Helene Bagge - A.H.S.Bagge@cwi.nl (Univ. Bergen)}
 module util::tasks::Manager
 
 alias Task = bool (Transaction tr, type[&T] key, &N name);
