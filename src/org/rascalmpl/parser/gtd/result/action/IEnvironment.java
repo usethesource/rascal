1e00ab10c20edf6c42d5d8a8c4d78e0252fa7473
Arnold Lankamp <arnold.lankamp@gmail.com>
2011-05-03 11:35:24 +0000

    -Removed unnecessary methods.

--- src/org/rascalmpl/parser/gtd/result/action/IEnvironment.java
+++ src/org/rascalmpl/parser/gtd/result/action/IEnvironment.java
@@ -1,10 +1,6 @@
 package org.rascalmpl.parser.gtd.result.action;
 
 public interface IEnvironment{
-	IEnvironment getParent();
-	
-	boolean isRoot();
-	
 	boolean isEqual(IEnvironment environment);
 	
 	IEnvironment split();
