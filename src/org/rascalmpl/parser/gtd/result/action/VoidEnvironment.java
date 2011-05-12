f53ecf2db86f24bc5c762619da72bfbe0f3b37c2
Arnold Lankamp <arnold.lankamp@gmail.com>
2011-05-12 09:54:47 +0000

    -Updated environment interface.

--- src/org/rascalmpl/parser/gtd/result/action/VoidEnvironment.java
+++ src/org/rascalmpl/parser/gtd/result/action/VoidEnvironment.java
@@ -3,11 +3,7 @@ package org.rascalmpl.parser.gtd.result.action;
 public class VoidEnvironment implements IEnvironment{
 	public final static VoidEnvironment ROOT_VOID_ENVIRONMENT = new VoidEnvironment();
 	
-	public VoidEnvironment(){
+	private VoidEnvironment(){
 		super();
-	}
-	
-	public boolean isEqual(IEnvironment environment){
-		return false;
 	}
 }
