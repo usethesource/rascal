929438584f2dbdd5b256b5b622fe4347af9e3706
Arnold Lankamp <arnold.lankamp@gmail.com>
2011-06-07 09:57:52 +0000

    -Moved filtering tracker.

--- src/org/rascalmpl/parser/gtd/result/out/INodeConverter.java
+++ src/org/rascalmpl/parser/gtd/result/out/INodeConverter.java
@@ -3,7 +3,6 @@ package org.rascalmpl.parser.gtd.result.out;
 import org.eclipse.imp.pdb.facts.IConstructor;
 import org.rascalmpl.parser.gtd.location.PositionStore;
 import org.rascalmpl.parser.gtd.result.AbstractNode;
-import org.rascalmpl.parser.gtd.result.AbstractNode.FilteringTracker;
 import org.rascalmpl.parser.gtd.result.action.IActionExecutor;
 
 public interface INodeConverter{
