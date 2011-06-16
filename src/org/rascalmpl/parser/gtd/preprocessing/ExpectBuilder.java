2dcc88464c601225f59e90fe25bf2b13ee15df10
Arnold Lankamp <arnold.lankamp@gmail.com>
2011-06-16 11:08:53 +0000

    -Updated for the Rascal version of the parser.

--- src/org/rascalmpl/parser/gtd/preprocessing/ExpectBuilder.java
+++ src/org/rascalmpl/parser/gtd/preprocessing/ExpectBuilder.java
@@ -1,28 +1,30 @@
 package org.rascalmpl.parser.gtd.preprocessing;
 
+import org.eclipse.imp.pdb.facts.IConstructor;
 import org.rascalmpl.parser.gtd.stack.AbstractStackNode;
-import org.rascalmpl.parser.gtd.util.ArrayList;
+import org.rascalmpl.parser.gtd.util.DoubleArrayList;
 import org.rascalmpl.parser.gtd.util.IntegerObjectList;
 import org.rascalmpl.parser.gtd.util.SortedIntegerObjectList;
 
+// TODO Don't match on id's, check true equality.
 public class ExpectBuilder{
-	private final SortedIntegerObjectList<ArrayList<AbstractStackNode[]>> alternatives;
+	private final SortedIntegerObjectList<DoubleArrayList<IConstructor, AbstractStackNode[]>> alternatives;
 	
 	public ExpectBuilder(){
 		super();
 		
-		alternatives = new SortedIntegerObjectList<ArrayList<AbstractStackNode[]>>();
+		alternatives = new SortedIntegerObjectList<DoubleArrayList<IConstructor, AbstractStackNode[]>>();
 	}
 	
-	public void addAlternative(AbstractStackNode... alternative){
+	public void addAlternative(IConstructor production, AbstractStackNode... alternative){
 		int alternativeLength = alternative.length;
-		ArrayList<AbstractStackNode[]> alternativesList = alternatives.findValue(alternativeLength);
+		DoubleArrayList<IConstructor, AbstractStackNode[]> alternativesList = alternatives.findValue(alternativeLength);
 		if(alternativesList == null){
-			alternativesList = new ArrayList<AbstractStackNode[]>();
+			alternativesList = new DoubleArrayList<IConstructor, AbstractStackNode[]>();
 			alternatives.add(alternativeLength, alternativesList);
 		}
 		
-		alternativesList.add(alternative);
+		alternativesList.add(production, alternative);
 	}
 	
 	// Builds the expect matrix and calculates sharing.
@@ -30,15 +32,17 @@ public class ExpectBuilder{
 		IntegerObjectList<AbstractStackNode[]> constructedExpects = new IntegerObjectList<AbstractStackNode[]>();
 		
 		for(int i = alternatives.size() - 1; i >= 0; --i){
-			ArrayList<AbstractStackNode[]> alternativesList = alternatives.getValue(i);
+			DoubleArrayList<IConstructor, AbstractStackNode[]> alternativesList = alternatives.getValue(i);
 			
 			for(int j = alternativesList.size() - 1; j >= 0; --j){
-				AbstractStackNode[] alternative = alternativesList.get(j);
+				IConstructor production = alternativesList.getFirst(j);
+				AbstractStackNode[] alternative = alternativesList.getSecond(j);
 				
 				int identifier = alternative[0].getId();
 				AbstractStackNode[] sharedExpect = constructedExpects.findValue(identifier);
 				if(sharedExpect == null){
 					alternative[alternative.length - 1].setProduction(alternative);
+					alternative[alternative.length - 1].setParentProduction(production);
 					alternative[alternative.length - 1].markAsEndNode();
 					
 					for(int k = alternative.length - 2; k >= 0; --k){
@@ -60,6 +64,7 @@ public class ExpectBuilder{
 					if(k < alternative.length){
 						sharedExpect[k - 1].addProduction(alternative);
 					}else{
+						sharedExpect[alternative.length - 1].setParentProduction(production);
 						sharedExpect[alternative.length - 1].markAsEndNode();
 					}
 					
@@ -67,6 +72,7 @@ public class ExpectBuilder{
 						alternative[k].setProduction(alternative);
 					}
 					
+					alternative[alternative.length - 1].setParentProduction(production);
 					alternative[alternative.length - 1].markAsEndNode();
 				}
 			}
