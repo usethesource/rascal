fabb8d503cfb155a85e40bd551d101239231ddbd
Arnold Lankamp <arnold.lankamp@gmail.com>
2011-07-07 09:34:21 +0000

    -Implemented look-ahead splitting and indexing.

--- src/org/rascalmpl/parser/gtd/grammar/GrammarBuilder.java
+++ src/org/rascalmpl/parser/gtd/grammar/GrammarBuilder.java
@@ -11,71 +11,126 @@ import org.rascalmpl.parser.gtd.util.IntegerList;
 import org.rascalmpl.parser.gtd.util.ObjectKeyedIntegerMap;
 import org.rascalmpl.parser.gtd.util.IntegerKeyedHashMap.Entry;
 
-// TODO Split look ahead ranges into unique sections.
 public class GrammarBuilder{
 	private final IntegerKeyedHashMap<ArrayList<Alternative>> productions;
 	private final ObjectKeyedIntegerMap<String> sortMappings;
-	private final ObjectKeyedIntegerMap<LookAheadRange> lookAheadMappings;
+	private LookAheadRange lookAheadChain;
 	private final HashMap<Symbol, ArrayList<Symbol>> restrictions;
 	
 	private int sortIdentifierCounter = -1;
-	private int lookAheadIdentifierCounter = -1;
 	
 	public GrammarBuilder(){
 		super();
 		
 		productions = new IntegerKeyedHashMap<ArrayList<Alternative>>();
 		sortMappings = new ObjectKeyedIntegerMap<String>();
-		lookAheadMappings = new ObjectKeyedIntegerMap<LookAheadRange>();
+		lookAheadChain = new LookAheadRange(-1, -1);
 		restrictions = new HashMap<Symbol, ArrayList<Symbol>>();
 	}
 	
 	private static class LookAheadRange{
-		public final char[] lookAheadRange;
+		public final int start;
+		public int end;
 		
-		public LookAheadRange(char[] lookAheadRange){
+		public LookAheadRange next;
+		
+		public LookAheadRange(int start, int end){
 			super();
 			
-			this.lookAheadRange = lookAheadRange;
+			this.start = start;
+			this.end = end;
 		}
 		
-		public int hashCode(){
-			return lookAheadRange[0] + lookAheadRange[1];
+		public void insert(LookAheadRange lookAheadRange){
+			if(end == lookAheadRange.start){
+				LookAheadRange oldNext = next;
+				if(lookAheadRange.start != lookAheadRange.end){
+					next = new LookAheadRange(lookAheadRange.start, lookAheadRange.start);
+					LookAheadRange nextNext = new LookAheadRange(lookAheadRange.start + 1, lookAheadRange.end);
+					next.next = nextNext;
+					nextNext.insert(oldNext);
+				}else{
+					next = lookAheadRange;
+					next.next = oldNext;
+				}
+			}else if(end < lookAheadRange.start){
+				if(next != null){
+					next.insert(lookAheadRange);
+				}else{
+					next = lookAheadRange;
+				}
+			}else{
+				if(start == lookAheadRange.start){
+					if(end == lookAheadRange.end){
+						return;
+					}else if(end < lookAheadRange.end){
+						LookAheadRange oldNext = next;
+						next = new LookAheadRange(end + 1, lookAheadRange.end);
+						next.insert(oldNext);
+					}else{
+						LookAheadRange oldNext = next;
+						next = new LookAheadRange(lookAheadRange.end + 1, end);
+						next.next = oldNext;
+					}
+				}else{
+					if(end == lookAheadRange.end){
+						LookAheadRange oldNext = next;
+						next = lookAheadRange;
+						end = lookAheadRange.start - 1;
+						next.next = oldNext;
+					}else{
+						LookAheadRange oldNext = next;
+						next = lookAheadRange;
+						end = lookAheadRange.start - 1;
+						next.insert(oldNext);
+					}
+				}
+			}
 		}
 		
-		public boolean equals(Object other){
-			if(!(other instanceof LookAheadRange)) return false;
+		public int countRanges(){
+			int counter = 1;
+			LookAheadRange lar = this;
+			do{
+				++counter;
+			}while((lar = lar.next) != null);
 			
-			LookAheadRange otherRange = (LookAheadRange) other;
+			return counter;
+		}
+		
+		public IntegerList getIdentifiers(int start, int end){
+			IntegerList identifiers = new IntegerList();
 			
-			return (lookAheadRange[0] == otherRange.lookAheadRange[0] && lookAheadRange[1] == otherRange.lookAheadRange[1]);
+			LookAheadRange lar = this;
+			int index = 0;
+			do{
+				if(lar.start >= end){
+					identifiers.add(index);
+				}
+				++index;
+			}while((lar = lar.next) != null && lar.end <= start);
+			
+			return identifiers;
 		}
 	}
 	
 	private static class Alternative{
 		public final Symbol[] symbols;
-		public final IntegerList lookAheadRangeIdentifiers;
+		public final char[][] lookAheadRanges;
 		
-		public Alternative(Symbol[] symbols, IntegerList lookAheadRangeIdentifiers){
+		public Alternative(Symbol[] symbols, char[][] lookAheadRanges){
 			super();
 			
 			this.symbols = symbols;
-			this.lookAheadRangeIdentifiers = lookAheadRangeIdentifiers;
+			this.lookAheadRanges = lookAheadRanges;
 		}
 	}
 	
 	public void registerAlternative(String sortName, Symbol[] symbols, char[][] lookAheadRanges){
-		IntegerList lookAheadRangeIdentifiers = new IntegerList();
-		
 		// Register the missing look-ahead ranges (if any).
 		for(int i = lookAheadRanges.length - 1; i >= 0; --i){
 			char[] lookAheadRange = lookAheadRanges[i];
-			LookAheadRange lookAheadRangeNode = new LookAheadRange(lookAheadRange);
-			int lookAheadRangeIdentifier = lookAheadMappings.get(lookAheadRangeNode);
-			if(lookAheadRangeIdentifier == -1){
-				lookAheadMappings.putUnsafe(lookAheadRangeNode, (lookAheadRangeIdentifier = ++lookAheadIdentifierCounter));
-			}
-			lookAheadRangeIdentifiers.add(lookAheadRangeIdentifier);
+			lookAheadChain.insert(new LookAheadRange(lookAheadRange[0], lookAheadRange[1]));
 		}
 		
 		// Register the sort (if it hasn't been already).
@@ -91,7 +146,7 @@ public class GrammarBuilder{
 			productions.putUnsafe(sortIdentifier, alternatives);
 		}
 		
-		alternatives.add(new Alternative(symbols, lookAheadRangeIdentifiers));
+		alternatives.add(new Alternative(symbols, lookAheadRanges));
 	}
 	
 	public void restrict(Symbol child, Symbol parent){
@@ -105,10 +160,11 @@ public class GrammarBuilder{
 	}
 	
 	public Grammar build(){
+		int nrOfLookAheadRanges = lookAheadChain.countRanges() - 1;
+		// TODO Assign look-ahead identifiers.
+		
 		int nrOfSorts = sortIdentifierCounter + 1;
 		AbstractStackNode[][][] expectMatrix = new AbstractStackNode[nrOfSorts][][];
-		
-		int nrOfLookAheadRanges = lookAheadIdentifierCounter + 1;
 		
 		Iterator<Entry<ArrayList<Alternative>>> productionIterator = productions.entryIterator();
 		while(productionIterator.hasNext()){
@@ -119,14 +175,14 @@ public class GrammarBuilder{
 			// Get Look-ahead clusters.
 			for(int i = alternatives.size() - 1; i >= 0; --i){
 				Alternative alternative = alternatives.get(i);
-				IntegerList lookAheadRangeIdentifiers = alternative.lookAheadRangeIdentifiers;
 				Symbol[] symbols = alternative.symbols;
+				char[][] lookAheadRanges = alternative.lookAheadRanges;
 				
 				// TODO Get the alternatives
 				
 				AbstractStackNode[][] expectTable = new AbstractStackNode[nrOfLookAheadRanges][];
-				for(int j = lookAheadRangeIdentifiers.size() - 1; j >= 0; --j){
-					int lookAheadRangeIdentifier = lookAheadRangeIdentifiers.get(j);
+				for(int j = lookAheadRanges.length - 1; j >= 0; --j){
+					char[] lookAheadRangeIdentifier = lookAheadRanges[j];
 					
 					// TODO Insert the alternatives.
 				}
