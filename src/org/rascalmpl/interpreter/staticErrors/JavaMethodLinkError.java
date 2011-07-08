7e474f5240208c9a01fd88b6e13fb78e7382041a
Arnold Lankamp <arnold.lankamp@gmail.com>
2011-07-08 07:43:39 +0000

    -Added cause to method link exceptions.

--- src/org/rascalmpl/interpreter/staticErrors/JavaMethodLinkError.java
+++ src/org/rascalmpl/interpreter/staticErrors/JavaMethodLinkError.java
@@ -16,7 +16,7 @@ import org.rascalmpl.ast.AbstractAST;
 public class JavaMethodLinkError extends StaticError {
 	private static final long serialVersionUID = 3867556518416718308L;
 
-	public JavaMethodLinkError(String name, String cause, AbstractAST ast) {
-		super("Can not link method " + name + " because: " + cause, ast);
+	public JavaMethodLinkError(String name, String message, AbstractAST ast, Throwable cause) {
+		super("Can not link method " + name + " because: " + message, ast, cause);
 	}
 }
