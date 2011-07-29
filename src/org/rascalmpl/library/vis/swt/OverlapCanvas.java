75e59d9a18a5315feef9d17bd8e557bb92b6c5b0
Atze van der Ploeg <A.J.van.der.Ploeg@cwi.nl>
2011-07-29 15:16:33 +0000

    vis library: fixes event handling in swt overlaps

--- src/org/rascalmpl/library/vis/swt/OverlapCanvas.java
+++ src/org/rascalmpl/library/vis/swt/OverlapCanvas.java
@@ -1,6 +1,8 @@
 package org.rascalmpl.library.vis.swt;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseMoveListener;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.GC;
@@ -15,14 +17,20 @@ import org.rascalmpl.library.vis.graphics.GraphicsContext;
 import org.rascalmpl.library.vis.graphics.SWTGraphicsContext;
 import org.rascalmpl.library.vis.util.Rectangle;
 
-public class OverlapCanvas extends Canvas implements PaintListener {
+public class OverlapCanvas extends Canvas implements PaintListener, MouseMoveListener {
 
 	private Figure fig;
 	private Rectangle overlap;
+	private FigureSWTApplet parent;
 	
-	public OverlapCanvas(Composite parent) {
-		super(parent, SWT.NORMAL);
+	public OverlapCanvas(FigureSWTApplet parent,Composite floor) {
+		super(floor, SWT.NORMAL);
 		this.addPaintListener(this);
+		this.parent = parent;
+		addPaintListener(this);
+		addMouseMoveListener(this);
+		addMouseListener(parent);
+		addKeyListener(parent);
 	}
 	
 	public void setOverlap(Figure fig,Rectangle overlap){
@@ -32,7 +40,6 @@ public class OverlapCanvas extends Canvas implements PaintListener {
 		setLocation(FigureApplet.round(overlap.getX()),FigureApplet.round(overlap.getY()));
 	}
 	
-	
 
 	@Override
 	public void paintControl(PaintEvent e) {
@@ -40,6 +47,13 @@ public class OverlapCanvas extends Canvas implements PaintListener {
 		gc.translate( -overlap.getX(),-overlap.getY());
 		fig.drawPart(overlap, gc);
 		e.gc.dispose();
+	}
+
+	@Override
+	public void mouseMove(MouseEvent e) {
+		e.x+=fig.globalLocation.getX();
+		e.y+=fig.globalLocation.getY();
+		parent.mouseMove(e);
 	}
 	
 	
