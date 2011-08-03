8a889f697042c1e1e62c9d49ae9730ed63a250c4
Arnold Lankamp <arnold.lankamp@gmail.com>
2011-08-03 13:34:57 +0000

    -More documentation.

--- src/org/rascalmpl/parser/gtd/result/out/FilteringTracker.java
+++ src/org/rascalmpl/parser/gtd/result/out/FilteringTracker.java
@@ -1,5 +1,10 @@
 package org.rascalmpl.parser.gtd.result.out;
 
+/**
+ * This class can be used to keep track of post-parse filtering errors. These
+ * kinds of errors can occur during parse tree conversion, due to semantic
+ * actions.
+ */
 public class FilteringTracker{
 	private int offset;
 	private int endOffset;
@@ -8,15 +13,26 @@ public class FilteringTracker{
 		super();
 	}
 	
-	public void setLastFilered(int offset, int endOffset){
+	/**
+	 * Marks the last filtered location.
+	 */
+	public void setLastFiltered(int offset, int endOffset){
 		this.offset = offset;
 		this.endOffset = endOffset;
 	}
 	
+	/**
+	 * Returns the begin location in the input string of the last filtered
+	 * location.
+	 */
 	public int getOffset(){
 		return offset;
 	}
 	
+	/**
+	 * Returns the end location in the input string of the last filtered
+	 * location.
+	 */
 	public int getEndOffset(){
 		return endOffset;
 	}
--- src/org/rascalmpl/parser/gtd/result/out/INodeConverter.java
+++ src/org/rascalmpl/parser/gtd/result/out/INodeConverter.java
@@ -9,7 +9,15 @@ import org.rascalmpl.parser.gtd.result.action.IActionExecutor;
  * trees to some other representation.
  */
 public interface INodeConverter{
+	/**
+	 * Convert the given parse tree.
+	 */
 	Object convert(AbstractNode parseTree, PositionStore positionStore, IActionExecutor actionExecutor, Object rootEnvironment, FilteringTracker filteringTracker);
 	
+	/**
+	 * Convert the given parse tree, taking case of potential errors in the
+	 * result. These errors can be either the result of post-parse filtering or
+	 * due to the tree being incomplete because of a parse error.
+	 */
 	Object convertWithErrors(AbstractNode parseTree, PositionStore positionStore, IActionExecutor actionExecutor, Object rootEnvironment);
 }
