c798870831c5e0671bc21e55a646cb3a7561b330
Arnold Lankamp <arnold.lankamp@gmail.com>
2011-08-09 07:46:30 +0000

    -Fixed issue related to hidden-right-recursion in combination with prefix-sharing. -More documentation.

--- src/org/rascalmpl/parser/gtd/result/out/INodeConverter.java
+++ src/org/rascalmpl/parser/gtd/result/out/INodeConverter.java
@@ -21,6 +21,9 @@ public interface INodeConverter{
 	 */
 	Object convertWithErrors(AbstractNode parseTree, PositionStore positionStore, IActionExecutor actionExecutor, Object rootEnvironment);
 	
+	// Error tree related stuff.
+	// TODO This doesn't really belong here, find a better place to put it.
+	
 	/**
 	 * Checks if the given production is a list production.
 	 */
