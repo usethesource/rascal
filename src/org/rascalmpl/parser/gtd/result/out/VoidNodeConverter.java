4b97ad723c58db98665c358c421c39c1f5ea80ef
Arnold Lankamp <arnold.lankamp@gmail.com>
2011-08-17 11:35:53 +0000

    -Added documentation.

--- src/org/rascalmpl/parser/gtd/result/out/VoidNodeConverter.java
+++ src/org/rascalmpl/parser/gtd/result/out/VoidNodeConverter.java
@@ -4,16 +4,26 @@ import org.rascalmpl.parser.gtd.location.PositionStore;
 import org.rascalmpl.parser.gtd.result.AbstractNode;
 import org.rascalmpl.parser.gtd.result.action.IActionExecutor;
 
+/**
+ * A node converter that doesn't do anything.
+ * This may be useful for benchmarking and debugging and such.
+ */
 public class VoidNodeConverter implements INodeConverter{
 	
 	public VoidNodeConverter(){
 		super();
 	}
 	
+	/**
+	 * Returns the given tree.
+	 */
 	public AbstractNode convert(AbstractNode parseTree, PositionStore positionStore, IActionExecutor actionExecutor, Object rootEnvironment, FilteringTracker filteringTracker){
 		return parseTree;
 	}
 	
+	/**
+	 * Returns the given tree.
+	 */
 	public AbstractNode convertWithErrors(AbstractNode parseTree, PositionStore positionStore, IActionExecutor actionExecutor, Object rootEnvironment){
 		return parseTree;
 	}
