f3a69433b3367d798690f10817e27bcaee11f20a
Paul Klint <Paul.Klint@cwi.nl>
2011-09-20 21:50:58 +0000

    Working on Tutor course for writing Tutor courses.

---
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/Tutor/course.js
@@ -0,0 +1,54 @@
+/* Generated code for course Tutor, generated $2011-09-20T23:40:32.797+0200 */
+
+$.setRootConcept("Tutor");
+
+var baseConcepts = new Array("#","##","###","*","**","***",",",":","<","</","<\\","==","===","====",">",">`","Benefits","BulletLists","Concept","Description","Details","Examples","Function","Markup","Name","NamedParagraph","NewParagraph","NumberedLists","Pitfalls","Questions","StructureMarkup","Synopsis","Syntax","Tutor","Types","`<","figure","listing","screen","warning");
+
+var conceptNames = new Array("Tutor","Tutor/Concept","Tutor/Concept/Benefits","Tutor/Concept/Description","Tutor/Concept/Details","Tutor/Concept/Examples","Tutor/Concept/Function","Tutor/Concept/Name","Tutor/Concept/Pitfalls","Tutor/Concept/Questions","Tutor/Concept/Syntax","Tutor/Concept/Types","Tutor/Markup","Tutor/Markup/StructureMarkup","Tutor/Markup/StructureMarkup/BulletLists","Tutor/Markup/StructureMarkup/NamedParagraph","Tutor/Markup/StructureMarkup/NewParagraph","Tutor/Markup/StructureMarkup/NumberedLists","Tutor/Markup/StructureMarkup/figure","Tutor/Markup/StructureMarkup/listing","Tutor/Markup/StructureMarkup/screen","Tutor/Markup/StructureMarkup/warning");
+
+var searchTerms = {};
+
+
+searchTerms["Tutor/Concept/Pitfalls"] = null;
+
+searchTerms["Tutor/Concept/Description"] = new Array(":","Description");
+
+searchTerms["Tutor/Concept/Benefits"] = null;
+
+searchTerms["Tutor/Markup/StructureMarkup/warning"] = new Array("</","<","warning",">");
+
+searchTerms["Tutor/Concept/Types"] = new Array("Types",":");
+
+searchTerms["Tutor/Concept/Function"] = null;
+
+searchTerms["Tutor/Markup/StructureMarkup/NamedParagraph"] = new Array("==","====","===");
+
+searchTerms["Tutor"] = null;
+
+searchTerms["Tutor/Markup/StructureMarkup/BulletLists"] = new Array("**","*","***");
+
+searchTerms["Tutor/Markup/StructureMarkup/NewParagraph"] = null;
+
+searchTerms["Tutor/Concept"] = new Array("Details","Syntax","Synopsis","Function","Types","Examples",":","Name","Description","Benefits",",","Pitfalls","Questions");
+
+searchTerms["Tutor/Markup/StructureMarkup/screen"] = new Array("<\\","screen","<",">");
+
+searchTerms["Tutor/Markup/StructureMarkup"] = null;
+
+searchTerms["Tutor/Concept/Questions"] = null;
+
+searchTerms["Tutor/Markup/StructureMarkup/NumberedLists"] = new Array("##","#","###");
+
+searchTerms["Tutor/Concept/Name"] = new Array(":","Name");
+
+searchTerms["Tutor/Concept/Examples"] = null;
+
+searchTerms["Tutor/Markup/StructureMarkup/figure"] = new Array("</","figure","<",">");
+
+searchTerms["Tutor/Concept/Syntax"] = new Array("Syntax",":");
+
+searchTerms["Tutor/Markup/StructureMarkup/listing"] = new Array(">`","#","listing","`<");
+
+searchTerms["Tutor/Concept/Details"] = new Array("Details",":",",");
+
+searchTerms["Tutor/Markup"] = null;
