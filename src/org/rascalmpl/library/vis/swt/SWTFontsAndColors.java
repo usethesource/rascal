1a2d83fa9b0c06d6772d70777dfd619abde417a1
Paul Klint <Paul.Klint@cwi.nl>
2011-09-22 20:48:55 +0000

    Added (non-functioning) fontNames function.

--- src/org/rascalmpl/library/vis/swt/SWTFontsAndColors.java
+++ src/org/rascalmpl/library/vis/swt/SWTFontsAndColors.java
@@ -1,5 +1,9 @@
 package org.rascalmpl.library.vis.swt;
 
+import org.eclipse.imp.pdb.facts.IList;
+import org.eclipse.imp.pdb.facts.IListWriter;
+import org.eclipse.imp.pdb.facts.IValueFactory;
+import org.eclipse.imp.pdb.facts.type.TypeFactory;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Device;
 import org.eclipse.swt.graphics.Font;
@@ -8,8 +12,16 @@ import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.widgets.Display;
 import org.rascalmpl.library.vis.graphics.FontStyle;
 import org.rascalmpl.library.vis.util.FigureColorUtils;
+import org.rascalmpl.values.ValueFactoryFactory;
 
 public class SWTFontsAndColors {
+	
+	static IValueFactory vf = ValueFactoryFactory.getValueFactory();
+	private static TypeFactory tf = TypeFactory.getInstance();
+
+	public SWTFontsAndColors(IValueFactory factory) {
+		vf = factory;
+	}
 	
 	static private Device device = null;
 	static private GC gc = null;
@@ -19,12 +31,26 @@ public class SWTFontsAndColors {
 		if(device == null){
 			device = Display.getCurrent();
 			if(device == null){
-				throw new Error("Trying to create get display from non-userinterfacethread!");
+				throw new Error("Trying to create get display from non-user interface thread!");
 			}
 		}
 		if(gc == null || gc.isDisposed()){
 			gc = new GC(device);
 		}
+	}
+	
+	// TODO: how to execute this on the right thread?
+	
+	public static IList fontNames(){
+		System.err.println("fontNames");
+		Display display = Display.getDefault();
+		FontData[] fd = display.getFontList(null, true);
+		IListWriter w = vf.listWriter(tf.stringType());
+		for(int i = 0; i < fd.length; i++){
+			System.err.println("adding " + fd[i].getName());
+			w.append(vf.string(fd[i].getName()));
+		}
+		return w.done();
 	}
 	
 	public static double textAscent(String fontName, int fontSize,
