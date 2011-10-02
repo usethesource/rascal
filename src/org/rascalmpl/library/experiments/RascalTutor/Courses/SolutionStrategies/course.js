03337d973cf44252a9da7d88e6bfaf162c353cdc
Paul Klint <Paul.Klint@cwi.nl>
2011-10-02 20:29:09 +0000

    Renamed ProblemsSolvingStrategies to SolutionStrategies. Reorganized main page of Tutor.

---
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/SolutionStrategies/course.js
@@ -0,0 +1,12 @@
+/* Generated code for course SolutionStrategies */
+
+$.setRootConcept("SolutionStrategies");
+
+var baseConcepts = new Array();
+
+var conceptNames = new Array();
+
+var searchTerms = {};
+
+
+searchTerms["SolutionStrategies"] = null;
