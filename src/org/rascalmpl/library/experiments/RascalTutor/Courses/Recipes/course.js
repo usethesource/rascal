f1b323249eb39ad64c939d83461e16bcf9d2a950
Paul Klint <Paul.Klint@cwi.nl>
2011-10-03 09:16:49 +0000

    Renamed Simple to Basic

--- src/org/rascalmpl/library/experiments/RascalTutor/Courses/Recipes/course.js
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/Recipes/course.js
@@ -2,19 +2,25 @@
 
 $.setRootConcept("Recipes");
 
-var baseConcepts = new Array("ColoredTrees","Factorial","Hello","Recipes","Simple");
+var baseConcepts = new Array("Basic","ColoredTrees","Factorial","Hello","Recipes","Simple");
 
-var conceptNames = new Array("Recipes","Recipes/Simple","Recipes/Simple/ColoredTrees","Recipes/Simple/Factorial","Recipes/Simple/Hello");
+var conceptNames = new Array("Recipes","Recipes/Basic","Recipes/Basic/ColoredTrees","Recipes/Basic/Factorial","Recipes/Basic/Hello","Recipes/Simple/ColoredTrees","Recipes/Simple/Factorial","Recipes/Simple/Hello");
 
 var searchTerms = {};
 
 
 searchTerms["Recipes/Simple/Hello"] = null;
 
-searchTerms["Recipes/Simple"] = null;
+searchTerms["Recipes/Basic/ColoredTrees"] = null;
+
+searchTerms["Recipes/Basic/Factorial"] = null;
+
+searchTerms["Recipes/Basic/Hello"] = null;
 
 searchTerms["Recipes/Simple/ColoredTrees"] = null;
 
 searchTerms["Recipes"] = null;
+
+searchTerms["Recipes/Basic"] = null;
 
 searchTerms["Recipes/Simple/Factorial"] = null;
