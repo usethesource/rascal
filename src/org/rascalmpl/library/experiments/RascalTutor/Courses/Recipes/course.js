6711ffdd441e372678610379a7ed85230fa9ac60
Paul Klint <Paul.Klint@cwi.nl>
2011-10-03 06:09:43 +0000

    Started with the (long awaited) Recipes course.

---
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/Recipes/course.js
@@ -0,0 +1,20 @@
+/* Generated code for course Recipes */
+
+$.setRootConcept("Recipes");
+
+var baseConcepts = new Array("ColoredTrees","Factorial","Hello","Recipes","Simple");
+
+var conceptNames = new Array("Recipes","Recipes/Simple","Recipes/Simple/ColoredTrees","Recipes/Simple/Factorial","Recipes/Simple/Hello");
+
+var searchTerms = {};
+
+
+searchTerms["Recipes/Simple/Hello"] = null;
+
+searchTerms["Recipes/Simple"] = null;
+
+searchTerms["Recipes/Simple/ColoredTrees"] = null;
+
+searchTerms["Recipes"] = null;
+
+searchTerms["Recipes/Simple/Factorial"] = null;
