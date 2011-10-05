54874f468293952df72262be7ad1a0ea53a24aef
Paul Klint <Paul.Klint@cwi.nl>
2011-10-05 16:29:27 +0000

    Created a separate EASY course. Restructured the main page of the Tutor. Many minor edits.

---
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/EASY/course.js
@@ -0,0 +1,22 @@
+/* Generated code for course EASY */
+
+$.setRootConcept("EASY");
+
+var baseConcepts = new Array("Concurrency","EASY","Forensics","ModelDrivenEngineering","Renovation","Security");
+
+var conceptNames = new Array("EASY","EASY/Concurrency","EASY/Forensics","EASY/ModelDrivenEngineering","EASY/Renovation","EASY/Security");
+
+var searchTerms = {};
+
+
+searchTerms["EASY/Security"] = null;
+
+searchTerms["EASY/Forensics"] = null;
+
+searchTerms["EASY/Concurrency"] = null;
+
+searchTerms["EASY/ModelDrivenEngineering"] = null;
+
+searchTerms["EASY"] = null;
+
+searchTerms["EASY/Renovation"] = null;
