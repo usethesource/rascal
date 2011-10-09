1bf158cfb66618a4747901b7d17b411ed439882b
Paul Klint <Paul.Klint@cwi.nl>
2011-10-09 19:55:44 +0000

    Some progress with the Recipes course.

--- src/org/rascalmpl/library/experiments/RascalTutor/Courses/Recipes/course.js
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/Recipes/course.js
@@ -1,13 +1,23 @@
 /* Generated code for course Recipes */
 
-var baseConcepts = new Array("Basic","ColoredTrees","Factorial","Hello","Recipes");
+var baseConcepts = new Array("Basic","ColoredTrees","Common","CountInLine1","CountInLine2","CountInLine3","Factorial","Hello","Jabberwocky","Languages","Recipes","Squares","WordCount");
 
-var conceptNames = new Array("Recipes","Recipes/Basic","Recipes/Basic/ColoredTrees","Recipes/Basic/Factorial","Recipes/Basic/Hello");
+var conceptNames = new Array("Recipes","Recipes/Basic","Recipes/Basic/ColoredTrees","Recipes/Basic/Factorial","Recipes/Basic/Hello","Recipes/Basic/Squares","Recipes/Common","Recipes/Common/WordCount","Recipes/Common/WordCount/CountInLine1","Recipes/Common/WordCount/CountInLine2","Recipes/Common/WordCount/CountInLine3","Recipes/Common/WordCount/Jabberwocky","Recipes/Languages");
 
 var searchTerms = {};
 
 
+searchTerms["Recipes/Common/WordCount/CountInLine3"] = null;
+
+searchTerms["Recipes/Common/WordCount/CountInLine2"] = null;
+
 searchTerms["Recipes/Basic/ColoredTrees"] = null;
+
+searchTerms["Recipes/Common/WordCount/CountInLine1"] = null;
+
+searchTerms["Recipes/Common/WordCount"] = null;
+
+searchTerms["Recipes/Basic/Squares"] = null;
 
 searchTerms["Recipes/Basic/Factorial"] = null;
 
@@ -15,4 +25,10 @@ searchTerms["Recipes/Basic/Hello"] = null;
 
 searchTerms["Recipes"] = null;
 
+searchTerms["Recipes/Common"] = null;
+
 searchTerms["Recipes/Basic"] = null;
+
+searchTerms["Recipes/Languages"] = null;
+
+searchTerms["Recipes/Common/WordCount/Jabberwocky"] = null;
