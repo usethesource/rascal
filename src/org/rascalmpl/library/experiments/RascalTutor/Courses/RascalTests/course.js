967babb5151ccab9c834a43d8726bc00fccf6207
Paul Klint <Paul.Klint@cwi.nl>
2011-10-09 19:55:14 +0000

    Adapted to new structure. Regenerated.

---
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/RascalTests/course.js
@@ -0,0 +1,12 @@
+/* Generated code for course RascalTests */
+
+var baseConcepts = new Array("RascalTests","Test_Software_Evolution");
+
+var conceptNames = new Array("RascalTests","RascalTests/Test_Software_Evolution");
+
+var searchTerms = {};
+
+
+searchTerms["RascalTests/Test_Software_Evolution"] = null;
+
+searchTerms["RascalTests"] = null;
