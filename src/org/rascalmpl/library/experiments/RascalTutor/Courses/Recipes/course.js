51ce9eb6f2c50b984576be07a342fccd27f7441a
Paul Klint <Paul.Klint@cwi.nl>
2011-10-10 13:21:52 +0000

    Making progress with recipes.

--- src/org/rascalmpl/library/experiments/RascalTutor/Courses/Recipes/course.js
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/Recipes/course.js
@@ -1,19 +1,21 @@
 /* Generated code for course Recipes */
 
-var baseConcepts = new Array("Basic","ColoredTrees","Common","CountInLine1","CountInLine2","CountInLine3","Factorial","Hello","Jabberwocky","Languages","Recipes","Squares","WordCount");
+var baseConcepts = new Array("Basic","CallAnalysis","CallLifting","ColoredTrees","Common","CountInLine1","CountInLine2","CountInLine3","Factorial","Hello","Jabberwocky","Languages","Recipes","Squares","StringTemplate","WordCount");
 
-var conceptNames = new Array("Recipes","Recipes/Basic","Recipes/Basic/ColoredTrees","Recipes/Basic/Factorial","Recipes/Basic/Hello","Recipes/Basic/Squares","Recipes/Common","Recipes/Common/WordCount","Recipes/Common/WordCount/CountInLine1","Recipes/Common/WordCount/CountInLine2","Recipes/Common/WordCount/CountInLine3","Recipes/Common/WordCount/Jabberwocky","Recipes/Languages");
+var conceptNames = new Array("Recipes","Recipes/Basic","Recipes/Basic/Factorial","Recipes/Basic/Hello","Recipes/Basic/Squares","Recipes/Common","Recipes/Common/CallAnalysis","Recipes/Common/CallLifting","Recipes/Common/ColoredTrees","Recipes/Common/StringTemplate","Recipes/Common/WordCount","Recipes/Common/WordCount/CountInLine1","Recipes/Common/WordCount/CountInLine2","Recipes/Common/WordCount/CountInLine3","Recipes/Common/WordCount/Jabberwocky","Recipes/Languages");
 
 var searchTerms = {};
 
 
 searchTerms["Recipes/Common/WordCount/CountInLine3"] = null;
 
+searchTerms["Recipes/Common/CallLifting"] = null;
+
 searchTerms["Recipes/Common/WordCount/CountInLine2"] = null;
 
-searchTerms["Recipes/Basic/ColoredTrees"] = null;
-
 searchTerms["Recipes/Common/WordCount/CountInLine1"] = null;
+
+searchTerms["Recipes/Common/ColoredTrees"] = null;
 
 searchTerms["Recipes/Common/WordCount"] = null;
 
@@ -25,7 +27,11 @@ searchTerms["Recipes/Basic/Hello"] = null;
 
 searchTerms["Recipes"] = null;
 
+searchTerms["Recipes/Common/StringTemplate"] = null;
+
 searchTerms["Recipes/Common"] = null;
+
+searchTerms["Recipes/Common/CallAnalysis"] = null;
 
 searchTerms["Recipes/Basic"] = null;
 
