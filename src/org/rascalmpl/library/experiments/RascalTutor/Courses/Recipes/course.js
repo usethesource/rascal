99b2280e9c1b1c22054d5bce6b002123c137ba81
Paul Klint <Paul.Klint@cwi.nl>
2011-10-10 15:52:08 +0000

    For the benefit of deployment on the web site: - moves images directory inside Courses. - all generated references to images changed accordingly. - regenerated all courses.

--- src/org/rascalmpl/library/experiments/RascalTutor/Courses/Recipes/course.js
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/Recipes/course.js
@@ -1,15 +1,31 @@
 /* Generated code for course Recipes */
 
-var baseConcepts = new Array("Basic","CallAnalysis","CallLifting","ColoredTrees","Common","CountInLine1","CountInLine2","CountInLine3","Factorial","Hello","Jabberwocky","Languages","Recipes","Squares","StringTemplate","WordCount");
+var baseConcepts = new Array("Abstract","Basic","CallAnalysis","CallLifting","ColoredTrees","Common","Concrete","CountInLine1","CountInLine2","CountInLine3","Exp","Factorial","Hello","Jabberwocky","Languages","Recipes","Squares","StringTemplate","WordCount");
 
-var conceptNames = new Array("Recipes","Recipes/Basic","Recipes/Basic/Factorial","Recipes/Basic/Hello","Recipes/Basic/Squares","Recipes/Common","Recipes/Common/CallAnalysis","Recipes/Common/CallLifting","Recipes/Common/ColoredTrees","Recipes/Common/StringTemplate","Recipes/Common/WordCount","Recipes/Common/WordCount/CountInLine1","Recipes/Common/WordCount/CountInLine2","Recipes/Common/WordCount/CountInLine3","Recipes/Common/WordCount/Jabberwocky","Recipes/Languages");
+var conceptNames = new Array("Recipes","Recipes/Basic","Recipes/Basic/Factorial","Recipes/Basic/Hello","Recipes/Basic/Squares","Recipes/Common","Recipes/Common/CallAnalysis","Recipes/Common/CallLifting","Recipes/Common/ColoredTrees","Recipes/Common/StringTemplate","Recipes/Common/WordCount","Recipes/Common/WordCount/CountInLine1","Recipes/Common/WordCount/CountInLine2","Recipes/Common/WordCount/CountInLine3","Recipes/Common/WordCount/Jabberwocky","Recipes/Languages","Recipes/Languages/Exp","Recipes/Languages/Exp/Abstract","Recipes/Languages/Exp/Concrete");
 
 var searchTerms = {};
 
 
-searchTerms["Recipes/Common/WordCount/CountInLine3"] = null;
+searchTerms["Recipes/Languages/Exp"] = null;
 
 searchTerms["Recipes/Common/CallLifting"] = null;
+
+searchTerms["Recipes/Basic/Factorial"] = null;
+
+searchTerms["Recipes/Languages/Exp/Concrete"] = null;
+
+searchTerms["Recipes/Common"] = null;
+
+searchTerms["Recipes/Common/CallAnalysis"] = null;
+
+searchTerms["Recipes/Languages/Exp/Abstract"] = null;
+
+searchTerms["Recipes/Languages"] = null;
+
+searchTerms["Recipes/Common/WordCount/Jabberwocky"] = null;
+
+searchTerms["Recipes/Common/WordCount/CountInLine3"] = null;
 
 searchTerms["Recipes/Common/WordCount/CountInLine2"] = null;
 
@@ -21,20 +37,10 @@ searchTerms["Recipes/Common/WordCount"] = null;
 
 searchTerms["Recipes/Basic/Squares"] = null;
 
-searchTerms["Recipes/Basic/Factorial"] = null;
-
 searchTerms["Recipes/Basic/Hello"] = null;
 
 searchTerms["Recipes"] = null;
 
 searchTerms["Recipes/Common/StringTemplate"] = null;
 
-searchTerms["Recipes/Common"] = null;
-
-searchTerms["Recipes/Common/CallAnalysis"] = null;
-
 searchTerms["Recipes/Basic"] = null;
-
-searchTerms["Recipes/Languages"] = null;
-
-searchTerms["Recipes/Common/WordCount/Jabberwocky"] = null;
