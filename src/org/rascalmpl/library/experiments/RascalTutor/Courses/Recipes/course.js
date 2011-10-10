aa41793877c2b37eddd01e39add329e1062f5196
Paul Klint <Paul.Klint@cwi.nl>
2011-10-10 21:54:11 +0000

    Progress with recipes.

--- src/org/rascalmpl/library/experiments/RascalTutor/Courses/Recipes/course.js
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/Recipes/course.js
@@ -1,8 +1,8 @@
 /* Generated code for course Recipes */
 
-var baseConcepts = new Array("Abstract","Basic","CallAnalysis","CallLifting","ColoredTrees","Common","Concrete","CountInLine1","CountInLine2","CountInLine3","Exp","Factorial","Hello","Jabberwocky","Languages","Recipes","Squares","StringTemplate","WordCount");
+var baseConcepts = new Array("Abstract","Basic","CallAnalysis","CallLifting","ColoredTrees","Common","Concrete","CountInLine1","CountInLine2","CountInLine3","Exp","Factorial","Hello","Jabberwocky","Languages","NoLayout","Recipes","Squares","StringTemplate","WithLayout","WordCount");
 
-var conceptNames = new Array("Recipes","Recipes/Basic","Recipes/Basic/Factorial","Recipes/Basic/Hello","Recipes/Basic/Squares","Recipes/Common","Recipes/Common/CallAnalysis","Recipes/Common/CallLifting","Recipes/Common/ColoredTrees","Recipes/Common/StringTemplate","Recipes/Common/WordCount","Recipes/Common/WordCount/CountInLine1","Recipes/Common/WordCount/CountInLine2","Recipes/Common/WordCount/CountInLine3","Recipes/Common/WordCount/Jabberwocky","Recipes/Languages","Recipes/Languages/Exp","Recipes/Languages/Exp/Abstract","Recipes/Languages/Exp/Concrete");
+var conceptNames = new Array("Recipes","Recipes/Basic","Recipes/Basic/Factorial","Recipes/Basic/Hello","Recipes/Basic/Squares","Recipes/Common","Recipes/Common/CallAnalysis","Recipes/Common/CallLifting","Recipes/Common/ColoredTrees","Recipes/Common/StringTemplate","Recipes/Common/WordCount","Recipes/Common/WordCount/CountInLine1","Recipes/Common/WordCount/CountInLine2","Recipes/Common/WordCount/CountInLine3","Recipes/Common/WordCount/Jabberwocky","Recipes/Languages","Recipes/Languages/Exp","Recipes/Languages/Exp/Abstract","Recipes/Languages/Exp/Concrete","Recipes/Languages/Exp/Concrete/NoLayout","Recipes/Languages/Exp/Concrete/WithLayout");
 
 var searchTerms = {};
 
@@ -37,10 +37,14 @@ searchTerms["Recipes/Common/WordCount"] = null;
 
 searchTerms["Recipes/Basic/Squares"] = null;
 
+searchTerms["Recipes/Languages/Exp/Concrete/WithLayout"] = null;
+
 searchTerms["Recipes/Basic/Hello"] = null;
 
 searchTerms["Recipes"] = null;
 
 searchTerms["Recipes/Common/StringTemplate"] = null;
+
+searchTerms["Recipes/Languages/Exp/Concrete/NoLayout"] = null;
 
 searchTerms["Recipes/Basic"] = null;
