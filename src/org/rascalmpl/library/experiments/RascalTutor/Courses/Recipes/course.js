14e9345417694eba44ab54569871419fb4c1149e
Paul Klint <Paul.Klint@cwi.nl>
2011-10-11 21:19:59 +0000

    Add more content to Recipes.

--- src/org/rascalmpl/library/experiments/RascalTutor/Courses/Recipes/course.js
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/Recipes/course.js
@@ -1,23 +1,21 @@
 /* Generated code for course Recipes */
 
-var baseConcepts = new Array("Abstract","Basic","CallAnalysis","CallLifting","ColoredTrees","Common","Concrete","CountInLine1","CountInLine2","CountInLine3","Exp","Factorial","Hello","Jabberwocky","Languages","NoLayout","Recipes","Squares","StringTemplate","WithLayout","WordCount");
+var baseConcepts = new Array("Abstract","AbstractSyntax","Automatic","Basic","CallAnalysis","CallLifting","ColoredTrees","Combined","Common","Concrete","ConcreteSyntax","CountInLine1","CountInLine2","CountInLine3","Eval0","Eval1","Eval2","Eval3","Exp","Factorial","Func","Hello","Jabberwocky","Languages","LoadAST","Manual","NoLayout","Parse","ParseTree","Recipes","Squares","StringTemplate","Visualization","WithLayout","WordCount");
 
-var conceptNames = new Array("Recipes","Recipes/Basic","Recipes/Basic/Factorial","Recipes/Basic/Hello","Recipes/Basic/Squares","Recipes/Common","Recipes/Common/CallAnalysis","Recipes/Common/CallLifting","Recipes/Common/ColoredTrees","Recipes/Common/StringTemplate","Recipes/Common/WordCount","Recipes/Common/WordCount/CountInLine1","Recipes/Common/WordCount/CountInLine2","Recipes/Common/WordCount/CountInLine3","Recipes/Common/WordCount/Jabberwocky","Recipes/Languages","Recipes/Languages/Exp","Recipes/Languages/Exp/Abstract","Recipes/Languages/Exp/Concrete","Recipes/Languages/Exp/Concrete/NoLayout","Recipes/Languages/Exp/Concrete/WithLayout");
+var conceptNames = new Array("Recipes","Recipes/Basic","Recipes/Basic/Factorial","Recipes/Basic/Hello","Recipes/Basic/Squares","Recipes/Common","Recipes/Common/CallAnalysis","Recipes/Common/CallLifting","Recipes/Common/ColoredTrees","Recipes/Common/StringTemplate","Recipes/Common/WordCount","Recipes/Common/WordCount/CountInLine1","Recipes/Common/WordCount/CountInLine2","Recipes/Common/WordCount/CountInLine3","Recipes/Common/WordCount/Jabberwocky","Recipes/Languages","Recipes/Languages/Exp","Recipes/Languages/Exp/Abstract","Recipes/Languages/Exp/Combined","Recipes/Languages/Exp/Combined/Automatic","Recipes/Languages/Exp/Combined/Manual","Recipes/Languages/Exp/Concrete","Recipes/Languages/Exp/Concrete/NoLayout","Recipes/Languages/Exp/Concrete/WithLayout","Recipes/Languages/Func","Recipes/Languages/Func/AbstractSyntax","Recipes/Languages/Func/ConcreteSyntax","Recipes/Languages/Func/Eval0","Recipes/Languages/Func/Eval1","Recipes/Languages/Func/Eval2","Recipes/Languages/Func/Eval3","Recipes/Languages/Func/LoadAST","Recipes/Languages/Func/Parse","Recipes/Visualization","Recipes/Visualization/ParseTree");
 
 var searchTerms = {};
 
 
-searchTerms["Recipes/Languages/Exp"] = null;
+searchTerms["Recipes/Visualization/ParseTree"] = null;
 
-searchTerms["Recipes/Common/CallLifting"] = null;
+searchTerms["Recipes/Visualization"] = null;
 
 searchTerms["Recipes/Basic/Factorial"] = null;
 
-searchTerms["Recipes/Languages/Exp/Concrete"] = null;
-
-searchTerms["Recipes/Common"] = null;
-
 searchTerms["Recipes/Common/CallAnalysis"] = null;
+
+searchTerms["Recipes/Languages/Func/AbstractSyntax"] = null;
 
 searchTerms["Recipes/Languages/Exp/Abstract"] = null;
 
@@ -29,22 +27,52 @@ searchTerms["Recipes/Common/WordCount/CountInLine3"] = null;
 
 searchTerms["Recipes/Common/WordCount/CountInLine2"] = null;
 
-searchTerms["Recipes/Common/ColoredTrees"] = null;
-
 searchTerms["Recipes/Common/WordCount/CountInLine1"] = null;
+
+searchTerms["Recipes/Languages/Func/Eval3"] = null;
+
+searchTerms["Recipes/Languages/Exp/Concrete/WithLayout"] = null;
+
+searchTerms["Recipes/Languages/Func/Eval2"] = null;
+
+searchTerms["Recipes/Languages/Func/Eval1"] = null;
+
+searchTerms["Recipes/Languages/Func/Eval0"] = null;
+
+searchTerms["Recipes/Basic/Hello"] = null;
+
+searchTerms["Recipes/Languages/Exp/Concrete/NoLayout"] = null;
+
+searchTerms["Recipes/Languages/Exp/Combined/Manual"] = null;
+
+searchTerms["Recipes/Languages/Exp"] = null;
+
+searchTerms["Recipes/Common/CallLifting"] = null;
+
+searchTerms["Recipes/Languages/Func/ConcreteSyntax"] = null;
+
+searchTerms["Recipes/Languages/Exp/Concrete"] = null;
+
+searchTerms["Recipes/Languages/Exp/Combined"] = null;
+
+searchTerms["Recipes/Common"] = null;
+
+searchTerms["Recipes/Languages/Exp/Combined/Automatic"] = null;
+
+searchTerms["Recipes/Languages/Func/LoadAST"] = null;
+
+searchTerms["Recipes/Common/ColoredTrees"] = null;
 
 searchTerms["Recipes/Common/WordCount"] = null;
 
 searchTerms["Recipes/Basic/Squares"] = null;
 
-searchTerms["Recipes/Languages/Exp/Concrete/WithLayout"] = null;
-
-searchTerms["Recipes/Basic/Hello"] = null;
-
-searchTerms["Recipes/Common/StringTemplate"] = null;
+searchTerms["Recipes/Languages/Func/Parse"] = null;
 
 searchTerms["Recipes"] = null;
 
-searchTerms["Recipes/Languages/Exp/Concrete/NoLayout"] = null;
+searchTerms["Recipes/Common/StringTemplate"] = null;
 
 searchTerms["Recipes/Basic"] = null;
+
+searchTerms["Recipes/Languages/Func"] = null;
