542a42e396852543c91c3ad4adbd284abb0de1af
Paul Klint <Paul.Klint@cwi.nl>
2011-10-15 14:08:07 +0000

    - Fixed Exp/Automatic example. - Added Bubble.

--- src/org/rascalmpl/library/experiments/RascalTutor/Courses/Recipes/course.js
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/Recipes/course.js
@@ -1,8 +1,8 @@
 /* Generated code for course Recipes */
 
-var baseConcepts = new Array("Abstract","AbstractSyntax","Automatic","Basic","CallAnalysis","CallLifting","ColoredTrees","Combined","Common","Concrete","ConcreteSyntax","CountInLine1","CountInLine2","CountInLine3","Eval0","Eval1","Eval2","Eval3","Exp","Factorial","Func","Hello","Jabberwocky","Languages","LoadAST","Manual","NoLayout","Parse","ParseTree","Recipes","Squares","StringTemplate","Visualization","WithLayout","WordCount");
+var baseConcepts = new Array("Abstract","AbstractSyntax","Automatic","Basic","Bubble","CallAnalysis","CallLifting","ColoredTrees","Combined","Common","Concrete","ConcreteSyntax","CountInLine1","CountInLine2","CountInLine3","Eval0","Eval1","Eval2","Eval3","Exp","Factorial","Func","Hello","Jabberwocky","Languages","LoadAST","Manual","NoLayout","Parse","ParseTree","Recipes","Squares","StringTemplate","Visualization","WithLayout","WordCount");
 
-var conceptNames = new Array("Recipes","Recipes/Basic","Recipes/Basic/Factorial","Recipes/Basic/Hello","Recipes/Basic/Squares","Recipes/Common","Recipes/Common/CallAnalysis","Recipes/Common/CallLifting","Recipes/Common/ColoredTrees","Recipes/Common/StringTemplate","Recipes/Common/WordCount","Recipes/Common/WordCount/CountInLine1","Recipes/Common/WordCount/CountInLine2","Recipes/Common/WordCount/CountInLine3","Recipes/Common/WordCount/Jabberwocky","Recipes/Languages","Recipes/Languages/Exp","Recipes/Languages/Exp/Abstract","Recipes/Languages/Exp/Combined","Recipes/Languages/Exp/Combined/Automatic","Recipes/Languages/Exp/Combined/Manual","Recipes/Languages/Exp/Concrete","Recipes/Languages/Exp/Concrete/NoLayout","Recipes/Languages/Exp/Concrete/WithLayout","Recipes/Languages/Func","Recipes/Languages/Func/AbstractSyntax","Recipes/Languages/Func/ConcreteSyntax","Recipes/Languages/Func/Eval0","Recipes/Languages/Func/Eval1","Recipes/Languages/Func/Eval2","Recipes/Languages/Func/Eval3","Recipes/Languages/Func/LoadAST","Recipes/Languages/Func/Parse","Recipes/Visualization","Recipes/Visualization/ParseTree");
+var conceptNames = new Array("Recipes","Recipes/Basic","Recipes/Basic/Bubble","Recipes/Basic/Factorial","Recipes/Basic/Hello","Recipes/Basic/Squares","Recipes/Common","Recipes/Common/CallAnalysis","Recipes/Common/CallLifting","Recipes/Common/ColoredTrees","Recipes/Common/StringTemplate","Recipes/Common/WordCount","Recipes/Common/WordCount/CountInLine1","Recipes/Common/WordCount/CountInLine2","Recipes/Common/WordCount/CountInLine3","Recipes/Common/WordCount/Jabberwocky","Recipes/Languages","Recipes/Languages/Exp","Recipes/Languages/Exp/Abstract","Recipes/Languages/Exp/Combined","Recipes/Languages/Exp/Combined/Automatic","Recipes/Languages/Exp/Combined/Manual","Recipes/Languages/Exp/Concrete","Recipes/Languages/Exp/Concrete/NoLayout","Recipes/Languages/Exp/Concrete/WithLayout","Recipes/Languages/Func","Recipes/Languages/Func/AbstractSyntax","Recipes/Languages/Func/ConcreteSyntax","Recipes/Languages/Func/Eval0","Recipes/Languages/Func/Eval1","Recipes/Languages/Func/Eval2","Recipes/Languages/Func/Eval3","Recipes/Languages/Func/LoadAST","Recipes/Languages/Func/Parse","Recipes/Visualization","Recipes/Visualization/ParseTree");
 
 var searchTerms = {};
 
@@ -48,6 +48,8 @@ searchTerms["Recipes/Languages/Exp/Combined/Manual"] = null;
 searchTerms["Recipes/Languages/Exp"] = null;
 
 searchTerms["Recipes/Common/CallLifting"] = null;
+
+searchTerms["Recipes/Basic/Bubble"] = null;
 
 searchTerms["Recipes/Languages/Func/ConcreteSyntax"] = null;
 
