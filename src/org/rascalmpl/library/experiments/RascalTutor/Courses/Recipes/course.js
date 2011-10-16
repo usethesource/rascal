0a1d582071cd7beca3a261c3b1097fbf3bbfb36e
Paul Klint <Paul.Klint@cwi.nl>
2011-10-16 16:03:07 +0000

    Added examples - 99 bottles of beer - quine
    Fixed various issues.

--- src/org/rascalmpl/library/experiments/RascalTutor/Courses/Recipes/course.js
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/Recipes/course.js
@@ -1,8 +1,8 @@
 /* Generated code for course Recipes */
 
-var baseConcepts = new Array("Abstract","AbstractSyntax","Automatic","Basic","Bubble","CallAnalysis","CallLifting","ColoredTrees","Combined","Common","Concrete","ConcreteSyntax","CountInLine1","CountInLine2","CountInLine3","Eval0","Eval1","Eval2","Eval3","Exp","Factorial","Func","Hello","Jabberwocky","Languages","LoadAST","Manual","NoLayout","Parse","ParseTree","Recipes","Squares","StringTemplate","Visualization","WithLayout","WordCount");
+var baseConcepts = new Array("Abstract","AbstractSyntax","Automatic","Basic","BottlesOfBeer","Bubble","CallAnalysis","CallLifting","ColoredTrees","Combined","Common","Concrete","ConcreteSyntax","CountInLine1","CountInLine2","CountInLine3","Eval0","Eval1","Eval2","Eval3","Exp","Factorial","Func","Hello","Jabberwocky","Languages","LoadAST","Manual","NoLayout","Parse","ParseTree","Quine","Recipes","Squares","StringTemplate","Visualization","WithLayout","WordCount");
 
-var conceptNames = new Array("Recipes","Recipes/Basic","Recipes/Basic/Bubble","Recipes/Basic/Factorial","Recipes/Basic/Hello","Recipes/Basic/Squares","Recipes/Common","Recipes/Common/CallAnalysis","Recipes/Common/CallLifting","Recipes/Common/ColoredTrees","Recipes/Common/StringTemplate","Recipes/Common/WordCount","Recipes/Common/WordCount/CountInLine1","Recipes/Common/WordCount/CountInLine2","Recipes/Common/WordCount/CountInLine3","Recipes/Common/WordCount/Jabberwocky","Recipes/Languages","Recipes/Languages/Exp","Recipes/Languages/Exp/Abstract","Recipes/Languages/Exp/Combined","Recipes/Languages/Exp/Combined/Automatic","Recipes/Languages/Exp/Combined/Manual","Recipes/Languages/Exp/Concrete","Recipes/Languages/Exp/Concrete/NoLayout","Recipes/Languages/Exp/Concrete/WithLayout","Recipes/Languages/Func","Recipes/Languages/Func/AbstractSyntax","Recipes/Languages/Func/ConcreteSyntax","Recipes/Languages/Func/Eval0","Recipes/Languages/Func/Eval1","Recipes/Languages/Func/Eval2","Recipes/Languages/Func/Eval3","Recipes/Languages/Func/LoadAST","Recipes/Languages/Func/Parse","Recipes/Visualization","Recipes/Visualization/ParseTree");
+var conceptNames = new Array("Recipes","Recipes/Basic","Recipes/Basic/BottlesOfBeer","Recipes/Basic/Bubble","Recipes/Basic/Factorial","Recipes/Basic/Hello","Recipes/Basic/Quine","Recipes/Basic/Squares","Recipes/Common","Recipes/Common/CallAnalysis","Recipes/Common/CallLifting","Recipes/Common/ColoredTrees","Recipes/Common/StringTemplate","Recipes/Common/WordCount","Recipes/Common/WordCount/CountInLine1","Recipes/Common/WordCount/CountInLine2","Recipes/Common/WordCount/CountInLine3","Recipes/Common/WordCount/Jabberwocky","Recipes/Languages","Recipes/Languages/Exp","Recipes/Languages/Exp/Abstract","Recipes/Languages/Exp/Combined","Recipes/Languages/Exp/Combined/Automatic","Recipes/Languages/Exp/Combined/Manual","Recipes/Languages/Exp/Concrete","Recipes/Languages/Exp/Concrete/NoLayout","Recipes/Languages/Exp/Concrete/WithLayout","Recipes/Languages/Func","Recipes/Languages/Func/AbstractSyntax","Recipes/Languages/Func/ConcreteSyntax","Recipes/Languages/Func/Eval0","Recipes/Languages/Func/Eval1","Recipes/Languages/Func/Eval2","Recipes/Languages/Func/Eval3","Recipes/Languages/Func/LoadAST","Recipes/Languages/Func/Parse","Recipes/Visualization","Recipes/Visualization/ParseTree");
 
 var searchTerms = {};
 
@@ -61,6 +61,8 @@ searchTerms["Recipes/Common"] = null;
 
 searchTerms["Recipes/Languages/Exp/Combined/Automatic"] = null;
 
+searchTerms["Recipes/Basic/Quine"] = null;
+
 searchTerms["Recipes/Languages/Func/LoadAST"] = null;
 
 searchTerms["Recipes/Common/ColoredTrees"] = null;
@@ -68,6 +70,8 @@ searchTerms["Recipes/Common/ColoredTrees"] = null;
 searchTerms["Recipes/Common/WordCount"] = null;
 
 searchTerms["Recipes/Basic/Squares"] = null;
+
+searchTerms["Recipes/Basic/BottlesOfBeer"] = null;
 
 searchTerms["Recipes/Languages/Func/Parse"] = null;
 
