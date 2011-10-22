5c437d84d05982f0b5189bd7b8884c623421b2f3
Paul Klint <Paul.Klint@cwi.nl>
2011-10-22 21:55:24 +0000

    Added a Contents page for all courses (using the new toc feature).
    Also added several summary pages.

--- src/org/rascalmpl/library/experiments/RascalTutor/Courses/EASY/course.js
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/EASY/course.js
@@ -1,8 +1,8 @@
 /* Generated code for course EASY */
 
-var baseConcepts = new Array("Concurrency","EASY","Forensics","ModelDrivenEngineering","Renovation","Security");
+var baseConcepts = new Array("Concurrency","Contents","EASY","Forensics","ModelDrivenEngineering","Renovation","Security");
 
-var conceptNames = new Array("EASY","EASY/Concurrency","EASY/Forensics","EASY/ModelDrivenEngineering","EASY/Renovation","EASY/Security");
+var conceptNames = new Array("EASY","EASY/Concurrency","EASY/Contents","EASY/Forensics","EASY/ModelDrivenEngineering","EASY/Renovation","EASY/Security");
 
 var searchTerms = {};
 
--- src/org/rascalmpl/library/experiments/RascalTutor/Courses/SolutionStrategies/course.js
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/SolutionStrategies/course.js
@@ -1,8 +1,8 @@
 /* Generated code for course SolutionStrategies */
 
-var baseConcepts = new Array("Analysis","Extraction","SolutionStrategies","Synthesis");
+var baseConcepts = new Array("Analysis","Contents","Extraction","SolutionStrategies","Synthesis");
 
-var conceptNames = new Array("SolutionStrategies","SolutionStrategies/Analysis","SolutionStrategies/Extraction","SolutionStrategies/Synthesis");
+var conceptNames = new Array("SolutionStrategies","SolutionStrategies/Analysis","SolutionStrategies/Contents","SolutionStrategies/Extraction","SolutionStrategies/Synthesis");
 
 var searchTerms = {};
 
