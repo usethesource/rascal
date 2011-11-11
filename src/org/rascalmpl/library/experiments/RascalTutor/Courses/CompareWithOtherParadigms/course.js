42f97a518683024c98ec5e6ff7267d0944eb27a9
Paul Klint <Paul.Klint@cwi.nl>
2011-11-11 13:53:01 +0000

    Added a new course that compares Rascal with other paradigms.

---
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/CompareWithOtherParadigms/course.js
@@ -0,0 +1,8 @@
+/* Generated code for course CompareWithOtherParadigms */
+
+var baseConcepts = new Array("ASFPlusSDF","CompareWithOtherParadigms","OO");
+
+var conceptNames = new Array("CompareWithOtherParadigms","CompareWithOtherParadigms/ASFPlusSDF","CompareWithOtherParadigms/OO");
+
+var searchTerms = {};
+
