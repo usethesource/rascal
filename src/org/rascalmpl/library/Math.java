98f440056cb0586465d706dce7746526bf82b025
Paul Klint <Paul.Klint@cwi.nl>
2011-12-20 12:30:03 +0000

    Added new Math library that will replace the libraries Integer, Real and Rational.

---
+++ src/org/rascalmpl/library/Math.java
@@ -0,0 +1,151 @@
+/*******************************************************************************
+ * Copyright (c) 2009-2011 CWI
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+
+ *   * Jurgen J. Vinju - Jurgen.Vinju@cwi.nl - CWI
+ *   * Bert Lisser - Bert.Lisser@cwi.nl (CWI)
+ *   * Davy Landman - Davy.Landman@cwi.nl - CWI
+*******************************************************************************/
+package org.rascalmpl.library;
+
+import java.util.Random;
+
+import org.eclipse.imp.pdb.facts.IInteger;
+import org.eclipse.imp.pdb.facts.INumber;
+import org.eclipse.imp.pdb.facts.IRational;
+import org.eclipse.imp.pdb.facts.IValue;
+import org.eclipse.imp.pdb.facts.IValueFactory;
+import org.rascalmpl.interpreter.result.RealResult;
+
+public class Math {
+	private final IValueFactory values;
+	private final Random random;
+	
+	public Math(IValueFactory values){
+		super();
+		
+		this.values = values;
+		random = new Random();
+	}
+
+	public IValue arbInt()
+	//@doc{arbInt -- return an arbitrary integer value}
+	{
+	   return values.integer(random.nextInt());
+	}
+
+	public IValue arbInt(IInteger limit)
+	//@doc{arbInt -- return an arbitrary integer value in the interval [0, limit).}
+	{
+		// TODO allow big ints
+	   return values.integer(random.nextInt(limit.intValue()));
+	}
+
+	public IValue arbReal()
+	//@doc{arbReal -- returns an arbitrary real value in the interval [0.0,1.0).}
+	{
+	  return values.real(random.nextDouble());
+	}
+	
+	public IValue cos(INumber x){
+		return x.toReal().cos(RealResult.PRECISION);
+	}
+	
+	public IValue denominator(IRational n)
+	{
+	  return n.denominator();
+	}
+	
+	public IValue E()
+	//@doc{e -- returns the constant E}
+	{
+		return values.e(RealResult.PRECISION);
+	}
+	
+	public IValue exp(INumber x){
+		return x.toReal().exp(RealResult.PRECISION);
+	}
+	
+	public IValue floor(INumber x){
+		return x.toReal().floor().toInteger();
+	}
+	
+	public IValue ln(INumber x) {
+		return x.toReal().ln(RealResult.PRECISION);
+	}
+
+	public IValue log(INumber x, INumber base) {
+		return x.toReal().log(base.toReal(), RealResult.PRECISION);
+	}
+	
+	public IValue numerator(IRational n)
+	{
+		return n.numerator();
+	}
+
+	public IValue nroot(INumber x, IInteger y){
+		return x.toReal().nroot(y, RealResult.PRECISION);
+	}
+	
+	public IValue PI()
+	//@doc{pi -- returns the constant PI}
+	{
+		return values.pi(RealResult.PRECISION);
+	}
+	
+	public IValue pow(INumber x, IInteger y){
+		return x.toReal().pow(y);
+	}
+	
+	public IValue remainder(IRational n)
+	{
+	  return n.remainder();
+	}
+	
+	public IValue round(INumber d) {
+		return d.toReal().round().toInteger();
+	}
+	
+	public IValue sin(INumber x){
+		return x.toReal().sin(RealResult.PRECISION);
+	}
+	
+	public IValue sqrt(INumber x){
+		return x.toReal().sqrt(RealResult.PRECISION);
+	}
+	
+	public IValue tan(INumber x){
+		return x.toReal().tan(RealResult.PRECISION);
+	}
+	
+	public IValue toInt(INumber d)
+	//@doc{toInteger -- convert a real to integer.}
+	{
+	  return d.toInteger();
+	}
+	
+	public IValue toRat(IInteger numerator, IInteger denominator)
+	//@doc{toRat -- convert two integers to a rat value.}
+	{
+	  return values.rational(numerator, denominator);
+	}
+	
+	public IValue toReal(INumber n)
+	//@doc{toReal -- convert a number value to a real value.}
+	{
+	  return n.toReal();
+	}
+
+	public IValue toString(INumber d)
+	//@doc{toString -- convert a real to a string.}
+	{
+	  return values.string(d.toString());
+	}
+
+	
+}
