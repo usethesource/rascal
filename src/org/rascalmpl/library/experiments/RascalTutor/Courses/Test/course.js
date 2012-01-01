eafb6fa0b2cc8845a1f019de5d5415379d0bd14c
Paul Klint <Paul.Klint@cwi.nl>
2012-01-01 20:55:41 +0000

    Varia.

--- src/org/rascalmpl/library/experiments/RascalTutor/Courses/Test/course.js
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/Test/course.js
@@ -1,8 +1,8 @@
 /* Generated code for course Test */
 
-var baseConcepts = new Array("(","),",",","AAA","BBB","BBB1","BBB2","BBB3","Bla","Bla1","Bla2","Bla3","Bla4","Cat1","Cat11","Cat12","Cat13","Cat2","Cat21","Cat22","Exam","LinkBreak","Test","[","]","lub","map");
+var baseConcepts = new Array("(","),",",","AAA","AAA1","AAA2","AAA3","BBB","BBB1","BBB2","BBB3","Bla","Bla1","Bla2","Bla3","Bla4","Cat1","Cat11","Cat12","Cat13","Cat2","Cat21","Cat22","Eval","Exam","Libraries","LinkBreak","T20111114","T20111128","Test","[","]","eval","evalType","lub","map","shell","util");
 
-var conceptNames = new Array("Test","Test/AAA","Test/BBB","Test/BBB/BBB1","Test/BBB/BBB2","Test/BBB/BBB3","Test/BBB/Cat1","Test/Bla","Test/Bla/Bla1","Test/Bla/Bla2","Test/Bla/Bla3","Test/Bla/Bla4","Test/Cat1","Test/Cat1/Cat11","Test/Cat1/Cat12","Test/Cat1/Cat13","Test/Cat2","Test/Cat2/Cat21","Test/Cat2/Cat22","Test/Exam","Test/LinkBreak");
+var conceptNames = new Array("Test","Test/AAA","Test/AAA/AAA1","Test/AAA/AAA2","Test/AAA/AAA3","Test/BBB","Test/BBB/BBB1","Test/BBB/BBB2","Test/BBB/BBB3","Test/BBB/Cat1","Test/Bla","Test/Bla/Bla1","Test/Bla/Bla2","Test/Bla/Bla3","Test/Bla/Bla4","Test/Cat1","Test/Cat1/Cat11","Test/Cat1/Cat12","Test/Cat1/Cat13","Test/Cat2","Test/Cat2/Cat21","Test/Cat2/Cat22","Test/Exam","Test/Libraries","Test/Libraries/util","Test/Libraries/util/Eval","Test/Libraries/util/Eval/eval","Test/Libraries/util/Eval/evalType","Test/Libraries/util/Eval/shell","Test/LinkBreak","Test/T20111114","Test/T20111128");
 
 var searchTerms = {};
 
