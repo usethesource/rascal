d2c35c14e0aa809ae4fb0e7044c6191ea89ec741
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2012-01-05 18:22:36 +0000

    * fixed final issue with unicode * added implementation of utf32 escapes

--- src/org/rascalmpl/interpreter/utils/StringUtils.java
+++ src/org/rascalmpl/interpreter/utils/StringUtils.java
@@ -12,112 +12,8 @@
 *******************************************************************************/
 package org.rascalmpl.interpreter.utils;
 
-import org.eclipse.imp.pdb.facts.IConstructor;
-import org.eclipse.imp.pdb.facts.IString;
-import org.eclipse.imp.pdb.facts.IValue;
-import org.rascalmpl.ast.AbstractAST;
-import org.rascalmpl.interpreter.env.Environment;
-import org.rascalmpl.interpreter.result.Result;
-import org.rascalmpl.interpreter.staticErrors.UninitializedVariableError;
-import org.rascalmpl.interpreter.types.NonTerminalType;
-import org.rascalmpl.values.uptr.TreeAdapter;
 
 public final class StringUtils {
-	
-	// TODO remove this one
-	public static String unescape(String str, AbstractAST ast, Environment env) {
-		byte[] bytes = str.getBytes();
-		StringBuffer result = new StringBuffer();
-		
-		for (int i = 1; i < bytes.length - 1; i++) {
-			char b = (char) bytes[i];
-			switch (b) {
-			/*
-			 * Replace <var> by var's value.
-			 */
-			case '<':
-				StringBuffer var = new StringBuffer();
-				char varchar;
-				while((varchar = (char) bytes[++i]) != '>'){
-					var.append(varchar);
-				}
-				Result<IValue> val = env.getVariable(var.toString());
-				String replacement;
-				if(val == null || val.getValue() == null) {
-					throw new UninitializedVariableError(var.toString(), ast);
-				}
-				
-				if(val.getType().isStringType()){
-					replacement = ((IString)val.getValue()).getValue();
-				} 
-				else if (val.getType() instanceof NonTerminalType) {
-					replacement = TreeAdapter.yield((IConstructor) val.getValue());
-				} 
-				else {
-					replacement = val.getValue().toString();
-				}
-//				replacement = replacement.replaceAll("<", "\\\\<"); TODO: maybe we need this after all?
-				result.append(replacement);
-				continue;
-			case '\\':
-				switch (bytes[++i]) {
-				case '\\':
-					b = '\\'; 
-					break;
-				case 'n':
-					b = '\n'; 
-					break;
-				case '"':
-					b = '"'; 
-					break;
-				case 't':
-					b = '\t'; 
-					break;
-				case 'b':
-					b = '\b'; 
-					break;
-				case 'f':
-					b = '\f'; 
-					break;
-				case 'r':
-					b = '\r'; 
-					break;
-				case '<':
-					b = '<'; 
-					break;
-				case '>':
-					b = '>';
-					break;
-				case '0':
-				case '1':
-				case '2':
-				case '3':
-				case '4':
-				case '5':
-				case '6':
-				case '7':
-					b = (char) (bytes[i] - '0');
-					if (i < bytes.length - 1 && Character.isDigit(bytes[i+1])) {
-						b = (char) (b * 8 + (bytes[++i] - '0'));
-						
-						if (i < bytes.length - 1 && Character.isDigit(bytes[i+1])) {
-							b = (char) (b * 8 + (bytes[++i] - '0'));
-						}
-					}
-					break;
-				case 'u':
-					// TODO unicode escape
-					break;
-				default:
-				    b = '\\';	
-				}
-			}
-			
-			result.append(b);
-		}
-		
-		return result.toString();
-	}
 	
 	public static String unescapeSingleQuoteAndBackslash(String str) {
 		char[] chars = str.toCharArray();
@@ -208,7 +104,12 @@ public final class StringUtils {
 					}
 					break;
 				case 'u':
-					// TODO unicode escape
+					StringBuilder u = new StringBuilder();
+					u.append(chars[++i]);
+					u.append(chars[++i]);
+					u.append(chars[++i]);
+					u.append(chars[++i]);
+					b = (char) Integer.parseInt(u.toString(), 16);
 					break;
 				default:
 				    b = '\\';	
