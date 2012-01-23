e3e833d37d2d39b01526ef5cf280d7cff764b910
Paul Klint <Paul.Klint@cwi.nl>
2012-01-23 21:52:42 +0000

    Created Rascalopedia, a glossary of concepts and terms that are relevant for metaprogrammers. The descriptions are aiming at under-graduate students and have the following properties: - < 1 A4 page - Concepts are first explained in general terms and then the Rascal counterpart is mentioned (with references to relevant Tutor pages). - No extensive duplication with other pages. - References to external (open) sources for details. - Where possible in the examples section: "Concept X in daily life" with images, see List, Set, Relation, ADT and enjoy ;-).
    Note that not yet all concepts that are listed in the Contents section have a description. Work in progress!

---
+++ src/org/rascalmpl/library/experiments/RascalTutor/Courses/Rascalopedia/course.js
@@ -0,0 +1,8 @@
+/* Generated code for course Rascalopedia */
+
+var baseConcepts = new Array("AbstractDataType","AbstractSyntaxTree","Compiler","Contents","DomainSpecificLanguage","FiniteStateMachine","Grammar","Interpreter","LanguageDefinition","List","MetaProgramming","ParseTree","Parser","Rascalopedia","Refactoring","RegularExpression","Relation","Scope","Set","SoftwareEngineering","SoftwareEvolution","SoftwareMetric","Substitution","Syntax","Testing","Tuple","Typechecker","Visualization");
+
+var conceptNames = new Array("Rascalopedia","Rascalopedia/AbstractDataType","Rascalopedia/AbstractSyntaxTree","Rascalopedia/Compiler","Rascalopedia/Contents","Rascalopedia/DomainSpecificLanguage","Rascalopedia/FiniteStateMachine","Rascalopedia/Grammar","Rascalopedia/Interpreter","Rascalopedia/LanguageDefinition","Rascalopedia/List","Rascalopedia/MetaProgramming","Rascalopedia/ParseTree","Rascalopedia/Parser","Rascalopedia/Refactoring","Rascalopedia/RegularExpression","Rascalopedia/Relation","Rascalopedia/Scope","Rascalopedia/Set","Rascalopedia/SoftwareEngineering","Rascalopedia/SoftwareEvolution","Rascalopedia/SoftwareMetric","Rascalopedia/Substitution","Rascalopedia/Syntax","Rascalopedia/Testing","Rascalopedia/Tuple","Rascalopedia/Typechecker","Rascalopedia/Visualization");
+
+var searchTerms = {};
+
