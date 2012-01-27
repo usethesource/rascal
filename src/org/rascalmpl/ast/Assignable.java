936d90d69bba6536a42a8e5d98039e18435eb10c
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2012-01-27 10:29:52 +0000

    * regenerated AST classes for ambiguious parts of Rascal, which needed to be changed because the thrown exceptions depended on the presence of a parse tree.

--- src/org/rascalmpl/ast/Assignable.java
+++ src/org/rascalmpl/ast/Assignable.java
@@ -16,9 +16,9 @@ package org.rascalmpl.ast;
 
 
 import org.eclipse.imp.pdb.facts.IConstructor;
+import org.rascalmpl.interpreter.asserts.Ambiguous;
 import org.eclipse.imp.pdb.facts.IValue;
 import org.rascalmpl.interpreter.Evaluator;
-import org.rascalmpl.interpreter.asserts.Ambiguous;
 import org.rascalmpl.interpreter.env.Environment;
 import org.rascalmpl.interpreter.result.Result;
 
@@ -121,12 +121,12 @@ public abstract class Assignable extends AbstractAST {
   
     @Override
     public Result<IValue> interpret(Evaluator __eval) {
-      throw new Ambiguous(node);
+      throw new Ambiguous(src);
     }
       
     @Override
     public org.eclipse.imp.pdb.facts.type.Type typeOf(Environment env) {
-      throw new Ambiguous(node);
+      throw new Ambiguous(src);
     }
     
     public java.util.List<org.rascalmpl.ast.Assignable> getAlternatives() {
