8a79c6f800e5f551a29288e6b8e7af38459cbc68
Paul Klint <Paul.Klint@cwi.nl>
2012-01-27 14:48:47 +0000

    Fixed hashing of cases in visit [by Jurgen].

--- src/org/rascalmpl/semantics/dynamic/Visit.java
+++ src/org/rascalmpl/semantics/dynamic/Visit.java
@@ -23,6 +23,7 @@ import org.rascalmpl.ast.Expression;
 import org.rascalmpl.ast.Strategy;
 import org.rascalmpl.interpreter.Evaluator;
 import org.rascalmpl.interpreter.TraversalEvaluator;
+import org.rascalmpl.interpreter.TraversalEvaluator.CaseBlockList;
 import org.rascalmpl.interpreter.TraversalEvaluator.DIRECTION;
 import org.rascalmpl.interpreter.TraversalEvaluator.FIXEDPOINT;
 import org.rascalmpl.interpreter.TraversalEvaluator.PROGRESS;
@@ -34,20 +35,21 @@ public abstract class Visit extends org.rascalmpl.ast.Visit {
 
 	static public class DefaultStrategy extends
 			org.rascalmpl.ast.Visit.DefaultStrategy {
+		private final CaseBlockList blocks;
 
 		public DefaultStrategy(IConstructor __param1, Expression __param2,
 				List<Case> __param3) {
 			super(__param1, __param2, __param3);
+			blocks = new CaseBlockList(Cases.precompute(getCases()));
 		}
 
 		@Override
 		public Result<IValue> interpret(Evaluator __eval) {
 			Result<IValue> subject = this.getSubject().interpret(__eval);
-			List<Case> cases = this.getCases();
 			TraversalEvaluator te = new TraversalEvaluator(__eval);
 
 			IValue val = te.traverse(subject.getValue(),
-					te.new CaseBlockList(Cases.precompute(cases)), DIRECTION.BottomUp,
+					blocks, DIRECTION.BottomUp,
 					PROGRESS.Continuing, FIXEDPOINT.No);
 			Type t = val.getType();
 			return org.rascalmpl.interpreter.result.ResultFactory.makeResult(t,
@@ -59,10 +61,12 @@ public abstract class Visit extends org.rascalmpl.ast.Visit {
 
 	static public class GivenStrategy extends
 			org.rascalmpl.ast.Visit.GivenStrategy {
+		private final CaseBlockList blocks;
 
 		public GivenStrategy(IConstructor __param1, Strategy __param2,
 				Expression __param3, List<Case> __param4) {
 			super(__param1, __param2, __param3, __param4);
+			blocks = new CaseBlockList(Cases.precompute(getCases()));
 		}
 
 		@Override
@@ -78,7 +82,6 @@ public abstract class Visit extends org.rascalmpl.ast.Visit {
 				subjectType = subjectType.getAbstractDataType();
 			}
 
-			List<Case> cases = this.getCases();
 			Strategy s = this.getStrategy();
 
 			DIRECTION direction = DIRECTION.BottomUp;
@@ -107,8 +110,7 @@ public abstract class Visit extends org.rascalmpl.ast.Visit {
 
 			TraversalEvaluator te = new TraversalEvaluator(__eval);
 			IValue val = te
-					.traverse(subject.getValue(), te.new CaseBlockList(Cases.precompute(cases)),
-							direction, progress, fixedpoint);
+					.traverse(subject.getValue(), blocks,direction, progress, fixedpoint);
 			Type t = val.getType();
 			return org.rascalmpl.interpreter.result.ResultFactory.makeResult(t,
 					val, __eval);
