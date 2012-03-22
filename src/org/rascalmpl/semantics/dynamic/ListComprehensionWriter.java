a88821a8f9a16dfe9683c681e0962ade18e0ddf8
Davy Landman <davy.landman@cwi.nl>
2012-03-22 21:12:39 +0000

    Re-implemented List, Set, and Map comprehension writers. Now lubbing works correctly.
    Added bonus:
     - Faster algorithm (just one iteration)
     - Less memory
     - Less code

--- src/org/rascalmpl/semantics/dynamic/ListComprehensionWriter.java
+++ src/org/rascalmpl/semantics/dynamic/ListComprehensionWriter.java
@@ -5,90 +5,37 @@ import org.eclipse.imp.pdb.facts.IListWriter;
 import org.eclipse.imp.pdb.facts.IValue;
 import org.rascalmpl.ast.Expression;
 import org.rascalmpl.interpreter.result.Result;
-import org.rascalmpl.interpreter.staticErrors.NonVoidTypeRequired;
-import org.rascalmpl.interpreter.staticErrors.UnexpectedTypeError;
 
 public class ListComprehensionWriter extends ComprehensionWriter {
 
-	private boolean splicing[];
-	private Result<IValue> rawElements[];
 
-	@SuppressWarnings("unchecked")
 	public ListComprehensionWriter(java.util.List<Expression> resultExprs,
 			org.rascalmpl.interpreter.Evaluator ev) {
 		super(resultExprs, ev);
-		this.splicing = new boolean[resultExprs.size()];
-		this.rawElements = new Result[resultExprs.size()];
+		this.writer = VF.listWriter();
+		this.elementType1 = TF.voidType();
 	}
-
+	
 	@Override
 	public void append() {
-		// the first time we need to find out the type of the elements
-		// first, and whether or not to splice them, and evaluate them
-		if (this.writer == null) {
-			int k = 0;
-			this.elementType1 = TF.voidType();
-
-			for (Expression resExpr : this.resultExprs) {
-			
-				org.eclipse.imp.pdb.facts.type.Type elementType;
-				this.splicing[k] = false;
-				
-				if(resExpr.isSplice() || resExpr.isSplicePlus()){
-					this.rawElements[k] = resExpr.getArgument().interpret(this.ev);
-					elementType = this.rawElements[k].getType();
-					if(elementType.isListType()){
-						elementType = elementType.getElementType();
-						this.splicing[k] = true;
-					}
-				} else {
-					this.rawElements[k] = resExpr.interpret(this.ev);
-					elementType = this.rawElements[k].getType();
-				}
-				this.elementType1 = this.elementType1.lub(elementType);
-				k++;
-			}
-
-			this.resultType = TF.listType(this.elementType1);
-			this.writer = this.resultType.writer(VF);
-		}
-		// the second time we only need to evaluate and add the elements
-		else {
-			int k = 0;
-			for (Expression resExpr : this.resultExprs) {
-				this.rawElements[k++] = (resExpr.isSplice() || resExpr.isSplicePlus()) ? resExpr.getArgument().interpret(this.ev) : resExpr.interpret(this.ev);
-			}
-		}
-
-		// here we finally add the elements
-		int k = 0;
 		for (Expression resExpr : this.resultExprs) {
-			if (this.splicing[k]) {
-				/*
-				 * Splice elements of the value of the result expression in
-				 * the result list
-				 */
-				if (!this.rawElements[k].getType().getElementType()
-						.isSubtypeOf(this.elementType1)) {
-					throw new UnexpectedTypeError(this.elementType1,
-							this.rawElements[k].getType().getElementType(),
-							resExpr);
+			if(resExpr.isSplice() || resExpr.isSplicePlus()){
+				Result<IValue> list = resExpr.getArgument().interpret(this.ev);
+				if (list.getType().isListType()) {
+					elementType1 = elementType1.lub(list.getType().getElementType());
+					((IListWriter)writer).appendAll((IList)list.getValue());
 				}
-
-				Result<?> list = this.rawElements[k];
-				if (list.getType().isVoidType()) {
-					throw new NonVoidTypeRequired(resExpr);
+				else {
+					// original code supported slicing on no list?
+					elementType1 = elementType1.lub(list.getType());
+					((IListWriter)writer).append(list.getValue());
 				}
-				for (IValue val : (IList) list.getValue()) {
-					((IListWriter) this.writer).append(val);
-				}
-			} else {
-				this.check(this.rawElements[k], this.elementType1, "list",
-						resExpr);
-				((IListWriter) this.writer).append(this.rawElements[k]
-						.getValue());
 			}
-			k++;
+			else {
+				Result<IValue> res = resExpr.interpret(this.ev);
+				elementType1 = elementType1.lub(res.getType());
+				((IListWriter)writer).append(res.getValue());
+			}
 		}
 	}
 
--- src/org/rascalmpl/semantics/dynamic/SetComprehensionWriter.java
+++ src/org/rascalmpl/semantics/dynamic/SetComprehensionWriter.java
@@ -1,92 +1,39 @@
 package org.rascalmpl.semantics.dynamic;
 
 import org.eclipse.imp.pdb.facts.ISet;
-import org.eclipse.imp.pdb.facts.ISetWriter;
 import org.eclipse.imp.pdb.facts.IValue;
 import org.rascalmpl.ast.Expression;
 import org.rascalmpl.interpreter.result.Result;
-import org.rascalmpl.interpreter.staticErrors.NonVoidTypeRequired;
-import org.rascalmpl.interpreter.staticErrors.UnexpectedTypeError;
 
 public class SetComprehensionWriter extends ComprehensionWriter {
-	private boolean splicing[];
-	private Result<IValue> rawElements[];
 
-	@SuppressWarnings("unchecked")
 	public SetComprehensionWriter(java.util.List<Expression> resultExprs,
 			org.rascalmpl.interpreter.Evaluator ev) {
 		super(resultExprs, ev);
-		this.splicing = new boolean[resultExprs.size()];
-		this.rawElements = new Result[resultExprs.size()];
+		this.writer = VF.setWriter();
+		this.elementType1 = TF.voidType();
 	}
-
+	
 	@Override
 	public void append() {
-		// the first time we need to find out the type of the elements
-		// first, and whether or not to splice them, and evaluate them
-		if (this.writer == null) {
-			int k = 0;
-			this.elementType1 = TF.voidType();
-
-			for (Expression resExpr : this.resultExprs) {
-				
-				org.eclipse.imp.pdb.facts.type.Type elementType;
-				this.splicing[k] = false;
-				
-				if(resExpr.isSplice() || resExpr.isSplicePlus()){
-					this.rawElements[k] = resExpr.getArgument().interpret(this.ev);
-					elementType = this.rawElements[k].getType();
-					if (elementType.isSetType()) {
-						elementType = elementType.getElementType();
-						this.splicing[k] = true;
-					}
-				} else {
-					this.rawElements[k] = resExpr.interpret(this.ev);
-					elementType = this.rawElements[k].getType();
-				}
-				this.elementType1 = this.elementType1.lub(elementType);
-				k++;
-			}
-
-			this.resultType = TF.setType(this.elementType1);
-			this.writer = this.resultType.writer(VF);
-		}
-		// the second time we only need to evaluate and add the elements
-		else {
-			int k = 0;
-			for (Expression resExpr : this.resultExprs) {
-				this.rawElements[k++] = (resExpr.isSplice() || resExpr.isSplicePlus()) ? resExpr.getArgument().interpret(this.ev) : resExpr.interpret(this.ev);
-			}
-		}
-
-		// here we finally add the elements
-		int k = 0;
 		for (Expression resExpr : this.resultExprs) {
-			if (this.splicing[k]) {
-				/*
-				 * Splice elements of the value of the result expression in
-				 * the result list
-				 */
-				if (!this.rawElements[k].getType().getElementType()
-						.isSubtypeOf(this.elementType1)) {
-					throw new UnexpectedTypeError(this.elementType1,
-							this.rawElements[k].getType().getElementType(),
-							resExpr);
+			if(resExpr.isSplice() || resExpr.isSplicePlus()){
+				Result<IValue> set = resExpr.getArgument().interpret(this.ev);
+				if (set.getType().isSetType()) {
+					elementType1 = elementType1.lub(set.getType().getElementType());
+					writer.insertAll((ISet)set.getValue());
 				}
-
-				Result<?> set = this.rawElements[k];
-				if (set.getType().isVoidType()) {
-					throw new NonVoidTypeRequired(resExpr);
+				else {
+					// original code supported slicing on no set?
+					elementType1 = elementType1.lub(set.getType());
+					writer.insert(set.getValue());
 				}
-				for (IValue val : (ISet) set.getValue()) {
-					((ISetWriter) this.writer).insert(val);
-				}
-			} else {
-				this.check(this.rawElements[k], this.elementType1, "set",						resExpr);
-				((ISetWriter) this.writer).insert(this.rawElements[k]
-						.getValue());
 			}
-			k++;
+			else {
+				Result<IValue> res = resExpr.interpret(this.ev);
+				elementType1 = elementType1.lub(res.getType());
+				writer.insert(res.getValue());
+			}
 		}
 	}
 
