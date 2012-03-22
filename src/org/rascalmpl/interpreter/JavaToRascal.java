97877485c62654d9bccd8908cba77a7d35de33ac
Bert Lisser <Bert.Lisser@cwi.nl>
2012-03-22 14:57:04 +0000

    Extension of Java Rascal API.

--- src/org/rascalmpl/interpreter/JavaToRascal.java
+++ src/org/rascalmpl/interpreter/JavaToRascal.java
@@ -15,6 +15,7 @@ import java.net.URI;
 import java.util.ArrayList;
 
 import org.eclipse.imp.pdb.facts.IInteger;
+import org.eclipse.imp.pdb.facts.IBool;
 import org.eclipse.imp.pdb.facts.IList;
 import org.eclipse.imp.pdb.facts.IString;
 import org.eclipse.imp.pdb.facts.IValue;
@@ -52,30 +53,143 @@ public class JavaToRascal {
 		this.evaluator = evaluator;
 	}
 
-	// public String call(String name, String... args) {
-	// IString[] vals = new IString[args.length];
-	// for (int i=0;i<args.length;i++) vals[i] = vf.string(args[i]);
-	// return evaluator.call(name, vals).toString();
-	// }
-
-	public IValue call(String name, IValue... args) {
-		return evaluator.call(name, args);
+	public Object call(String name, Object... args) {
+		IValue[] vals = new IValue[args.length];
+		for (int i = 0; i < args.length; i++)
+			vals[i] = rascalObject(args[i]);
+		return javaObject(evaluator.call(name, vals));
 	}
 
-	public String eval(String command, String location) {
+//	public IValue call(String name, IValue... args) {
+//		return evaluator.call(name, args);
+//	}
+
+	public void voidValue(String command, String location) {
+		evaluator.eval(null, command, URI.create(location));
+	}
+
+	/**
+	 * Evaluates Rascal Command without returning a result.
+	 * 
+	 * @param command
+	 *            Rascal command.
+	 * 
+	 */
+	public void voidValue(String command) {
+		voidValue(command, "stdin:///");
+	}
+
+	public String stringValue(String command, String location) {
 		Result<IValue> result = evaluator.eval(null, command,
 				URI.create(location));
-		// System.err.println("Result:"+result.getType()+" "+result.getType().isStringType()+" "+result.getType().isVoidType());
-		if (result.getType().isVoidType()) {
-			return "ok";
-		}
-		if (result.getType().isStringType()) {
-			return ((IString) (result.getValue())).getValue();
-		}
-		return result.toString();
+		return ((IString) (result.getValue())).getValue();
 	}
 
-	public String eval(String command) {
+	/**
+	 * @param command
+	 * @return result of Rascal Command evaluation which has type string
+	 */
+	public String stringValue(String command) {
+		return stringValue(command, "stdin:///");
+	}
+
+	public int intValue(String command, String location) {
+		Result<IValue> result = evaluator.eval(null, command,
+				URI.create(location));
+		return ((IInteger) (result.getValue())).intValue();
+	}
+
+	/**
+	 * @param command
+	 * @return result of Rascal Command evaluation which has type int
+	 */
+	public int intValue(String command) {
+		return intValue(command, "stdin:///");
+	}
+
+	public boolean boolValue(String command, String location) {
+		Result<IValue> result = evaluator.eval(null, command,
+				URI.create(location));
+		return ((IBool) (result.getValue())).getValue();
+	}
+
+	/**
+	 * @param command
+	 * @return result of Rascal Command evaluation which has type boolean
+	 */
+	public boolean boolValue(String command) {
+		return boolValue(command, "stdin:///");
+	}
+
+	private Object javaObject(IValue v) {
+		if (v.getType().isBoolType())
+			return new Boolean(((IBool) v).getValue());
+		if (v.getType().isIntegerType())
+			return new Integer(((IInteger) v).intValue());
+		if (v.getType().isStringType())
+			return ((IString) v).getValue();
+		if (v.getType().isListType())
+			return _listValue((IList) v);
+		return null;
+	}
+
+	private IValue rascalObject(Object v) {
+		if (v == null)
+			return null;
+		if (v instanceof Integer)
+			return vf.integer(((Integer) v).intValue());
+		if (v instanceof String)
+			return vf.string(((String) v));
+		if (v instanceof Boolean)
+			return vf.bool(((Boolean) v).booleanValue());
+		return null;
+	}
+
+	private Object[] _listValue(IList q) {
+		ArrayList<Object> r = new ArrayList<Object>();
+		for (IValue v : q) {
+			r.add(javaObject(v));
+		}
+		return r.toArray(new Object[] {});
+	}
+
+	public Object[] listValue(String command, String location) {
+		Result<IValue> result = evaluator.eval(null, command,
+				URI.create(location));
+		return _listValue((IList) (result.getValue()));
+	}
+
+	/**
+	 * @param command
+	 * @return result of Rascal Command evaluation which has type Object[]
+	 */
+	public Object[] listValue(String command) {
+		return listValue(command, "stdin:///");
+	}
+
+	public Object eval(String command, String location) {
+		Result<IValue> result = evaluator.eval(null, command,
+				URI.create(location));
+		if (result.getType().isBoolType())
+			return new Boolean(((IBool) (result.getValue())).getValue());
+		if (result.getType().isIntegerType())
+			return new Integer(((IInteger) (result.getValue())).intValue());
+		if (result.getType().isStringType())
+			return ((IString) (result.getValue())).getValue();
+		if (result.getType().isVoidType())
+			return null;
+		if (result.getType().isListType()) {
+			return _listValue((IList) (result.getValue()));
+		}
+		return result;
+	}
+
+	/**
+	 * @param command
+	 * @return result of Rascal Command evaluation which can have type: Integer,
+	 *         Bool, String or Object[],
+	 */
+	public Object eval(String command) {
 		return eval(command, "stdin:///");
 	}
 
@@ -96,7 +210,7 @@ public class JavaToRascal {
 			evaluator.unwind(old);
 		}
 	}
-	
+
 	public boolean isStringInModule(String moduleName, String procedureName) {
 		Environment old = evaluator.getCurrentEnvt();
 		try {
@@ -118,9 +232,9 @@ public class JavaToRascal {
 	public static void main(String[] args) {
 		final JavaToRascal jr = new JavaToRascal(new PrintWriter(System.out),
 				new PrintWriter(System.err));
-		System.out.println(jr.eval("import List;"));
-		System.out.println(jr.eval("\"<2+3>\";"));
-		System.out.println(jr.eval("\"aap:<size([2,3])>\";"));
+		jr.voidValue("import List;");
+		System.out.println(jr.stringValue("\"<2+3>\";"));
+		System.out.println(jr.stringValue("\"aap:<size([2,3])>\";"));
 		final IInteger d1 = vf.integer(1), d2 = vf.integer(2);
 		final IList l = vf.list(d1, d2);
 		System.out.println(jr.call("size", l));
