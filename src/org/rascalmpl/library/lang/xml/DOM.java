66ae5e01dd59be660a56776c2eedd57e1d3939e9
Paul Klint <Paul.Klint@cwi.nl>
2012-04-03 13:55:50 +0000

    Rmoved outdated IO module. Removed unnecessary functions form DOM module.

--- src/org/rascalmpl/library/lang/xml/DOM.java
+++ src/org/rascalmpl/library/lang/xml/DOM.java
@@ -14,9 +14,6 @@ package org.rascalmpl.library.lang.xml;
 
 import java.io.CharArrayReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
 
@@ -24,7 +21,6 @@ import org.eclipse.imp.pdb.facts.IConstructor;
 import org.eclipse.imp.pdb.facts.IInteger;
 import org.eclipse.imp.pdb.facts.IList;
 import org.eclipse.imp.pdb.facts.IListWriter;
-import org.eclipse.imp.pdb.facts.ISourceLocation;
 import org.eclipse.imp.pdb.facts.IString;
 import org.eclipse.imp.pdb.facts.IValue;
 import org.eclipse.imp.pdb.facts.IValueFactory;
@@ -42,7 +38,6 @@ import org.jdom.Text;
 import org.jdom.input.SAXBuilder;
 import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
-import org.rascalmpl.interpreter.IEvaluatorContext;
 import org.rascalmpl.values.xml.Factory;
 
 public class DOM {
@@ -55,15 +50,6 @@ public class DOM {
 	private static class Skip extends Exception {
 		private static final long serialVersionUID = -6330585199877497106L;
 	}
-
-	public IConstructor readXMLDOMTrim(ISourceLocation file, IEvaluatorContext ctx) throws IOException, JDOMException {
-		return readXMLDOM(file, true, ctx);
-	}
-
-	public IConstructor readXMLDOM(ISourceLocation file, IEvaluatorContext ctx) throws IOException, JDOMException {
-		return readXMLDOM(file, false, ctx);
-	}
-	
 	
 	public IConstructor parseXMLDOMTrim(IString str) throws IOException, JDOMException {
 		return parseXMLDOM(str, true);
@@ -72,8 +58,6 @@ public class DOM {
 	public IConstructor parseXMLDOM(IString str) throws IOException, JDOMException {
 		return parseXMLDOM(str, false);
 	}
-
-
 	
 	public IString xmlRaw(IConstructor node) throws IOException {
 		return xmlToString(node, Format.getRawFormat());
@@ -87,38 +71,11 @@ public class DOM {
 		return xmlToString(node, Format.getCompactFormat());
 	}
 	
-	public void writeXMLRaw(ISourceLocation file, IConstructor node, IEvaluatorContext ctx) throws IOException {
-		writeXML(file, node, Format.getRawFormat(), ctx);
-	}
-	
-	public void writeXMLPretty(ISourceLocation file, IConstructor node, IEvaluatorContext ctx) throws IOException {
-		writeXML(file, node, Format.getPrettyFormat(), ctx);
-	}
-
-	public void writeXMLCompact(ISourceLocation file, IConstructor node, IEvaluatorContext ctx) throws IOException {
-		writeXML(file, node, Format.getCompactFormat(), ctx);
-	}
-	
-	
-	private IConstructor readXMLDOM(ISourceLocation file, boolean trim, IEvaluatorContext ctx) throws IOException, JDOMException {
-		SAXBuilder builder = new SAXBuilder();
-		InputStream stream = ctx.getResolverRegistry().getInputStream(file.getURI());
-		Document doc = builder.build(stream);
-		return convertDocument(doc, trim);
-	}
-	
 	private IConstructor parseXMLDOM(IString str, boolean trim) throws JDOMException, IOException {
 		SAXBuilder builder = new SAXBuilder();
 		CharArrayReader reader = new CharArrayReader(str.getValue().toCharArray());
 		Document doc = builder.build(reader);
 		return convertDocument(doc, trim);
-	}
-	
-
-	
-	private void writeXML(ISourceLocation file, IConstructor node, Format format, IEvaluatorContext ctx) throws IOException {
-		OutputStream stream = ctx.getResolverRegistry().getOutputStream(file.getURI(), false);
-		writeXML(new PrintWriter(stream), nodeToDocument(node), format);
 	}
 	
 	private IString xmlToString(IConstructor node, Format format) throws IOException {
