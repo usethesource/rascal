ce08216849cacf6a463b73aca14fc6b09e0c0a9e
Wietse Venema <wietsevenema@gmail.com>
2012-07-10 18:48:02 +0000

    Refactoring

--- src/org/rascalmpl/interpreter/result/OverloadedFunction.java
+++ src/org/rascalmpl/interpreter/result/OverloadedFunction.java
@@ -10,7 +10,8 @@
  *   * Jurgen J. Vinju - Jurgen.Vinju@cwi.nl - CWI
  *   * Mark Hills - Mark.Hills@cwi.nl (CWI)
  *   * Arnold Lankamp - Arnold.Lankamp@cwi.nl
-*******************************************************************************/
+ *   * Wietse Venema - wietsevenema@gmail.com - CWI
+ *******************************************************************************/
 package org.rascalmpl.interpreter.result;
 
 import java.util.ArrayList;
@@ -38,7 +39,7 @@ import org.rascalmpl.interpreter.staticErrors.UnguardedFailError;
 
 public class OverloadedFunction extends Result<IValue> implements IExternalValue, ICallableValue {
 	private final static TypeFactory TF = TypeFactory.getInstance();
-	
+
 	private final List<AbstractFunction> primaryCandidates; // it should be a list to allow proper shadowing
 	private final List<AbstractFunction> defaultCandidates; // it should be a list to allow proper shadowing
 	private final String name;
@@ -46,51 +47,51 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 
 	public OverloadedFunction(String name, Type type, List<AbstractFunction> candidates, List<AbstractFunction> defaults, IEvaluatorContext ctx) {
 		super(type, null, ctx);
-		
+
 		if (candidates.size() + defaults.size() <= 0) {
 			throw new ImplementationError("at least need one function");
 		}
 		this.name = name;
-		
+
 		this.primaryCandidates = new ArrayList<AbstractFunction>(candidates.size());
 		this.defaultCandidates = new ArrayList<AbstractFunction>(candidates.size());
-		
+
 		addAll(primaryCandidates, candidates, true);
 		addAll(defaultCandidates, defaults, false);
 
-	    isStatic = checkStatic(primaryCandidates) && checkStatic(defaultCandidates);
+		isStatic = checkStatic(primaryCandidates) && checkStatic(defaultCandidates);
 	}
-	
+
 	public OverloadedFunction(AbstractFunction function) {
 		super(function.getType(), null, function.getEval());
 		this.name = function.getName();
-		
+
 		this.primaryCandidates = new ArrayList<AbstractFunction>(1);
 		this.defaultCandidates = new ArrayList<AbstractFunction>(1);
-		
+
 		if (function.isDefault()) {
 			defaultCandidates.add(function);
 		}
 		else {
 			primaryCandidates.add(function);
 		}
-		
+
 		this.isStatic = function.isStatic();
 	}
-	
+
 	public OverloadedFunction(String name, List<AbstractFunction> funcs) {
 		super(lub(funcs), null, funcs.iterator().next().getEval());
 		this.name = name;
-		
+
 		this.primaryCandidates = new ArrayList<AbstractFunction>(1);
 		this.defaultCandidates = new ArrayList<AbstractFunction>(1);
-		
+
 		addAll(primaryCandidates, funcs, true);
 		addAll(defaultCandidates, funcs, false);
-		
+
 		this.isStatic = checkStatic(funcs);
 	}
-	
+
 	/**
 	 * This function groups  occurrences of pattern dispatched functions as one "PatternFunction" that
 	 * has a hash table to look up based on outermost function symbol. A group is a bunch
@@ -102,8 +103,8 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 		Map<String, List<AbstractFunction>> constructors = new HashMap<String,List<AbstractFunction>>();
 		Map<IConstructor, List<AbstractFunction>> productions = new HashMap<IConstructor, List<AbstractFunction>>();
 		List<AbstractFunction> other = new LinkedList<AbstractFunction>();
-		
-		
+
+
 		for (AbstractFunction func : candidates) {
 			if (nonDefault && func.isDefault()) {
 				continue;
@@ -111,10 +112,10 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 			if (!nonDefault && !func.isDefault()) {
 				continue;
 			}
-			
+
 			String label = null;
 			IConstructor prod = null;
-			
+
 			if (func.isPatternDispatched()) {
 				// this one is already hashed, but we might find more to add to that map in the next round
 				Map<String, List<AbstractFunction>> funcMap = ((AbstractPatternDispatchedFunction) func).getMap();
@@ -133,7 +134,7 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 				// a new function definition, that may be hashable
 				label = func.getFirstOutermostConstructorLabel();
 				prod = func.getFirstOutermostProduction();
-				
+
 				if (label != null) {
 					// we found another one to hash
 					addFuncToMap(constructors, func, label);
@@ -146,7 +147,7 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 				}
 			}
 		}
-		
+
 		if (!constructors.isEmpty()) {
 			container.add(new AbstractPatternDispatchedFunction(ctx.getEvaluator(), type, constructors));
 		}
@@ -165,7 +166,7 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 		}
 		l.add(func);
 	}
-	
+
 	private void addProdToMap(Map<IConstructor, List<AbstractFunction>> map,
 			AbstractFunction func, IConstructor label) {
 		List<AbstractFunction> l = map.get(label);
@@ -175,7 +176,7 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 		}
 		l.add(func);
 	}
-	
+
 	private void addFuncsToMap(Map<String, List<AbstractFunction>> map,
 			List<AbstractFunction> funcs, String label) {
 		List<AbstractFunction> l = map.get(label);
@@ -185,7 +186,7 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 		}
 		l.addAll(funcs);
 	}
-	
+
 	private void addProdsToMap(Map<IConstructor, List<AbstractFunction>> map,
 			List<AbstractFunction> funcs, IConstructor label) {
 		List<AbstractFunction> l = map.get(label);
@@ -200,14 +201,14 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 	public int getArity() {
 		throw new UnsupportedOperationException();
 	}
-	
+
 	@Override
 	public boolean hasVarArgs() {
 		throw new UnsupportedOperationException();
 	}
-	
-	
-	
+
+
+
 	@Override
 	public boolean isStatic() {
 		return isStatic;
@@ -221,7 +222,7 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 		}
 		return true;
 	}
-	
+
 	@Override
 	public IValue getValue() {
 		return this;
@@ -229,14 +230,15 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 
 	private static Type lub(List<AbstractFunction> candidates) {
 		Type lub = TF.voidType();
-		
+
 		for (AbstractFunction l : candidates) {
 			lub = lub.lub(l.getType());
 		}
-		
+
 		return lub;
 	}
 
+	@Override
 	public Result<IValue> call(IRascalMonitor monitor, Type[] argTypes,
 			IValue[] argValues) {
 		IRascalMonitor old = ctx.getEvaluator().setMonitor(monitor);
@@ -247,11 +249,11 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 			ctx.getEvaluator().setMonitor(old);
 		}
 	}
-	
+
 	@Override 
 	public Result<IValue> call(Type[] argTypes, IValue[] argValues) {
 		Result<IValue> result = callWith(primaryCandidates, argTypes, argValues, defaultCandidates.size() <= 0);
-		
+
 		if (result == null && defaultCandidates.size() > 0) {
 			result = callWith(defaultCandidates, argTypes, argValues, true);
 		}
@@ -262,14 +264,14 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 			all.addAll(defaultCandidates);
 			throw new ArgumentsMismatchError(name, all, argTypes, ctx.getCurrentAST());
 		}
-		
+
 		return result;
 	}
 
 	private static Result<IValue> callWith(List<AbstractFunction> candidates, Type[] argTypes, IValue[] argValues, boolean mustSucceed) {
 		AbstractFunction failed = null;
 		Failure failure = null;
-		
+
 		for (AbstractFunction candidate : candidates) {
 			if ((candidate.hasVarArgs() && argValues.length >= candidate.getArity() - 1)
 					|| candidate.getArity() == argValues.length) {
@@ -286,52 +288,52 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 				}
 			}
 		}
-		
+
 		if (failed != null && mustSucceed) {
 			throw new UnguardedFailError(failed.ast, failure);
 		}
-		
+
 		return null;
 	}
-	
+
 	public OverloadedFunction join(OverloadedFunction other) {
 		if (other == null) {
 			return this;
 		}
 		List<AbstractFunction> joined = new ArrayList<AbstractFunction>(other.primaryCandidates.size() + primaryCandidates.size());
 		List<AbstractFunction> defJoined = new ArrayList<AbstractFunction>(other.defaultCandidates.size() + defaultCandidates.size());
-		
+
 		joined.addAll(primaryCandidates);
 		defJoined.addAll(defaultCandidates);
-		
+
 		for (AbstractFunction cand : other.primaryCandidates) {
 			if (!joined.contains(cand)) {
 				joined.add(cand);
 			}
 		}
-		
+
 		for (AbstractFunction cand : other.defaultCandidates) {
 			if (!defJoined.contains(cand)) {
 				defJoined.add(cand);
 			}
 		}
-		
+
 		return new OverloadedFunction(name, lub(joined).lub(lub(defJoined)), joined, defJoined, ctx);
 	}
-	
+
 	public OverloadedFunction add(AbstractFunction candidate) {
 		List<AbstractFunction> joined = new ArrayList<AbstractFunction>(primaryCandidates.size() + 1);
 		joined.addAll(primaryCandidates);
 		List<AbstractFunction> defJoined = new ArrayList<AbstractFunction>(defaultCandidates.size() + 1);
 		defJoined.addAll(defaultCandidates);
-		
+
 		if (candidate.isDefault() && !defJoined.contains(candidate)) {
 			defJoined.add(candidate);
 		}
 		else if (!candidate.isDefault() && !joined.contains(candidate)) {
 			joined.add(candidate);
 		}
-		
+
 		return new OverloadedFunction(name, lub(joined).lub(lub(defJoined)), joined, defJoined, ctx);
 	}
 
@@ -340,13 +342,13 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 		if (obj instanceof OverloadedFunction) {
 			OverloadedFunction other = (OverloadedFunction) obj;
 			return primaryCandidates.containsAll(other.primaryCandidates)
-			&& other.primaryCandidates.containsAll(primaryCandidates)
-			&& defaultCandidates.containsAll(other.defaultCandidates)
-			&& other.defaultCandidates.containsAll(defaultCandidates);
+					&& other.primaryCandidates.containsAll(primaryCandidates)
+					&& defaultCandidates.containsAll(other.defaultCandidates)
+					&& other.defaultCandidates.containsAll(defaultCandidates);
 		}
 		return false;
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder b = new StringBuilder();
@@ -358,96 +360,98 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 			b.append(l.toString());
 			b.append(' ');
 		}
-		
+
 		return b.toString();
 	}
 
+	@Override
 	public <T> T accept(IValueVisitor<T> v) throws VisitorException {
 		return v.visitExternal(this);
 	}
 
+	@Override
 	public boolean isEqual(IValue other) {
 		if (other instanceof OverloadedFunction) {
 			return primaryCandidates.equals(((OverloadedFunction) other).primaryCandidates);
 		}
 		return false;
 	}
-	
+
 	@Override
 	public <U extends IValue, V extends IValue> Result<U> equals(
 			Result<V> that) {
 		return that.equalToOverloadedFunction(this);
 	}
-	
+
 	@Override
 	public <U extends IValue> Result<U> equalToOverloadedFunction(
 			OverloadedFunction that) {
 		return ResultFactory.bool(primaryCandidates.equals(that.primaryCandidates) && defaultCandidates.equals(that.defaultCandidates), ctx);
 	}
-	
+
 	@Override
 	public <U extends IValue, V extends IValue> Result<U> compare(
 			Result<V> that) {
 		return that.compareOverloadedFunction(this);
 	}
-	
+
 	@Override
 	public <U extends IValue> Result<U> compareOverloadedFunction(
 			OverloadedFunction that) {
 		if (that == this) {
 			return ResultFactory.makeResult(TF.integerType(), getValueFactory().integer(0), ctx);
 		}
-		
+
 		if (primaryCandidates.size() > that.primaryCandidates.size()) {
 			return  ResultFactory.makeResult(TF.integerType(), getValueFactory().integer(1), ctx);
 		}
-		
+
 		if (primaryCandidates.size() < that.primaryCandidates.size()) {
-			 ResultFactory.makeResult(TF.integerType(), getValueFactory().integer(-1), ctx);
+			ResultFactory.makeResult(TF.integerType(), getValueFactory().integer(-1), ctx);
 		}
-		
+
 		for (AbstractFunction f : primaryCandidates) {
 			for (AbstractFunction g : that.primaryCandidates) {
 				Result<U> result = f.compare(g);
-				
+
 				if (!((IInteger) result.getValue()).getStringRepresentation().equals("0")) {
 					return result;
 				}
 			}
 		}
-		
+
 		if (defaultCandidates.size() > that.defaultCandidates.size()) {
 			return  ResultFactory.makeResult(TF.integerType(), getValueFactory().integer(1), ctx);
 		}
-		
+
 		if (defaultCandidates.size() < that.defaultCandidates.size()) {
-			 ResultFactory.makeResult(TF.integerType(), getValueFactory().integer(-1), ctx);
+			ResultFactory.makeResult(TF.integerType(), getValueFactory().integer(-1), ctx);
 		}
-		
+
 		for (AbstractFunction f : defaultCandidates) {
 			for (AbstractFunction g : that.defaultCandidates) {
 				Result<U> result = f.compare(g);
-				
+
 				if (!((IInteger) result.getValue()).getStringRepresentation().equals("0")) {
 					return result;
 				}
 			}
 		}
-		
+
 		return ResultFactory.makeResult(TF.integerType(), getValueFactory().integer(0), ctx);
 	}
-	
+
 	@Override
 	public <U extends IValue, V extends IValue> Result<U> compose(Result<V> right) {
 		return right.composeOverloadedFunction(this);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public <U extends IValue> Result<U> composeOverloadedFunction(OverloadedFunction that) {
 		List<AbstractFunction> newAlternatives = new ArrayList<AbstractFunction>(primaryCandidates.size());
-		
-		
+
+
 		for (AbstractFunction f : primaryCandidates) {
 			for (AbstractFunction g : that.primaryCandidates) {
 				if (getTypeFactory().tupleType(f.getReturnType()).isSubtypeOf(g.getFunctionType().getArgumentTypes())) {
@@ -455,9 +459,9 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 				}
 			}
 		}
-		
+
 		List<AbstractFunction> newDefaults = new ArrayList<AbstractFunction>(defaultCandidates.size());
-		
+
 		for (AbstractFunction f : defaultCandidates) {
 			for (AbstractFunction g : that.defaultCandidates) {
 				if (getTypeFactory().tupleType(f.getReturnType()).isSubtypeOf(g.getFunctionType().getArgumentTypes())) {
@@ -465,14 +469,14 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 				}
 			}
 		}
-		
+
 		if (newAlternatives.size() + newDefaults.size() == 0) {
 			return undefinedError("composition", that);
 		}
-		
+
 		return (Result<U>) new OverloadedFunction(name, getType(), newAlternatives, newDefaults, ctx);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public <U extends IValue> Result<U> composeFunction(AbstractFunction g) {
@@ -483,7 +487,7 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 				newAlternatives.add(new ComposedFunctionResult(f, g, ctx));
 			}
 		}
-		
+
 		List<AbstractFunction> newDefaults = new ArrayList<AbstractFunction>(defaultCandidates.size());
 
 		for (AbstractFunction f : defaultCandidates) {
@@ -491,35 +495,42 @@ public class OverloadedFunction extends Result<IValue> implements IExternalValue
 				newAlternatives.add(new ComposedFunctionResult(f, g, ctx));
 			}
 		}
-		
+
 		if (newAlternatives.size() == 0) {
 			return undefinedError("composition", g);
 		}
-		
+
 		return (Result<U>) new OverloadedFunction(name, getType(), newAlternatives, newDefaults, ctx);
 	}
-	
-	public List<AbstractFunction> getTests() {
+
+	public List<AbstractFunction> getFunctions(){
 		List<AbstractFunction> result = new LinkedList<AbstractFunction>();
 		for (AbstractFunction f : primaryCandidates) {
-			if (f.isTest()) {
-				result.add(f);
-			}
+			result.add(f);
 		}
-		
+
 		for (AbstractFunction f : defaultCandidates) {
-			if (f.isTest()) {
-				result.add(f);
-			}
+			result.add(f);
 		}
-		
+
 		return result;
 	}
-	
+
+	public List<AbstractFunction> getTests() {
+		List<AbstractFunction> result = new LinkedList<AbstractFunction>();
+		for (AbstractFunction f : getFunctions()) {
+			if (f.isTest()) {
+				result.add(f);
+			}
+		}
+		return result;
+	}
+
 	public String getName() {
 		return name;
 	}
-	
+
+	@Override
 	public Evaluator getEval(){
 		return (Evaluator) ctx;
 	}
