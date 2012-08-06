3a4665491a738f09de0d09bb56cefb2dddbb1207
Davy Landman <davy.landman@gmail.com>
2012-08-06 19:09:20 +0200

    Moved the unit test into subfolders to allow better selection, and remove the TestSuites

---
+++ src/org/rascalmpl/test/functionality/BackTrackingTests.java
@@ -0,0 +1,74 @@
+/*******************************************************************************
+ * Copyright (c) 2009-2011 CWI
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+
+ *   * Jurgen J. Vinju - Jurgen.Vinju@cwi.nl - CWI
+ *   * Paul Klint - Paul.Klint@cwi.nl - CWI
+*******************************************************************************/
+package org.rascalmpl.test.functionality;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.rascalmpl.test.TestFramework;
+
+public class BackTrackingTests extends TestFramework {
+	
+	@Test public void testSimple(){
+		assertTrue(runTest("int i <- [1,4] && int k := i && k >= 4;"));
+		assertTrue(runTest("int i <- [1,4] && int j <- [2,1] && int k := i + j && k >= 5;"));
+	}
+	
+	@Test public void testList() {
+		
+		assertTrue(runTest("{([list[int] L1, int N, list[int] L2] := [1,2,3,4]) && (N == 3) && " + 
+								"(L1 == [1,2]) && (N == 3) && (L2 == [4]);}"));
+		
+		assertTrue(runTest("{([list[int] L1, int N, list[int] L2] := [1,2,3,4]) && ((N == 3) || (N==4)) && " + 
+		"(L1 == [1,2]) && (N == 3) && (L2 == [4]);}"));
+		
+		assertTrue(runTest("{([list[int] L1, int N, list[int] L2] := [1,2,3,4]) && " +
+								"([list[int] L3, int M, list[int] L4] := [3,4]) && (N > M) && " + 
+							  "(N == 4);}"));
+
+		assertTrue(runTest("{[1, [list[int] P, int N, list[int] Q], 3] := [1, [1,2,3,2], 3];}"));
+		assertTrue(runTest("{[list[int] P, int N, list[int] Q]:= [1,2,3,2] && N > 1;}"));
+		
+		assertTrue(runTest("{[1, [list[int] P, int N, list[int] Q], 3] := [1, [10,20], 3] && N > 10;}"));
+		
+	}
+	
+	@Test public void testSet() {
+		
+		assertTrue(runTest("{({set[int] S1, int N} := {1,2,3,4}) && (N == 1);}"));
+		assertTrue(runTest("{({set[int] S1, int N} := {1,2,3,4}) && (N == 2);}"));
+		assertTrue(runTest("{({set[int] S1, int N} := {1,2,3,4}) && (N == 3);}"));
+		assertTrue(runTest("{({set[int] S1, int N} := {1,2,3,4}) && (N == 4);}"));
+
+		
+		assertTrue(runTest("{({set[int] S1, int N, set[int] S2} := {1,2,3,4}) && (N == 1);}"));
+		assertTrue(runTest("{({set[int] S1, int N, set[int] S2} := {1,2,3,4}) && (N == 2);}"));
+		assertTrue(runTest("{({set[int] S1, int N, set[int] S2} := {1,2,3,4}) && (N == 3);}"));
+		assertTrue(runTest("{({set[int] S1, int N, set[int] S2} := {1,2,3,4}) && (N == 4);}"));
+		
+		assertTrue(runTest("{{1, {set[int] S1, int N}, 3} := {1, {1,2,3}, 3};}"));
+		assertTrue(runTest("{{set[int] S1, int N}:= {1,2,3,2} && N == 1;}"));
+		assertTrue(runTest("{{set[int] S1, int N}:= {1,2,3,2} && N == 2;}"));
+		assertTrue(runTest("{{set[int] S1, int N}:= {1,2,3,2} && N == 3;}"));
+		
+		assertTrue(runTest("{{1, {set[int] S1, int N, set[int] S2}, 3} := {1, {1,2,3}, 3};}"));
+//		assertTrue(runTest("{{1, {set[int] S1, int N, set[int] S2}, 3} := {1, {1,2,3}, 3} && N == 1;}"));
+//		assertTrue(runTest("{{1, {set[int] S1, int N, set[int] S2}, 3} := {1, {1,2,3}, 3} && N == 2;}"));
+//		assertTrue(runTest("{{1, {set[int] S1, int N, set[int] S2}, 3} := {1, {1,2,3}, 3} && N == 3;}"));
+	}
+	
+	@Test public void and(){
+		assertTrue(runTest("int i <- [0, 1] && [\"a\",\"b\"][i] == \"a\";"));
+		assertTrue(runTest("int i <- [0, 1] && [\"a\",\"b\"][i] == \"b\";"));
+	}
+}
---
+++ src/org/rascalmpl/test/functionality/RecoveryTests.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2009-2011 CWI
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+
+ *   * Jurgen J. Vinju - Jurgen.Vinju@cwi.nl - CWI
+ *   * Tijs van der Storm - Tijs.van.der.Storm@cwi.nl
+*******************************************************************************/
+package org.rascalmpl.test.functionality;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.rascalmpl.test.TestFramework;
+
+public class RecoveryTests extends TestFramework {
+
+
+	@Before
+	public void prepare() {
+		prepare("import RecoveryTests;");
+	}
+	
+	@Test @Ignore
+	public void testRecoveryOfLocalVariable() {
+		assertTrue(runTestInSameEvaluator("recoveryOfLocalVariable() == 0;"));
+	}
+
+	@Test @Ignore
+	public void testNestedRecoveryOfLocalVariable() {
+		assertTrue(runTestInSameEvaluator("nestedRecoveryOfLocalVariable() == 3;"));
+	}
+
+	@Test @Ignore
+	public void testNoNestedRecovery() {
+		assertTrue(runTestInSameEvaluator("noNestedRecovery() == 12;"));
+	}
+
+	@Test @Ignore
+	public void testRecoveryOfLocalVariableUsingIfThen() {
+		assertTrue(runTestInSameEvaluator("recoveryOfLocalVariableUsingIfThen() == 0;"));
+	}
+
+	@Test @Ignore
+	public void testRecoveryOfGlobalVariable() {
+		assertTrue(runTestInSameEvaluator("recoveryOfGlobalVariable() == 0;"));
+	}
+
+	@Test @Ignore
+	public void testRecoveryAfterFailingRule() {
+		assertTrue(runTestInSameEvaluator("recoveryOfGlobalAfterFailingRule() == 0;"));
+	}
+	
+	@Test @Ignore
+	public void testRecoveryOfGlobalDuringComprehension() {
+		assertTrue(runTestInSameEvaluator("recoveryOfGlobalDuringComprehension() == 0;"));
+	}
+}
