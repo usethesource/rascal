e5d9dc644d45db2095394b06b610b5ad313907e1
Davy Landman <davy.landman@gmail.com>
2012-08-07 13:55:34 +0200

    Added library test for the permutations function

--- src/org/rascalmpl/test/library/ListTests.java
+++ src/org/rascalmpl/test/library/ListTests.java
@@ -154,6 +154,17 @@ public class ListTests extends TestFramework {
 		assertTrue(runTestInSameEvaluator("permutations([1,2,3]) ==  {[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]};"));
 
 	}
+	
+	@Test
+	public void distribution() {
+
+		prepare("import List;");
+
+		assertTrue(runTestInSameEvaluator("distribution([]) == ();"));
+		assertTrue(runTestInSameEvaluator("distribution([1]) == (1:1);"));
+		assertTrue(runTestInSameEvaluator("distribution([1,2]) == (1:1, 2:1);"));
+		assertTrue(runTestInSameEvaluator("distribution([1,2, 2]) == (1:1, 2:2);"));
+	}
 
 	@Test
 	public void reducer() {
