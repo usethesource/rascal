93f2db82882b0395cafb7945708743c353a5e66d
Arnold Lankamp <arnoldlankamp@Arnold-Lankamps-MacBook-Pro.local>
2012-09-07 08:39:39 +0200

    -Corrected license header. -Added missing license header.

--- src/org/rascalmpl/parser/uptr/recovery/Recoverer.java
+++ src/org/rascalmpl/parser/uptr/recovery/Recoverer.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2009-2011 CWI
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+
+ *   * Jurgen J. Vinju - Jurgen.Vinju@cwi.nl - CWI
+ *   * Arnold Lankamp - Arnold.Lankamp@cwi.nl
+*******************************************************************************/
 package org.rascalmpl.parser.uptr.recovery;
 
 import org.rascalmpl.parser.gtd.recovery.IRecoverer;
