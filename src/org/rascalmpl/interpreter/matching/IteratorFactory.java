adecd7691c4a1f7d2299c22988c6887bae679d38
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2012-10-01 15:01:58 +0200

    fixed bug in iteratorfactory regarding rationals (fix by Bert Lisser)

--- src/org/rascalmpl/interpreter/matching/IteratorFactory.java
+++ src/org/rascalmpl/interpreter/matching/IteratorFactory.java
@@ -151,6 +151,7 @@ public class IteratorFactory {
 				subjectType.isRealType() ||
 				subjectType.isStringType() ||
 				subjectType.isSourceLocationType() ||
+				subjectType.isRationalType() ||
 				subjectType.isDateTimeType())
 		{
 			if (shallow) {
