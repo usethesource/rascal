07c81f0f35d57c1709bdc408f3bded89c7ad96bc
Davy Landman <davy.landman@gmail.com>
2012-11-20 08:40:32 +0100

    Refactored the InputStreamReader to determine the encoding only on the first read.
    This moves the possible IO exceptions to the read instead of the constructor

---
+++ src/org/rascalmpl/unicode/ConcatInputStream.java
@@ -0,0 +1,57 @@
+package org.rascalmpl.unicode;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+class ConcatInputStream extends InputStream {
+	private final InputStream first;
+	private boolean firstEmpty;
+	private final InputStream second;
+
+	public ConcatInputStream(InputStream first, InputStream second) {
+		this.first = first;
+		this.firstEmpty = false;
+		this.second = second;
+	}
+
+	@Override
+	public int read() throws IOException {
+		if (!firstEmpty) {
+			int firstResult = first.read();
+			if (firstResult != -1)  {
+				return firstResult;
+			} 
+			else {
+				firstEmpty = true;
+			}
+		}
+		return second.read();
+	}
+	
+	@Override
+	public int read(byte[] b, int off, int len) throws IOException {
+		// not strictly needed, but can speedup the concatting
+		if (!firstEmpty) {
+			int readTotal = first.read(b, off, len);
+			if (readTotal == len) {
+				return readTotal;
+			}
+			else {
+				if (readTotal < 0)
+					readTotal = 0;
+				firstEmpty = true;
+				int secondRead = second.read(b, off + readTotal, len - readTotal);
+				if (readTotal == 0 && secondRead <= 0) {
+					return -1;
+				}
+				if (secondRead < 0)
+					return readTotal;
+				else
+					return readTotal + secondRead;
+			}
+			
+		}
+		return second.read(b, off, len);
+	}
+	
+}
