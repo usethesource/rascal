cbcc77c867180edaf85699636ba20aff7e0940ab
paulklint <Paul.Klint@cwi.nl>
2013-01-10 00:56:19 +0100

    Improved message

--- src/org/rascalmpl/interpreter/staticErrors/NoKeywordParameters.java
+++ src/org/rascalmpl/interpreter/staticErrors/NoKeywordParameters.java
@@ -4,7 +4,7 @@ import org.rascalmpl.ast.AbstractAST;
 
 public class NoKeywordParameters extends StaticError {
 	public NoKeywordParameters(String name, AbstractAST ast) {
-		super("constructor function " + name + " cannot be called with keyword parameters", ast);
+		super("function " + name + " cannot be called with keyword parameters", ast);
 	}
 
 	private static final long serialVersionUID = 7512965714991339935L;
