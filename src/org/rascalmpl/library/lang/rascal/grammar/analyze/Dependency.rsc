f57d7c6ec50ab597e46622ae0a9f2c798ab82a94
grammarware <vadim@grammarware.net>
2013-01-16 18:04:50 +0100

    issue #110: adding the license info to many Rascal and Java files

--- src/org/rascalmpl/library/lang/rascal/grammar/analyze/Dependency.rsc
+++ src/org/rascalmpl/library/lang/rascal/grammar/analyze/Dependency.rsc
@@ -1,3 +1,11 @@
+@license{
+  Copyright (c) 2011-2013 CWI
+  All rights reserved. This program and the accompanying materials
+  are made available under the terms of the Eclipse Public License v1.0
+  which accompanies this distribution, and is available at
+  http://www.eclipse.org/legal/epl-v10.html
+}
+@contributor{Jurgen J. Vinju - Jurgen.Vinju@cwi.nl - CWI}
 module lang::rascal::grammar::analyze::Dependency
 
 import Grammar;
--- src/org/rascalmpl/parser/gtd/util/ForwardLink.java
+++ src/org/rascalmpl/parser/gtd/util/ForwardLink.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2013 CWI
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+
+ *   * Arnold Lankamp - Arnold.Lankamp@cwi.nl
+*******************************************************************************/
 package org.rascalmpl.parser.gtd.util;
 
 @SuppressWarnings({"rawtypes","unchecked"})
