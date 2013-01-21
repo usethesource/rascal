6e6fe078c3eae2ecc0bcd01d46a81b76993a053f
Anya Helene Bagge <anya@ii.uib.no>
2013-01-21 23:38:32 +0100

    Bah, @Override problems

--- src/org/rascalmpl/library/resource/jdbc/JDBC.java
+++ src/org/rascalmpl/library/resource/jdbc/JDBC.java
@@ -889,31 +889,31 @@ class DriverShim implements Driver {
 	DriverShim(Driver d) {
 		this.driver = d;
 	}
-	@Override
+	
 	public boolean acceptsURL(String u) throws SQLException {
 		return this.driver.acceptsURL(u);
 	}
-	@Override
+
 	public Connection connect(String u, Properties p) throws SQLException {
 		return this.driver.connect(u, p);
 	}
-	@Override
+
 	public int getMajorVersion() {
 		return this.driver.getMajorVersion();
 	}
-	@Override
+
 	public int getMinorVersion() {
 		return this.driver.getMinorVersion();
 	}
-	@Override
+
 	public DriverPropertyInfo[] getPropertyInfo(String u, Properties p) throws SQLException {
 		return this.driver.getPropertyInfo(u, p);
 	}
-	@Override
+
 	public boolean jdbcCompliant() {
 		return this.driver.jdbcCompliant();
 	}
-	@Override
+
 	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
 		// TODO Auto-generated method stub
 		return null;
