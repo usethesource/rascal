a6e7032970921b2fce7cba686cab8f78ecc2a7fa
Ali Afroozeh <ali.afroozeh@cwi.nl>
2013-02-07 00:47:48 +0100

    GrammarToJigll updated to work with the new parser interface. Compiles, but needs testing.

--- src/org/rascalmpl/parser/GrammarToJigll.java
+++ src/org/rascalmpl/parser/GrammarToJigll.java
@@ -1,10 +1,14 @@
 package org.rascalmpl.parser;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.imp.pdb.facts.IConstructor;
 import org.eclipse.imp.pdb.facts.IInteger;
@@ -14,119 +18,134 @@ import org.eclipse.imp.pdb.facts.ISet;
 import org.eclipse.imp.pdb.facts.IString;
 import org.eclipse.imp.pdb.facts.IValue;
 import org.eclipse.imp.pdb.facts.IValueFactory;
-import org.jgll.grammar.Character;
+import org.jgll.grammar.BodyGrammarSlot;
 import org.jgll.grammar.CharacterClass;
+import org.jgll.grammar.EpsilonGrammarSlot;
 import org.jgll.grammar.Grammar;
-import org.jgll.grammar.GrammarSlot;
 import org.jgll.grammar.LastGrammarSlot;
 import org.jgll.grammar.Nonterminal;
 import org.jgll.grammar.NonterminalGrammarSlot;
 import org.jgll.grammar.Range;
+import org.jgll.grammar.Rule;
+import org.jgll.grammar.Symbol;
+import org.jgll.grammar.Terminal;
 import org.jgll.grammar.TerminalGrammarSlot;
 
 public class GrammarToJigll {
-  public GrammarToJigll(IValueFactory vf) {
-  }
-  
-  public void generate(IString name, IConstructor grammar) {
-    IConstructor start = null;
-    for (IValue st : (ISet) grammar.get("starts")) {
-      start = (IConstructor) st;
-    }
-    
-    Grammar g = convert(name.getValue(), start, grammar);
-    
-    for (Nonterminal n : g.getNonterminals()) {
-      System.err.println(n.code());
-    }
-  }
-  
-  static public Grammar convert(String name, IConstructor start, IConstructor grammar) {
-    IMap definitions = (IMap) grammar.get("rules");
-    Map<IValue, Nonterminal> nonterminals = new HashMap<IValue, Nonterminal>();
-    List<GrammarSlot> slots = new ArrayList<>();
+	
+	public GrammarToJigll(IValueFactory vf) {
+	}
 
-    int id = 0;
-    for (IValue nonterminal : definitions) {
-      nonterminals.put(nonterminal, new Nonterminal(id, nonterminal.toString(), false));
-    }
+	public void generate(IString name, IConstructor grammar) {
 
-    for (IValue nonterminal : definitions) {
-      IConstructor choice = (IConstructor) definitions.get(nonterminal);
-      convertNonterminal(nonterminals, slots, nonterminal, choice);
-    }
+		Grammar g = convert(name.getValue(), grammar);
 
-    return new Grammar(name, new ArrayList<>(nonterminals.values()), slots, nonterminals.get(start));
-  }
+		try (StringWriter out = new StringWriter()) {
+			g.code(out, "test");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-  static private void convertNonterminal(Map<IValue, Nonterminal> nonterminals, List<GrammarSlot> slots, IValue nonterminal,
-      IConstructor choice) {
-    assert choice.getName().equals("choice");
-    Nonterminal head = nonterminals.get(nonterminal);
-    ISet alts = (ISet) choice.get("alternatives");
+	static public Grammar convert(String name, IConstructor grammar) {
+		IMap definitions = (IMap) grammar.get("rules");
+		Map<IValue, Nonterminal> nonterminals = new HashMap<IValue, Nonterminal>();
+		List<BodyGrammarSlot> slots = new ArrayList<>();
 
-    for (IValue alt : alts) {
-      IConstructor prod = (IConstructor) alt;
-      convertProduction(nonterminals, slots, head, prod);
-    }
-  }
+		int id = 0;
+		for (IValue nonterminal : definitions) {
+			nonterminals.put(nonterminal, new Nonterminal(id, nonterminal.toString(), false));
+		}
 
-  static private void convertProduction(Map<IValue, Nonterminal> nonterminals, List<GrammarSlot> slots, Nonterminal head,
-      IConstructor prod) {
-    assert prod.getName().equals("prod");
-    GrammarSlot slot = null;
+		for (IValue nonterminal : definitions) {
+			IConstructor choice = (IConstructor) definitions.get(nonterminal);
+			convertNonterminal(nonterminals, slots, nonterminal, choice);
+		}
+		
+		Set<Nonterminal> startSymbols = new HashSet<>();
+		for (IValue st : (ISet) grammar.get("starts")) {
+			startSymbols.add(nonterminals.get(st));
+		}
 
-    IList rhs = (IList) prod.get("symbols");
+		return new Grammar(name, new ArrayList<>(nonterminals.values()), slots, startSymbols);
+	}
 
-    if (rhs.length() == 0) { // epsilon
-      convertEpsilonProduction(nonterminals, slots, head, prod, slot);
-    } else {
-      convertNonEpsilonProduction(nonterminals, slots, head, prod, slot, rhs);
-    }
-  }
+	static private void convertNonterminal(Map<IValue, Nonterminal> nonterminals, List<BodyGrammarSlot> slots, IValue nonterminal, IConstructor choice) {
+		assert choice.getName().equals("choice");
+		Nonterminal head = nonterminals.get(nonterminal);
+		ISet alts = (ISet) choice.get("alternatives");
 
-  static  private void convertNonEpsilonProduction(Map<IValue, Nonterminal> nonterminals, List<GrammarSlot> slots,
-      Nonterminal head, IConstructor prod, GrammarSlot slot, IList rhs) {
-    int symId = 0;
-    for (IValue elem : rhs) {
-      IConstructor symbol = (IConstructor) elem;
-      switch (symbol.getName()) {
-      case "char-class":
-        List<Range> targetRanges = buildRanges(symbol);
-        slot = new TerminalGrammarSlot(slots.size() + nonterminals.size(), prod.toString(), symId++, slot,
-            new CharacterClass(targetRanges));
-      default:
-        slot = new NonterminalGrammarSlot(slots.size() + nonterminals.size(), prod.toString(), symId++, slot,
-            nonterminals.get(symbol), null);
-      }
+		for (IValue alt : alts) {
+			IConstructor prod = (IConstructor) alt;
+			convertProduction(nonterminals, slots, head, prod);
+		}
+	}
 
-      if (symId == 0) {
-        slots.add(slot);
-        head.addAlternate(slot);
-      }
+	static private void convertProduction(Map<IValue, Nonterminal> nonterminals, List<BodyGrammarSlot> slots, Nonterminal head, IConstructor prod) {
+		assert prod.getName().equals("prod");
+		BodyGrammarSlot slot = null;
 
-      slots.add(new LastGrammarSlot(slots.size() + nonterminals.size(), prod.toString(), rhs.length(), head, slot));
-    }
-  }
+		IList rhs = (IList) prod.get("symbols");
+		List<Symbol> body = getSymbolList(rhs, nonterminals);
+		Rule rule = new Rule(head, body);
 
-  static private void convertEpsilonProduction(Map<IValue, Nonterminal> nonterminals, List<GrammarSlot> slots,
-      Nonterminal head, IConstructor prod, GrammarSlot slot) {
-    slot = new TerminalGrammarSlot(slots.size() + nonterminals.size(), prod.toString(), 0, slot, new Character(
-        (char) -1));
-    slots.add(slot);
-    head.addAlternate(slot);
-  }
+		if (rhs.length() == 0) { // epsilon
+			convertEpsilonProduction(nonterminals, slots, rule, slot);
+		} else {
+			convertNonEpsilonProduction(nonterminals, slots, rule, slot);
+		}
+	}
 
-  static private List<Range> buildRanges(IConstructor symbol) {
-    List<Range> targetRanges = new LinkedList<Range>();
-    IList ranges = (IList) symbol.get("ranges");
-    for (IValue r : ranges) {
-      IConstructor range = (IConstructor) r;
-      int begin = ((IInteger) range.get("begin")).intValue();
-      int end = ((IInteger) range.get("end")).intValue();
-      targetRanges.add(new Range(begin, end));
-    }
-    return targetRanges;
-  }
+	static private void convertNonEpsilonProduction(Map<IValue, Nonterminal> nonterminals, List<BodyGrammarSlot> slots, Rule rule, BodyGrammarSlot slot) {
+		int symId = 0;
+		for (Symbol s : rule.getBody()) {
+			if(s instanceof Terminal) {
+				slot = new TerminalGrammarSlot(rule, slots.size() + nonterminals.size(), symId++, slot, (Terminal) s);
+			} else {
+				slot = new NonterminalGrammarSlot(rule, slots.size() + nonterminals.size(), symId++, slot, (Nonterminal) s, null);
+			}
+
+			if (symId == 0) {
+				slots.add(slot);
+				rule.getHead().addAlternate(slot);
+			}
+
+			slots.add(new LastGrammarSlot(rule, slots.size() + nonterminals.size(), rule.getBody().size(), slot));
+		}
+	}
+
+	static private void convertEpsilonProduction(Map<IValue, Nonterminal> nonterminals, List<BodyGrammarSlot> slots, Rule rule, BodyGrammarSlot slot) {
+		slot = new EpsilonGrammarSlot(rule, slots.size() + nonterminals.size(), 0, slot, null);
+		slots.add(slot);
+		rule.getHead().addAlternate(slot);
+	}
+
+	static private List<Range> buildRanges(IConstructor symbol) {
+		List<Range> targetRanges = new LinkedList<Range>();
+		IList ranges = (IList) symbol.get("ranges");
+		for (IValue r : ranges) {
+			IConstructor range = (IConstructor) r;
+			int begin = ((IInteger) range.get("begin")).intValue();
+			int end = ((IInteger) range.get("end")).intValue();
+			targetRanges.add(new Range(begin, end));
+		}
+		return targetRanges;
+	}
+	
+	
+	private static List<Symbol> getSymbolList(IList rhs, Map<IValue, Nonterminal> nonterminals) {
+		List<Symbol> result = new ArrayList<>();
+		for (IValue elem : rhs) {
+			IConstructor symbol = (IConstructor) elem;
+			switch (symbol.getName()) {
+			case "char-class":
+				List<Range> targetRanges = buildRanges(symbol);
+				result.add(new CharacterClass(targetRanges));
+			default:
+				result.add(nonterminals.get(symbol));
+			}
+		}
+		return result;
+	}
 
 }
