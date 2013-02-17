2f7de61a0a2e48ea27e5d019b0c2bc2367aaf844
Anastassija <A.Izmaylova@cwi.nl>
2013-02-17 17:41:44 +0100

    Removed unsafe downcasts in the results of adding a tuple to a relation and list relation
    - otherwise the exception is thrown if the tuple is of a different
    arity.

--- src/org/rascalmpl/interpreter/result/RelationResult.java
+++ src/org/rascalmpl/interpreter/result/RelationResult.java
@@ -286,7 +286,7 @@ public class RelationResult extends SetOrRelationResult<IRelation> {
 		<U extends IValue, V extends IValue> Result<U> insertTuple(TupleResult tuple) {
 			// TODO: check arity 
 			Type newType = getTypeFactory().setType(tuple.getType().lub(getType().getElementType()));
-			return makeResult(newType, (IRelation) getValue().insert(tuple.getValue()), ctx);
+			return makeResult(newType, /*(IRelation)*/ getValue().insert(tuple.getValue()), ctx); // do not see a reason for the unsafe cast
 		}
 
 		@Override
