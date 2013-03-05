32fd006384a5b65e0c737dd9f21e603a2dd8cc92
Anastassija <A.Izmaylova@cwi.nl>
2013-03-05 17:42:15 +0100

    Fix the use of name qualification in computation of the pattern constructor type.
    Fixes #156.

--- src/org/rascalmpl/semantics/dynamic/Expression.java
+++ src/org/rascalmpl/semantics/dynamic/Expression.java
@@ -363,7 +363,10 @@ public abstract class Expression extends org.rascalmpl.ast.Expression {
 				org.rascalmpl.ast.Expression nameExpr) {
 			java.util.List<AbstractFunction> functions = new LinkedList<AbstractFunction>();
 			String cons = Names.consName(nameExpr.getQualifiedName());
-			eval.getCurrentEnvt().getAllFunctions(cons, functions);
+			Type adt = eval.getCurrentEnvt().lookupAbstractDataType(Names.moduleName(nameExpr.getQualifiedName()));
+			if(adt != null)
+				eval.getCurrentEnvt().getAllFunctions(adt, cons, functions);
+			else eval.getCurrentEnvt().getAllFunctions(cons, functions);
 			
 			if (functions.isEmpty()) {
 			  return null;
