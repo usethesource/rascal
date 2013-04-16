cb03b6f5559ce2c5a8767953ace8c7764b5ddaf1
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2013-04-16 20:31:26 +0200

    fixed unused import

--- src/org/rascalmpl/interpreter/result/JavaMethod.java
+++ src/org/rascalmpl/interpreter/result/JavaMethod.java
@@ -26,7 +26,6 @@ import org.eclipse.imp.pdb.facts.IValue;
 import org.eclipse.imp.pdb.facts.type.Type;
 import org.rascalmpl.ast.FunctionDeclaration;
 import org.rascalmpl.ast.Tag;
-import org.rascalmpl.interpreter.Configuration;
 import org.rascalmpl.interpreter.IEvaluator;
 import org.rascalmpl.interpreter.StackTrace;
 import org.rascalmpl.interpreter.asserts.ImplementationError;
