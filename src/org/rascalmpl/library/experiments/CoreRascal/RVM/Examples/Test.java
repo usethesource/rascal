c5e1f25f8faca7b9cedb31c6e70adc56fbf814ec
paulklint <Paul.Klint@cwi.nl>
2013-08-05 13:19:38 +0200

    Primitives are now called with reflection.
    The considerations are:
    - This is much better for maintainability of the RVM code.
    - This is slower in execution.
    - This will not hurt when we generate JVM code directly.

--- src/org/rascalmpl/library/experiments/CoreRascal/RVM/Examples/Test.java
+++ src/org/rascalmpl/library/experiments/CoreRascal/RVM/Examples/Test.java
@@ -1,5 +1,7 @@
 package org.rascalmpl.library.experiments.CoreRascal.RVM.Examples;
 
+import java.lang.reflect.InvocationTargetException;
+
 import org.eclipse.imp.pdb.facts.IValue;
 import org.eclipse.imp.pdb.facts.IValueFactory;
 import org.rascalmpl.library.experiments.CoreRascal.RVM.CodeBlock;
@@ -20,14 +22,19 @@ public class Test {
 		
 		rvm.declareConst("LST", v.list(v.integer(0), v.integer(1), v.integer(2)));
 		
-		rvm.declareConst("ONE", v.integer(1));
-		rvm.declareConst("FOUR", v.integer(4));
+		rvm.declareConst("0", v.integer(0));
+		rvm.declareConst("1", v.integer(1));
+		rvm.declareConst("2", v.integer(2));
+		rvm.declareConst("3", v.integer(3));
+		rvm.declareConst("4", v.integer(4));
 		
 		rvm.declare(new Function("main", 0, 0, 1, 6,
 					new CodeBlock().
-						loadcon("LST").
-						loadcon("ONE").
-						callprim(Primitive.subscript_list_int).
+						loadcon("1").
+						loadcon("2").
+						loadcon("3").
+						loadcon("3").
+						callprim(Primitive.make_list).
 						halt()));
 	
 		rvm.executeProgram("main", new IValue[] {});
--- src/org/rascalmpl/library/experiments/CoreRascal/RVM/Primitives.java
+++ src/org/rascalmpl/library/experiments/CoreRascal/RVM/Primitives.java
@@ -1,26 +1,123 @@
 package org.rascalmpl.library.experiments.CoreRascal.RVM;
 
+import org.eclipse.imp.pdb.facts.IBool;
 import org.eclipse.imp.pdb.facts.IInteger;
 import org.eclipse.imp.pdb.facts.IList;
+import org.eclipse.imp.pdb.facts.IListWriter;
 import org.eclipse.imp.pdb.facts.IMap;
 import org.eclipse.imp.pdb.facts.IValue;
+import org.eclipse.imp.pdb.facts.IValueFactory;
 
 public class Primitives {
+	
+	private static IValueFactory vf;
+	private static IBool TRUE;
+	private static IBool FALSE;
+	
+	public static void init(IValueFactory fact){
+		vf = fact;
+		TRUE = vf.bool(true);
+		FALSE = vf.bool(false);
+		Primitive.bindMethods();
+	}
+	
+	// addition 
 
-	public static IValue subscript_list_int(IList lst, IInteger n){
-		return lst.get(n.intValue());
+// appendAfter 
+// asType 
+// composition 
+// division 
+// equals 
+
+// fieldAccess 
+// fieldUpdate 
+// fieldProject 
+// getAnnotation 
+// greater 
+
+// greaterThan 
+// greaterThanOrEq 
+// has 
+// insertBefore 
+// intersection 
+// in 
+// is 
+// isDefined 
+// join 
+// lessThan 
+// lessThanOrEq 
+// makeList 
+		
+// mod 
+// multiplication 
+// negation 
+// negative 
+// nonEquals 
+// product 
+// remainder 
+// slice 
+// splice 
+// setAnnotation 
+// subscript 
+// substraction 	
+// transitiveClosure 
+// transitiveRefleixiveClosure 
+	
+	public static int addition_int_int(Object[] stack, int sp) {
+		stack[sp - 2] = ((IInteger) stack[sp - 2]).add((IInteger) stack[sp - 1]);
+		return sp - 1;
+	}
+	
+	public static int equal_int_int(Object[] stack, int sp) {
+		stack[sp - 2] = ((IInteger) stack[sp - 2]).equal((IInteger) stack[sp - 1]);
+		return sp - 1;
+	}
+	
+	public static int greater_int_int(Object[] stack, int sp) {
+		stack[sp - 2] = ((IInteger) stack[sp - 2]).greater((IInteger) stack[sp - 1]).getValue() ? TRUE : FALSE;
+		return sp - 1;
+	}
+	
+	public static int multiplication_int_int(Object[] stack, int sp) {
+		stack[sp - 2] = ((IInteger) stack[sp - 2]).multiply((IInteger) stack[sp - 1]);
+		return sp - 1;
+	}
+	
+	public static int substraction_int_int(Object[] stack, int sp) {
+		stack[sp - 2] = ((IInteger) stack[sp - 2]).subtract((IInteger) stack[sp - 1]);
+		return sp - 1;
 	}
 
-	public static IList addition_list_list(IList lst1, IList lst2) {
-		return lst1.concat(lst2);
+	public static int subscript_list_int(Object[] stack, int sp){
+		stack[sp - 2] = ((IList) stack[sp - 2]).get(((IInteger) stack[sp - 1]).intValue());
+		return sp - 1;
 	}
 
-	public static Object subscript_map(IMap map, IValue val) {
-		return map.get(val);
+	public static int addition_list_list(Object[] stack, int sp) {
+		stack[sp - 2] = ((IList) stack[sp - 2]).concat((IList) stack[sp - 1]);
+		return sp - 1;
 	}
 
-	public static Object subscript2(IValue iValue, IValue idx1, IValue idx2) {
-		return null;
+	public static Object subscript_map(Object[] stack, int sp) {
+		stack[sp - 2] = ((IMap) stack[sp - 2]).get((IValue) stack[sp - 1]);
+		return sp - 1;
+	}
+
+//	public static Object subscript2(IValue iValue, IValue idx1, IValue idx2) {
+//		return null;
+//	}
+
+	public static int make_list(Object[] stack, int sp) {
+		int len = ((IInteger) stack[sp - 1]).intValue();
+		IListWriter writer = vf.listWriter();
+		
+		for(int i = len - 1; i >= 0; i--){
+			writer.append((IValue)stack[sp - 2 - i]);
+		}
+		sp = sp - len;
+		stack[sp - 1] = writer.done();
+		
+		return sp;
 	}
 
 }
