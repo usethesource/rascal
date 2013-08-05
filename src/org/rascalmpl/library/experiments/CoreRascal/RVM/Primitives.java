5cfa1ab8e3acb93e0f7e2370a1addcee3267f2c8
paulklint <Paul.Klint@cwi.nl>
2013-08-05 11:13:27 +0200

    Added primitives.

--- src/org/rascalmpl/library/experiments/CoreRascal/RVM/Primitives.java
+++ src/org/rascalmpl/library/experiments/CoreRascal/RVM/Primitives.java
@@ -2,6 +2,7 @@ package org.rascalmpl.library.experiments.CoreRascal.RVM;
 
 import org.eclipse.imp.pdb.facts.IInteger;
 import org.eclipse.imp.pdb.facts.IList;
+import org.eclipse.imp.pdb.facts.IMap;
 import org.eclipse.imp.pdb.facts.IValue;
 
 public class Primitives {
@@ -12,6 +13,14 @@ public class Primitives {
 
 	public static IList addition_list_list(IList lst1, IList lst2) {
 		return lst1.concat(lst2);
+	}
+
+	public static Object subscript_map(IMap map, IValue val) {
+		return map.get(val);
+	}
+
+	public static Object subscript2(IValue iValue, IValue idx1, IValue idx2) {
+		return null;
 	}
 
 }
