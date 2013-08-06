bccdf5ef699019ebeeb14a450f1fa8c8d1c48cc9
paulklint <Paul.Klint@cwi.nl>
2013-08-06 11:45:02 +0200

    Implemented proper closures.

--- src/org/rascalmpl/library/experiments/CoreRascal/RVM/Frame.java
+++ src/org/rascalmpl/library/experiments/CoreRascal/RVM/Frame.java
@@ -1,16 +1,22 @@
 package org.rascalmpl.library.experiments.CoreRascal.RVM;
 
 public class Frame {
-	final int scope;
-    final Frame previous;
+	final int scopeId;
+    final Frame previousCallFrame;
+    final Frame previousScope;
 	final Object[] stack;
 	int sp;
 	int pc;
 	final Function function;
 	
-	Frame(int scope, Frame previous,  int stackSize, Function function){
-		this.scope = scope;
-		this.previous = previous;
+	Frame(int scopeId, Frame previousCallFrame, int stackSize, Function function){
+		this(scopeId, previousCallFrame, previousCallFrame, stackSize, function);
+	}
+	
+	Frame(int scopeId, Frame previousCallFrame, Frame previousScope, int stackSize, Function function){
+		this.scopeId = scopeId;
+		this.previousCallFrame = previousCallFrame;
+		this.previousScope = previousScope;
 		this.stack = new Object[stackSize];
 		this.pc = 0;
 		this.sp = 0;
