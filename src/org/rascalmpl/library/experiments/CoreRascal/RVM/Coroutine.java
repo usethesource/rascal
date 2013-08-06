f175b02e9e2019dcedd971535f3a4da633dc6496
Anastassija <A.Izmaylova@cwi.nl>
2013-08-07 00:49:42 +0200

    Implemented 'CREATEDYN' and 'HASNEXT' coroutine instructions; a few bug fixes; also renaming here and there.

--- src/org/rascalmpl/library/experiments/CoreRascal/RVM/Coroutine.java
+++ src/org/rascalmpl/library/experiments/CoreRascal/RVM/Coroutine.java
@@ -2,21 +2,29 @@ package org.rascalmpl.library.experiments.CoreRascal.RVM;
 
 public class Coroutine {
 	
-	final Frame init; // stack frame of the main coroutine function 
+	final Frame start; // stack frame of the main coroutine function 
 	Frame frame; // the current active stack frame of the coroutine
 	
+	boolean suspended = false;
+	
 	public Coroutine(Frame frame) {
-		this.init = frame;
+		this.start = frame;
 		this.frame = frame;
 	}
 	
-	public void resume(Frame previousCallFrame) {
-		this.init.previousCallFrame = previousCallFrame;
+	public void next(Frame previousCallFrame) {
+		this.suspended = false;
+		this.start.previousCallFrame = previousCallFrame;
 	}
 	
 	public void suspend(Frame current) {
-		this.init.previousCallFrame = null;
+		this.start.previousCallFrame = null;
 		this.frame = current; // sets the current stack frame of the active co-routine
+		this.suspended = true;
+	}
+	
+	public boolean hasNext() {
+		return suspended;
 	}
 	
 }
