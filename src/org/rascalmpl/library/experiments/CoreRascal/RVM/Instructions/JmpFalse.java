b9c83b9c001b1bef620b15dc5c81aca83636f19c
paulklint <Paul.Klint@cwi.nl>
2013-08-07 15:03:24 +0200

    Added checks ton undeclared labels.

--- src/org/rascalmpl/library/experiments/CoreRascal/RVM/Instructions/JmpFalse.java
+++ src/org/rascalmpl/library/experiments/CoreRascal/RVM/Instructions/JmpFalse.java
@@ -15,6 +15,9 @@ public class JmpFalse extends Instruction {
 	
 	public void generate(){
 		ins.addCode(opcode.getOpcode());
+		if(ins.labels.get(label) == null){
+			throw new RuntimeException("PANIC: undefined label " + label);
+		}
 		ins.addCode(ins.labels.get(label));
 	}
 }
