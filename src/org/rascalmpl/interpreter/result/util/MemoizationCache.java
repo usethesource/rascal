b676083b8e6a73a176c2c97f35ad06dcb143e168
Davy Landman <davy.landman@gmail.com>
2013-08-16 16:30:43 +0200

    Removed the Type argument from the memoization call

--- src/org/rascalmpl/interpreter/result/util/MemoizationCache.java
+++ src/org/rascalmpl/interpreter/result/util/MemoizationCache.java
@@ -11,18 +11,12 @@ import org.rascalmpl.interpreter.result.Result;
 public class MemoizationCache {
 	private class Key {
 		private final int storedHash;
-		private final Type[] types;
 		private final IValue[] params;
 		private final Map<String, IValue> keyArgs;
 		
-		public Key(Type[] types, IValue[] params, Map<String, IValue> keyArgs) {
-			Object[] values = new Object[types.length + params.length + 1];
-			System.arraycopy(types, 0, values, 0, types.length);
-			System.arraycopy(params, 0, values, types.length, params.length);
-			values[values.length-1] = keyArgs;
-			storedHash = Arrays.hashCode(values);
+		public Key(IValue[] params, Map<String, IValue> keyArgs) {
+			storedHash = Arrays.hashCode(params) + (keyArgs == null ? 0 : keyArgs.hashCode());
 			
-			this.types = types;
 			this.params = params;
 			this.keyArgs = keyArgs;
 		}
@@ -37,12 +31,6 @@ public class MemoizationCache {
 			if (obj instanceof Key) {
 				Key other = (Key)obj;
 				if (other.storedHash == this.storedHash) {
-					if (other.types.length != this.types.length)
-						return false;
-					for (int i = 0; i < types.length; i++) {
-						if (!other.types[i].equivalent(types[i]))
-							return false;
-					}
 					if (other.params.length != this.params.length)
 						return false;
 					for (int i = 0; i < params.length; i++) {
@@ -67,11 +55,11 @@ public class MemoizationCache {
 		}
 	}
 	private Map<Key, Result<IValue>> store = new HashMap<>();
-	public Result<IValue> getStoredResult(Type[] types, IValue[] params, Map<String, IValue> keyArgs) {
-		return store.get(new Key(types, params, keyArgs));
+	public Result<IValue> getStoredResult(IValue[] params, Map<String, IValue> keyArgs) {
+		return store.get(new Key(params, keyArgs));
 	}
 	
-	public void storeResult(Type[] types, IValue[] params, Map<String, IValue> keyArgs, Result<IValue> result) {
-		store.put(new Key(types, params, keyArgs), result);
+	public void storeResult(IValue[] params, Map<String, IValue> keyArgs, Result<IValue> result) {
+		store.put(new Key(params, keyArgs), result);
 	}
 }
