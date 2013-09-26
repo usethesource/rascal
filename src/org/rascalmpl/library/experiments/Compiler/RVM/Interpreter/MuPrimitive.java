c4d21679114b920f3f7a038127bc5b562618d2bb
paulklint <Paul.Klint@cwi.nl>
2013-09-26 13:44:08 +0200

    Implemented solve statement

--- src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/MuPrimitive.java
+++ src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/MuPrimitive.java
@@ -72,7 +72,7 @@ public enum MuPrimitive {
 	regexp_find,
 	regexp_group,
 	set2list,
-	size_array_or_list_or_set_or_map_or_tuple,
+//	size_array_or_list_or_set_or_map_or_tuple,
 	size_array,
 	size_list,
 	size_set,
@@ -522,22 +522,6 @@ public enum MuPrimitive {
 		return sp;
 	}
 		
-	public static int size_array_or_list_or_set_or_map_or_tuple(Object[] stack, int sp, int arity) {
-		assert arity == 1;
-		if(stack[sp - 1] instanceof Object[]){
-			stack[sp - 1] = ((Object[]) stack[sp - 1]).length;
-		} else if(stack[sp - 1] instanceof IList){
-			stack[sp - 1] = ((IList) stack[sp - 1]).length();
-		} else if(stack[sp - 1] instanceof ISet){
-			stack[sp - 1] = ((ISet) stack[sp - 1]).size();
-		} else if(stack[sp - 1] instanceof IMap){
-			stack[sp - 1] = ((IMap) stack[sp - 1]).size();
-		} else if(stack[sp - 1] instanceof ITuple){
-			stack[sp - 1] = ((ITuple) stack[sp - 1]).arity();
-		} else
-			throw new RuntimeException("size_array_or_list_mint -- not defined on " + stack[sp - 1].getClass());
-		return sp;
-	}
 	public static int size_array(Object[] stack, int sp, int arity) {
 		assert arity == 1;
 		stack[sp - 1] = ((Object[]) stack[sp - 1]).length;
