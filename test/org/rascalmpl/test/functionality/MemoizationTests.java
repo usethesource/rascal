0cc2e88167c0d1ebc9b23bd3ef64a9b2a878abbe
Davy Landman <davy.landman@gmail.com>
2013-10-09 11:57:11 +0200

    Added test to hit the OutOfMemory scenario, as to cleanup the memoization cache

---
+++ test/org/rascalmpl/test/functionality/MemoizationTests.java
@@ -0,0 +1,26 @@
+package org.rascalmpl.test.functionality;
+
+import java.util.ArrayList;
+import org.junit.Test;
+import org.rascalmpl.test.infrastructure.TestFramework;
+
+
+public class MemoizationTests extends TestFramework {
+	@Test
+	public void memoryIsReleased() throws InterruptedException {
+		prepare("@memo list[int] OneMB(int w) = [ w | i <- [0..(1024*1024/8)]];");
+		runTestInSameEvaluator("( true | it && OneMB(i)[0] == i | i <- [0..10])");
+		// Force an OoM
+		// use all memory to cause the memoization to cleanup
+	    try {
+	        final ArrayList<Object[]> allocations = new ArrayList<Object[]>();
+	        while(true)
+	            allocations.add( new Object[(int) Runtime.getRuntime().maxMemory()] );
+	    } catch( OutOfMemoryError e ) {
+	        // great!
+	    }
+		// actually hit the cache to cause a cleanup
+		runTestInSameEvaluator("OneMB(1)[0]==1");
+	}
+
+}
