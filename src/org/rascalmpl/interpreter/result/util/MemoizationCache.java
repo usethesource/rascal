3376285dba91c04608d9d9eb6a5fb9c440104a9d
Davy Landman <davy.landman@gmail.com>
2013-10-15 22:13:20 +0200

    Replaced large hash function with a small simple one
    The hashmap rehashes it anyway

--- src/org/rascalmpl/interpreter/result/util/MemoizationCache.java
+++ src/org/rascalmpl/interpreter/result/util/MemoizationCache.java
@@ -15,57 +15,19 @@ import org.rascalmpl.interpreter.result.Result;
 public class MemoizationCache {
 	
 	private static final int PRIME1 = (int) 2654435761L;
-	private static final int PRIME2 = (int) 2246822519L;
 	private static final int PRIME3 = (int) 3266489917L;
 	private static final int PRIME4 = 668265263;
-	private static final int PRIME5 = 0x165667b1;
 	
 	private static int calculateHash(IValue[] params, Map<String, IValue> keyArgs) {
-		//xxHash
-		int h;
-		int len = params.length;
-		int i = 0;
-		if (len >= 4) {
-			int v1 = PRIME5 + PRIME1 + PRIME2;
-			int v2 = PRIME5 + PRIME2;
-			int v3 = PRIME5 + 0;
-			int v4 = PRIME5 - PRIME1;
-			int limit = len - 4;
-			do {
-				v1 += params[i++].hashCode() * PRIME2;
-				v1 = Integer.rotateLeft(v1, 13) * PRIME1;
-				v2 += params[i++].hashCode() * PRIME2;
-				v2 = Integer.rotateLeft(v2, 13) * PRIME1;
-				v3 += params[i++].hashCode() * PRIME2;
-				v3 = Integer.rotateLeft(v3, 13) * PRIME1;
-				v4 += params[i++].hashCode() * PRIME2;
-				v4 = Integer.rotateLeft(v4, 13) * PRIME1;
-			} while(i < limit);
-			h = Integer.rotateLeft(v1, 1)
-			  + Integer.rotateLeft(v2, 7)
-			  + Integer.rotateLeft(v3, 12)
-			  + Integer.rotateLeft(v4, 18);
-		}
-		else {
-			h = PRIME5;
-		}
-		
-		while (i < len) {
-			h += params[i++].hashCode() * PRIME3;
+		int h = PRIME1;
+		for (int i =0; i < params.length; i++) {
+			h += params[i].hashCode() * PRIME3;
 			h = Integer.rotateLeft(h, 17) * PRIME4;
 		}
-		
 		if (keyArgs != null) {
-			h += keyArgs.hashCode() * PRIME3;
-			h = Integer.rotateLeft(h, 17) * PRIME4;
+			h += keyArgs.hashCode();
+			h = Integer.rotateLeft(h, 17);
 		}
-
-		h ^= h >>> 15;
-		h *= PRIME2;
-		h ^= h >>> 13;
-		h *= PRIME3;
-		h ^= h >>> 16;
-		
 		return h;
 	}
 	
