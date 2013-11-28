97e24271cb2e3bf0e6df8e34d9db714119e0cdae
paulklint <Paul.Klint@cwi.nl>
2013-11-28 16:27:36 +0100

    Added missing files.

---
+++ src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/Instructions/LoadLoc0.java
@@ -0,0 +1,12 @@
+package org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.Instructions;
+
+import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.CodeBlock;
+
+
+public class LoadLoc0 extends Instruction {
+
+	public LoadLoc0(CodeBlock ins){
+		super(ins, Opcode.LOADLOC0);
+	}
+
+}
