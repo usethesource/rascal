c175f6f9897f1452bc2d55436b6b20d0bbad6900
Anastassija <A.Izmaylova@cwi.nl>
2013-12-12 18:06:08 +0100

    Added keyword parameter related instructions to the RVM interpreter.

---
+++ src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/Instructions/StoreVarKwp.java
@@ -0,0 +1,22 @@
+package org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.Instructions;
+
+import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.CodeBlock;
+
+public class StoreVarKwp extends Instruction {
+	
+	final String fuid;
+	final String name;
+	
+	public StoreVarKwp(CodeBlock ins, String fuid, String name) {
+		super(ins, Opcode.STOREVARKWP);
+		this.fuid = fuid;
+		this.name = name;
+	}
+	
+	public String toString() { return "STOREVARKWP " + fuid + ", " + name + " [" + codeblock.getFunctionIndex(fuid) + ", " + codeblock.getConstantIndex(codeblock.vf.string(name)) + "]"; }
+	
+	public void generate(){
+		codeblock.addCode2(opcode.getOpcode(), codeblock.getFunctionIndex(fuid), codeblock.getConstantIndex(codeblock.vf.string(name)));
+	}
+
+}
