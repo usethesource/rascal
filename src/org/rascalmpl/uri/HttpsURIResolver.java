7a976d40a32b6e72e7619607b27f2902aade088c
Cindy <c.berghuizen@gmail.com>
2014-04-25 11:16:28 +0200

    Refactor HttpsURIResolver
    HttpsURIResolver now extends HttpURIResolver

--- src/org/rascalmpl/uri/HttpsURIResolver.java
+++ src/org/rascalmpl/uri/HttpsURIResolver.java
@@ -12,75 +12,11 @@
 *******************************************************************************/
 package org.rascalmpl.uri;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.nio.charset.Charset;
 
-import org.rascalmpl.uri.IURIInputStreamResolver;
-
-public class HttpsURIResolver implements IURIInputStreamResolver {
-
-	public InputStream getInputStream(URI uri) throws IOException {
-		return new BufferedInputStream(uri.toURL().openStream());
-	}
-
-	public String scheme() {
-		return "https";
-	}
-
-	public boolean exists(URI uri) {
-		try {
-			uri.toURL().openConnection();
-			return true;
-		}
-		catch (IOException e) {
-			return false;
-		}
-	}
-
-	public boolean isDirectory(URI uri) {
-		return false;
-	}
-
-	public boolean isFile(URI uri) {
-		return exists(uri);
-	}
-
-	public long lastModified(URI uri) {
-		try {
-			return uri.toURL().openConnection().getLastModified();
-		}
-		catch (IOException e) {
-			return 0L;
-		}
-	}
-
-	public String[] listEntries(URI uri) {
-		String [] ls = {};
-		return ls;
-	}
-
-	public String absolutePath(URI uri) {
-		return uri.getPath();
-	}
-
-	public boolean supportsHost() {
-		return true;
-	}
+public class HttpsURIResolver extends HttpURIResolver {
 
 	@Override
-	public Charset getCharset(URI uri) throws IOException {
-		try {
-			String encoding = uri.toURL().openConnection().getContentEncoding();
-			if (encoding != null && Charset.isSupported(encoding)) {
-				return Charset.forName(encoding);
-			}
-			return null;
-		}
-		catch (IOException e) {
-			return null;
-		}
+	public String scheme() {
+		return "https";
 	}
 }
