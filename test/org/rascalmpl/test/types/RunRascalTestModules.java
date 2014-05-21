a6b7bc9f3910abbcac4ebfdd872432263fa93333
paulklint <Paul.Klint@cwi.nl>
2014-05-21 11:44:57 +0200

    Adapted testing framework setup

---
+++ test/org/rascalmpl/test/types/RunRascalTestModules.java
@@ -0,0 +1,9 @@
+package org.rascalmpl.test.types;
+
+import org.junit.runner.RunWith;
+import org.rascalmpl.test.infrastructure.RascalJUnitTestPrefix;
+import org.rascalmpl.test.infrastructure.RascalJUnitTestRunner;
+
+@RunWith(RascalJUnitTestRunner.class)
+@RascalJUnitTestPrefix("lang::rascal::tests::types")
+public class RunRascalTestModules { }
