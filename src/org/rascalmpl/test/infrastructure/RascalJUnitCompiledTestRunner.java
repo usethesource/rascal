128f083e0d756a4a13d4c4e5b440323b6a0907ad
paulklint <Paul.Klint@cwi.nl>
2014-06-02 23:03:47 +0200

    Split test suites in interpreted versus compiled
    This avoids that AllSuite also runs the compiled tests.
    
    For now this can be done using AllSuiteCompiled.
    
    Be aware work in progress!!

--- src/org/rascalmpl/test/infrastructure/RascalJUnitCompiledTestRunner.java
+++ src/org/rascalmpl/test/infrastructure/RascalJUnitCompiledTestRunner.java
@@ -13,6 +13,7 @@ package org.rascalmpl.test.infrastructure;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URISyntaxException;
+import java.util.HashMap;
 
 import org.eclipse.imp.pdb.facts.ISourceLocation;
 import org.junit.runner.Description;
@@ -23,7 +24,6 @@ import org.junit.runner.notification.RunNotifier;
 import org.rascalmpl.interpreter.Evaluator;
 import org.rascalmpl.interpreter.ITestResultListener;
 import org.rascalmpl.interpreter.NullRascalMonitor;
-import org.rascalmpl.interpreter.TestEvaluator;
 import org.rascalmpl.interpreter.asserts.ImplementationError;
 import org.rascalmpl.interpreter.env.GlobalEnvironment;
 import org.rascalmpl.interpreter.env.ModuleEnvironment;
@@ -43,36 +43,45 @@ public class RascalJUnitCompiledTestRunner extends Runner {
 	private static PrintWriter stdout;
 	private Description desc;
 	private String prefix;
+	private HashMap<String, Integer> testCount;
 
 	static {
 		heap = new GlobalEnvironment();
 		root = heap.addModule(new ModuleEnvironment("___junit_test___", heap));
-		
+
 		stderr = new PrintWriter(System.err);
 		stdout = new PrintWriter(System.out);
 		evaluator = new Evaluator(ValueFactoryFactory.getValueFactory(), stderr, stdout,  root, heap);
 		evaluator.addRascalSearchPathContributor(StandardLibraryContributor.getInstance());
 		evaluator.getConfiguration().setErrors(true);
-		evaluator.doImport(new NullRascalMonitor(), "experiments::Compiler::Execute");
+
+		// Import the compiler's Execute module
+		NullRascalMonitor monitor = new NullRascalMonitor();
+		
+		System.err.println("**** Loading Compiler (takes a minute!) ***");
+		evaluator.doImport(monitor, "experiments::Compiler::Execute");		
+		System.err.println("**** Compiler Loaded ***");
 	}  
 	
 	public RascalJUnitCompiledTestRunner(Class<?> clazz) {
 		this(clazz.getAnnotation(RascalJUnitTestPrefix.class).value());
+		desc = null;
+		testCount = new HashMap<String, Integer>();
 		try {
-      Object instance = clazz.newInstance();
-      if (instance instanceof IRascalJUnitTestSetup) {
-        ((IRascalJUnitTestSetup) instance).setup(evaluator);
-      }
-      else {
-        IURIInputStreamResolver resolver = new ClassResourceInput(evaluator.getResolverRegistry(), "junit", clazz, "/");
-        evaluator.getResolverRegistry().registerInput(resolver);
-        evaluator.addRascalSearchPath(URIUtil.rootScheme("junit"));
-      }
-    } catch (InstantiationException e) {
-      throw new ImplementationError("could not setup tests for: " + clazz.getCanonicalName(), e);
-    } catch (IllegalAccessException e) {
-      throw new ImplementationError("could not setup tests for: " + clazz.getCanonicalName(), e);
-    }
+			Object instance = clazz.newInstance();
+			if (instance instanceof IRascalJUnitTestSetup) {
+				((IRascalJUnitTestSetup) instance).setup(evaluator);
+			}
+			else {
+				IURIInputStreamResolver resolver = new ClassResourceInput(evaluator.getResolverRegistry(), "junit", clazz, "/");
+				evaluator.getResolverRegistry().registerInput(resolver);
+				evaluator.addRascalSearchPath(URIUtil.rootScheme("junit"));
+			}
+		} catch (InstantiationException e) {
+			throw new ImplementationError("could not setup tests for: " + clazz.getCanonicalName(), e);
+		} catch (IllegalAccessException e) {
+			throw new ImplementationError("could not setup tests for: " + clazz.getCanonicalName(), e);
+		}
 	}
 	
 	public RascalJUnitCompiledTestRunner(String prefix) {
@@ -81,34 +90,41 @@ public class RascalJUnitCompiledTestRunner extends Runner {
 	}
 	
 	static protected String computeTestName(String name, ISourceLocation loc) {
-		return name + ":" + loc.getEndLine();
+		String res = name + ":" + loc.getEndLine();
+		//System.err.println("computeTestName: " + res);
+		return res;
 	}
 	
 	@Override
 	public Description getDescription() {
+		if(desc != null)
+			return desc;
 		Description desc = Description.createSuiteDescription(prefix);
 		this.desc = desc;
-		
+
 		try {
 			String[] modules = evaluator.getResolverRegistry().listEntries(URIUtil.create("rascal", "", "/" + prefix.replaceAll("::", "/")));
-			
+
 			for (String module : modules) {
 				if (!module.endsWith(".rsc")) {
 					continue;
 				}
 				String name = prefix + "::" + module.replaceFirst(".rsc", "");
+				System.err.println("Adding tests for module: " + name);
 				evaluator.doImport(new NullRascalMonitor(), name);
 				Description modDesc = Description.createSuiteDescription(name);
 				desc.addChild(modDesc);
-				
+				int count = 0;
 				for (AbstractFunction f : heap.getModule(name.replaceAll("\\\\","")).getTests()) {
-				  if (!(f.hasTag("ignore") || f.hasTag("Ignore") || f.hasTag("ignoreCompiler") || f.hasTag("IgnoreCompiler"))) {
-				    modDesc.addChild(Description.createTestDescription(getClass(), computeTestName(f.getName(), f.getAst().getLocation())));
-				  }
+					if (!(f.hasTag("ignore") || f.hasTag("Ignore") || f.hasTag("ignoreCompiler") || f.hasTag("IgnoreCompiler"))) {
+						modDesc.addChild(Description.createTestDescription(getClass(), computeTestName(f.getName(), f.getAst().getLocation())));
+						count++;
+					}
 				}
+				testCount.put(name,  count);
+				System.err.println(module + ": " + count + " tests");
 			}
 			
-		
 			return desc;
 		} catch (IOException e) {
 			throw new RuntimeException("could not create test suite", e);
@@ -123,55 +139,70 @@ public class RascalJUnitCompiledTestRunner extends Runner {
 			desc = getDescription();
 		}
 		notifier.fireTestRunStarted(desc);
+		
+		System.err.println("run: testCount = " + desc.testCount());
 
 		for (Description mod : desc.getChildren()) {
-			TestExecutor runner = new TestExecutor(evaluator, new Listener(notifier, mod));
-			runner.test(mod.getDisplayName());
+			
+			Listener listener = new Listener(notifier, mod);
+			TestExecutor runner = new TestExecutor(evaluator, listener);
+			try {
+				runner.test(mod.getDisplayName(), testCount.get(mod.getClassName())); 
+				listener.done();
+			} catch (Exception e) {
+				System.err.println("RascalJunitCompiledTestrunner.run: " + mod.getMethodName() + " unexpected exception: " + e.getMessage());
+				e.printStackTrace(System.err);
+				
+			}
+			notifier.fireTestStarted(mod);	// Make sure that the test count is decremented for the test suite itself
+			notifier.fireTestFinished(mod);
 		}
-		
+
 		notifier.fireTestRunFinished(new Result());
 	}
 
 	private final class Listener implements ITestResultListener {
 		private final RunNotifier notifier;
 		private final Description module;
-	
+
 		private Listener(RunNotifier notifier, Description module) {
 			this.notifier = notifier;
 			this.module = module;
 		}
-	
+
 		private Description getDescription(String name, ISourceLocation loc) {
 			String testName = computeTestName(name, loc);
-			
+
 			for (Description child : module.getChildren()) {
 				if (child.getMethodName().equals(testName)) {
 					return child;
 				}
 			}
-			
+
 			throw new IllegalArgumentException(name + " test was never registered");
 		}
 
-		
 		@Override
 		public void start(int count) {
+			System.out.println("RascalJunitCompiledTestRunner.start: " + count);
 			notifier.fireTestRunStarted(module);
 		}
-	
+
 		@Override
-		public void report(boolean successful, String test, ISourceLocation loc,	String message, Throwable t) {
+		public void report(boolean successful, String test, ISourceLocation loc, String message, Throwable t) {
+			
+			//System.err.println("RascalJunitCompiledTestRunner.report: " + successful + ", test = " + test + ", at " + loc + ", message = " + message);
 			Description desc = getDescription(test, loc);
 			notifier.fireTestStarted(desc);
-			
+
 			if (!successful) {
-				notifier.fireTestFailure(new Failure(desc, t != null ? t : new Exception(message != null ? message : "no message")));
+				notifier.fireTestFailure(new Failure(desc, null)); //t != null ? t : new Exception(message != null ? message : "no message")));
 			}
 			else {
 				notifier.fireTestFinished(desc);
 			}
 		}
-	
+
 		@Override
 		public void done() {
 			notifier.fireTestRunFinished(new Result());
---
+++ test/org/rascalmpl/test/imports_compiled/RunRascalCompiledTestModules.java
@@ -0,0 +1,10 @@
+package org.rascalmpl.test.imports_compiled;
+
+import org.junit.runner.RunWith;
+import org.rascalmpl.test.infrastructure.RascalJUnitCompiledTestRunner;
+import org.rascalmpl.test.infrastructure.RascalJUnitTestPrefix;
+
+@RunWith(RascalJUnitCompiledTestRunner.class)
+@RascalJUnitTestPrefix("lang::rascal::tests::imports")
+public class RunRascalCompiledTestModules { }
+
