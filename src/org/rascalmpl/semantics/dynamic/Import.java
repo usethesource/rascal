48537aaf7e64c2f95592958aac8e259aa06a1b66
paulklint <Paul.Klint@cwi.nl>
2014-06-06 16:56:04 +0200

    OutOfMemoryError disappeared in evalImport, closes #587
    Replaced a catch (Throwable e) by catch (StaticError e).
    
    Note: all places where we do catch (Throwable e) are suspicious!

--- src/org/rascalmpl/semantics/dynamic/Import.java
+++ src/org/rascalmpl/semantics/dynamic/Import.java
@@ -437,7 +437,7 @@ public abstract class Import {
 	  try {
 		  imp.interpret(eval);
 	  }
-	  catch (Throwable e) {
+	  catch (StaticError e) {
 		  // parsing the current module should be robust wrt errors in modules it depends on.
 		  eval.getMonitor().warning("could not load module " + TreeAdapter.yield(mod) + "[" + e.getMessage() + "]", imp != null ? imp.getLocation() : eval.getCurrentAST().getLocation());
 		  if(eval.isInterrupted()) {
