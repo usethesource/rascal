a429eeaf53f1d8372ba24635260041cbce5e4f0b
paulklint <Paul.Klint@cwi.nl>
2014-06-29 00:59:39 +0200

    Reorganized definition and handling of input events.

--- src/org/rascalmpl/library/experiments/vis2/JSFigure.js
+++ src/org/rascalmpl/library/experiments/vis2/JSFigure.js
@@ -1,5 +1,4 @@
 
-
 /****************** The figure prototype with all defaults *********************/
 
 var Figure = {
@@ -93,9 +92,9 @@ function buildFigure1(description, parent) {
                 var accessor = prop_val.use;
                 if(prop === "accessor"){
                     Object.defineProperty(f, prop, {get: function(){ return accessor;}  });
-                } else
-                if(Figure.propertiesWithNumberArg.indexOf(prop) >= 0){
-                    Object.defineProperty(f, prop, {get: function(){ return parseInt(eval(accessor));}  });
+//                 } else
+//                 if(Figure.propertiesWithNumberArg.indexOf(prop) >= 0){
+//                     Object.defineProperty(f, prop, {get: function(){ return parseInt(eval(accessor));}  });
                 } else {
                     Object.defineProperty(f, prop, {get: function(){ return eval(accessor);}  });
                 }
@@ -130,20 +129,35 @@ function redrawFigure(){
     drawFigure1(Figure.figure_root);
 }
 
-
 /****************** AddInteraction to a figure *************************/
 
 function addInteraction(selection, fig) {
-    if (fig.hasOwnProperty("onClick")) {
+    if (fig.hasOwnProperty("event")) {
     	selection.style("cursor", "crosshair");
-        selection.on("dbclick", function(e) {d3.event.stopPropagation();});
-        selection.on("click", function(e) {
+        selection.on(fig.event, function(e) {
            d3.event.stopPropagation();
-           Figure.setModelElement(fig.accessor, fig.replacement);
-           refreshFromServer();
+           if(fig.hasOwnProperty("replacement")){
+                Figure.setModelElement(fig.accessor, fig.replacement);
+                refreshFromServer();
+           } else {
+                redrawFigure();
+           }
         });
     }
     return selection;
+}
+
+function handleUserInput(fig, v) {
+    d3.event.stopPropagation();
+    
+    if(fig.hasOwnProperty("replacement")){
+        Figure.setModelElement(fig.accessor, fig.replacement);
+        refreshFromServer();
+    } else {
+        Figure.setModelElement(fig.accessor, v);
+        redrawFigure();
+    }
+    return false;
 }
 
 /****************** Askserver and redraw a figure object ****************/
@@ -695,105 +709,7 @@ drawFunction.graph = function (selection, x, y) {
     });
 }
 
-/********************* strInput ***************************/
-
-bboxFunction.strInput = function() {
-    if (this.width == 0) {
-        this.width = 200;
-    }
-    if (this.height == 0) {
-        this.height = 200;
-    }
-}
-
-drawFunction.strInput = function (selection, x, y) {
-    var foreign = selection.append("foreignObject")
-        .attr("x", x).attr("y", y).attr("width", this.width).attr("height", this.height);
-    
-    foreign.append("xhtml:body")
-        .append("form").attr("action", "")
-        .append("input")
-            .style("width", this.width + "px").style("height", this.height + "px")
-            .style("font-family", this.fontName).style("font-size", this.fontSize)
-            .attr("type", "text").attr("value", "");
-    
-     var accessor = this.accessor;
-        
-     foreign.on("mousedown", function() { d3.event.stopPropagation(); });
-     foreign.on("submit", function() {
-            var v = "'" + foreign.select("input")[0][0].value + "'";
-            d3.event.preventDefault();
-            Figure.setModelElement(accessor, v);
-            redrawFigure()
-     });
-}
-
-/********************* colorInput ***************************/
-
-bboxFunction.colorInput = function() {
-    if (this.width == 0) {
-        this.width = 200;
-    }
-    if (this.height == 0) {
-        this.height = 200;
-    }
-}
-
-drawFunction.colorInput = function (selection, x, y) {
-    var foreign = selection.append("foreignObject")
-        .attr("x", x).attr("y", y).attr("width", this.width).attr("height", this.height);
-    
-    foreign.append("xhtml:body")
-        .append("form").attr("action", "")
-        .append("input")
-            .style("width", this.width + "px").style("height", this.height + "px")
-            .style("font-family", this.fontName).style("font-size", this.fontSize)
-            .attr("type", "color").attr("value", "");
-    
-     var accessor = this.accessor;
-        
-     foreign.on("mousedown", function() { d3.event.stopPropagation(); });
-     foreign.on("change", function() {
-            var v = "'" + foreign.select("input")[0][0].value + "'";
-            d3.event.preventDefault();
-            Figure.setModelElement(accessor, v);
-            redrawFigure()
-     });
-}
-
-/********************* numInput ***************************/
-
-bboxFunction.numInput = function() {
-    if (this.width == 0) {
-        this.width = 200;
-    }
-    if (this.height == 0) {
-        this.height = 200;
-    }
-}
-
-drawFunction.numInput = function (selection, x, y) {
-    var foreign = selection.append("foreignObject")
-        .attr("x", x).attr("y", y).attr("width", this.width).attr("height", this.height);
-    
-    foreign.append("xhtml:body")
-        .append("form").attr("action", "")
-        .append("input")
-            .style("width", this.width + "px").style("height", this.height + "px")
-            .style("font-family", this.fontName).style("font-size", this.fontSize)
-            .attr("type", "number").attr("value", "");
-    
-     var accessor = this.accessor;
-        
-     foreign.on("mousedown", function() { d3.event.stopPropagation(); });
-     foreign.on("submit", function() {
-            var v = foreign.select("input")[0][0].value;
-            d3.event.preventDefault();
-            Figure.setModelElement(accessor, v);
-            redrawFigure()
-     });
-}
-
+// visibility control
 
 /********************* fswitch ***************************/
 
@@ -811,13 +727,34 @@ drawFunction.fswitch = function (selection, x, y) {
     var selector = Math.min(Math.max(Figure.getModelElement(this.selector),0), inner.length -1);
     var selected = this.inner[selector];
     return selected.draw(selection, x, y);
-}  
+}
 
+/********************* visible ***************************/
 
-/********************* rangeInput ***************************/
+bboxFunction.visible = function() {
+    var inner = this.inner;
+    var visible = Figure.getModelElement(this.selector);
+    if(visible){
+        inner.bbox();
+        this.width = inner.width;
+        this.height = inner.height;
+    } else {
+        this.width = this.height = 0;
+    }
+}
 
-bboxFunction.rangeInput = function() {
-     if (this.width == 0) {
+drawFunction.visible = function (selection, x, y) {
+    var inner = this.inner;
+    var visible = Figure.getModelElement(this.selector);
+    return visible ? inner.draw(selection, x, y) : selection;
+} 
+
+// Input elements
+
+/********************* buttonInput ***************************/
+
+bboxFunction.buttonInput = function() {
+    if (this.width == 0) {
         this.width = 200;
     }
     if (this.height == 0) {
@@ -825,8 +762,36 @@ bboxFunction.rangeInput = function() {
     }
 }
 
-drawFunction.rangeInput = function (selection, x, y) {   
+drawFunction.buttonInput = function (selection, x, y) {
+    var fig = this;
+    var accessor = this.accessor; 
+    var b = Figure.getModelElement(accessor);
+    
+    var foreign = selection.append("foreignObject")
+        .attr("x", x).attr("y", y).attr("width", this.width).attr("height", this.height);
+   
+    foreign.append("xhtml:body")
+        .append("form").attr("action", "")
+        .append("input")
+            .style("width", this.width + "px").style("height", this.height + "px")
+            .attr("type", "button").attr("value", b ? this.trueText : this.falseText);
         
+     foreign.on("mousedown", function() {
+          var b = !Figure.getModelElement(accessor);
+          //foreign.select("input").attr("value", b ? this.trueText : this.falseText);  
+          return handleUserInput(fig, b);
+     });
+}
+
+/********************* checkboxInput ***************************/
+
+bboxFunction.checkboxInput = bboxFunction.buttonInput;
+
+drawFunction.checkboxInput = function (selection, x, y) {
+    var fig = this;
+    var accessor = this.accessor; 
+    var b = Figure.getModelElement(accessor);
+    
     var foreign = selection.append("foreignObject")
         .attr("x", x).attr("y", y).attr("width", this.width).attr("height", this.height);
     
@@ -834,18 +799,136 @@ drawFunction.rangeInput = function (selection, x, y) {
         .append("form").attr("action", "")
         .append("input")
             .style("width", this.width + "px").style("height", this.height + "px")
+            .attr("type", "checkbox");
+     if(b){
+         foreign.select("input").attr("checked", "checked");
+     }
+        
+     foreign.on("mousedown", function() {
+         return handleUserInput(fig, !Figure.getModelElement(accessor));
+     });
+}
+
+/********************* strInput ***************************/
+
+bboxFunction.strInput = bboxFunction.buttonInput
+
+drawFunction.strInput = function (selection, x, y) {
+    var fig = this;
+    var accessor = this.accessor; 
+     
+    var foreign = selection.append("foreignObject")
+        .attr("x", x).attr("y", y).attr("width", this.width).attr("height", this.height);
+    
+    foreign.append("xhtml:body")
+        .append("form").attr("action", "").attr("onsubmit", "return false")
+        .append("input")
+            .style("width", this.width + "px").style("height", this.height + "px")
+            .attr("type", "text").attr("value", Figure.getModelElement(accessor));
+  
+     foreign.on(fig.event, function() {
+        return handleUserInput(fig, "'" + foreign.select("input")[0][0].value + "'");
+     });
+}
+
+/********************* colorInput ***************************/
+
+function colorNameToHex(color)
+{
+    var colors = {"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff",
+    "beige":"#f5f5dc","bisque":"#ffe4c4","black":"#000000","blanchedalmond":"#ffebcd","blue":"#0000ff","blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887",
+    "cadetblue":"#5f9ea0","chartreuse":"#7fff00","chocolate":"#d2691e","coral":"#ff7f50","cornflowerblue":"#6495ed","cornsilk":"#fff8dc","crimson":"#dc143c","cyan":"#00ffff",
+    "darkblue":"#00008b","darkcyan":"#008b8b","darkgoldenrod":"#b8860b","darkgray":"#a9a9a9","darkgreen":"#006400","darkkhaki":"#bdb76b","darkmagenta":"#8b008b","darkolivegreen":"#556b2f",
+    "darkorange":"#ff8c00","darkorchid":"#9932cc","darkred":"#8b0000","darksalmon":"#e9967a","darkseagreen":"#8fbc8f","darkslateblue":"#483d8b","darkslategray":"#2f4f4f","darkturquoise":"#00ced1",
+    "darkviolet":"#9400d3","deeppink":"#ff1493","deepskyblue":"#00bfff","dimgray":"#696969","dodgerblue":"#1e90ff",
+    "firebrick":"#b22222","floralwhite":"#fffaf0","forestgreen":"#228b22","fuchsia":"#ff00ff",
+    "gainsboro":"#dcdcdc","ghostwhite":"#f8f8ff","gold":"#ffd700","goldenrod":"#daa520","gray":"#808080","green":"#008000","greenyellow":"#adff2f",
+    "honeydew":"#f0fff0","hotpink":"#ff69b4",
+    "indianred ":"#cd5c5c","indigo":"#4b0082","ivory":"#fffff0","khaki":"#f0e68c",
+    "lavender":"#e6e6fa","lavenderblush":"#fff0f5","lawngreen":"#7cfc00","lemonchiffon":"#fffacd","lightblue":"#add8e6","lightcoral":"#f08080","lightcyan":"#e0ffff","lightgoldenrodyellow":"#fafad2",
+    "lightgrey":"#d3d3d3","lightgreen":"#90ee90","lightpink":"#ffb6c1","lightsalmon":"#ffa07a","lightseagreen":"#20b2aa","lightskyblue":"#87cefa","lightslategray":"#778899","lightsteelblue":"#b0c4de",
+    "lightyellow":"#ffffe0","lime":"#00ff00","limegreen":"#32cd32","linen":"#faf0e6",
+    "magenta":"#ff00ff","maroon":"#800000","mediumaquamarine":"#66cdaa","mediumblue":"#0000cd","mediumorchid":"#ba55d3","mediumpurple":"#9370d8","mediumseagreen":"#3cb371","mediumslateblue":"#7b68ee",
+    "mediumspringgreen":"#00fa9a","mediumturquoise":"#48d1cc","mediumvioletred":"#c71585","midnightblue":"#191970","mintcream":"#f5fffa","mistyrose":"#ffe4e1","moccasin":"#ffe4b5",
+    "navajowhite":"#ffdead","navy":"#000080",
+    "oldlace":"#fdf5e6","olive":"#808000","olivedrab":"#6b8e23","orange":"#ffa500","orangered":"#ff4500","orchid":"#da70d6",
+    "palegoldenrod":"#eee8aa","palegreen":"#98fb98","paleturquoise":"#afeeee","palevioletred":"#d87093","papayawhip":"#ffefd5","peachpuff":"#ffdab9","peru":"#cd853f","pink":"#ffc0cb","plum":"#dda0dd","powderblue":"#b0e0e6","purple":"#800080",
+    "red":"#ff0000","rosybrown":"#bc8f8f","royalblue":"#4169e1",
+    "saddlebrown":"#8b4513","salmon":"#fa8072","sandybrown":"#f4a460","seagreen":"#2e8b57","seashell":"#fff5ee","sienna":"#a0522d","silver":"#c0c0c0","skyblue":"#87ceeb","slateblue":"#6a5acd","slategray":"#708090","snow":"#fffafa","springgreen":"#00ff7f","steelblue":"#4682b4",
+    "tan":"#d2b48c","teal":"#008080","thistle":"#d8bfd8","tomato":"#ff6347","turquoise":"#40e0d0",
+    "violet":"#ee82ee",
+    "wheat":"#f5deb3","white":"#ffffff","whitesmoke":"#f5f5f5",
+    "yellow":"#ffff00","yellowgreen":"#9acd32"};
+
+    if (typeof colors[color.toLowerCase()] != 'undefined')
+        return colors[color.toLowerCase()];
+
+    return color;
+}
+
+bboxFunction.colorInput = bboxFunction.buttonInput;
+
+drawFunction.colorInput = function (selection, x, y) {
+    var fig = this;
+    var accessor = this.accessor;
+    
+    var foreign = selection.append("foreignObject")
+        .attr("x", x).attr("y", y).attr("width", this.width).attr("height", this.height);
+    
+    console.log(Figure.getModelElement(accessor));
+    foreign.append("xhtml:body")
+        .append("form").attr("action", "").attr("onsubmit", "return false")
+        .append("input")
+            .style("width", this.width + "px").style("height", this.height + "px")
+            .attr("type", "color").attr("value", Figure.getModelElement(accessor));
+            
+    foreign.on("change", function() {
+            return handleUserInput(fig, "\"" + foreign.select("input")[0][0].value + "\"");
+     });
+}
+
+/********************* numInput ***************************/
+
+bboxFunction.numInput  = bboxFunction.buttonInput;
+
+drawFunction.numInput = function (selection, x, y) {
+    var fig = this;
+    var accessor = this.accessor;
+    
+    var foreign = selection.append("foreignObject")
+        .attr("x", x).attr("y", y).attr("width", this.width).attr("height", this.height);
+    
+    foreign.append("xhtml:body")
+        .append("form").attr("action", "").attr("onsubmit", "return false")
+        .append("input")
+            .style("width", this.width + "px").style("height", this.height + "px")
+            .attr("type", "number").attr("value", Figure.getModelElement(accessor));
+     
+     foreign.on(this.event, function() {
+            return handleUserInput(fig, foreign.select("input")[0][0].value);
+     });
+}
+
+/********************* rangeInput ***************************/
+
+bboxFunction.rangeInput = bboxFunction.buttonInput
+
+drawFunction.rangeInput = function (selection, x, y) { 
+    var fig = this;
+    var accessor = this.accessor;
+    
+    var foreign = selection.append("foreignObject")
+        .attr("x", x).attr("y", y).attr("width", this.width).attr("height", this.height);
+    
+    foreign.append("xhtml:body")
+        .append("form").attr("action", "").attr("onsubmit", "return false")
+        .append("input")
+            .style("width", this.width + "px").style("height", this.height + "px")
             .attr("type", "range")
             .attr("min", this.min).attr("max", this.max).attr("step", this.step)
             .attr("value", Figure.getModelElement(this.accessor));
         
-     var accessor = this.accessor;
-     
-     var handler = function() {
-            var v = foreign.select("input")[0][0].value;
-            d3.event.preventDefault();
-            Figure.setModelElement(accessor, v);
-            redrawFigure();
-        };
-
-     foreign.on("change", handler);
+     foreign.on(fig.event, function(){
+            return handleUserInput(fig, foreign.select("input")[0][0].value);
+     });
 }
--- src/org/rascalmpl/library/experiments/vis2/Properties.rsc
+++ src/org/rascalmpl/library/experiments/vis2/Properties.rsc
@@ -1,5 +1,7 @@
 module experiments::vis2::Properties
 
+alias Cursor[&T] = &T;
+
 data Bind[&T]
     = bind(Cursor[&T] accessor)
     | bind(Cursor[&T] accessor, &T val)
@@ -44,7 +46,7 @@ data FProperty =
 
 	// interaction
 	
-	| onClick(Bind[value] binder)
+	| on(str event, Bind[value] binder)
 	
 	// data sets	
 	
@@ -57,4 +59,42 @@ data HAlign = left() | hcenter() | right();
 data VAlign = top() | vcenter() | bottom();
 	
 public alias FProperties = list[FProperty];
+
+public set[str] legalEvents = {
+
+// Form events
+	"blur",				// Fires the moment that the element loses focus
+	"change",			// Fires the moment when the value of the element is changed
+	"contextmenu",		// Script to be run when a context menu is triggered
+	"focus",			// Fires the moment when the element gets focus
+	"formchange",		// Script to be run when a form changes
+	"forminput",		// Script to be run when a form gets user input
+	"input",			// Script to be run when an element gets user input
+	"invalid",			// Script to be run when an element is invalid
+	"select",			// Fires after some text has been selected in an element
+	"submit",			// Fires when a form is submitted
+	
+// Keyboard events
+	"keydown",			// Fires when a user is pressing a key
+	"keypress",			// Fires when a user presses a key
+	"keyup",			// Fires when a user releases a key
+	
+// Mouse events
+	"click",			// Fires on a mouse click on the element
+	"dbclick",			// Fires on a mouse double-click on the element
+	"drag",				// Script to be run when an element is dragged
+	"dragend",			// Script to be run at the end of a drag operation
+	"dragenter",		// Script to be run when an element has been dragged to a valid drop target
+	"dragleave",		// Script to be run when an element leaves a valid drop target
+	"dragover",			// Script to be run when an element is being dragged over a valid drop target
+	"dragstart",		// Script to be run at the start of a drag operation
+	"drop",				// Script to be run when dragged element is being dropped
+	"mousedown",		// Fires when a mouse button is pressed down on an element
+	"mousemove",		// Fires when the mouse pointer moves over an element
+	"mouseeout",		// Fires when the mouse pointer moves out of an element
+	"mouseover",		// Fires when the mouse pointer moves over an element
+	"mouseup",			// Fires when a mouse button is released over an element
+	"mousewheel",		// Script to be run when the mouse wheel is being rotated
+	"scroll"			// Script to be run when an element's scrollbar is being scrolled
+	};
 
