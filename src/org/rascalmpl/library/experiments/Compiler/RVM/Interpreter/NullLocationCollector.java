0162ce288b74153fb830318c96e88e1f7aa9504d
paulklint <Paul.Klint@cwi.nl>
2014-12-19 09:03:14 +0100

    Turned NullLocationCollector into a singleton.

--- src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/NullLocationCollector.java
+++ src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/NullLocationCollector.java
@@ -3,6 +3,14 @@ package org.rascalmpl.library.experiments.Compiler.RVM.Interpreter;
 import org.eclipse.imp.pdb.facts.ISourceLocation;
 
 public class NullLocationCollector implements ILocationCollector {
+	
+	private static final NullLocationCollector instance = new NullLocationCollector();
+	
+	private NullLocationCollector() { }
+	
+	public static NullLocationCollector getInstance() {
+		return instance;
+	}
 
 	public void registerLocation(ISourceLocation src) {
 		//System.err.println("NullLocationCollector.registerLocation: " + src);
