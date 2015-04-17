11edab10f666a145c0a00dc6a413a2cb5fd54286
Ferry Rietveld <Ferry.Rietveld@gmail.com>
2015-04-17 11:15:32 +0200

    New executable class.

---
+++ src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/RVMJVMExecutable.java
@@ -0,0 +1,28 @@
+package org.rascalmpl.library.experiments.Compiler.RVM.Interpreter;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.eclipse.imp.pdb.facts.IMap;
+import org.eclipse.imp.pdb.facts.IValueFactory;
+import org.eclipse.imp.pdb.facts.type.Type;
+import org.eclipse.imp.pdb.facts.type.TypeStore;
+
+public class RVMJVMExecutable extends RVMExecutable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6074410394887825412L;
+	
+	byte[] jvmByteCode ;
+
+	public RVMJVMExecutable(String module_name, IMap tags, IMap symbol_definitions, Map<String, Integer> functionMap, ArrayList<Function> functionStore,
+			Map<String, Integer> constructorMap, ArrayList<Type> constructorStore, Map<String, Integer> resolver, ArrayList<OverloadedFunction> overloadedStore,
+			ArrayList<String> initializers, ArrayList<String> testsuites, String uid_module_init, String uid_module_main, String uid_module_main_testsuite, TypeStore ts,
+			IValueFactory vfactory) {
+		super(module_name, tags, symbol_definitions, functionMap, functionStore, constructorMap, constructorStore, resolver, overloadedStore, initializers, testsuites,
+				uid_module_init, uid_module_main, uid_module_main_testsuite, ts, vfactory);
+	}
+
+}
