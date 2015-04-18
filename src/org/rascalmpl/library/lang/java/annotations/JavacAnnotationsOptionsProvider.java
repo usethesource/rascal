63d5f04f982ed621725f0c8fe28c7357dfd0a85c
Jurgen J. Vinju <jurgen@vinju.org>
2015-04-18 07:26:56 +0200

    minor issues

--- src/org/rascalmpl/library/lang/java/annotations/JavacAnnotationsOptionsProvider.java
+++ src/org/rascalmpl/library/lang/java/annotations/JavacAnnotationsOptionsProvider.java
@@ -4,25 +4,15 @@ import java.io.File;
 import java.util.Collections;
 import java.util.Map;
 
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.tools.JavaCompiler;
 import javax.tools.StandardJavaFileManager;
 import javax.tools.StandardLocation;
 import javax.tools.ToolProvider;
 
 public class JavacAnnotationsOptionsProvider implements AnnotationsOptionsProvider {
-	private final StandardJavaFileManager fm;
+	private StandardJavaFileManager fm;
 
-	public JavacAnnotationsOptionsProvider() {
-		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-		
-		if (compiler != null) {
-			this.fm = compiler.getStandardFileManager(null, null, null);
-		}
-		else {
-			this.fm = null;
-		}
-	}
-	
 	@Override
 	public Iterable<? extends File> getSourcePath() {
 		return fm != null ? fm.getLocation(StandardLocation.SOURCE_PATH) : Collections.<File>emptyList();
@@ -41,5 +31,17 @@ public class JavacAnnotationsOptionsProvider implements AnnotationsOptionsProvid
 	@Override
 	public Map<String, String> getOptions() {
 		return Collections.emptyMap();
+	}
+
+	@Override
+	public void init(ProcessingEnvironment processingEnv) {
+		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+		
+		if (compiler != null) {
+			fm = compiler.getStandardFileManager(null, null, null);
+		}
+		else {
+			fm = null;
+		}
 	}
 }
