5689a669b160a00edc3f5a9d7e76eedc94e162d2
paulklint <Paul.Klint@cwi.nl>
2015-05-05 14:31:34 +0200

    Optimized FSTSerializableIValue
    - Using BinaryWriter and BinaryReader
    - Avoid as much object creation as possible

--- src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/FSTSerializableIValue.java
+++ src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/FSTSerializableIValue.java
@@ -1,9 +1,9 @@
 package org.rascalmpl.library.experiments.Compiler.RVM.Interpreter;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.Serializable;
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,8 +14,8 @@ import org.eclipse.imp.pdb.facts.IValue;
 import org.eclipse.imp.pdb.facts.IValueFactory;
 import org.eclipse.imp.pdb.facts.exceptions.FactTypeUseException;
 import org.eclipse.imp.pdb.facts.impl.AbstractValueFactoryAdapter;
-import org.eclipse.imp.pdb.facts.io.StandardTextReader;
-import org.eclipse.imp.pdb.facts.io.StandardTextWriter;
+import org.eclipse.imp.pdb.facts.io.BinaryValueReader;
+import org.eclipse.imp.pdb.facts.io.BinaryValueWriter;
 import org.eclipse.imp.pdb.facts.type.Type;
 import org.eclipse.imp.pdb.facts.type.TypeFactory;
 import org.eclipse.imp.pdb.facts.type.TypeStore;
@@ -24,36 +24,56 @@ import org.rascalmpl.values.uptr.Factory;
 
 import de.ruedigermoeller.serialization.FSTBasicObjectSerializer;
 import de.ruedigermoeller.serialization.FSTClazzInfo;
-import de.ruedigermoeller.serialization.FSTObjectInput;
 import de.ruedigermoeller.serialization.FSTClazzInfo.FSTFieldInfo;
+import de.ruedigermoeller.serialization.FSTObjectInput;
 import de.ruedigermoeller.serialization.FSTObjectOutput;
 
+/**
+ * FSTSerializableIValue acts as a serializer and wrapper for IValues
+ * - On writing a FSTSerializableIValue oject is written
+ * - On reading, the wrapped IValue is returned.
+ *
+ */
+@SuppressWarnings("deprecation")
 public class FSTSerializableIValue extends FSTBasicObjectSerializer implements Serializable   {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 6704614265562953320L;
 	
 	private transient static IValueFactory vf;
 	private transient static TypeStore store;
 	private transient static TypeReifier tr;
+	private transient static ByteArrayOutputStream byteStream;
+
+	private static BinaryValueWriter binaryWriter;
+
+	private static BinaryValueReader binaryReader;
+
+	private static Type valueType;
 
 	public static void initSerialization(IValueFactory vfactory, TypeStore ts){
 		vf = vfactory;
 		store = ts;
 		store.extendStore(Factory.getStore());
 		tr = new TypeReifier(vf);
+		byteStream = new ByteArrayOutputStream();	// TODO should we remove it as well?
+		binaryWriter = new BinaryValueWriter();
+		binaryReader = new BinaryValueReader();
+		valueType = TypeFactory.getInstance().valueType();
 	}
 	
 	private transient IValue value;
 	
-	public FSTSerializableIValue(){
-		value = null;
+	/**
+	 *  Constructor used for registration of this serializer
+	 */
+	public FSTSerializableIValue() {
 	}
 
+
+	/**
+	 * Constructir used to wrap an IValue to be serialized
+	 */
 	public FSTSerializableIValue(IValue value) {
-		//System.out.println("FSTSerializableIValue.new: " + value);
 		this.value = value;
 	}
 	
@@ -65,9 +85,10 @@ public class FSTSerializableIValue extends FSTBasicObjectSerializer implements S
 	public void writeObject(FSTObjectOutput out, Object toWrite,
 			FSTClazzInfo clzInfo, FSTFieldInfo arg3, int arg4)
 					throws IOException {
-		StringWriter sw = new StringWriter();
-		new StandardTextWriter().write(((FSTSerializableIValue)toWrite).getValue(), sw);
-		out.writeObject(sw.toString());
+		byteStream.reset();
+		binaryWriter.write(((FSTSerializableIValue)toWrite).getValue(), byteStream, false);
+		byte[] bytes = byteStream.toByteArray();
+		out.writeObject(bytes);
 	}
 
 	@Override
@@ -81,16 +102,25 @@ public class FSTSerializableIValue extends FSTBasicObjectSerializer implements S
 	{		
 		FSTSerializableIValue res = null;
 		try {
-			String s = (String) in.readObject();
-			//System.out.println("FSTSerializableIValue read string: " + s);
-			StringReader sr = new StringReader(s);
-			IValue value = new StandardTextReader().read(new RascalValuesValueFactory(), store, TypeFactory.getInstance().valueType(), sr);
-			this.value = value;
-			res = new FSTSerializableIValue(value);
-			res.value = value;
-			in.registerObject(res,streamPosition,serializationInfo, referencee);
-			//System.out.println("FSTSerializableIValue.instantiate: " + value);
-			return res;
+			byte[] bytes = (byte[]) in.readObject();
+			ByteArrayInputStream bs = new ByteArrayInputStream(bytes);
+			
+			IValue v = binaryReader.read(new RascalValuesValueFactory(), store, valueType, bs);
+			bs.close();
+			
+			// TODO: why is this not handled by RascalValuesValueFactory????
+			if(v.getType().isNode()){
+				INode nd = (INode) v;
+				if(nd.getName().equals("type")){
+					//System.out.println("FSTSerializableIValue.instantiate: " + v);
+					java.util.Map<Type,Type> bindings = new HashMap<Type,Type>();
+					bindings.put(Factory.TypeParam, tr.symbolToType((IConstructor) nd.get(0), (IMap) nd.get(1)));
+					
+					v = vf.constructor(Factory.Type_Reified.instantiate(bindings), nd.get(0), nd.get(1));
+				}
+			}
+			in.registerObject(v,streamPosition,serializationInfo, referencee);
+			return v;
 		} catch (FactTypeUseException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -105,34 +135,87 @@ public class FSTSerializableIValue extends FSTBasicObjectSerializer implements S
 		public RascalValuesValueFactory() {
 			super(vf);
 		}
-
+		
 		@Override
-		public IConstructor constructor(Type constructor, IValue[] children,
-				Map<String, IValue> keyArgValues) {
-			IConstructor res = specializeType(constructor.getName(), children, keyArgValues);
-			return res != null ? res: super.constructor(constructor, children, keyArgValues);
+		public INode node(String name, IValue... children) {
+			
+//			System.out.println("node1: " + name);
+			IConstructor res = specializeType(name, children);
+			
+			return res != null ? res: vf.node(name, children);
 		}
-
+		
 		@Override
-		public INode node(String name, IValue[] children,
-				Map<String, IValue> keyArgValues) throws FactTypeUseException {
-			IConstructor res = specializeType(name, children, keyArgValues);
+		public INode node(String name, Map<String, IValue> annotations, IValue... children) throws FactTypeUseException {
+			
+//			System.out.println("node2: " + name);
+			IConstructor res = specializeType(name, children);
+			
+			return res != null ? res: vf.node(name, annotations, children);
+		}
+		
+		@Override
+		public INode node(String name, IValue[] children, Map<String, IValue> keyArgValues) throws FactTypeUseException {
+//			System.out.println("node3: " + name);
+			IConstructor res = specializeType(name, children);
+			
 			return res != null ? res: vf.node(name, children, keyArgValues);
 		}
 
-		public IConstructor specializeType(String name, IValue[] children,
-				Map<String, IValue> keyArgValues) {
-			if(name.equals("type") 
-					&& children.length == 2
-					&& children[0].getType().isSubtypeOf(Factory.Type_Reified.getFieldType(0))
-					&& children[1].getType().isSubtypeOf(Factory.Type_Reified.getFieldType(1))) {
-
+		private IConstructor specializeType(String name, IValue... children) {
+			if ("type".equals(name) 
+					//&& children.length == 2
+					//&& children[0].getType().isSubtypeOf(Factory.Type_Reified.getFieldType(0))
+					//&& children[1].getType().isSubtypeOf(Factory.Type_Reified.getFieldType(1))
+					) {
 				java.util.Map<Type,Type> bindings = new HashMap<Type,Type>();
 				bindings.put(Factory.TypeParam, tr.symbolToType((IConstructor) children[0], (IMap) children[1]));
-
+				
 				return vf.constructor(Factory.Type_Reified.instantiate(bindings), children[0], children[1]);
 			}
+			
 			return null;
 		}
+		
+		@Override
+		public IConstructor constructor(Type constructor, IValue[] children,
+				Map<String, IValue> keyArgValues) {
+			System.out.println("constructor1: " + constructor.getName());
+			IConstructor res = specializeType(constructor.getName(), children);
+			return res != null ? res: super.constructor(constructor, children, keyArgValues);
+		}
+		
+		 @Override
+		  public IConstructor constructor(Type constructor, Map<String, IValue> annotations, IValue... children)
+		      throws FactTypeUseException {
+			 System.out.println("constructor2: " + constructor.getName());
+			 IConstructor res = specializeType(constructor.getName(), children);
+			 return res != null ? res: super.constructor(constructor, children, annotations);
+		  }
+//	
+//
+//		@Override
+//		public INode node(String name, IValue[] children,
+//				Map<String, IValue> keyArgValues) throws FactTypeUseException {
+//			System.out.println("node: " + name);
+//			IConstructor res = specializeType(name, children, keyArgValues);
+//			return res != null ? res: vf.node(name, children, keyArgValues);
+//		}
+//
+//		public IConstructor specializeType(String name, IValue[] children,
+//				Map<String, IValue> keyArgValues) {
+//			System.out.println("specializeType: " + name);
+//			if(name.equals("type") 
+//					&& children.length == 2
+//					&& children[0].getType().isSubtypeOf(Factory.Type_Reified.getFieldType(0))
+//					&& children[1].getType().isSubtypeOf(Factory.Type_Reified.getFieldType(1))) {
+//
+//				java.util.Map<Type,Type> bindings = new HashMap<Type,Type>();
+//				bindings.put(Factory.TypeParam, tr.symbolToType((IConstructor) children[0], (IMap) children[1]));
+//
+//				return vf.constructor(Factory.Type_Reified.instantiate(bindings), children[0], children[1]);
+//			}
+//			return null;
+//		}
 	}
 }
