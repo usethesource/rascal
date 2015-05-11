23ec4b622bbe503ef013be5a458e45ae6fd89b97
Davy Landman <davy.landman@gmail.com>
2015-05-11 17:46:16 +0200

    Had to re-layout, it was a massacre of tabs and spaces

--- src/org/rascalmpl/uri/JarURIResolver.java
+++ src/org/rascalmpl/uri/JarURIResolver.java
@@ -9,7 +9,7 @@
 
  *   * Jurgen J. Vinju - Jurgen.Vinju@cwi.nl - CWI
  *   * Arnold Lankamp - Arnold.Lankamp@cwi.nl
-*******************************************************************************/
+ *******************************************************************************/
 package org.rascalmpl.uri;
 
 import java.io.File;
@@ -26,203 +26,203 @@ import java.util.jar.JarFile;
 import org.eclipse.imp.pdb.facts.ISourceLocation;
 
 public class JarURIResolver implements ISourceLocationInput{
-	
-	public JarURIResolver() {
-		super();
-	}
-	
-	private String getJar(ISourceLocation uri) throws IOException {
-		String path = uri.getPath();
-		if (path == null) {
-			path = uri.toString();
-		}
-		int bang = path.indexOf('!');
-		if (bang != -1) {
-		  return path.substring(path.indexOf("/"), bang);
-		}
-		else {
-			throw new IOException("The jar and the internal path should be separated with a !");
-		}
-	}
-	
-	private String getPath(ISourceLocation uri) {
-		String path = uri.getPath();
-		if (path == null) {
-			path = uri.toString();
-		}
-		int bang = path.indexOf('!');
-		
-		if (bang != -1) {
-		  path = path.substring(bang + 1);
-		  while (path.startsWith("/")) {
-		    path = path.substring(1);
-		  }
-		  return path;
-		}
-		else {
-		  return "";
-		}
-	}
-	
-	public InputStream getInputStream(ISourceLocation uri) throws IOException {
-	  String jar = getJar(uri);
+
+  public JarURIResolver() {
+    super();
+  }
+
+  private String getJar(ISourceLocation uri) throws IOException {
+    String path = uri.getPath();
+    if (path == null) {
+      path = uri.toString();
+    }
+    int bang = path.indexOf('!');
+    if (bang != -1) {
+      return path.substring(path.indexOf("/"), bang);
+    }
+    else {
+      throw new IOException("The jar and the internal path should be separated with a !");
+    }
+  }
+
+  private String getPath(ISourceLocation uri) {
+    String path = uri.getPath();
+    if (path == null) {
+      path = uri.toString();
+    }
+    int bang = path.indexOf('!');
+
+    if (bang != -1) {
+      path = path.substring(bang + 1);
+      while (path.startsWith("/")) {
+        path = path.substring(1);
+      }
+      return path;
+    }
+    else {
+      return "";
+    }
+  }
+
+  public InputStream getInputStream(ISourceLocation uri) throws IOException {
+    String jar = getJar(uri);
     String path = getPath(uri);
-    
+
     @SuppressWarnings("resource")
     // jarFile's are closed the moment when there are no more references to it.
     JarFile jarFile = new JarFile(jar);
     JarEntry jarEntry = jarFile.getJarEntry(path);
     return jarFile.getInputStream(jarEntry);
-	}
-	
-	public boolean exists(ISourceLocation uri) {
-		try {
-			String jar = getJar(uri);
-			String path = getPath(uri);
+  }
 
-			if (path == null || path.isEmpty() || path.equals("/")) {
-			  return new File(jar).exists();
-			}
-			try(JarFile jarFile = new JarFile(jar)) {
-			  JarEntry jarEntry = jarFile.getJarEntry(path);
-			  if (jarEntry != null) {
-			    return true;
-			  }
-			  // it might be a jar without separate directory entries.
-			  // so let's trye the more expensive check
-			  return jarFile.stream().anyMatch(e -> e.getName().startsWith(path));
-			}
-		} catch (IOException e) {
-			return false;
-		}
-	}
-	
-	public boolean isDirectory(ISourceLocation uri){
-		try {
-			if (uri.getPath() != null && (uri.getPath().endsWith(".jar!") || uri.getPath().endsWith(".jar!/"))) {
-				// if the uri is the root of a jar, and it ends with a ![/], it should be considered a directory
-				return true;
-			}
-			String jar = getJar(uri);
-			String path = getPath(uri);
-			
-			if (!path.endsWith("/")) {
-				path = path + "/";
-			}
-			
-			JarFile jarFile = new JarFile(jar);
-			try {
-				JarEntry jarEntry = jarFile.getJarEntry(path);
-				if (jarEntry != null && jarEntry.isDirectory()) {
-					return true;
-				}
-				// maybe the path is not in the jar as a seperate entry, but there are files in the path
-				Enumeration<JarEntry> entries =  jarFile.entries();
-				while (entries.hasMoreElements()) {
-					if (entries.nextElement().getName().startsWith(path)) {
-						return true;
-					}
-				}
-				return false;
-			}
-			finally {
-				jarFile.close();
-			}
-		} catch (IOException e) {
-			return false;
-		}
-	}
-	
-	public boolean isFile(ISourceLocation uri){
-		try {
-			String jar = getJar(uri);
-			String path = getPath(uri);
-			
-			JarFile jarFile = new JarFile(jar);
-			JarEntry jarEntry = jarFile.getJarEntry(path);
-			jarFile.close();
-			return(jarEntry != null && !jarEntry.isDirectory());
-		} catch (IOException e) {
-			return false;
-		}
-	}
-	
-	public long lastModified(ISourceLocation uri) throws IOException{
-		String jar = getJar(uri);
-		String path = getPath(uri);
-		
-		JarFile jarFile = new JarFile(jar);
-		JarEntry jarEntry = jarFile.getJarEntry(path);
-		jarFile.close();
-		
-		if (jarEntry == null) {
-			throw new FileNotFoundException(uri.toString());
-		}
-		
-		return jarEntry.getTime();
-	}
-	
-	@Override
-	public String[] list(ISourceLocation uri) throws IOException {
-		String jar = getJar(uri);
-		String path = getPath(uri);
-		
-		if (!path.endsWith("/") && !path.isEmpty()) {
-			path = path + "/";
-		}
-		
-		JarFile jarFile = new JarFile(jar);
-		
-		Enumeration<JarEntry> entries =  jarFile.entries();
-		ArrayList<String> matchedEntries = new ArrayList<>();
-		while (entries.hasMoreElements()) {
-			JarEntry je = entries.nextElement();
-			String name = je.getName();
-			
-			if (name.equals(path)) {
-				continue;
-			}
-			int index = name.indexOf(path);
-			
-			if (index == 0) {
-				String result = name.substring(path.length());
-				
-				index = result.indexOf("/");
-				
-				if (index == -1) {
-					matchedEntries.add(result);
-				} else {
-					result = result.substring(0, index);
-					boolean entryPresent = false;
-					for (Iterator<String> it = matchedEntries.iterator(); it.hasNext(); ) {
-						if (result.equals(it.next())) {
-							entryPresent = true;
-							break;
-						}
-					}
-					if (!entryPresent) {
-						matchedEntries.add(result);
-					}
-				}
-			}
-		}
-		jarFile.close();
-		
-		String[] listedEntries = new String[matchedEntries.size()];
-		return matchedEntries.toArray(listedEntries);
-	}
-	
-	public String scheme() {
-		return "jar";
-	}
+  public boolean exists(ISourceLocation uri) {
+    try {
+      String jar = getJar(uri);
+      String path = getPath(uri);
 
-	public boolean supportsHost() {
-		return false;
-	}
+      if (path == null || path.isEmpty() || path.equals("/")) {
+        return new File(jar).exists();
+      }
+      try(JarFile jarFile = new JarFile(jar)) {
+        JarEntry jarEntry = jarFile.getJarEntry(path);
+        if (jarEntry != null) {
+          return true;
+        }
+        // it might be a jar without separate directory entries.
+        // so let's trye the more expensive check
+        return jarFile.stream().anyMatch(e -> e.getName().startsWith(path));
+      }
+    } catch (IOException e) {
+      return false;
+    }
+  }
 
-	@Override
-	public Charset getCharset(ISourceLocation uri) throws IOException {
-		// TODO need to see if we can detect the charset inside a jar
-		return null;
-	}
+  public boolean isDirectory(ISourceLocation uri){
+    try {
+      if (uri.getPath() != null && (uri.getPath().endsWith(".jar!") || uri.getPath().endsWith(".jar!/"))) {
+        // if the uri is the root of a jar, and it ends with a ![/], it should be considered a directory
+        return true;
+      }
+      String jar = getJar(uri);
+      String path = getPath(uri);
+
+      if (!path.endsWith("/")) {
+        path = path + "/";
+      }
+
+      JarFile jarFile = new JarFile(jar);
+      try {
+        JarEntry jarEntry = jarFile.getJarEntry(path);
+        if (jarEntry != null && jarEntry.isDirectory()) {
+          return true;
+        }
+        // maybe the path is not in the jar as a seperate entry, but there are files in the path
+        Enumeration<JarEntry> entries =  jarFile.entries();
+        while (entries.hasMoreElements()) {
+          if (entries.nextElement().getName().startsWith(path)) {
+            return true;
+          }
+        }
+        return false;
+      }
+      finally {
+        jarFile.close();
+      }
+    } catch (IOException e) {
+      return false;
+    }
+  }
+
+  public boolean isFile(ISourceLocation uri){
+    try {
+      String jar = getJar(uri);
+      String path = getPath(uri);
+
+      JarFile jarFile = new JarFile(jar);
+      JarEntry jarEntry = jarFile.getJarEntry(path);
+      jarFile.close();
+      return(jarEntry != null && !jarEntry.isDirectory());
+    } catch (IOException e) {
+      return false;
+    }
+  }
+
+  public long lastModified(ISourceLocation uri) throws IOException{
+    String jar = getJar(uri);
+    String path = getPath(uri);
+
+    JarFile jarFile = new JarFile(jar);
+    JarEntry jarEntry = jarFile.getJarEntry(path);
+    jarFile.close();
+
+    if (jarEntry == null) {
+      throw new FileNotFoundException(uri.toString());
+    }
+
+    return jarEntry.getTime();
+  }
+
+  @Override
+  public String[] list(ISourceLocation uri) throws IOException {
+    String jar = getJar(uri);
+    String path = getPath(uri);
+
+    if (!path.endsWith("/") && !path.isEmpty()) {
+      path = path + "/";
+    }
+
+    JarFile jarFile = new JarFile(jar);
+
+    Enumeration<JarEntry> entries =  jarFile.entries();
+    ArrayList<String> matchedEntries = new ArrayList<>();
+    while (entries.hasMoreElements()) {
+      JarEntry je = entries.nextElement();
+      String name = je.getName();
+
+      if (name.equals(path)) {
+        continue;
+      }
+      int index = name.indexOf(path);
+
+      if (index == 0) {
+        String result = name.substring(path.length());
+
+        index = result.indexOf("/");
+
+        if (index == -1) {
+          matchedEntries.add(result);
+        } else {
+          result = result.substring(0, index);
+          boolean entryPresent = false;
+          for (Iterator<String> it = matchedEntries.iterator(); it.hasNext(); ) {
+            if (result.equals(it.next())) {
+              entryPresent = true;
+              break;
+            }
+          }
+          if (!entryPresent) {
+            matchedEntries.add(result);
+          }
+        }
+      }
+    }
+    jarFile.close();
+
+    String[] listedEntries = new String[matchedEntries.size()];
+    return matchedEntries.toArray(listedEntries);
+  }
+
+  public String scheme() {
+    return "jar";
+  }
+
+  public boolean supportsHost() {
+    return false;
+  }
+
+  @Override
+  public Charset getCharset(ISourceLocation uri) throws IOException {
+    // TODO need to see if we can detect the charset inside a jar
+    return null;
+  }
 }
