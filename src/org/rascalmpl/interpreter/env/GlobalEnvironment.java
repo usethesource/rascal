3ad39a16a6bf0b2130032409c2d0abf9a14c15f3
Jurgen J. Vinju <jurgen@vinju.org>
2015-05-26 19:27:05 +0200

    Revert "* simplified and fixed getExtendingModules function. This fixes problems with reloading extended modules."
    This reverts commit a4e93edfdfb87e81c7cc4588a212493e0a8a59f1.

--- src/org/rascalmpl/interpreter/env/GlobalEnvironment.java
+++ src/org/rascalmpl/interpreter/env/GlobalEnvironment.java
@@ -214,13 +214,25 @@ public class GlobalEnvironment {
 	
 	public Set<String> getExtendingModules(String mod) {
 		Set<String> result = new HashSet<String>();
+		List<String> todo = new LinkedList<String>();
+		todo.add(mod);
 		
-		for (ModuleEnvironment env : moduleEnvironment.values()) {
-			if (env.getExtends().contains(mod)) {
-				result.add(env.getName());
+		while (!todo.isEmpty()) {
+			String next = todo.remove(0);
+			
+			for (ModuleEnvironment env : moduleEnvironment.values()) {
+				if (env.getExtends().contains(next)) {
+					if (!result.contains(next)) {
+						todo.add(env.getName());
+						result.add(next);
+						todo.removeAll(result);
+					}
+				}
 			}
+			result.add(next);
 		}
-
+		
+		result.remove(mod);
 		return result;
 	}
 	
