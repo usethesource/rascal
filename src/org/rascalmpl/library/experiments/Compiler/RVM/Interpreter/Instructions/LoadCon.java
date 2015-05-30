bf8524b46fa85b232c6c063be8ec53d9d4164bcb
Ferry Rietveld <Ferry.Rietveld@gmail.com>
2015-05-30 16:27:31 +0200

    Removed the initialization functions for a local constantStore and typeConstant store.

--- src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/Instructions/LoadCon.java
+++ src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/Instructions/LoadCon.java
@@ -7,23 +7,27 @@ import org.rascalmpl.library.experiments.Compiler.RVM.ToJVM.BytecodeGenerator;
 public class LoadCon extends Instruction {
 
 	int constant;
-	
-	public LoadCon(CodeBlock cb, int constant){
+
+	public LoadCon(CodeBlock cb, int constant) {
 		super(cb, Opcode.LOADCON);
 		this.constant = constant;
 	}
-	
-	public String toString() { return "LOADCON " + constant + "[" + codeblock.getConstantValue(constant) + "]"; }
-	
-	public void generate(){
+
+	public String toString() {
+		return "LOADCON " + constant + "[" + codeblock.getConstantValue(constant) + "]";
+	}
+
+	public void generate() {
 		codeblock.addCode1(opcode.getOpcode(), constant);
 	}
-	public void generateByteCode(BytecodeGenerator codeEmittor, boolean debug){
-		if ( debug ) 
+
+	public void generateByteCode(BytecodeGenerator codeEmittor, boolean debug) {
+		if (debug) {
 			codeEmittor.emitDebugCall(opcode.name());
-		
-		IValue val = codeblock.getConstantValue(constant) ;
-		codeEmittor.emitCallWithArgsSSFI("insnLOADCON", constant, debug);
-//		codeEmittor.emitInlineLoadConOrType(constant,true,debug);
+			codeEmittor.emitCallWithArgsSSFI("insnLOADCON", constant, debug);
+		}
+
+		IValue val = codeblock.getConstantValue(constant);
+		codeEmittor.emitInlineLoadConOrType(constant,true,debug);
 	}
 }
