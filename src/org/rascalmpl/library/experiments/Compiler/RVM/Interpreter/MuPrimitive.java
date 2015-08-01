b5f7f44bf35406b3e4e8fd888e6e0161f314a26a
paulklint <Paul.Klint@cwi.nl>
2015-08-01 22:25:10 +0200

    Added bool_or_bool and elm_add_rel

--- src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/MuPrimitive.java
+++ src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/MuPrimitive.java
@@ -971,13 +971,13 @@ public enum MuPrimitive {
 			assert arity == 4;
 			IString id = (IString) stack[sp - 4];
 			DescendantDescriptor desc = descendantDescriptorMap.get(id);
-			if(desc == null){
+			//if(desc == null){
 				ISet symbolset = (ISet) stack[sp - 3];
 				IBool concreteMatch = (IBool) stack[sp - 2];
 				IMap definitions = (IMap) stack[sp - 1];
 				desc = new DescendantDescriptor(vf, symbolset, definitions, concreteMatch);
 				descendantDescriptorMap.put(id,  desc);
-			}
+			//}
 			stack[sp - 4] = desc;
 			return sp - 3;
 		};
