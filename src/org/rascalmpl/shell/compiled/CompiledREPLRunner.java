99859d0ce6a21902b4f2217b8599e316414f3b4f
paulklint <Paul.Klint@cwi.nl>
2015-08-28 22:48:25 +0200

    First steps towards CompiledREPL

---
+++ src/org/rascalmpl/shell/compiled/CompiledREPLRunner.java
@@ -0,0 +1,46 @@
+package org.rascalmpl.shell.compiled;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+import jline.TerminalFactory;
+
+import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.repl.CompiledRascalREPL;
+import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.repl.Executor;
+import org.rascalmpl.shell.ShellRunner;
+
+public class CompiledREPLRunner extends CompiledRascalREPL  implements ShellRunner {
+
+  private static File getHistoryFile() throws IOException {
+    File home = new File(System.getProperty("user.home"));
+    File rascal = new File(home, ".rascal");
+    if (!rascal.exists()) {
+      rascal.mkdirs();
+    }
+    File historyFile = new File(rascal, ".repl-history-rascal-terminal");
+    if (!historyFile.exists()) {
+      historyFile.createNewFile();
+    }
+    return historyFile;
+  }
+
+  public CompiledREPLRunner(InputStream stdin, OutputStream stdout) throws IOException {
+    super(stdin, stdout, true, true, getHistoryFile(), TerminalFactory.get());
+    setMeasureCommandTime(false);
+  }
+
+  @Override
+  protected Executor constructExecutor(Writer stdout, Writer stderr) {
+    return new Executor(new PrintWriter(stdout), new PrintWriter(stderr));
+  }
+
+  @Override
+  public void run(String[] args) throws IOException {
+    // there are no args for now
+    run();
+  }
+}
---
+++ src/org/rascalmpl/shell/compiled/CompiledRascalShell.java
@@ -0,0 +1,65 @@
+package org.rascalmpl.shell.compiled;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.Manifest;
+
+import org.rascalmpl.interpreter.utils.RascalManifest;
+import org.rascalmpl.shell.ManifestRunner;
+import org.rascalmpl.shell.ModuleRunner;
+import org.rascalmpl.shell.ShellRunner;
+
+
+public class CompiledRascalShell  {
+
+  private static void printVersionNumber(){
+    try {
+      Enumeration<URL> resources = CompiledRascalShell.class.getClassLoader().getResources("META-INF/MANIFEST.MF");
+      while (resources.hasMoreElements()) {
+        Manifest manifest = new Manifest(resources.nextElement().openStream());
+        String bundleName = manifest.getMainAttributes().getValue("Bundle-Name");
+        if (bundleName != null && bundleName.equals("rascal-shell")) {
+          String result = manifest.getMainAttributes().getValue("Bundle-Version");
+          if (result != null) {
+            System.out.println("Version: " + result);
+            return;
+          }
+        }
+      }
+    } catch (IOException E) {
+    }
+    System.out.println("Version: unknown");
+  }
+
+
+  public static void main(String[] args) throws IOException {
+    printVersionNumber();
+    RascalManifest mf = new RascalManifest();
+    System.err.println("CompiledRascalShell");
+    try {
+      ShellRunner runner; 
+      if (mf.hasManifest(CompiledRascalShell.class) && mf.hasMainModule(CompiledRascalShell.class)) {
+        runner = new ManifestRunner(mf, new PrintWriter(System.out), new PrintWriter(System.err));
+      } 
+      else if (args.length > 0) {
+        runner = new ModuleRunner(new PrintWriter(System.out), new PrintWriter(System.err));
+      } 
+      else {
+        runner = new CompiledREPLRunner(System.in, System.out);
+      }
+      runner.run(args);
+
+      System.exit(0);
+    }
+    catch (Throwable e) {
+      System.err.println("\n\nunexpected error: " + e.getMessage());
+      System.exit(1);
+    }
+    finally {
+      System.out.flush();
+      System.err.flush();
+    }
+  }
+}
