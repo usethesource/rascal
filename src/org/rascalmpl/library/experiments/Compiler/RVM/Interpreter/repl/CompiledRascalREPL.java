97ced105f2c822b64eb099bb9117ff6481e1268c
paulklint <Paul.Klint@cwi.nl>
2015-09-08 23:17:59 +0200

    Evolving RascalShell for compiler

--- src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/repl/CompiledRascalREPL.java
+++ src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/repl/CompiledRascalREPL.java
@@ -37,6 +37,7 @@ public abstract class CompiledRascalREPL extends BaseRascalREPL {
 
   protected Executor executor;
   private boolean measureCommandTime;
+  private boolean semiColonAdded = false;
   
   public CompiledRascalREPL(InputStream stdin, OutputStream stdout, boolean prettyPrompt, boolean allowColors, File persistentHistory, Terminal terminal)
       throws IOException {
@@ -70,84 +71,93 @@ public abstract class CompiledRascalREPL extends BaseRascalREPL {
 
   @Override
   protected IRascalResult evalStatement(String statement, String lastLine) throws InterruptedException {
-    try {
-        if(statement.equals(":quit")){
-    		stop();
-    		return null;
-    	}
-      Timing tm = new Timing();
-      tm.start();
-      IValue value = executor.eval(null, statement, URIUtil.rootLocation("prompt"));
-      long duration = tm.duration();
-      if (measureCommandTime) {
-        executor.getStdErr().println("\nTime: " + (duration / 1000000) + "ms");
-      }
-      return new IRascalResult() {
-		
-		@Override
-		public IValue getValue() {
-			return value;
-		}
-		
-		@Override
-		public Type getType() {
-			return value.getType();	// TODO: change to static type?
-		}
-	};
-    }
-    catch (ParseError pe) {
-      executor.getStdErr().println(parseErrorMessage(lastLine, "prompt", pe));
-      return null;
-    }
-    catch (StaticError e) {
-      executor.getStdErr().println(staticErrorMessage(e));
-      return null;
-    }
-    catch (Throw e) {
-      executor.getStdErr().println(throwMessage(e));
-      return null;
-    }
-    catch (QuitException q) {
-      executor.getStdErr().println("Quiting REPL");
-      throw new InterruptedException();
-    }
-//    catch (Throwable e) {
-//      eval.getStdErr().println(throwableMessage(e, eval.getStackTrace()));
-//      return null;
-//    }
+	  try {
+		  if(semiColonAdded){
+			  statement = statement + ";";
+			  semiColonAdded = false;
+		  }
+		  if(statement.trim().equals(":quit")){
+			  stop();
+			  return null;
+		  }
+		  Timing tm = new Timing();
+		  tm.start();
+		  IValue value = executor.eval(null, statement, URIUtil.rootLocation("prompt"));
+		  long duration = tm.duration();
+		  if (measureCommandTime) {
+			  executor.getStdErr().println("Time: " + (duration / 1000000) + "ms");
+		  }
+		  return new IRascalResult() {
+
+			  @Override
+			  public IValue getValue() {
+				  return value;
+			  }
+
+			  @Override
+			  public Type getType() {
+				  return value.getType();	// TODO: change to static type?
+			  }
+		  };
+	  }
+	  catch (ParseError pe) {
+		  executor.getStdErr().println(parseErrorMessage(lastLine, "prompt", pe));
+		  return null;
+	  }
+	  catch (StaticError e) {
+		  executor.getStdErr().println(staticErrorMessage(e));
+		  return null;
+	  }
+	  catch (Throw e) {
+		  executor.getStdErr().println(throwMessage(e));
+		  return null;
+	  }
+	  catch (QuitException q) {
+		  executor.getStdErr().println("Quiting REPL");
+		  throw new InterruptedException();
+	  }
+	  //    catch (Throwable e) {
+	  //      eval.getStdErr().println(throwableMessage(e, eval.getStackTrace()));
+	  //      return null;
+	  //    }
   }
 
   @Override
   protected boolean isStatementComplete(String command) {
-    try {
-      executor.parseCommand(null, command, URIUtil.rootLocation("prompt"));
-    }
-    catch (ParseError pe) {
-      String[] commandLines = command.split("\n");
-      int lastLine = commandLines.length;
-      int lastColumn = commandLines[lastLine - 1].length();
+	  try {
+		  executor.parseCommand(null, command, URIUtil.rootLocation("prompt"));
+	  }
+	  catch (ParseError pe) {
+		  String[] commandLines = command.split("\n");
+		  int lastLine = commandLines.length;
+		  int lastColumn = commandLines[lastLine - 1].length();
 
-      if (pe.getEndLine() + 1 == lastLine && lastColumn <= pe.getEndColumn()) { 
-        return false;
-      }
-    }
-    return true;
+		  if (pe.getEndLine() + 1 == lastLine && lastColumn < pe.getEndColumn()) { 
+			  semiColonAdded = false;
+			  return false;
+		  }
+		  if (pe.getEndLine() + 1 == lastLine && lastColumn == pe.getEndColumn()) { 
+			  semiColonAdded = true;
+			  return isStatementComplete(command + ";");
+		  }
+	  }
+	  return true;
   }
 
   @Override
   protected boolean supportsCompletion() {
-    return false;
+	  return true;
   }
 
   @Override
   protected CompletionResult completeFragment(String line, int cursor) {
-    OffsetLengthTerm identifier = StringUtils.findRascalIdentifierAtOffset(line, cursor);
-    if (identifier != null) {
-      Collection<String> suggestions = executor.completePartialIdentifier(identifier.term);
-      if (suggestions != null && ! suggestions.isEmpty()) {
-        return new CompletionResult(identifier.offset, identifier.length, suggestions);
-      }
-    }
-    return null;
+	  OffsetLengthTerm identifier = StringUtils.findRascalIdentifierAtOffset(line, cursor);
+	  if (identifier != null) {
+		  Collection<String> suggestions = executor.completePartialIdentifier(identifier.term);
+		  if (suggestions != null && ! suggestions.isEmpty()) {
+			  return new CompletionResult(identifier.offset, identifier.length, suggestions);
+		  }
+	  }
+	  return null;
   }
 }
