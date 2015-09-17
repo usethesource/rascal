841d24bc4dbe9002db275728737fdef3c4c6f050
Davy Landman <davy.landman@gmail.com>
2015-09-17 17:16:07 +0200

    First support for completing source locations in the repl

--- src/org/rascalmpl/repl/BaseRascalREPL.java
+++ src/org/rascalmpl/repl/BaseRascalREPL.java
@@ -6,19 +6,41 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.Writer;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.Set;
+import java.util.TreeSet;
 
 import jline.Terminal;
 
 import org.eclipse.imp.pdb.facts.IConstructor;
+import org.eclipse.imp.pdb.facts.ISourceLocation;
 import org.eclipse.imp.pdb.facts.IValue;
 import org.eclipse.imp.pdb.facts.io.StandardTextWriter;
 import org.eclipse.imp.pdb.facts.type.Type;
 import org.rascalmpl.interpreter.result.IRascalResult;
 import org.rascalmpl.interpreter.utils.ReadEvalPrintDialogMessages;
+import org.rascalmpl.interpreter.utils.StringUtils;
+import org.rascalmpl.interpreter.utils.StringUtils.OffsetLengthTerm;
+import org.rascalmpl.uri.URIResolverRegistry;
+import org.rascalmpl.values.ValueFactoryFactory;
 import org.rascalmpl.values.uptr.RascalValueFactory;
 import org.rascalmpl.values.uptr.TreeAdapter;
 
 public abstract class BaseRascalREPL extends BaseREPL {
+    protected enum State {
+        FRESH,
+        CONTINUATION,
+        DEBUG,
+        DEBUG_CONTINUATION
+    }
+
+    private State currentState = State.FRESH;
+    
+    protected State getState() {
+        return currentState;
+    }
 
   private final static int LINE_LIMIT = 200;
   private final static int CHAR_LIMIT = LINE_LIMIT * 20;
@@ -55,6 +77,7 @@ public abstract class BaseRascalREPL extends BaseREPL {
         getErrorWriter().println(ReadEvalPrintDialogMessages.CANCELLED);
         currentPrompt = ReadEvalPrintDialogMessages.PROMPT;
         currentCommand = null;
+        currentState = State.FRESH;
         return;
       }
       if (currentCommand == null) {
@@ -65,6 +88,7 @@ public abstract class BaseRascalREPL extends BaseREPL {
         else {
           currentCommand = new StringBuffer(line);
           currentPrompt = ReadEvalPrintDialogMessages.CONTINUE_PROMPT;
+          currentState = State.CONTINUATION;
           return;
         }
       }
@@ -75,6 +99,7 @@ public abstract class BaseRascalREPL extends BaseREPL {
           printResult(evalStatement(currentCommand.toString(), line));
           currentPrompt = ReadEvalPrintDialogMessages.PROMPT;
           currentCommand = null;
+          currentState = State.FRESH;
           return;
         }
       }
@@ -126,5 +151,84 @@ public abstract class BaseRascalREPL extends BaseREPL {
 
   protected abstract boolean isStatementComplete(String command);
   protected abstract IRascalResult evalStatement(String statement, String lastLine) throws InterruptedException;
+  
+  protected abstract Collection<String> completePartialIdentifier(String term);
+  
+  @Override
+  protected CompletionResult completeFragment(String line, int cursor) {
+      if (currentState == State.FRESH) {
+          String trimmedLine = line.trim();
+          if (trimmedLine.startsWith(":")) {
+              return completeREPLCommand(line, cursor);
+          }
+          if (trimmedLine.startsWith("import ") || trimmedLine.startsWith("extend ")) {
+              return completeModule(line, cursor);
+          }
+      }
+      int locationStart = StringUtils.findRascalLocationStart(line, cursor);
+      if (locationStart != -1) {
+          return completeLocation(line, locationStart);
+      }
+      return completeIdentifier(line, cursor);
+  }
 
+  private CompletionResult completeIdentifier(String line, int cursor) {
+      OffsetLengthTerm identifier = StringUtils.findRascalIdentifierAtOffset(line, cursor);
+      if (identifier != null) {
+          Collection<String> suggestions = completePartialIdentifier(identifier.term);
+          if (suggestions != null && ! suggestions.isEmpty()) {
+              return new CompletionResult(identifier.offset, identifier.length, suggestions);
+          }
+      }
+      return null;
+  }
+  
+  private CompletionResult completeLocation(String line, int locationStart) {
+      int locationEnd = StringUtils.findRascalLocationEnd(line, locationStart);
+      try {
+          String locCandidate = line.substring(locationStart + 1, locationEnd + 1);
+          if (!locCandidate.contains("://")) {
+              return null;
+          }
+          ISourceLocation directory = ValueFactoryFactory.getValueFactory().sourceLocation(new URI(locCandidate));
+          String fileName = "";
+          URIResolverRegistry reg = URIResolverRegistry.getInstance();
+          if (!reg.isDirectory(directory)) {
+              // split filename and directory
+              String fullPath = directory.getPath();
+              int lastSeparator = fullPath.lastIndexOf('/');
+              fileName = fullPath.substring(lastSeparator +  1);
+              fullPath = fullPath.substring(0, lastSeparator);
+              directory = ValueFactoryFactory.getValueFactory().sourceLocation(directory.getScheme(), directory.getAuthority(), fullPath);
+              if (!reg.isDirectory(directory)) {
+                  return null;
+              }
+          }
+          // prefix is the directory location minus the |'s
+          String prefix = directory.toString().substring(1);
+          prefix = prefix.substring(0, prefix.length() - 1);
+          if (!prefix.endsWith("/")) {
+              prefix += "/";
+          }
+          String[] filesInPath = reg.listEntries(directory);
+          Set<String> result = new TreeSet<>(); // sort it up
+          for (String currentFile : filesInPath) {
+              if (currentFile.startsWith(fileName)) {
+                  result.add(prefix + currentFile);
+              }
+          }
+          return new CompletionResult(locationStart + 1, (locationEnd - locationStart) - 1, result);
+      }
+      catch (URISyntaxException|IOException e) {
+          return null;
+      }
+  }
+
+  private CompletionResult completeModule(String line, int cursor) {
+      return null;
+  }
+  
+  private CompletionResult completeREPLCommand(String line, int cursor) {
+      return null;
+  }
 }
