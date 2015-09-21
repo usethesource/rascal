12254331f55b17b49c73e48e093cb42b5a31f63e
Jurgen J. Vinju <jurgen@vinju.org>
2015-09-21 13:59:10 +0200

    Changes following refactoring of URIRegistry in Rascal project

--- src/org/rascalmpl/shell/ManifestRunner.java
+++ src/org/rascalmpl/shell/ManifestRunner.java
@@ -2,7 +2,6 @@ package org.rascalmpl.shell;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 
 import org.eclipse.imp.pdb.facts.IInteger;
 import org.eclipse.imp.pdb.facts.IValue;
@@ -12,58 +11,49 @@ import org.rascalmpl.interpreter.Evaluator;
 import org.rascalmpl.interpreter.NullRascalMonitor;
 import org.rascalmpl.interpreter.staticErrors.CommandlineError;
 import org.rascalmpl.interpreter.utils.RascalManifest;
-import org.rascalmpl.uri.ClassResourceInput;
-import org.rascalmpl.uri.URIResolverRegistry;
 import org.rascalmpl.uri.URIUtil;
 
 public class ManifestRunner implements ShellRunner {
+    private final RascalManifest mf;
+    private final Evaluator eval;
 
-  private final RascalManifest mf;
-  private final Evaluator eval;
 
-  public ManifestRunner(RascalManifest mf, PrintWriter stdout, PrintWriter stderr) {
-    assert mf.hasManifest(ManifestRunner.class);
-    this.mf = mf;
-    this.eval = ShellEvaluatorFactory.getDefaultEvaluator(stdout, stderr);
-    addExtraSourceFolders();
-  }
-
-  void addExtraSourceFolders() {
-    List<String> roots = mf.getSourceRoots(ManifestRunner.class);
-
-    int count = 0;
-    for (String root : roots) {
-      String scheme = "root" + count;
-      URIResolverRegistry.getInstance().registerInput(new ClassResourceInput(scheme, ManifestRunner.class, "/" + root));
-      eval.addRascalSearchPath(URIUtil.rootLocation(scheme)); 
+    public ManifestRunner(RascalManifest mf, PrintWriter stdout, PrintWriter stderr) {
+        assert mf.hasManifest(ManifestRunner.class);
+        this.mf = mf;
+        this.eval = ShellEvaluatorFactory.getDefaultEvaluator(stdout, stderr);
+        addExtraSourceFolders();
     }
-  }
 
-  @Override
-  public void run(String[] args) throws IOException {
-    IRascalMonitor monitor = new NullRascalMonitor();
-    String module = mf.getMainModule(ManifestRunner.class);
-    assert module != null;
-    eval.doImport(monitor, module);
-
-    String main = mf.getMainFunction(ManifestRunner.class);
-    main = main != null ? main : RascalManifest.DEFAULT_MAIN_FUNCTION;
-
-    try {
-      IValue v = eval.main(monitor, module, main, args);
-
-      if (v.getType().isInteger()) {
-        System.exit(((IInteger) v).intValue());
-      } else {
-        new StandardTextWriter(true).write(v, eval.getStdOut());
-        eval.getStdOut().flush();
-        System.exit(0);
-      }
-    } catch (CommandlineError e) {
-      System.err.println(e.getMessage());
-      System.err.println(e.help("java -jar ..."));
+    void addExtraSourceFolders() {
+        for (String root : mf.getSourceRoots(ManifestRunner.class)) {
+            eval.addRascalSearchPath(URIUtil.getChildLocation(URIUtil.rootLocation("manifest"), root)); 
+        }
     }
-  }
 
+    @Override
+    public void run(String[] args) throws IOException {
+        IRascalMonitor monitor = new NullRascalMonitor();
+        String module = mf.getMainModule(ManifestRunner.class);
+        assert module != null;
+        eval.doImport(monitor, module);
 
+        String main = mf.getMainFunction(ManifestRunner.class);
+        main = main != null ? main : RascalManifest.DEFAULT_MAIN_FUNCTION;
+
+        try {
+            IValue v = eval.main(monitor, module, main, args);
+
+            if (v.getType().isInteger()) {
+                System.exit(((IInteger) v).intValue());
+            } else {
+                new StandardTextWriter(true).write(v, eval.getStdOut());
+                eval.getStdOut().flush();
+                System.exit(0);
+            }
+        } catch (CommandlineError e) {
+            System.err.println(e.getMessage());
+            System.err.println(e.help("java -jar ..."));
+        }
+    }
 }
