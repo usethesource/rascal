ab72273826a2f4757cc914d38c192441f71dfa72
Tijs van der Storm <storm@cwi.nl>
2015-10-01 11:16:59 +0200

    Term repls.

---
+++ src/org/rascalmpl/library/util/TermREPL.java
@@ -0,0 +1,143 @@
+package org.rascalmpl.library.util;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+import jline.TerminalFactory;
+
+import org.eclipse.imp.pdb.facts.IConstructor;
+import org.eclipse.imp.pdb.facts.IInteger;
+import org.eclipse.imp.pdb.facts.IList;
+import org.eclipse.imp.pdb.facts.ISourceLocation;
+import org.eclipse.imp.pdb.facts.IString;
+import org.eclipse.imp.pdb.facts.ITuple;
+import org.eclipse.imp.pdb.facts.IValue;
+import org.eclipse.imp.pdb.facts.IValueFactory;
+import org.eclipse.imp.pdb.facts.type.Type;
+import org.eclipse.imp.pdb.facts.type.TypeFactory;
+import org.rascalmpl.interpreter.Evaluator;
+import org.rascalmpl.interpreter.IEvaluatorContext;
+import org.rascalmpl.interpreter.result.ICallableValue;
+import org.rascalmpl.repl.BaseREPL;
+import org.rascalmpl.repl.CompletionResult;
+
+public class TermREPL {
+	
+	private final IValueFactory vf;
+
+	public TermREPL(IValueFactory vf) {
+		this.vf = vf;
+  }
+	
+	public void startREPL(IConstructor repl, IEvaluatorContext ctx) {
+		try {
+	    new TheREPL(repl, ctx).run();
+    } catch (IOException e) {
+	    e.printStackTrace(ctx.getStdErr());
+    }
+	}
+	
+	class TheREPL extends BaseREPL {
+		private final TypeFactory tf = TypeFactory.getInstance();
+		private PrintWriter stdout;
+		private PrintWriter stderr;
+		private final IConstructor repl;
+		private String currentPrompt;
+		private final ICallableValue handler;
+		private final IEvaluatorContext ctx;
+		private final ICallableValue completor;
+
+		public TheREPL(IConstructor repl, IEvaluatorContext ctx) throws IOException {
+			super(System.in, System.out, true, true, ((ISourceLocation)repl.get("history")), TerminalFactory.get());
+			this.repl = repl;
+			this.ctx = ctx;
+			this.handler = (ICallableValue)repl.get("handler");
+			this.completor = (ICallableValue)repl.get("completor");
+			this.currentPrompt = ((IString)repl.get("prompt")).getValue();
+    }
+		
+		
+		@Override
+    protected void initialize(Writer stdout, Writer stderr) {
+			this.stdout = new PrintWriter(stdout);
+			this.stderr = new PrintWriter(stderr);
+    }
+
+		@Override
+    protected String getPrompt() {
+	    return currentPrompt;
+    }
+
+		@Override
+    protected void handleInput(String line) throws InterruptedException {
+			ITuple result = (ITuple)call(handler, new Type[] { tf.stringType() }, new IValue[] { vf.string(line) });
+			String str = ((IString)result.get("result")).getValue();
+			if (!str.isEmpty()) {
+				stdout.write(str + "\n");
+			}
+			
+			IList errors = (IList)result.get("messages");
+			for (IValue v: errors) {
+				IConstructor msg = (IConstructor)v;
+				stderr.write(msg.toString() + "\n");
+			}
+			
+			currentPrompt = ((IString)result.get("prompt")).getValue();
+    }
+
+		@Override
+    protected boolean supportsCompletion() {
+	    return true;
+    }
+
+		@Override
+    protected boolean printSpaceAfterFullCompletion() {
+	    return false;
+    }
+
+		private IValue call(ICallableValue f, Type[] types, IValue[] args) {
+			IValue result;
+			synchronized (ctx) {
+				Evaluator eval = (Evaluator)ctx;
+				PrintWriter prevErr = eval.getStdErr();
+				PrintWriter prevOut = eval.getStdOut();
+				try {
+					eval.overrideDefaultWriters(stdout, stderr);
+					return f.call(types, args, null).getValue();
+				}
+				finally {
+					stdout.flush();
+					stderr.flush();
+					eval.overrideDefaultWriters(prevOut, prevErr);
+				}
+      }
+		}
+		
+		@Override
+    protected CompletionResult completeFragment(String line, int cursor) {
+			ITuple result = (ITuple)call(completor, new Type[] { tf.stringType(), tf.integerType() },
+																				new IValue[] { vf.string(line), vf.integer(cursor) }); 
+			
+			List<String> suggestions = new ArrayList<>();
+			
+			for (IValue v: (IList)result.get("suggestions")) {
+				suggestions.add(((IString)v).getValue());
+			}
+			
+			if (suggestions.isEmpty()) {
+				return null;
+			}
+			
+			int offset = ((IInteger)result.get("offset")).intValue();
+			
+			return new CompletionResult(offset, suggestions);
+    }
+		
+	}
+	
+	
+
+}
--- src/org/rascalmpl/repl/BaseREPL.java
+++ src/org/rascalmpl/repl/BaseREPL.java
@@ -18,6 +18,7 @@ import jline.console.ConsoleReader;
 import jline.console.completer.CandidateListCompletionHandler;
 import jline.console.completer.Completer;
 import jline.console.history.FileHistory;
+import jline.console.history.PersistentHistory;
 import jline.internal.ShutdownHooks;
 import jline.internal.ShutdownHooks.Task;
 
@@ -32,14 +33,22 @@ public abstract class BaseREPL {
   protected final Writer stdErr;
   protected volatile boolean keepRunning = true;
   private volatile Task historyFlusher = null;
-  private volatile FileHistory history = null;
+  private volatile PersistentHistory history = null;
   private final Queue<String> commandQueue = new ConcurrentLinkedQueue<String>();
 
-  public BaseREPL(InputStream stdin, OutputStream stdout, boolean prettyPrompt, boolean allowColors, File persistentHistory, Terminal terminal) throws IOException {
-    this.originalStdOut = stdout;
+  public BaseREPL(InputStream stdin, OutputStream stdout, boolean prettyPrompt, boolean allowColors, File file, Terminal terminal) throws IOException {
+  	this(stdin, stdout, prettyPrompt, allowColors, file != null ? new FileHistory(file) : null, terminal);
+  }
+  
+  public BaseREPL(InputStream stdin, PrintStream stdout, boolean prettyPrompt, boolean allowColors, ISourceLocation file, Terminal terminal) throws IOException {
+  	this(stdin, stdout, prettyPrompt, allowColors, file != null ? new SourceLocationHistory(file) : null, terminal);
+  }
+  
+  private BaseREPL(InputStream stdin, OutputStream stdout, boolean prettyPrompt, boolean allowColors, PersistentHistory history, Terminal terminal) throws IOException {
+      this.originalStdOut = stdout;
     reader = new ConsoleReader(stdin, stdout, terminal);
-    if (persistentHistory != null) {
-      history = new FileHistory(persistentHistory);
+    if (history != null) {
+      this.history = history;
       reader.setHistory(history);
       historyFlusher = new Task() {
         @Override
@@ -85,10 +94,7 @@ public abstract class BaseREPL {
   }
 
 
-  public BaseREPL(InputStream in, PrintStream out, boolean prettyPrompt2,
-      boolean allowColors2, ISourceLocation iSourceLocation, Terminal terminal) {
-	  throw new RuntimeException("make it so davy");
-  }
+ 
 
 
 
