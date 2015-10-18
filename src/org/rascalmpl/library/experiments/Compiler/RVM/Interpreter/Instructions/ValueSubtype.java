69e290d62e4e71665cfa6e2ac464c1f8f2f49180
paulklint <Paul.Klint@cwi.nl>
2015-10-18 15:42:36 +0200

    Code generation for VALUESUBTYPE

---
+++ src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/Instructions/ValueSubtype.java
@@ -0,0 +1,28 @@
+package org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.Instructions;
+
+import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.CodeBlock;
+import org.rascalmpl.library.experiments.Compiler.RVM.ToJVM.BytecodeGenerator;
+
+public class ValueSubtype extends Instruction {
+	
+	final int type;
+	
+	public ValueSubtype(CodeBlock ins, int type) {
+		super(ins, Opcode.VALUESUBTYPE);
+		this.type = type;
+	}
+	
+	public String toString() { return "VALUESUBTYPE " + type + "[" + codeblock.getConstantType(type) + "]"; }
+	
+	public void generate(){
+		codeblock.addCode(type);
+	}
+
+	public void generateByteCode(BytecodeGenerator codeEmittor, boolean debug){
+//		if ( debug ) {
+//			codeEmittor.emitDebugCall(opcode.name());
+//			codeEmittor.emitCallWithArgsSSFIII("insnCHECKARGTYPEANDCOPY",pos1,type,pos2,debug);
+//		}
+//		codeEmittor.emitInlineCheckArgTypeAndCopy(pos1,type,pos2,debug) ;
+	}
+}
