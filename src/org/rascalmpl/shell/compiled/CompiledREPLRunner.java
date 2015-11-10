d2c1f92ba7e9eb9a600242a5c624d1412bb0e38d
paulklint <Paul.Klint@cwi.nl>
2015-11-10 21:24:15 +0100

    Adapted for debugger

--- src/org/rascalmpl/shell/compiled/CompiledREPLRunner.java
+++ src/org/rascalmpl/shell/compiled/CompiledREPLRunner.java
@@ -5,42 +5,46 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.io.Writer;
+
+import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.repl.CommandExecutor;
+import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.repl.CompiledRascalREPL;
+import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.repl.DebugREPLFrameObserver;
+import org.rascalmpl.shell.ShellRunner;
 
 import jline.TerminalFactory;
 
-import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.repl.CompiledRascalREPL;
-import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.repl.CommandExecutor;
-import org.rascalmpl.shell.ShellRunner;
-
 public class CompiledREPLRunner extends CompiledRascalREPL  implements ShellRunner {
+	
+	private final DebugREPLFrameObserver debugObserver;
+	
+	public CompiledREPLRunner(InputStream stdin, OutputStream stdout) throws IOException {
+		super(stdin, stdout, true, true, getHistoryFile(), TerminalFactory.get());
+		debugObserver = new DebugREPLFrameObserver(reader.getInput(), stdout, true, true, getHistoryFile(), TerminalFactory.get());
+		executor.setDebugObserver(debugObserver);
+		setMeasureCommandTime(true);
+	}
 
-  private static File getHistoryFile() throws IOException {
-    File home = new File(System.getProperty("user.home"));
-    File rascal = new File(home, ".rascal");
-    if (!rascal.exists()) {
-      rascal.mkdirs();
-    }
-    File historyFile = new File(rascal, ".repl-history-rascal-terminal");
-    if (!historyFile.exists()) {
-      historyFile.createNewFile();
-    }
-    return historyFile;
-  }
+	private static File getHistoryFile() throws IOException {
+		File home = new File(System.getProperty("user.home"));
+		File rascal = new File(home, ".rascal");
+		if (!rascal.exists()) {
+			rascal.mkdirs();
+		}
+		File historyFile = new File(rascal, ".repl-history-rascal-terminal");
+		if (!historyFile.exists()) {
+			historyFile.createNewFile();
+		}
+		return historyFile;
+	}
 
-  public CompiledREPLRunner(InputStream stdin, OutputStream stdout) throws IOException {
-    super(stdin, stdout, true, true, getHistoryFile(), TerminalFactory.get());
-    setMeasureCommandTime(true);
-  }
+	@Override
+	protected CommandExecutor constructCommandExecutor(PrintWriter stdout, PrintWriter stderr) {
+		return new CommandExecutor(stdout, stderr);
+	}
 
-  @Override
-  protected CommandExecutor constructExecutor(Writer stdout, Writer stderr) {
-    return new CommandExecutor(new PrintWriter(stdout), new PrintWriter(stderr));
-  }
-
-  @Override
-  public void run(String[] args) throws IOException {
-    // there are no args for now
-    run();
-  }
+	@Override
+	public void run(String[] args) throws IOException {
+		// there are no args for now
+		run();
+	}
 }
