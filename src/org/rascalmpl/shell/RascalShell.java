1860bc3dbc9aa3503770a4322e349d593425b87b
Davy Landman <davy.landman@gmail.com>
2015-11-13 15:40:59 +0100

    Fixing indenting, again

--- src/org/rascalmpl/shell/RascalShell.java
+++ src/org/rascalmpl/shell/RascalShell.java
@@ -11,7 +11,7 @@
  *   * Tijs van der Storm - Tijs.van.der.Storm@cwi.nl
  *   * Paul Klint - Paul.Klint@cwi.nl - CWI
  *   * Arnold Lankamp - Arnold.Lankamp@cwi.nl
-*******************************************************************************/
+ *******************************************************************************/
 package org.rascalmpl.shell;
 
 import java.io.IOException;
@@ -25,53 +25,53 @@ import org.rascalmpl.interpreter.utils.RascalManifest;
 
 public class RascalShell  {
 
-  private static void printVersionNumber(){
-    try {
-      Enumeration<URL> resources = RascalShell.class.getClassLoader().getResources("META-INF/MANIFEST.MF");
-      while (resources.hasMoreElements()) {
-        Manifest manifest = new Manifest(resources.nextElement().openStream());
-        String bundleName = manifest.getMainAttributes().getValue("Bundle-Name");
-        if (bundleName != null && bundleName.equals("rascal-shell")) {
-          String result = manifest.getMainAttributes().getValue("Bundle-Version");
-          if (result != null) {
-            System.out.println("Version: " + result);
-            return;
-          }
+    private static void printVersionNumber(){
+        try {
+            Enumeration<URL> resources = RascalShell.class.getClassLoader().getResources("META-INF/MANIFEST.MF");
+            while (resources.hasMoreElements()) {
+                Manifest manifest = new Manifest(resources.nextElement().openStream());
+                String bundleName = manifest.getMainAttributes().getValue("Bundle-Name");
+                if (bundleName != null && bundleName.equals("rascal-shell")) {
+                    String result = manifest.getMainAttributes().getValue("Bundle-Version");
+                    if (result != null) {
+                        System.out.println("Version: " + result);
+                        return;
+                    }
+                }
+            }
+        } catch (IOException E) {
         }
-      }
-    } catch (IOException E) {
+        System.out.println("Version: unknown");
     }
-    System.out.println("Version: unknown");
-  }
 
 
-  public static void main(String[] args) throws IOException {
-    printVersionNumber();
-    RascalManifest mf = new RascalManifest();
+    public static void main(String[] args) throws IOException {
+        printVersionNumber();
+        RascalManifest mf = new RascalManifest();
 
-    try {
-      ShellRunner runner; 
-      if (mf.hasManifest(RascalShell.class) && mf.hasMainModule(RascalShell.class)) {
-        runner = new ManifestRunner(mf, new PrintWriter(System.out), new PrintWriter(System.err));
-      } 
-      else if (args.length > 0) {
-        runner = new ModuleRunner(new PrintWriter(System.out), new PrintWriter(System.err));
-      } 
-      else {
-        runner = new REPLRunner(System.in, System.out);
-      }
-      runner.run(args);
+        try {
+            ShellRunner runner; 
+            if (mf.hasManifest(RascalShell.class) && mf.hasMainModule(RascalShell.class)) {
+                runner = new ManifestRunner(mf, new PrintWriter(System.out), new PrintWriter(System.err));
+            } 
+            else if (args.length > 0) {
+                runner = new ModuleRunner(new PrintWriter(System.out), new PrintWriter(System.err));
+            } 
+            else {
+                runner = new REPLRunner(System.in, System.out);
+            }
+            runner.run(args);
 
-      System.exit(0);
+            System.exit(0);
+        }
+        catch (Throwable e) {
+            System.err.println("\n\nunexpected error: " + e.getMessage());
+            e.printStackTrace(System.err);
+            System.exit(1);
+        }
+        finally {
+            System.out.flush();
+            System.err.flush();
+        }
     }
-    catch (Throwable e) {
-      System.err.println("\n\nunexpected error: " + e.getMessage());
-      e.printStackTrace(System.err);
-      System.exit(1);
-    }
-    finally {
-      System.out.flush();
-      System.err.flush();
-    }
-  }
 }
