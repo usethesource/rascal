7f9f23ad31157e81f7f903c61fea7fddfe67159b
Davy Landman <davy.landman@gmail.com>
2015-12-23 13:25:23 +0100

    Added test runners for the parallel runners
    On my machine time went down from 332 seconds to 261 seconds

---
+++ test/org/rascalmpl/test/parallel/AllSuiteParallel.java
@@ -0,0 +1,17 @@
+package org.rascalmpl.test.parallel;
+
+import org.junit.runner.RunWith;
+import org.rascalmpl.test.infrastructure.RascalJUnitParallelRecursiveTestRunner;
+import org.rascalmpl.test.infrastructure.RecursiveRascalParallelTest;
+
+@RunWith(RascalJUnitParallelRecursiveTestRunner.class)
+@RecursiveRascalParallelTest({
+    "lang::rascal::tests::basic", 
+    "lang::rascal::tests::extends", 
+    "lang::rascal::tests::functionality", 
+    "lang::rascal::tests::imports",
+    "lang::rascal::tests::library",
+    "lang::rascal::syntax::tests"
+    })
+public class AllSuiteParallel {
+}
