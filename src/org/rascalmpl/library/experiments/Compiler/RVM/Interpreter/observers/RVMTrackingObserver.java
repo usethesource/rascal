49e1ee2daf863737a6b344a8dacb8e0e251f0229
paulklint <Paul.Klint@cwi.nl>
2015-12-24 11:23:42 +0100

    Now use pc before increment in observer

--- src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/observers/RVMTrackingObserver.java
+++ src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/observers/RVMTrackingObserver.java
@@ -15,13 +15,12 @@ public class RVMTrackingObserver implements IFrameObserver {
 
 	@Override
 	public boolean observeRVM(RVM rvm, Frame frame, int pc, Object[] stack, int sp) {
-		int startpc = pc - 1;
-		stdout.printf("[%03d] %s, scope %d\n", startpc, frame.function.getName(), frame.scopeId);
+		stdout.printf("[%03d] %s, scope %d\n", pc, frame.function.getName(), frame.scopeId);
 		
 		for (int i = 0; i < sp; i++) {
 			stdout.println("\t   " + (i < frame.function.getNlocals() ? "*" : " ") + i + ": " + rvm.asString(stack[i], 40));
 		}
-		stdout.printf("%5s %s\n" , "", frame.function.codeblock.toString(startpc));
+		stdout.printf("%5s %s\n" , "", frame.function.codeblock.toString(pc));
 		stdout.flush();
 		return true;
 	}
