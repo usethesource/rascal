5ebd4ddbcc6989860601f16c0f38d56558d4a692
paulklint <Paul.Klint@cwi.nl>
2016-01-28 23:35:22 +0100

    Added lost file

---
+++ src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/Instructions/PushVarRef.java
@@ -0,0 +1,34 @@
+package org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.Instructions;
+
+import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.CodeBlock;
+import org.rascalmpl.library.experiments.Compiler.RVM.ToJVM.BytecodeGenerator;
+
+public class PushVarRef extends Instruction {
+
+	final String fuid;
+	final int pos;
+
+	public PushVarRef(CodeBlock ins, String fuid, int pos) {
+		super(ins, Opcode.PUSHVARREF);
+		this.fuid = fuid;
+		this.pos = pos;
+	}
+
+	public String toString() {
+		return "PUSHVARREF " + fuid + " [ " + codeblock.getFunctionIndex(fuid) + " ] " + ", " + pos;
+	}
+
+	public void generate() {
+		int what = (pos == -1) ? codeblock.getConstantIndex(codeblock.vf.string(fuid)) : codeblock.getFunctionIndex(fuid) ;
+		codeblock.addCode2(opcode.getOpcode(), what, pos);
+	}
+
+	public void generateByteCode(BytecodeGenerator codeEmittor, boolean debug) {
+		if ( debug ) 
+			codeEmittor.emitDebugCall(opcode.name());
+
+		int what = (pos == -1) ? codeblock.getConstantIndex(codeblock.vf.string(fuid)) : codeblock.getFunctionIndex(fuid) ;
+		
+		codeEmittor.emitCallWithArgsSSFIIZ("insnPUSHVARREF", what, pos, CodeBlock.isMaxArg2(pos),debug);
+	}
+}
