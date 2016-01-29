394fd4210b0e5af415c7f07cc6c445d09ca0a528
paulklint <Paul.Klint@cwi.nl>
2016-01-29 15:36:28 +0100

    Finalized destination-driven code generator

--- src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/ParserGenerator.java
+++ src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/ParserGenerator.java
@@ -53,7 +53,7 @@ public class ParserGenerator {
 	private Function createHoleFunction;
 	private static final String packageName = "org.rascalmpl.java.parser.object";
 	private static final boolean debug = false;
-	private static final boolean useCompiledParserGenerator = false;
+	private static final boolean useCompiledParserGenerator = true;
 	
 	static {
 		if(useCompiledParserGenerator){
