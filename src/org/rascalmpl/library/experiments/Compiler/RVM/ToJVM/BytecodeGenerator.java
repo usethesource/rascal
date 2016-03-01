586419f935009173b10c7d9b051eafe5ff4a3404
paulklint <Paul.Klint@cwi.nl>
2016-03-01 14:56:13 +0100

    Added contributors (i.e. Ferry Rietveld)

--- src/org/rascalmpl/library/experiments/Compiler/RVM/ToJVM/BytecodeGenerator.java
+++ src/org/rascalmpl/library/experiments/Compiler/RVM/ToJVM/BytecodeGenerator.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2009-2016 CWI
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   * Ferry Rietveld - f.rietveld@hva.nl - HvA
+ *   * Paul Klint - Paul.Klint@cwi.nl - CWI
+*******************************************************************************/
+
+
 package org.rascalmpl.library.experiments.Compiler.RVM.ToJVM;
 
 import java.io.ByteArrayInputStream;
