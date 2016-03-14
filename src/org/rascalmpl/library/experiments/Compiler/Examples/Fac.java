fd37a00362a632f7d2b6ad10c1dba006fa42a52b
paulklint <Paul.Klint@cwi.nl>
2016-03-14 15:18:20 +0100

    Further reduced number of boot files

--- src/org/rascalmpl/library/experiments/Compiler/Examples/Fac.java
+++ src/org/rascalmpl/library/experiments/Compiler/Examples/Fac.java
@@ -1,51 +1,56 @@
 package org.rascalmpl.library.experiments.Compiler.Examples;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
-import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.RVMExecutable;
-import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.RascalExecutionContext;
-import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.RascalExecutionContextBuilder;
+import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.ExecutionTools;
+import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.OverloadedFunction;
+import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.RVMCore;
+import org.rascalmpl.value.IConstructor;
 import org.rascalmpl.value.IInteger;
-import org.rascalmpl.value.ISourceLocation;
+import org.rascalmpl.value.IString;
 import org.rascalmpl.value.IValue;
 import org.rascalmpl.value.IValueFactory;
 
-//import experiments.Compiler.Examples.Fac$Compiled;	// <== experiments.Compiler.Examples.Fac.rsc compiled to a class file
-
 public class Fac {
+	IValueFactory vf;
+//	private final OverloadedFunction fac_in_rascal;
+//	private final OverloadedFunction d1_in_rascal;
+//	private final OverloadedFunction d2_in_rascal;
+	private final OverloadedFunction getA_in_rascal;
+	private final OverloadedFunction getAs_in_rascal;
+	private final OverloadedFunction sizeAs_in_rascal;
+	
+	private final RVMCore rvm;
 
-//	Fac$Compiled facCompiled;
-//	IValueFactory vf;
-//
 	Fac(IValueFactory vf){
-//		this.vf = vf;
-//		ISourceLocation facBinLoc = null;
-//		try {
-//			facBinLoc = vf.sourceLocation("compressed+home", "","bin/experiments/Compiler/Examples/Fac.rvm.ser.gz");
-//		} catch (URISyntaxException e) {
-//			System.err.println("Could not create FacBin location");;
-//		}
-//		RVMExecutable rvmExecutable = null; 
-//		try {
-//			rvmExecutable = RVMExecutable.read(facBinLoc);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//
-//		RascalExecutionContext rex = 
-//				RascalExecutionContextBuilder.normalContext(vf, System.out, System.err)
-//				.forModule("Fac")
-//				.setJVM(true)
-//				.build();
-//
-//		facCompiled = new Fac$Compiled(rvmExecutable, rex);
+		rvm = ExecutionTools.initializedRVM("compressed+home", "bin/experiments/Compiler/Examples/Fac.rvm.ser.gz");
+//		fac_in_rascal = rvm.getOverloadedFunction("int fac (int n)");
+//		d1_in_rascal = rvm.getOverloadedFunction("D d1(int n)");
+//		d2_in_rascal = rvm.getOverloadedFunction("D d2(str s)");
+		getA_in_rascal = rvm.getOverloadedFunction("A getA()");
+		getAs_in_rascal = rvm.getOverloadedFunction("As getAs(int n)");
+		sizeAs_in_rascal = rvm.getOverloadedFunction("int size(As as)");
 	}
 
-	IInteger fac(IInteger n){
-//		return (IInteger)  facCompiled.executeRVMFunction("experiments::Compiler::Examples::Fac/fac(int();)#5", 
-//														new IValue[] { n }, 
-//														null);
-		return null;
+//	IInteger fac(IInteger n){
+//		return (IInteger) rvm.executeRVMFunction(fac_in_rascal, new IValue[] { n });
+//	}
+//	
+//	IConstructor d1(IInteger n){
+//		return (IConstructor) rvm.executeRVMFunction(d1_in_rascal, new IValue[] { n });
+//	}
+//	
+//	IConstructor d2(IString s){
+//		return (IConstructor) rvm.executeRVMFunction(d2_in_rascal, new IValue[] { s });
+//	}
+	
+	IConstructor getA(){
+		return (IConstructor) rvm.executeRVMFunction(getA_in_rascal, new IValue[] { });
+	}
+	
+	IConstructor getAs(IInteger n){
+		return (IConstructor) rvm.executeRVMFunction(getAs_in_rascal, new IValue[] { n });
+	}
+	
+	IInteger sizeAs(IConstructor c){
+		return (IInteger) rvm.executeRVMFunction(sizeAs_in_rascal, new IValue[] {c });
 	}
 }
--- src/org/rascalmpl/library/experiments/Compiler/Examples/FacUse.java
+++ src/org/rascalmpl/library/experiments/Compiler/Examples/FacUse.java
@@ -21,39 +21,45 @@ public class FacUse  {
 		return t;
 	}
 	
-	static long do_rfac(int n, int m){
-		IValueFactory vf = ValueFactoryFactory.getValueFactory();
-		Fac myFac = new Fac(vf);
-		long before = Timing.getCpuTime();
-		for(int i = 0; i < m; i++){
-			myFac.fac(vf.integer(n));
-		}
-		long t = (Timing.getCpuTime() - before)/1000;
-		System.out.println("rfac: " + t + " nsec");
-		return t;
-	}
+//	static long do_rfac(int n, int m){
+//		IValueFactory vf = ValueFactoryFactory.getValueFactory();
+//		Fac myFac = new Fac(vf);
+//		long before = Timing.getCpuTime();
+//		for(int i = 0; i < m; i++){
+//			myFac.fac(vf.integer(n));
+//		}
+//		long t = (Timing.getCpuTime() - before)/1000;
+//		System.out.println("rfac: " + t + " nsec");
+//		return t;
+//	}
 	
 	public static void main(String[] args) {
 		int n = 20;
 		
 		boolean use1 = false;
 
-		if(use1){
+//		if(use1){
 			IValueFactory vf = ValueFactoryFactory.getValueFactory();
 			
 			Fac myFac = new Fac(vf);					// <===
 			
 			long before = Timing.getCpuTime();
 			
-			IInteger result = myFac.fac(vf.integer(n));	// <===
+//			IInteger result = myFac.fac(vf.integer(n));	// <===
 
-			System.out.println("result = " + result + "\n" + (Timing.getCpuTime() - before)/1000 + " nsec");
-			before = Timing.getCpuTime();
-			System.out.println("jfac: " + jfac(n)+ "\n" + (Timing.getCpuTime() - before)/1000 + " nsec");
-		} else {
-			long jtime = do_jfac(n, 1000000);
-			long rtime = do_rfac(n, 1000000);
-			System.out.println("java speedup: " + rtime/jtime);
-		}
+//			System.out.println("result = " + result + "\n" + (Timing.getCpuTime() - before)/1000 + " nsec");
+//			
+//			
+//			System.out.println(myFac.d1(vf.integer(n)));
+//			System.out.println(myFac.d2(vf.string("abc")));
+			System.out.println(myFac.getA());
+			System.out.println(myFac.getAs(vf.integer(3)));
+//			before = Timing.getCpuTime();
+//			System.out.println("jfac: " + jfac(n)+ "\n" + (Timing.getCpuTime() - before)/1000 + " nsec");
+//		} else {
+//			long jtime = do_jfac(n, 1000000);
+//			long rtime = do_rfac(n, 1000000);
+//			System.out.println("java speedup: " + rtime/jtime);
+//		}
 	}
 }
