42387e92c7c2f4b01edac970bfe0f4214571b872
paulklint <Paul.Klint@cwi.nl>
2016-05-30 11:17:13 +0200

    Major cleanup and rewrite of tutor3
    - Removed all dependencies on commonmark
    - Everything based on asciidoc
    - Uses Lucene for index and search

--- src/org/rascalmpl/library/experiments/tutor3/commonmark/CustomAnsiRenderer.java
+++
@@ -1,8 +0,0 @@
-package org.rascalmpl.library.experiments.tutor3.commonmark;
-
-import org.commonmark.node.Node;
-import org.commonmark.node.Visitor;
-
-public interface CustomAnsiRenderer {
-	 boolean render(Node node, AnsiWriter ansiWriter, Visitor visitor);
-}
