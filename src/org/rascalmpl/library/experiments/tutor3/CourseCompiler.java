435e9828331c8c55c2f5fead8b12e1448bae2af9
paulklint <Paul.Klint@cwi.nl>
2016-06-17 09:04:30 +0200

    minor

--- src/org/rascalmpl/library/experiments/tutor3/CourseCompiler.java
+++ src/org/rascalmpl/library/experiments/tutor3/CourseCompiler.java
@@ -166,17 +166,18 @@ public class CourseCompiler {
 
 //		compileCourse(coursesSrcPath, "ADocTest", destPath, libPath, executor);
 		
+		compileCourse(coursesSrcPath, "WhyRascal", destPath, libPath, executor);
+		compileCourse(coursesSrcPath, "GettingStarted", destPath, libPath, executor);
+		compileCourse(coursesSrcPath, "GettingHelp", destPath, libPath, executor);
 		
-//		compileCourse(coursesSrcPath, "GettingHelp", destPath, libPath, executor);
-//		compileCourse(coursesSrcPath, "GettingStarted", destPath, libPath, executor);
-//		compileCourse(coursesSrcPath, "Errors", destPath, libPath, executor);
-//		compileCourse(coursesSrcPath, "Rascal", destPath, libPath, executor);
+		compileCourse(coursesSrcPath, "Errors", destPath, libPath, executor);
+		compileCourse(coursesSrcPath, "Rascal", destPath, libPath, executor);
 		compileCourse(coursesSrcPath, "RascalConcepts", destPath, libPath, executor);
-//		compileCourse(coursesSrcPath, "Libraries", destPath, libPath, executor);
-//		compileCourse(coursesSrcPath, "Rascalopedia", destPath, libPath, executor);
-//		compileCourse(coursesSrcPath, "Recipes", destPath, libPath, executor);
-//		compileCourse(coursesSrcPath, "TutorWebSite", destPath, libPath, executor);
-//		compileCourse(coursesSrcPath, "WhyRascal", destPath, libPath, executor);
+		compileCourse(coursesSrcPath, "Libraries", destPath, libPath, executor);
+		compileCourse(coursesSrcPath, "Rascalopedia", destPath, libPath, executor);
+		compileCourse(coursesSrcPath, "Recipes", destPath, libPath, executor);
+		compileCourse(coursesSrcPath, "TutorWebSite", destPath, libPath, executor);
+		
 		
 		err.flush();
 		writeFile(destPath + "/course-compilation-errors.txt", sw.toString());
