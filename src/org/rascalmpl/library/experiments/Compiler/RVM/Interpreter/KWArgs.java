b84628875082a76db70c41eaeb690591ed29bc9b
paulklint <Paul.Klint@cwi.nl>
2016-10-05 21:15:59 +0200

    Renamed

--- src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/KWArgs.java
+++ src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/KWArgs.java
@@ -14,17 +14,17 @@ import org.rascalmpl.value.IValueFactory;
  * Typical usage: 
  * 		new KWParams(vf).add("key1", vf.integer(1)).add("key2", vf.string("abc")).build();
  */
-public class KWParams {
-	private Map<String, IValue> kwargs;
-	private IValueFactory vf;
+public class KWArgs {
+	protected Map<String, IValue> kwArgs;
+	protected IValueFactory vf;
 
 	/**
 	 * Create a keyword parameter builder
 	 * @param vf ValueFactory to be used
 	 */
-	public KWParams(IValueFactory vf){
+	public KWArgs(IValueFactory vf){
 		this.vf = vf;
-		kwargs = new HashMap<>();
+		kwArgs = new HashMap<>();
 	}
 
 	/**
@@ -33,40 +33,40 @@ public class KWParams {
 	 * @param val	Value of keyword parameter
 	 * @return This KWParams, with the pair (key, val) added
 	 */
-	public KWParams add(String key, IValue val){
-	  kwargs.put(key,  val);
+	public KWArgs add(String key, IValue val){
+	  kwArgs.put(key,  val);
 	  return this;
 	}
 
-	public KWParams add(String key, boolean b){
-	  kwargs.put(key, vf.bool(b));
+	public KWArgs add(String key, boolean b){
+	  kwArgs.put(key, vf.bool(b));
 	  return this;
 	}
-	public KWParams add(String key, int n){
-	  kwargs.put(key, vf.integer(n));
-	  return this;
-	}
-
-	public KWParams add(String key, float f){
-	  kwargs.put(key, vf.real(f));
+	public KWArgs add(String key, int n){
+	  kwArgs.put(key, vf.integer(n));
 	  return this;
 	}
 
-	public KWParams add(String key, double d){
-	  kwargs.put(key, vf.real(d));
+	public KWArgs add(String key, float f){
+	  kwArgs.put(key, vf.real(f));
 	  return this;
 	}
-	public KWParams add(String key, String s){
-      kwargs.put(key, vf.string(s));
+
+	public KWArgs add(String key, double d){
+	  kwArgs.put(key, vf.real(d));
+	  return this;
+	}
+	public KWArgs add(String key, String s){
+      kwArgs.put(key, vf.string(s));
       return this;
     }
 	
-	public KWParams add(IMap kwmap){
+	public KWArgs add(IMap kwmap){
 	  for(IValue key : kwmap){
 	    if(!key.getType().isString()){
 	      throw new RuntimeException("Key in keywerd parameter map should be a string");
 	    }
-	    kwargs.put(((IString) key).getValue(), kwmap.get(key));
+	    kwArgs.put(((IString) key).getValue(), kwmap.get(key));
 	  }
 	  return this;
 	}
@@ -75,6 +75,6 @@ public class KWParams {
 	 * @return this KWParams as Map
 	 */
 	public Map<String, IValue> build(){
-		return kwargs;
+		return kwArgs;
 	}
 }
