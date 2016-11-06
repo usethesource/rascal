d9594c6987506011832b6ef8aa6cee3e45c9f09e
Davy Landman <davy.landman@gmail.com>
2016-11-06 22:08:34 +0100

    Special tests for util libraries of the new Serialization Format

---
+++ test/org/rascalmpl/value/io/TrackWritesTestBase.java
@@ -0,0 +1,93 @@
+package org.rascalmpl.value.io;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.rascalmpl.value.io.binary.util.TrackLastWritten;
+
+public abstract class TrackWritesTestBase {
+
+    public abstract TrackLastWritten<Object> getWritesWindow(int size);
+
+    @Test
+    public void canFindFirstObject() {
+        Object a = new Object();
+        TrackLastWritten<Object> w = getWritesWindow(5);
+        w.write(a);
+        assertEquals(0, w.howLongAgo(a));
+    }
+
+    @Test
+    public void canFindOldObject() {
+        Object a = new Object();
+        Object b = new Object();
+        TrackLastWritten<Object> w = getWritesWindow(5);
+        w.write(a);
+        w.write(b);
+        assertEquals(1, w.howLongAgo(a));
+    }
+    
+    @Test
+    public void canFindObjectAtEdge() {
+        Object a = new Object();
+        Object b = new Object();
+        Object c = new Object();
+        TrackLastWritten<Object> w = getWritesWindow(5);
+        w.write(a);
+        w.write(b);
+        w.write(c);
+        assertEquals(2, w.howLongAgo(a));
+    }
+
+    @Test
+    public void dropsObject() {
+        Object a = new Object();
+        Object b = new Object();
+        Object c = new Object();
+        Object d = new Object();
+        TrackLastWritten<Object> w = getWritesWindow(3);
+        w.write(a);
+        w.write(b);
+        w.write(c);
+        w.write(d);
+        assertEquals(-1, w.howLongAgo(a));
+        assertEquals(2, w.howLongAgo(b));
+        assertEquals(1, w.howLongAgo(c));
+        assertEquals(0, w.howLongAgo(d));
+    }
+    
+    @Test
+    public void dropsMany() {
+        Object a = new Object();
+        Object b = new Object();
+        Object c = new Object();
+        Object d = new Object();
+        TrackLastWritten<Object> w = getWritesWindow(3);
+        for (int i =0; i < 10000; i++) {
+            w.write(new Object());
+        }
+        w.write(a);
+        w.write(b);
+        w.write(c);
+        w.write(d);
+        assertEquals(-1, w.howLongAgo(a));
+        assertEquals(2, w.howLongAgo(b));
+        assertEquals(1, w.howLongAgo(c));
+        assertEquals(0, w.howLongAgo(d));
+    }
+    
+    @Test
+    public void randomTest() {
+        Object[] elements = new Object[10000];;
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] = new Object();
+        }
+        TrackLastWritten<Object> w = getWritesWindow(20);
+        for (int i = 0; i < elements.length; i++) {
+            w.write(elements[i]);
+            for (int j = Math.max(0, i - 19); j <=i; j++) {
+                assertEquals(i - j, w.howLongAgo(elements[j]));
+            }
+        }
+    }
+}
