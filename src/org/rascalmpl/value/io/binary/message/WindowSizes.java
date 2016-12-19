4583b142fc09d0e0238703b8d8c08fbdafcbe2e0
Davy Landman <davy.landman@gmail.com>
2016-12-19 14:09:11 +0100

    Made WindowSizes available

--- src/org/rascalmpl/value/io/binary/message/WindowSizes.java
+++ src/org/rascalmpl/value/io/binary/message/WindowSizes.java
@@ -10,16 +10,15 @@
  *  
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */ 
-package org.rascalmpl.value.io.binary.stream;
+package org.rascalmpl.value.io.binary.message;
 
 import org.rascalmpl.value.IValue;
-import org.rascalmpl.value.io.binary.stream.IValueOutputStream.CompressionRate;
 
-class WindowSizes {
-    final int uriWindow;
-    final int typeWindow;
-    final int valueWindow;
-    final int stringsWindow;
+public class WindowSizes {
+    public final int uriWindow;
+    public final int typeWindow;
+    public final int valueWindow;
+    public final int stringsWindow;
     public WindowSizes(int valueWindow, int uriWindow, int typeWindow, int stringsWindow) {
         this.stringsWindow = stringsWindow;
         this.typeWindow = typeWindow;
