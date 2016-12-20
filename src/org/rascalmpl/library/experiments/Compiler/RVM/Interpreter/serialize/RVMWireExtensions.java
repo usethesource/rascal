74267eaf3d587b8b1300566cca2417e47b2ed80f
paulklint <Paul.Klint@cwi.nl>
2016-12-20 22:04:18 +0100

    Removed writeMap/readMap and switched to getStringIntegerMap.

--- src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/serialize/RVMWireExtensions.java
+++ src/org/rascalmpl/library/experiments/Compiler/RVM/Interpreter/serialize/RVMWireExtensions.java
@@ -24,35 +24,35 @@ import org.rascalmpl.value.io.binary.wire.IWireOutputStream;
 
 public class RVMWireExtensions {
 
-    public static void writeMap(IWireOutputStream out, int fieldId, Map<String, Integer> map) throws IOException{
-        int n = map.size();
-        String[] keys = new String[n];
-        int[] values = new int[n];
-        
-        int i = 0;
-        for(Entry<String, Integer> entry : map.entrySet()){
-            keys[i] = entry.getKey();
-            values[i] = entry.getValue();
-            i++;
-        }
-        out.writeField(fieldId, keys);
-        out.writeField(fieldId, values);
-    }
+//    public static void writeMap(IWireOutputStream out, int fieldId, Map<String, Integer> map) throws IOException{
+//        int n = map.size();
+//        String[] keys = new String[n];
+//        int[] values = new int[n];
+//        
+//        int i = 0;
+//        for(Entry<String, Integer> entry : map.entrySet()){
+//            keys[i] = entry.getKey();
+//            values[i] = entry.getValue();
+//            i++;
+//        }
+//        out.writeField(fieldId, keys);
+//        out.writeField(fieldId, values);
+//    }
     
-    public static Map<String, Integer> readMap(IWireInputStream in) throws IOException{
-        assert in.getFieldType() == FieldKind.REPEATED && in.getRepeatedType() == FieldKind.STRING;
-        String[] keys = in.getStrings();
-        int fieldId = in.field();
-        in.next();
-        assert in.field() == fieldId;
-        int[] values = in.getIntegers();
-        assert keys.length == values.length;
-        Map<String, Integer> map = new HashMap<>();
-        for(int i = 0; i < keys.length; i++){
-            map.put(keys[i], values[i]);
-        }
-        return map;     
-    }
+//    public static Map<String, Integer> readMap(IWireInputStream in) throws IOException{
+//        assert in.getFieldType() == FieldKind.REPEATED && in.getRepeatedType() == FieldKind.STRING;
+//        String[] keys = in.getStrings();
+//        int fieldId = in.field();
+//        in.next();
+//        assert in.field() == fieldId;
+//        int[] values = in.getIntegers();
+//        assert keys.length == values.length;
+//        Map<String, Integer> map = new HashMap<>();
+//        for(int i = 0; i < keys.length; i++){
+//            map.put(keys[i], values[i]);
+//        }
+//        return map;     
+//    }
     
     public static void writeMapIntToInts(IWireOutputStream out, int fieldId, Map<Integer, int[]> map) throws IOException{
         int n = map.size();
