ba2a7faeef01b0810b955c4ff43c98e2940f76ac
Davy Landman <davy.landman@gmail.com>
2016-12-22 11:21:48 +0100

    Fixed bug in reading string arrays

--- test/org/rascalmpl/value/TestBinaryIO.java
+++ test/org/rascalmpl/value/TestBinaryIO.java
@@ -13,13 +13,22 @@ package org.rascalmpl.value;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Random;
 
 import org.rascalmpl.value.impl.fast.ValueFactory;
+import org.rascalmpl.value.io.binary.message.IValueReader;
+import org.rascalmpl.value.io.binary.message.IValueWriter;
 import org.rascalmpl.value.io.binary.stream.IValueInputStream;
 import org.rascalmpl.value.io.binary.stream.IValueOutputStream;
 import org.rascalmpl.value.io.binary.stream.IValueOutputStream.CompressionRate;
+import org.rascalmpl.value.io.binary.util.WindowSizes;
+import org.rascalmpl.value.io.binary.wire.IWireInputStream;
+import org.rascalmpl.value.io.binary.wire.IWireOutputStream;
+import org.rascalmpl.value.io.binary.wire.binary.BinaryWireInputStream;
+import org.rascalmpl.value.io.binary.wire.binary.BinaryWireOutputStream;
 import org.rascalmpl.value.type.Type;
+import org.rascalmpl.value.type.TypeFactory;
 import org.rascalmpl.value.type.TypeStore;
 import org.rascalmpl.value.util.RandomValues;
 
@@ -47,6 +56,61 @@ public class TestBinaryIO extends TestCase {
 	        ioRoundTrip(ts, value);
 	    }
 	}
+	
+	public void testConstructorTypeWithLabel() {
+        TypeFactory tf = TypeFactory.getInstance();
+	    TypeStore ts = new TypeStore();
+	    Type adt = tf.abstractDataType(ts, "A");
+	    Type cons = tf.constructor(ts, adt, "b", tf.integerType(), "i");
+	    iopRoundTrip(ts, cons);
+	}
+	public void testConstructorTypeWithParams() {
+        TypeFactory tf = TypeFactory.getInstance();
+	    TypeStore ts = new TypeStore();
+	    Type adt = tf.abstractDataType(ts, "A", tf.parameterType("T"));
+	    Type cons = tf.constructor(ts, adt, "b", tf.parameterType("T"), "tje");
+	    iopRoundTrip(ts, cons);
+	}
+	public void testConstructorTypeWithInstanciatedParams() {
+        TypeFactory tf = TypeFactory.getInstance();
+	    TypeStore ts = new TypeStore();
+	    Type adt = tf.abstractDataType(ts, "A", tf.parameterType("T"));
+	    Type cons = tf.constructor(ts, adt, "b", tf.parameterType("T"), "tje");
+	    
+	    HashMap<Type, Type> binding = new HashMap<>();
+	    binding.put(tf.parameterType("T"), tf.integerType());
+	    iopRoundTrip(ts, cons.instantiate(binding));
+	}
+	
+	public void testRandomTypesIO() {
+        TypeFactory tf = TypeFactory.getInstance();
+	    TypeStore ts = new TypeStore();
+        for (int i =0; i < 100; i++) {
+            Type tp = tf.randomType(ts);
+            iopRoundTrip(ts, tp);
+        }
+	}
+
+    private void iopRoundTrip(TypeStore ts, Type tp) {
+        try {
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            try (IWireOutputStream w = new BinaryWireOutputStream(buffer, 1000)) {
+                IValueWriter.write(w, ts, WindowSizes.SMALL_WINDOW, tp);
+            }
+            try (IWireInputStream read = new BinaryWireInputStream(new ByteArrayInputStream(buffer.toByteArray()))) {
+                Type result = IValueReader.readType(read, vf, new TypeStore());
+                if(!tp.equals(result)){
+                    String message = "Not equal: \n\t"+result+" expected: "+tp;
+                    System.err.println(message);
+                    fail(message);
+                }
+            }
+		}
+	    catch(IOException ioex){
+			ioex.printStackTrace();
+			fail(ioex.getMessage());
+		}
+    }
 
     private void ioRoundTrip(TypeStore ts, IValue value) {
         try {
