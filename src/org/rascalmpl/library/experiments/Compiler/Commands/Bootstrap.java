1e1d60f32cd26cd578da7635e335e91df0b9f006
paulklint <Paul.Klint@cwi.nl>
2017-01-08 13:29:51 +0100

    Running tests again in all boot phases

--- src/org/rascalmpl/library/experiments/Compiler/Commands/Bootstrap.java
+++ src/org/rascalmpl/library/experiments/Compiler/Commands/Bootstrap.java
@@ -451,10 +451,8 @@ public class Bootstrap {
       time("- compile Kernel",          () -> compileModule   (phase, classPath, bootPath, sourcePath, result, "lang::rascal::boot::Kernel", reloc));
       time("- compile ParserGenerator", () -> compileModule   (phase, classPath, bootPath, sourcePath, result, "lang::rascal::grammar::ParserGenerator", reloc));
      
-      if(phase >= 2){
-          time("- compile tests",           () -> compileTests    (phase, classPath, result.toAbsolutePath().toString(), sourcePath, testResults));
-          time("- run tests",               () -> runTests        (phase, testClassPath, result.toAbsolutePath().toString(), sourcePath, testResults));
-      }
+      time("- compile tests",           () -> compileTests    (phase, classPath, result.toAbsolutePath().toString(), sourcePath, testResults));
+      time("- run tests",               () -> runTests        (phase, testClassPath, result.toAbsolutePath().toString(), sourcePath, testResults));
       return result;
     }
 
