cba868b7ff279e2ac432faf9b3ecb94768447852
Michael Steindorfer <michael@steindorfer.name>
2017-01-27 13:21:28 +0100

    Resolving merge issues.

--- src/org/rascalmpl/value/impl/AbstractDefaultAnnotatable.java
+++ src/org/rascalmpl/value/impl/AbstractDefaultAnnotatable.java
@@ -11,16 +11,15 @@
  *******************************************************************************/
 package org.rascalmpl.value.impl;
 
-import java.util.Map;
-
+import io.usethesource.capsule.util.collection.AbstractSpecialisedImmutableMap;
 import org.rascalmpl.value.IAnnotatable;
 import org.rascalmpl.value.IValue;
 import org.rascalmpl.value.exceptions.FactTypeUseException;
 
-import io.usethesource.capsule.util.collection.AbstractSpecialisedImmutableMap;
+import java.util.Map;
 
 /**
- * A generic wrapper for an {@link IValue} that associates annotations to it. 
+ * A generic wrapper for an {@link IValue} that associates annotations to it.
  *
  * @param <T> the interface over which this annotation wrapper closes
  */
@@ -28,11 +27,11 @@ public abstract class AbstractDefaultAnnotatable<T extends IValue> implements IA
 
 	protected final T content;
 	protected final io.usethesource.capsule.api.deprecated.Map.Immutable<String, IValue> annotations;
-		
+
 	/**
 	 * Creates an {@link IAnnotatable} view on {@literal content} with empty
 	 * annotations.
-	 * 
+	 *
 	 * @param content
 	 *            is the wrapped object that supports annotations
 	 */
@@ -40,11 +39,11 @@ public abstract class AbstractDefaultAnnotatable<T extends IValue> implements IA
 		this.content = content;
 		this.annotations = AbstractSpecialisedImmutableMap.mapOf();
 	}
-	
+
 	/**
 	 * Creates an {@link IAnnotatable} view on {@link #content} with already
 	 * provided {@link #annotations}.
-	 * 
+	 *
 	 * @param content
 	 *            is the wrapped object that supports annotations
 	 * @param annotations
@@ -54,12 +53,12 @@ public abstract class AbstractDefaultAnnotatable<T extends IValue> implements IA
 		this.content = content;
 		this.annotations = annotations;
 	}
-	
+
 	/**
 	 * Wraps {@link #content} with other annotations. This methods is mandatory
 	 * because of PDB's immutable value nature: Once annotations are modified, a
 	 * new immutable view is returned.
-	 * 
+	 *
 	 * @param content
 	 *            is the wrapped object that supports annotations
 	 * @param annotations
@@ -68,27 +67,27 @@ public abstract class AbstractDefaultAnnotatable<T extends IValue> implements IA
 	 *         {@link #annotations}
 	 */
 	protected abstract T wrap(final T content, final io.usethesource.capsule.api.deprecated.Map.Immutable<String, IValue> annotations);
-	
+
 	@Override
 	public boolean hasAnnotations() {
 		return annotations.size() > 0;
 	}
-	
+
 	@Override
 	public Map<String, IValue> getAnnotations() {
 		return annotations;
 	}
-	
+
 	@Override
 	public T removeAnnotations() {
 		return content;
 	}
-	
+
 	@Override
 	public boolean hasAnnotation(String label) throws FactTypeUseException {
 		return annotations.containsKey(label);
 	}
-	
+
 	@Override
 	public IValue getAnnotation(String label) throws FactTypeUseException {
 		return annotations.get(label);
@@ -104,12 +103,12 @@ public abstract class AbstractDefaultAnnotatable<T extends IValue> implements IA
 	public T removeAnnotation(String label) {
 		return wrap(content, annotations.__remove(label));
 	}
-	
+
 	@Override
 	public T setAnnotations(Map<String, IValue> otherAnnotations) {
 		if (otherAnnotations.isEmpty())
 			return content;
-		
+
 		return wrap(content, AbstractSpecialisedImmutableMap.mapOf(otherAnnotations));
 	}
 
@@ -117,16 +116,16 @@ public abstract class AbstractDefaultAnnotatable<T extends IValue> implements IA
 	public T joinAnnotations(Map<String, IValue> otherAnnotations) {
 		return wrap(content, annotations.__putAll(otherAnnotations));
 	}
-		
+
 	@Override
 	public String toString() {
 		return content.toString();
 	}
-	
+
 	/**
 	 * This method is only to be used by internal methods, such as testing and fast iterators
 	 */
-	public ImmutableMap<String, IValue> internalGetAnnotations() {
+	public io.usethesource.capsule.api.deprecated.Map.Immutable<String, IValue> internalGetAnnotations() {
 	    return annotations;
 	}
 //	@Override
