b3435b91952e78f226eef3314f4f785f317ab7b7
Mauricio Verano Merino <m.verano239@gmail.com>
2017-06-08 19:10:54 +0200

    incomplete AmalgaREPL example using the TermREPL

--- src/org/rascalmpl/library/util/amalga/AmalgaREPL.java
+++ src/org/rascalmpl/library/util/amalga/AmalgaREPL.java
@@ -42,112 +42,11 @@ public class AmalgaREPL {
         vf = valueFactory;
     }
     
-    public IString example(IString name, IEvaluatorContext ctx){
+    public IValue handler(IString name, IEvaluatorContext ctx){
         return name;
     }
     
-    public IValue sum( IInteger n1, IInteger n2, IEvaluatorContext ctx){
-        return n1.add(n2);
-    }
-
-    public void startREPL(IConstructor repl, IEvaluatorContext ctx) {
-        try {
-            // TODO: this used to get a repl from the IEvaluatorContext but that was wrong. Need to fix later. 
-            new BaseREPL(new Amalga(repl, ctx), null, System.in, System.out, true, true, ((ISourceLocation)repl.get("history")), TerminalFactory.get(), null).run();
-        } catch (IOException | URISyntaxException e) {
-            e.printStackTrace(ctx.getStdErr());
-        }
-    }
-    class Amalga implements ILanguageProtocol{
-        private final TypeFactory tf = TypeFactory.getInstance();
-        private PrintWriter stdout;
-        private PrintWriter stderr;
-        private String currentPrompt;
-        private final ICallableValue handler;
-        private final IEvaluatorContext ctx;
-        private final ICallableValue completor;
-        
-        public Amalga(IConstructor repl, IEvaluatorContext ctx) throws IOException, URISyntaxException {
-            this.ctx = ctx;
-            this.handler = (ICallableValue)repl.get("handler");
-            this.completor = (ICallableValue)repl.get("completor");
-            this.currentPrompt = ((IString)repl.get("prompt")).getValue();
-            stdout = new PrintWriter(System.out);
-            assert stdout != null;
-            stdout.println(((IString)repl.get("welcome")).getValue());
-        }
-
-        @Override
-        public void initialize(Writer stdout, Writer stderr) {
-            // TODO Auto-generated method stub
-            
-        }
-
-        @Override
-        public String getPrompt() {
-            // TODO Auto-generated method stub
-            return null;
-        }
-
-        @Override
-        public void handleInput(String line, Map<String, String> output, Map<String, String> metadata)
-            throws InterruptedException {
-            // TODO Auto-generated method stub
-            
-        }
-
-        @Override
-        public void handleReset(Map<String, String> output, Map<String, String> metadata) throws InterruptedException {
-            // TODO Auto-generated method stub
-            
-        }
-
-        @Override
-        public boolean supportsCompletion() {
-            // TODO Auto-generated method stub
-            return false;
-        }
-
-        @Override
-        public boolean printSpaceAfterFullCompletion() {
-            // TODO Auto-generated method stub
-            return false;
-        }
-
-        @Override
-        public CompletionResult completeFragment(String line, int cursor) {
-            // TODO Auto-generated method stub
-            return null;
-        }
-
-        @Override
-        public void cancelRunningCommandRequested() {
-            // TODO Auto-generated method stub
-            
-        }
-
-        @Override
-        public void terminateRequested() {
-            // TODO Auto-generated method stub
-            
-        }
-
-        @Override
-        public void stackTraceRequested() {
-            // TODO Auto-generated method stub
-            
-        }
-
-        @Override
-        public boolean isStatementComplete(String command) {
-            // TODO Auto-generated method stub
-            return false;
-        }
-
-        @Override
-        public void stop() {
-            // TODO Auto-generated method stub
-            
-        }
+    public IValue completor( IString line, IInteger cursor, IEvaluatorContext ctx){
+        return null ;
     }
 }
