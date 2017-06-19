dde5e09e8c70ecb30bd130185522aa804006b70e
Davy Landman <davy.landman@swat.engineering>
2017-06-19 14:07:09 +0200

    Fixed indenting

--- src/org/rascalmpl/library/cobra/QuickCheck.java
+++ src/org/rascalmpl/library/cobra/QuickCheck.java
@@ -30,132 +30,132 @@ import io.usethesource.vallang.IValueFactory;
 import io.usethesource.vallang.type.Type;
 
 public class QuickCheck {
-	
-	private final String EXPECT_TAG = "expected";
 
-	private static class InstanceHolder {
-		public static final QuickCheck sInstance = new QuickCheck();
-	}
+    private final String EXPECT_TAG = "expected";
+
+    private static class InstanceHolder {
+        public static final QuickCheck sInstance = new QuickCheck();
+    }
 
 
-	public static QuickCheck getInstance() {
-		return InstanceHolder.sInstance;
-	}
-	
-	public IValue arbitrary(Type type, int depthLimit, Environment env,
-	    IValueFactory vf, Map<Type, Type> typeParameters) {
+    public static QuickCheck getInstance() {
+        return InstanceHolder.sInstance;
+    }
 
-	    RandomValueTypeVisitor visitor = new RandomValueTypeVisitor(vf,
-	        (ModuleEnvironment) env, depthLimit, typeParameters);
+    public IValue arbitrary(Type type, int depthLimit, Environment env,
+        IValueFactory vf, Map<Type, Type> typeParameters) {
 
-	    IValue result = visitor.generate(type);
-	    if (result == null) {
-	        throw new IllegalArgumentException("No construction possible at this depth or less.");
-	    }
-	    return result;
-	}
+        RandomValueTypeVisitor visitor = new RandomValueTypeVisitor(vf,
+            (ModuleEnvironment) env, depthLimit, typeParameters);
 
-	public boolean quickcheck(AbstractFunction function, int maxDepth,
-			int tries, boolean verbose, PrintWriter out) {
+        IValue result = visitor.generate(type);
+        if (result == null) {
+            throw new IllegalArgumentException("No construction possible at this depth or less.");
+        }
+        return result;
+    }
 
-		String fname = function.getEnv().getName() + "::" + function.getName();
-	
-		Environment declEnv = function.getEnv();
-		IValueFactory vf = function.getEval().getValueFactory();
-		Type formals = function.getFormals();
-		String expected = null;
-		
-		if(function.hasTag(EXPECT_TAG)){
-			expected = ((IString) function.getTag(EXPECT_TAG)).getValue();
-		}
-		
+    public boolean quickcheck(AbstractFunction function, int maxDepth,
+        int tries, boolean verbose, PrintWriter out) {
 
-		Type[] types = new Type[formals.getArity()];
-		IValue[] values = new IValue[formals.getArity()];
+        String fname = function.getEnv().getName() + "::" + function.getName();
 
-		for (int n = 0; n < formals.getArity(); n++) {
-			types[n] = formals.getFieldType(n);
-		}
+        Environment declEnv = function.getEnv();
+        IValueFactory vf = function.getEval().getValueFactory();
+        Type formals = function.getFormals();
+        String expected = null;
 
-		if (formals.getArity() == 0) {
-			tries = 1;
-		}
+        if(function.hasTag(EXPECT_TAG)){
+            expected = ((IString) function.getTag(EXPECT_TAG)).getValue();
+        }
 
-		TypeParameterVisitor tpvisit = new TypeParameterVisitor();
-		
-		for (int i = 0; i < tries; i++) {
-			values = new IValue[formals.getArity()];
 
-			HashMap<Type, Type> tpbindings = tpvisit.bindTypeParameters(formals);
-			for (int n = 0; n < formals.getArity(); n++) {
-				values[n] = arbitrary(types[n], maxDepth, declEnv.getRoot(), vf, tpbindings);
-			}
-			boolean expectedThrown = false;
-			try {
-				Type[] actualTypes = new Type[formals.getArity()];
-				for(int j = 0; j < types.length; j ++) {
-					actualTypes[j] = types[j].instantiate(tpbindings);
-				}
-				IValue result = function.call(actualTypes, values, null).getValue();
-				function.getEval().getStdOut().flush();
-				
-				if (expected != null) {
-					// no exception at all is thrown
-					return reportMissingException(fname, expected, out);
-				}
-				
-				if (!((IBool) result).getValue()) {
-					reportFailed(fname, "test returns false", tpbindings, formals, values, out);
-					return false;
-				} else if (verbose && formals.getArity() > 0) {
-					out.println((i + 1) + ": Checked with " + Arrays.toString(values) + ": true");
-				}
-			} 
-			catch (Throw e) {
-				if(expected == null || !((IConstructor)e.getException()).getName().equals(expected)){
-					return reportFailed(fname, e.getMessage(), tpbindings, formals, values, out);
-				}
-				expectedThrown = true;
-			}
-			catch (Throwable e) {
-				e.printStackTrace();
-				if(expected == null || !e.getClass().toString().endsWith("." + expected)){
-					return reportFailed(fname, e.getMessage(), tpbindings, formals, values, out);
-				}
-				expectedThrown = true;
-			}
-			
-			if(expected != null && !expectedThrown){
-				return reportMissingException(fname, expected, out);
-			}
-		}
+        Type[] types = new Type[formals.getArity()];
+        IValue[] values = new IValue[formals.getArity()];
 
-		out.println("Test " + fname + (formals.getArity() > 0 ? " not refuted after " + tries + " tries with maximum depth " + maxDepth
-				: " succeeded"));
+        for (int n = 0; n < formals.getArity(); n++) {
+            types[n] = formals.getFieldType(n);
+        }
 
-		return true;
-	}
-	
-	private boolean reportFailed(String name, String msg, HashMap<Type, Type> tpbindings, Type formals, IValue[] values, PrintWriter out){
-		out.println("Test " + name + " failed due to\n\t" + msg + "\n");
-		if(tpbindings.size() > 0){
-			out.println("Type parameters:");
-			for(Type key : tpbindings.keySet()){
-				out.println("\t" + key + " => " + tpbindings.get(key));
-			}
-		}
-		out.println("Actual parameters:");
-		for (int i = 0; i < formals.getArity(); i++) {
-			IValue arg = values[i];
-			out.println("\t" + formals.getFieldType(i) + " " + "=>" + arg);
-		}
-		out.println();
-		return false;
-	}
-	
-	private boolean reportMissingException(String name, String msg, PrintWriter out){
-		out.println("Test " + name + " failed due to\n\tmissing exception: " + msg + "\n");
-		return false;
-	}
+        if (formals.getArity() == 0) {
+            tries = 1;
+        }
+
+        TypeParameterVisitor tpvisit = new TypeParameterVisitor();
+
+        for (int i = 0; i < tries; i++) {
+            values = new IValue[formals.getArity()];
+
+            HashMap<Type, Type> tpbindings = tpvisit.bindTypeParameters(formals);
+            for (int n = 0; n < formals.getArity(); n++) {
+                values[n] = arbitrary(types[n], maxDepth, declEnv.getRoot(), vf, tpbindings);
+            }
+            boolean expectedThrown = false;
+            try {
+                Type[] actualTypes = new Type[formals.getArity()];
+                for(int j = 0; j < types.length; j ++) {
+                    actualTypes[j] = types[j].instantiate(tpbindings);
+                }
+                IValue result = function.call(actualTypes, values, null).getValue();
+                function.getEval().getStdOut().flush();
+
+                if (expected != null) {
+                    // no exception at all is thrown
+                    return reportMissingException(fname, expected, out);
+                }
+
+                if (!((IBool) result).getValue()) {
+                    reportFailed(fname, "test returns false", tpbindings, formals, values, out);
+                    return false;
+                } else if (verbose && formals.getArity() > 0) {
+                    out.println((i + 1) + ": Checked with " + Arrays.toString(values) + ": true");
+                }
+            } 
+            catch (Throw e) {
+                if(expected == null || !((IConstructor)e.getException()).getName().equals(expected)){
+                    return reportFailed(fname, e.getMessage(), tpbindings, formals, values, out);
+                }
+                expectedThrown = true;
+            }
+            catch (Throwable e) {
+                e.printStackTrace();
+                if(expected == null || !e.getClass().toString().endsWith("." + expected)){
+                    return reportFailed(fname, e.getMessage(), tpbindings, formals, values, out);
+                }
+                expectedThrown = true;
+            }
+
+            if(expected != null && !expectedThrown){
+                return reportMissingException(fname, expected, out);
+            }
+        }
+
+        out.println("Test " + fname + (formals.getArity() > 0 ? " not refuted after " + tries + " tries with maximum depth " + maxDepth
+            : " succeeded"));
+
+        return true;
+    }
+
+    private boolean reportFailed(String name, String msg, HashMap<Type, Type> tpbindings, Type formals, IValue[] values, PrintWriter out){
+        out.println("Test " + name + " failed due to\n\t" + msg + "\n");
+        if(tpbindings.size() > 0){
+            out.println("Type parameters:");
+            for(Type key : tpbindings.keySet()){
+                out.println("\t" + key + " => " + tpbindings.get(key));
+            }
+        }
+        out.println("Actual parameters:");
+        for (int i = 0; i < formals.getArity(); i++) {
+            IValue arg = values[i];
+            out.println("\t" + formals.getFieldType(i) + " " + "=>" + arg);
+        }
+        out.println();
+        return false;
+    }
+
+    private boolean reportMissingException(String name, String msg, PrintWriter out){
+        out.println("Test " + name + " failed due to\n\tmissing exception: " + msg + "\n");
+        return false;
+    }
 
 }
