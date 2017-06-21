3641b8d1530af73c4b2422de07232564d05f22e6
paulklint <Paul.Klint@cwi.nl>
2017-06-21 20:53:46 +0200

    Preparing relocation of MuLibrary
    Relocation of source locations was not applied when compiling the
    MuLibrary. Preparations have been made for this. Can be enabled after
    next boot release.

--- src/org/rascalmpl/library/experiments/Compiler/Commands/Bootstrap.java
+++ src/org/rascalmpl/library/experiments/Compiler/Commands/Bootstrap.java
@@ -452,7 +452,7 @@ public class Bootstrap {
       Path testResults = phaseTestFolder(phase, tmp);
       progress("phase " + phase + ": " + phaseResult);
 
-      time("- compile MuLibrary",       () -> compileMuLibrary(phase, classPath, bootPath, sourcePath, phaseResult));
+      time("- compile MuLibrary",       () -> compileMuLibrary(phase, classPath, bootPath, sourcePath, phaseResult, reloc));
       time("- compile Kernel",          () -> compileModule   (phase, classPath, bootPath, sourcePath, phaseResult, "lang::rascal::boot::Kernel", reloc));
 
       if (phase == 1) {
@@ -529,10 +529,12 @@ public class Bootstrap {
         }
     }
     
-    private static void compileMuLibrary(int phase, String classPath, String bootDLoc, String sourcePath, Path phaseResult) throws IOException, InterruptedException, BootstrapMessage {
+    private static void compileMuLibrary(int phase, String classPath, String bootDLoc, String sourcePath, Path phaseResult, String reloc) throws IOException, InterruptedException, BootstrapMessage {
         progress("\tcompiling MuLibrary (phase " + phase +")");
         
-        String[] paths = new String [] { "--bin", phaseResult.toAbsolutePath().toString(), "--src", sourcePath, "--boot", bootDLoc };
+        String[] paths = // transitional, switch after next boot release
+                         new String [] { "--bin", phaseResult.toAbsolutePath().toString(), "--src", sourcePath, "--boot", bootDLoc };
+                         //: new String [] { "--bin", phaseResult.toAbsolutePath().toString(), "--src", sourcePath, "--boot", bootDLoc, "--reloc", reloc };
         String[] otherArgs = VERBOSE? new String[] {"--verbose"} : new String[0];
 
         if (runMuLibraryCompiler(classPath, concat(paths, otherArgs)) != 0) {
