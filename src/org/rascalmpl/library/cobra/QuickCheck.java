0360068a6260086860b08c8dccc185a94d5d042c
Davy Landman <davy.landman@swat.engineering>
2017-06-23 10:45:47 +0200

    After a failing random test case is found, we try to find a smaller case
    Thanks to @wietsevenema for the inspiration, he pointed out that he never got around to that while developing Cobra

--- src/org/rascalmpl/library/cobra/QuickCheck.java
+++ src/org/rascalmpl/library/cobra/QuickCheck.java
@@ -9,13 +9,16 @@
 
  *   * Wietse Venema - wietsevenema@gmail.com - CWI
  *   * Paul Klint - Paul.Klint@cwi.nl - CWI - added type parameters
+ *   * Davy Landman - davy.landman@swat.engineering - SWAT.engineering
  *******************************************************************************/
 package org.rascalmpl.library.cobra;
 
 import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Random;
+import java.util.function.Function;
 
 import org.rascalmpl.interpreter.control_exceptions.Throw;
 import org.rascalmpl.interpreter.env.Environment;
@@ -29,6 +32,7 @@ import io.usethesource.vallang.IValueFactory;
 import io.usethesource.vallang.random.RandomValueGenerator;
 import io.usethesource.vallang.random.util.TypeParameterBinder;
 import io.usethesource.vallang.type.Type;
+import io.usethesource.vallang.type.TypeStore;
 
 public class QuickCheck {
 
@@ -99,24 +103,26 @@ public class QuickCheck {
                 }
 
                 if (!((IBool) result).getValue()) {
+                    // reducing to smallest case
+                    values = findSmallestInput(function, expected, maxDepth, maxWidth, tries, declEnv, vf, types, values, tpbindings, actualTypes);
                     reportFailed(fname, "test returns false", tpbindings, formals, values, out);
                     return false;
                 } else if (verbose && formals.getArity() > 0) {
                     out.println((i + 1) + ": Checked with " + Arrays.toString(values) + ": true");
                 }
             } 
-            catch (Throw e) {
-                if(expected == null || !((IConstructor)e.getException()).getName().equals(expected)){
-                    return reportFailed(fname, e.getMessage(), tpbindings, formals, values, out);
-                }
-                expectedThrown = true;
-            }
             catch (Throwable e) {
-                e.printStackTrace();
-                if(expected == null || !e.getClass().toString().endsWith("." + expected)){
+                if (!(e instanceof Throw)) {
+                    e.printStackTrace();
+                }
+                if (expected != null) {
+                    expectedThrown = e.getClass().toString().endsWith("." + expected)
+                        || (e instanceof Throw && ((IConstructor)((Throw)e).getException()).getName().equals(expected));
+                }
+                if (!expectedThrown) {
+                    values = findSmallestInput(function, expected, maxDepth, maxWidth, tries, declEnv, vf, types, values, tpbindings, actualTypes);
                     return reportFailed(fname, e.getMessage(), tpbindings, formals, values, out);
                 }
-                expectedThrown = true;
             }
 
             if(expected != null && !expectedThrown){
@@ -128,6 +134,76 @@ public class QuickCheck {
             : " succeeded"));
 
         return true;
+    }
+
+
+    private IValue[] findSmallestInput(AbstractFunction function, String expected, int maxDepth, int maxWidth, int tries, Environment declEnv,
+        IValueFactory vf, Type[] types, IValue[] values, Map<Type, Type> tpbindings, Type[] actualTypes) {
+        values = findSmallestFailing(
+            generateAlternatives(types, vf, stRandom, tries, maxDepth, maxWidth, declEnv.getRoot().getStore(), tpbindings),
+            (IValue[] args) -> {
+                try {
+                    return ((IBool)(function.call(actualTypes, args, null).getValue())).getValue();
+                } catch (Throwable e) {
+                    if (!(e instanceof Throw)) {
+                        e.printStackTrace();
+                    }
+                    if (expected != null) {
+                        return e.getClass().toString().endsWith("." + expected)
+                            || (e instanceof Throw && ((IConstructor)((Throw)e).getException()).getName().equals(expected));
+                    }
+                    return false;
+                }
+            }, 
+            values );
+        return values;
+    }
+    
+    private IValue[] findSmallestFailing(Iterator<IValue[]> values, Function<IValue[], Boolean> test, IValue[] originalFailure) {
+        while (values.hasNext()) {
+            IValue[] currentValues = values.next();
+            if (test.apply(currentValues)) {
+                return currentValues;
+            }
+        }
+        return originalFailure;
+    }
+    
+    
+    private Iterator<IValue[]> generateAlternatives(Type[] types, IValueFactory vf, Random random, int maxTries, int maxDepth, int maxWidth, TypeStore store, Map<Type, Type> parameterBindings) {
+        return new Iterator<IValue[]>() {
+            private int currentDepth = 1;
+            private int currentWidth = 1;
+            private int tries = 0;
+            private RandomValueGenerator generator = new RandomValueGenerator(vf, random, currentDepth, currentWidth);
+
+            @Override
+            public boolean hasNext() {
+                return tries < maxTries || currentDepth <= maxDepth || currentWidth <= maxWidth;
+            }
+
+            @Override
+            public IValue[] next() {
+                if (tries == maxTries) {
+                    if (currentWidth == maxWidth) {
+                        currentDepth++;
+                        currentWidth = 1;
+                    }
+                    else {
+                        currentWidth++;
+                    }
+                    generator = new RandomValueGenerator(vf, random, currentDepth, currentWidth);
+                    tries = 0;
+                }
+                tries++;
+                IValue[] result = new IValue[types.length];
+                for (int i = 0; i < result.length; i++) {
+                    result[i] = generator.generate(types[i], store, parameterBindings); 
+                }
+                return result;
+            }
+        };
+        
     }
 
     private boolean reportFailed(String name, String msg, Map<Type, Type> tpbindings, Type formals, IValue[] values, PrintWriter out){
