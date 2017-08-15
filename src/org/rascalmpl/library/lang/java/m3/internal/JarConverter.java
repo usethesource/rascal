674f2290ea890333afbec2c314c91c419683c196
lmove <lina.m8a@gmail.com>
2017-08-15 13:43:07 +0200

    Containment, uses, declarations, and name relations for Class nodes.

--- src/org/rascalmpl/library/lang/java/m3/internal/JarConverter.java
+++ src/org/rascalmpl/library/lang/java/m3/internal/JarConverter.java
@@ -19,52 +19,30 @@ package org.rascalmpl.library.lang.java.m3.internal;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Iterator;
 import java.util.Map;
 
-import org.eclipse.jdt.core.Signature;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.signature.SignatureReader;
-import org.objectweb.asm.signature.SignatureVisitor;
 import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.FieldNode;
 import org.objectweb.asm.tree.InnerClassNode;
-import org.objectweb.asm.tree.MethodNode;
 import org.rascalmpl.uri.URIResolverRegistry;
 import org.rascalmpl.uri.URIUtil;
-import org.rascalmpl.uri.jar.JarURIResolver;
 
-import io.usethesource.vallang.IConstructor;
 import io.usethesource.vallang.ISet;
 import io.usethesource.vallang.ISetWriter;
 import io.usethesource.vallang.ISourceLocation;
 import io.usethesource.vallang.IString;
-import io.usethesource.vallang.IValue;
 
 public class JarConverter extends M3Converter {
     
-    private final int CLASSE = 0;
-    private final int METHODE = 1;
-    private final int FIELDE = 2;
+    private final static String CLASS_SCHEME = "java+class";
+    private final static String COMP_UNIT_SCHEME = "java+compilationUnit";
+    private final static String ENUM_SCHEME = "java+enum";
+    private final static String INTERFACE_SCHEME = "java+interface";
+    private final static String PACKAGE_SCHEME = "java+package";
     
     private ISet packages;
     private ISet compilationUnits;
-    private ClassReader cr;
-    private ClassNode cn;
-    private String qualifiedName;
-    private String scheme;
-    
-    
-    private ISourceLocation jarLocOld;
-    private String jarFileOld;
-    private String classFileOld;
-    private String logPathOld;
-    private String classSchemeOld;
-    private String classNameOld;
-    private String packageNameOld;
-    private boolean classIsEnumOld;
-    
 
     public JarConverter(LimitedTypeStore typeStore, Map<String, ISourceLocation> cache) {
         super(typeStore, cache);
@@ -80,8 +58,6 @@ public class JarConverter extends M3Converter {
             resolvePackages(loc, packagesWriter, compUnitsWriter);
             this.packages = packagesWriter.done();
             this.compilationUnits = compUnitsWriter.done();
-            
-            setPackageRelations();
         }
         catch (URISyntaxException e) {
             e.printStackTrace();
@@ -93,68 +69,109 @@ public class JarConverter extends M3Converter {
     
     private void resolvePackages(ISourceLocation uri, ISetWriter packagesWriter, ISetWriter classesWriter) 
         throws IOException, URISyntaxException {
-        JarURIResolver resolver = new JarURIResolver(URIResolverRegistry.getInstance());
-        String[] content = resolver.list(uri);
+        URIResolverRegistry resgistry = URIResolverRegistry.getInstance();
+        String[] content = resgistry.listEntries(uri);
             
         for(String path : content) {
             ISourceLocation local = URIUtil.changePath(uri, uri.getPath() + "/" + path);
             
-            if(resolver.isFile(local) && local.getPath().endsWith(".class")) {
+            if(resgistry.isFile(local) && local.getPath().endsWith(".class")) {
                 packagesWriter.insert(uri);
                 classesWriter.insert(local);
-                setCompUnitAndPackageRelations(local);
+                
+                setCompilationUnitRelations(local);
+                setPackagesRelations(local);
+                setClassRelations(local);
             }
-            else if(resolver.isDirectory(local)) {
+            else if(resgistry.isDirectory(local)) {
                 resolvePackages(local, packagesWriter, classesWriter);
             }
         }
     }
     
-    private void setCompUnitAndPackageRelations(ISourceLocation uri) throws URISyntaxException {
-        String compUnit = uri.getPath().substring(uri.getPath().lastIndexOf("!") + 1).replace(".class", ".java");
-        IString compUnitName = values.string(compUnit.substring(compUnit.lastIndexOf("/") + 1).replace(".java", ""));
+    //uri - compUnitUri
+    private void setCompilationUnitRelations(ISourceLocation compUnitPhysical) throws URISyntaxException {
+        String compUnit = compUnitPhysical.getPath().substring(compUnitPhysical.getPath().lastIndexOf("!") + 1);
+        IString compUnitName = values.string(compUnit.substring(compUnit.lastIndexOf("/") + 1).replace(".class", ""));
+       
+        ISourceLocation packageLogical = values.sourceLocation(PACKAGE_SCHEME, "", compUnit.substring(0, compUnit.lastIndexOf("/")));
+        ISourceLocation compUnitLogical = values.sourceLocation(COMP_UNIT_SCHEME, "", compUnit);
+        
+        insert(containment, packageLogical, compUnitLogical);
+        //TODO: same offset of the contained class?
+        insert(declarations, compUnitLogical, compUnitPhysical);
+        insert(uses, compUnitPhysical, compUnitLogical);
+        //TODO: M3 from directory does not add it to the names relation. Check.
+        //insert(names, compUnitName, compUnitLogical); 
+    }
+    
+    private void setPackagesRelations(ISourceLocation compUnitPhysical) throws URISyntaxException {
+        String compUnit = compUnitPhysical.getPath().substring(compUnitPhysical.getPath().lastIndexOf("!") + 1);
         String currentPackage = compUnit.substring(0,compUnit.lastIndexOf("/"));
         int packages = currentPackage.length() - currentPackage.replace("/", "").length();
         
-        ISourceLocation packageLogical = values.sourceLocation("java+package", "", compUnit.substring(0, compUnit.lastIndexOf("/")));
-        ISourceLocation compUnitLogical = values.sourceLocation("java+compilationUnit", "", compUnit);
-        ISourceLocation compUnitPhysical = URIUtil.changePath(loc, loc.getPath() + compUnit);
-        
-        this.insert(this.containment, packageLogical, compUnitLogical);
-        this.insert(this.declarations, compUnitLogical, compUnitPhysical);
-        this.insert(this.uses, compUnitPhysical, compUnitLogical);
-        this.insert(this.names, compUnitName, compUnitLogical);
-        
         for(int i = 1; i < packages; i++) {
             String parentPackage = currentPackage.substring(0, currentPackage.lastIndexOf("/"));
-            IString currentName = values.string(currentPackage.substring(currentPackage.lastIndexOf("/") + 1));
             
-            ISourceLocation parentPkgLogical = values.sourceLocation("java+package", "", parentPackage);
+            IString currentName = values.string(currentPackage.substring(currentPackage.lastIndexOf("/") + 1));
+            ISourceLocation parentPkgLogical = values.sourceLocation(PACKAGE_SCHEME, "", parentPackage);
             ISourceLocation parentPkgPhysical = URIUtil.changePath(loc, loc.getPath() + parentPackage);
-            ISourceLocation currentPkgLogical = values.sourceLocation("java+package", "", currentPackage);
+            ISourceLocation currentPkgLogical = values.sourceLocation(PACKAGE_SCHEME, "", currentPackage);
             ISourceLocation currentPkgPhysical = URIUtil.changePath(loc, loc.getPath() + currentPackage);
             
-            this.insert(this.containment, parentPkgLogical, currentPkgLogical);
-            this.insert(this.declarations, currentPkgLogical, currentPkgPhysical);
-            this.insert(this.uses, currentPkgPhysical, currentPkgLogical);
-            this.insert(this.names, currentName, currentPkgLogical);
+            insert(containment, parentPkgLogical, currentPkgLogical);
+            insert(declarations, currentPkgLogical, currentPkgPhysical);
+            insert(uses, currentPkgPhysical, currentPkgLogical);
+            insert(names, currentName, currentPkgLogical);
             
             if(i == packages - 1) {
                 IString parentName = values.string(parentPackage.substring(parentPackage.lastIndexOf("/") + 1));
-                this.insert(this.declarations, parentPkgLogical, parentPkgPhysical);
-                this.insert(this.uses, parentPkgPhysical, parentPkgLogical);
-                this.insert(this.names, parentName, parentPkgLogical);
+                insert(declarations, parentPkgLogical, parentPkgPhysical);
+                insert(uses, parentPkgPhysical, parentPkgLogical);
+                insert(names, parentName, parentPkgLogical);
             }
             
             currentPackage = parentPackage;
         }
     }
     
-    private void setPackageRelations() throws URISyntaxException {
-        Iterator<IValue> iterator = packages.iterator();
+    private void setClassRelations(ISourceLocation compUnitPhysical) throws IOException, URISyntaxException {
+        ClassReader cr = new ClassReader(URIResolverRegistry.getInstance().getInputStream(compUnitPhysical));
+        ClassNode cn = new ClassNode();
+        cr.accept(cn, ClassReader.SKIP_DEBUG);
         
-        while(iterator.hasNext()) {
-            ISourceLocation local = (ISourceLocation) iterator.next();
+        String compUnit = compUnitPhysical.getPath().substring(compUnitPhysical.getPath().lastIndexOf("!") + 1);
+        String classPath = cn.name.replace("$", "/");
+       
+        IString className = values.string(classPath.substring(classPath.lastIndexOf("/") + 1));
+        ISourceLocation compUnitLogical = values.sourceLocation(COMP_UNIT_SCHEME, "", compUnit);
+        ISourceLocation classLogical = values.sourceLocation(getClassScheme(cn.access), "", classPath);
+        //TODO: check the offset and length info. 
+        ISourceLocation classPhysical = values.sourceLocation(compUnitPhysical, cr.header, cr.b.length);
+
+        insert(containment, compUnitLogical, classLogical);
+        insert(declarations, classLogical, classPhysical);
+        insert(uses, classPhysical, classLogical);
+        insert(names, className, classLogical);
+        
+        setInnerClassRelations(cn, classLogical);
+    }
+
+    private void setInnerClassRelations(ClassNode cn, ISourceLocation classLogical) throws URISyntaxException {
+        for(int i = 0; i < cn.innerClasses.size(); i++) {
+            InnerClassNode icn = (InnerClassNode) cn.innerClasses.get(i);
+            String innerClassPath = icn.name.replace("$", "/");
+            
+            if(!innerClassPath.equals(cn.name.replace("$", "/"))) {
+                System.out.println(cn.name.replace("$", "/") + " - " + icn.name.replace("$", "/"));
+                ISourceLocation innerClasslogical = values.sourceLocation(getClassScheme(icn.access), "", icn.name.replace("$", "/"));
+                insert(containment, classLogical, innerClasslogical);
+            }
         }
+    }
+    
+    private String getClassScheme(int access) {
+        return ((access & Opcodes.ACC_INTERFACE) != 0) ? INTERFACE_SCHEME : 
+            ((access & Opcodes.ACC_ENUM) != 0) ? ENUM_SCHEME : CLASS_SCHEME;
     }
 }
