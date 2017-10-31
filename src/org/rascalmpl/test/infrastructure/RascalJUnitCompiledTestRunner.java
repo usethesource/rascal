8e21f288b0973f68e64c21ee4c84389244ec0da7
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2017-10-31 09:42:48 +0100

    removed printing of pathConfig on stderr

--- src/org/rascalmpl/test/infrastructure/RascalJUnitCompiledTestRunner.java
+++ src/org/rascalmpl/test/infrastructure/RascalJUnitCompiledTestRunner.java
@@ -111,8 +111,6 @@ public class RascalJUnitCompiledTestRunner extends Runner {
         catch (IOException e) {
             assert false; // this project should exist
         }
-        
-        System.err.println(pcfg);
     }
 
     private void initializeKernel() {
