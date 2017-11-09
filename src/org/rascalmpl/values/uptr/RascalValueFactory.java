37d12ea9958fc337337b06267e4a2fdb31351e9e
paulklint <Paul.Klint@cwi.nl>
2017-11-09 15:18:23 +0100

    Fixed another off by one

--- src/org/rascalmpl/values/uptr/RascalValueFactory.java
+++ src/org/rascalmpl/values/uptr/RascalValueFactory.java
@@ -268,9 +268,9 @@ public class RascalValueFactory extends AbstractValueFactoryAdapter implements I
 	private final static ITree byteChars[];
 	private final static Type byteCharTypes[];
 	static {
-		byteChars = new ITree[Byte.MAX_VALUE + 1];
-		byteCharTypes = new Type[Byte.MAX_VALUE + 1];
-		for (byte i = 0; i <= Byte.MAX_VALUE; i++) {
+		byteChars = new ITree[Byte.MAX_VALUE];
+		byteCharTypes = new Type[Byte.MAX_VALUE];
+		for (byte i = 0; i < Byte.MAX_VALUE; i++) {
 			byteChars[i] = new CharByte(i);
 			byteCharTypes[i] = RascalTypeFactory.getInstance().nonTerminalType(bootFactory.constructor(Symbol_CharClass, bootFactory.list(bootFactory.constructor(CharRange_Range, bootFactory.integer(i), bootFactory.integer(i)))));
 		}
@@ -404,7 +404,7 @@ public class RascalValueFactory extends AbstractValueFactoryAdapter implements I
 	
 	@Override
 	public ITree character(int ch) {
-		if (ch >= 0 && ch <= Byte.MAX_VALUE) {
+		if (ch >= 0 && ch < Byte.MAX_VALUE) {
 			return character((byte) ch);
 		}
 		
