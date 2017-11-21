dda3b6acc0161ddf938b6dce3a20f34834098d62
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2017-11-21 14:42:22 +0100

    removed superfluous code

--- src/org/rascalmpl/test/infrastructure/RascalJUnitCompiledTestRunner.java
+++ src/org/rascalmpl/test/infrastructure/RascalJUnitCompiledTestRunner.java
@@ -310,18 +310,13 @@ public class RascalJUnitCompiledTestRunner extends Runner {
        
         notifier.fireTestRunStarted(desc);
 
-        if (desc.getAnnotations().stream().anyMatch(t -> t instanceof CompilationFailed)) {
-            notifier.fireTestFailure(new Failure(desc, new IllegalArgumentException("test module has compilation errors")));
-            return;
-        }
-        
         for (Description mod : desc.getChildren()) {
             RascalExecutionContext rex = RascalExecutionContextBuilder.normalContext(pcfg).build();
             ISourceLocation binary = null;
             RVMCore rvmCore = null;
             
             if (mod.getAnnotations().stream().anyMatch(t -> t instanceof CompilationFailed)) {
-                notifier.fireTestFailure(new Failure(desc, new IllegalArgumentException("test module has compilation errors")));
+                notifier.fireTestFailure(new Failure(desc, new IllegalArgumentException(mod.getDisplayName() + " had compilation errors")));
                 continue;
             }
             
