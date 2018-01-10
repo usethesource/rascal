27306e2812c24310502c551ffe98d52d41d0cc97
Rodin Aarssen <rodin.aarssen@cwi.nl>
2018-01-10 18:20:05 +0100

    Unused imports

--- src/org/rascalmpl/parser/ASTBuilder.java
+++ src/org/rascalmpl/parser/ASTBuilder.java
@@ -21,9 +21,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map.Entry;
 
 import org.rascalmpl.ast.AbstractAST;
 import org.rascalmpl.ast.BooleanLiteral;
