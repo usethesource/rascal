507ecc7d4e8fe929f41be3797adf51d69aaa4382
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2018-06-20 11:07:07 +0200

    added missing changes

--- src/org/rascalmpl/library/PreludeCompiled.java
+++ src/org/rascalmpl/library/PreludeCompiled.java
@@ -303,13 +303,13 @@ public class PreludeCompiled extends Prelude {
 	// end of sorting functions
 	
 	// public java &T<:Tree parse(type[&T<:Tree] begin, str input);
-	public IValue parse(IValue start, ISourceLocation input, IBool allowAmbiguity, RascalExecutionContext rex) {
-		return rex.getParsingTools().parse(super.values.string(rex.getFullModuleName()), start, input, allowAmbiguity.getValue(), null, rex);
+	public IValue parse(IValue start, ISourceLocation input, IBool allowAmbiguity, IBool hasSideEffects, RascalExecutionContext rex) {
+		return rex.getParsingTools().parse(super.values.string(rex.getFullModuleName()), start, input, allowAmbiguity.getValue(), hasSideEffects.getValue(), null, rex);
 	}
 
 	// public java &T<:Tree parse(type[&T<:Tree] begin, str input, loc origin);
-	public IValue parse(IValue start, IString input, IBool allowAmbiguity, RascalExecutionContext rex) {
-		return rex.getParsingTools().parse(super.values.string(rex.getFullModuleName()), start, input, allowAmbiguity.getValue(), null, rex);
+	public IValue parse(IValue start, IString input, IBool allowAmbiguity, IBool hasSideEffects, RascalExecutionContext rex) {
+		return rex.getParsingTools().parse(super.values.string(rex.getFullModuleName()), start, input, allowAmbiguity.getValue(), hasSideEffects.getValue(), null, rex);
 	}
 	
 	private TypeStore typeStore = new TypeStore();
