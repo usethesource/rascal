d4001fd900b642f0fa31f9f29ee931be7bc1b3b1
paulklint <Paul.Klint@cwi.nl>
2018-12-31 08:56:50 +0100

    Fixed compilation errors

--- src/org/rascalmpl/runtime/utils/RascalExceptionFactory.java
+++ src/org/rascalmpl/runtime/utils/RascalExceptionFactory.java
@@ -13,9 +13,11 @@
  *   * Paul Klint - Paul.Klint@cwi.nl - CWI
  *   * Mark Hills - Mark.Hills@cwi.nl (CWI)
  *   * Arnold Lankamp - Arnold.Lankamp@cwi.nl
-*******************************************************************************/
+ *******************************************************************************/
 package org.rascalmpl.core.library.lang.rascalcore.compile.runtime.utils;
 
+import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.Frame;
+import org.rascalmpl.library.experiments.Compiler.RVM.Interpreter.Thrown;
 import org.rascalmpl.values.ValueFactoryFactory;
 
 import io.usethesource.vallang.IInteger;
@@ -40,9 +42,9 @@ public class RascalExceptionFactory {
 	private static IValueFactory VF = ValueFactoryFactory.getValueFactory();
 	public static final TypeStore TS = new TypeStore();
 	public static final Type Exception = TF.abstractDataType(TS, "RuntimeException");
-	
+
 	public static final Type StackOverflow = TF.constructor(TS, Exception, "StackOverflow");
-    public static final Type IndexOutOfBounds = TF.constructor(TS, Exception, "IndexOutOfBounds", TF.integerType(), "index");
+	public static final Type IndexOutOfBounds = TF.constructor(TS, Exception, "IndexOutOfBounds", TF.integerType(), "index");
 	public static final Type AssertionFailed = TF.constructor(TS,Exception,"AssertionFailed");
 	public static final Type LabeledAssertionFailed = TF.constructor(TS,Exception,"AssertionFailed", TF.stringType(), "label");
 	public static final Type EmptyList = TF.constructor(TS,Exception,"EmptyList");
@@ -50,18 +52,20 @@ public class RascalExceptionFactory {
 	public static final Type EmptyMap = TF.constructor(TS,Exception,"EmptyMap");
 	public static final Type CallFailed = TF.constructor(TS, Exception, "CallFailed", TF.sourceLocationType(), "caller", TF.listType(TF.valueType()), "arguments");
 	public static final Type NoSuchElement = TF.constructor(TS,Exception,"NoSuchElement",TF.valueType(), "v");
-	public static final Type UnavailableInformation = TF.constructor(TS,Exception, "UnavailableInformation");
+	//public static final Type UnavailableInformation = TF.constructor(TS,Exception, "UnavailableInformation");
+	public static final Type UnavailableInformation = TF.constructor(TS,Exception, "UnavailableInformation", TF.stringType(), "message");
+
 	public static final Type IllegalArgument = TF.constructor(TS,Exception,"IllegalArgument",TF.valueType(), "v", TF.stringType(), "message");
 	public static final Type IllegalTypeArgument = TF.constructor(TS,Exception,"IllegalTypeArgument",TF.stringType(), "type", TF.stringType(), "message");
 
 	public static final Type AnonymousIllegalArgument = TF.constructor(TS,Exception,"IllegalArgument");
-	
+
 	public static final Type InvalidArgument = TF.constructor(TS,Exception,"InvalidArgument",TF.valueType(), "v", TF.stringType(), "message");
 	public static final Type AnonymousInvalidArgument = TF.constructor(TS,Exception,"InvalidArgument");
-	
+
 	public static final Type IO = TF.constructor(TS,Exception,"IO",TF.stringType(), "message");
 	public static final Type PathNotFound = TF.constructor(TS,Exception,"PathNotFound",TF.sourceLocationType(), "location");
-	
+
 	public static final Type LocationNotFound = TF.constructor(TS,Exception,"LocationNotFound",TF.sourceLocationType(), "location");
 	public static final Type PermissionDenied = TF.constructor(TS,Exception,"PermissionDenied",TF.stringType(), "message");
 	public static final Type AnonymousPermissionDenied = TF.constructor(TS,Exception,"PermissionDenied");
@@ -82,7 +86,7 @@ public class RascalExceptionFactory {
 
 	public static final Type Java = TF.constructor(TS, Exception, "Java", TF.stringType(), "class", TF.stringType(), "message");
 	public static final Type JavaWithCause = TF.constructor(TS, Exception, "Java", TF.stringType(), "class", TF.stringType(), "message", Exception, "cause");
-  
+
 	public static final Type InvalidUseOfDate = TF.constructor(TS, Exception, "InvalidUseOfDate", TF.dateTimeType(), "msg");
 	public static final Type InvalidUseOfTime = TF.constructor(TS, Exception, "InvalidUseOfTime", TF.dateTimeType(), "msg");
 	public static final Type InvalidUseOfDateTime = TF.constructor(TS, Exception, "InvalidUseOfDateTime", TF.dateTimeType(), "msg");
@@ -91,81 +95,81 @@ public class RascalExceptionFactory {
 	public static final Type DateTimePrintingError = TF.constructor(TS, Exception, "DateTimePrintingError", TF.stringType(), "message");
 	public static final Type Timeout = TF.constructor(TS, Exception, "Timeout");
 	public static final Type Figure = TF.constructor(TS, Exception, "Figure", TF.stringType(), "message", TF.valueType(), "figure");
-	
+
 	public static final Type ImplodeError = TF.constructor(TS, Exception, "ImplodeError", TF.stringType(), "message");
 
 	public static final Type ArithmeticException = TF.constructor(TS, Exception, "ArithmeticException", TF.stringType(), "message");
-	
+
 	// The "official" exceptions that a Rascal program can catch (alphabetical order)
-	
+
 	public static RascalException ambiguity(ISourceLocation loc, IString type, IString string) {
-	  return new RascalException(VF.constructor(Ambiguity, loc, type, string));
+		return new RascalException(VF.constructor(Ambiguity, loc, type, string));
 	}
-	
+
 	public static RascalException callFailed(ISourceLocation loc, IList arguments) {
-	    return new RascalException(VF.constructor(CallFailed, loc, arguments));
+		return new RascalException(VF.constructor(CallFailed, loc, arguments));
 	}
-	            
+
 	public static RascalException arithmeticException(String msg) {
 		return new RascalException(VF.constructor(ArithmeticException, VF.string(msg)));
 	}
-	
+
 	public static RascalException assertionFailed() {
 		return new RascalException(VF.constructor(AssertionFailed));
 	}
-	
+
 	public static RascalException assertionFailed(IString msg) {
-    	return new RascalException(VF.constructor(LabeledAssertionFailed, msg));
-    }
-	
+		return new RascalException(VF.constructor(LabeledAssertionFailed, msg));
+	}
+
 	public static RascalException emptyList() {
 		return new RascalException(VF.constructor(EmptyList));
 	}
-	
+
 	public static RascalException emptyMap() {
-	  return new RascalException(VF.constructor(EmptyMap));
+		return new RascalException(VF.constructor(EmptyMap));
 	}
-	
+
 	public static RascalException emptySet() {
 		return new RascalException(VF.constructor(EmptySet));
 	}
-	
+
 	public static RascalException illegalArgument() {
 		return new RascalException(VF.constructor(AnonymousIllegalArgument));	
 	}
-	
+
 	public static RascalException illegalArgument(IValue v) {
 		return new RascalException(VF.constructor(IllegalArgument));	
 	}
-	
+
 	public static RascalException illegalArgument(IValue v, String message) {
 		return new RascalException(VF.constructor(IllegalArgument, v, VF.string(message)));	
 	}
-	
+
 	public static RascalException invalidArgument() {
 		return new RascalException(VF.constructor(AnonymousInvalidArgument));	
 	}
-	
+
 	public static RascalException invalidArgument(IValue v) {
-		return new RascalException(VF.constructor(InvalidArgument));	
+		return new RascalException(VF.constructor(InvalidArgument, v));	
 	}
-	
-	public static RascalException invalidrgument(IValue v, String message) {
+
+	public static RascalException invalidArgument(IValue v, String message) {
 		return new RascalException(VF.constructor(InvalidArgument, v, VF.string(message)));	
 	}
-	
+
 	public static RascalException indexOutOfBounds(IInteger i) {
-    	return new RascalException(VF.constructor(IndexOutOfBounds, i));
-    }
-	
+		return new RascalException(VF.constructor(IndexOutOfBounds, i));
+	}
+
 	public static RascalException io(IString msg) {
 		return new RascalException(VF.constructor(IO, msg));
 	}
-	
+
 	public static RascalException moduleNotFound(IString module) {
 		return new RascalException(VF.constructor(ModuleNotFound, module));
 	}
-	
+
 	public static RascalException noSuchAnnotation(String label) {
 		return new RascalException(VF.constructor(NoSuchAnnotation, VF.string(label)));
 	}
@@ -173,54 +177,54 @@ public class RascalExceptionFactory {
 	public static RascalException noSuchKey(IValue v) {
 		return new RascalException(VF.constructor(NoSuchKey, v));
 	}
-	
+
 	public static RascalException parseError(ISourceLocation loc) {
 		return new RascalException(VF.constructor(ParseError, loc));
 	}
-	
+
 	public static RascalException pathNotFound(ISourceLocation loc) {
 		return new RascalException(VF.constructor(PathNotFound, loc));
 	}
-	
+
 	public static RascalException stackOverflow() {
 		return new RascalException(VF.constructor(StackOverflow));
 	}
-	
+
 	// The status of the following exceptions is to be determined (alphabetical order)
-	
+
 	public static RascalException arityMismatch(int expected, int got) {
 		return new RascalException(VF.constructor(ArityMismatch, VF.integer(expected), VF.integer(got)));
 	}
-	
+
 	public static RascalException dateTimeParsingError(String message) {
 		return new RascalException(VF.constructor(DateTimeParsingError, VF.string(message)));
 	}
-	
+
 	public static RascalException dateTimePrintingError(String message) {
 		return new RascalException(VF.constructor(DateTimePrintingError, VF.string(message)));
 	}	
-	
+
 	public static RascalException figureException(String message, IValue v) {
 		return new RascalException(VF.constructor(Figure, VF.string(message), v));
 	}
-	
+
 	public static RascalException illegalCharacter(IInteger i) {
 		return new RascalException(VF.constructor(IllegalChar, i));
 	}
-	
-//	public static RascalException illegalIdentifier(String name,
-//			) {
-//		return new RascalException(IllegalIdentifier.make(VF, VF.string(name)));
-//	}
-	
+
+	//	public static RascalException illegalIdentifier(String name,
+	//			) {
+	//		return new RascalException(IllegalIdentifier.make(VF, VF.string(name)));
+	//	}
+
 	public static RascalException illegalTypeArgument(String type, String message){
 		return new RascalException(VF.constructor(IllegalTypeArgument,VF.string(type),VF.string(message)));	
 	}
-	
+
 	public static RascalException illegalTypeArgument(String type){
 		return new RascalException(VF.constructor(IllegalTypeArgument,VF.string(type), VF.string("")));	
 	}
-	
+
 	public static RascalException implodeError(String msg) {
 		return new RascalException(VF.constructor(ImplodeError, VF.string(msg)));
 	}
@@ -228,39 +232,39 @@ public class RascalExceptionFactory {
 	public static RascalException invalidUseOfLocation(String msg) {
 		return new RascalException(VF.constructor(InvalidUseOfLocation, VF.string(msg)));
 	}	
-	
-	public static RascalException invalidUseOfDateException(String message) {
+
+	public static RascalException invalidUseOfDate(String message) {
 		return new RascalException(VF.constructor(InvalidUseOfDate, VF.string(message)));
 	}
-	
-	public static RascalException invalidUseOfTimeException(String message) {
+
+	public static RascalException invalidUseOfTime(String message) {
 		return new RascalException(VF.constructor(InvalidUseOfTime, VF.string(message)));
 	}
-	
-	public static RascalException invalidUseOfDateTimeException(String message) {
+
+	public static RascalException invalidUseOfDateTime(String message) {
 		return new RascalException(VF.constructor(InvalidUseOfDateTime, VF.string(message)));
 	}
-	
-//	public static RascalException locationNotFound(ISourceLocation loc) {
-//		return new RascalException(LocationNotFound.make(VF, loc));
-//	}
-	
+
+	//	public static RascalException locationNotFound(ISourceLocation loc) {
+	//		return new RascalException(LocationNotFound.make(VF, loc));
+	//	}
+
 	public static RascalException malformedURI(String uri) {
 		return new RascalException(VF.constructor(MalFormedURI, VF.string(uri)));
 	}
-	
+
 	public static RascalException MultipleKey(IValue v, IValue first, IValue second) {
 		return new RascalException(VF.constructor(MultipleKey, v, first, second));
 	}
-	
+
 	public static RascalException nameMismatch(String expected, String got) {
 		return new RascalException(VF.constructor(NameMismatch, VF.string(expected), VF.string(got)));
 	}
-	
+
 	public static RascalException noParent(ISourceLocation loc) {
 		return new RascalException(VF.constructor(NoParent, loc));
 	}
-	
+
 	public static RascalException noSuchElement(IValue v) {
 		return new RascalException(VF.constructor(NoSuchElement,v));	
 	}
@@ -268,25 +272,29 @@ public class RascalExceptionFactory {
 	public static RascalException noSuchField(String name) {
 		return new RascalException(VF.constructor(NoSuchField, VF.string(name)));
 	}
-	
+
 	public static RascalException permissionDenied() {
 		return new RascalException(VF.constructor(AnonymousPermissionDenied));
 	}
-	
+
 	public static RascalException permissionDenied(IString msg) {
 		return new RascalException(VF.constructor(PermissionDenied, msg));
 	}
 
-	public static RascalException unavailableInformation(){
-		return new RascalException(VF.constructor(UnavailableInformation));	
+	//	public static RascalException unavailableInformation(){
+	//		return new RascalException(VF.constructor(UnavailableInformation));	
+	//	}
+
+	public static RascalException unavailableInformation(String message){
+		return new RascalException(VF.constructor(UnavailableInformation, VF.string(message)));   
 	}
 
 	public static RascalException schemeNotSupported(ISourceLocation file) {
 		return new RascalException(VF.constructor(SchemeNotSupported, file));
 	}
-	
+
 	public static RascalException timeout() {
-    	return new RascalException(VF.constructor(Timeout));
-    }
-	
+		return new RascalException(VF.constructor(Timeout));
+	}
+
 }
