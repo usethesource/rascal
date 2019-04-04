7c62ebf6052e6ea0cf9f51e8a328e4e28c9a169b
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2019-04-04 10:11:28 +0200

    minor cleanup

--- src/org/rascalmpl/library/analysis/text/search/LuceneAdapter.java
+++ src/org/rascalmpl/library/analysis/text/search/LuceneAdapter.java
@@ -24,8 +24,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
