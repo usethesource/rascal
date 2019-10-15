6c33bf8a846c06bb5f2ad8feba394c43d6329cd2
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2019-10-15 09:54:05 +0200

    making commandline Rascal REPL also read META-INF/MANIFEST.MF and configure itself with it

--- src/org/rascalmpl/shell/ShellEvaluatorFactory.java
+++ src/org/rascalmpl/shell/ShellEvaluatorFactory.java
@@ -1,9 +1,9 @@
 package org.rascalmpl.shell;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URISyntaxException;
-import java.util.List;
 
 import org.rascalmpl.interpreter.ConsoleRascalMonitor;
 import org.rascalmpl.interpreter.Evaluator;
@@ -11,12 +11,15 @@ import org.rascalmpl.interpreter.env.GlobalEnvironment;
 import org.rascalmpl.interpreter.env.ModuleEnvironment;
 import org.rascalmpl.interpreter.load.StandardLibraryContributor;
 import org.rascalmpl.interpreter.utils.RascalManifest;
+import org.rascalmpl.library.util.PathConfig;
 import org.rascalmpl.uri.URIResolverRegistry;
 import org.rascalmpl.uri.URIUtil;
+import org.rascalmpl.uri.classloaders.SourceLocationClassLoader;
 import org.rascalmpl.uri.project.ProjectURIResolver;
 import org.rascalmpl.values.ValueFactoryFactory;
 
 import io.usethesource.vallang.ISourceLocation;
+import io.usethesource.vallang.IValue;
 import io.usethesource.vallang.IValueFactory;
 
 public class ShellEvaluatorFactory {
@@ -42,14 +45,22 @@ public class ShellEvaluatorFactory {
 
   private static void configureProjectEvaluator(Evaluator evaluator, ISourceLocation projectRoot) {
       URIResolverRegistry reg = URIResolverRegistry.getInstance();
+
       String projectName = new RascalManifest().getProjectName(projectRoot);
       reg.registerLogical(new ProjectURIResolver(projectRoot, projectName));
-      List<String> sourceRoots = new RascalManifest().getSourceRoots(projectRoot);
       
-      ISourceLocation root = URIUtil.correctLocation("project", projectName, "");
-      for (String src : sourceRoots) {
-          ISourceLocation path = URIUtil.getChildLocation(root, src);
-          evaluator.addRascalSearchPath(path);
+      try {
+          PathConfig pcfg = PathConfig.fromSourceProjectRascalManifest(projectRoot);
+          
+          for (IValue path : pcfg.getSrcs()) {
+              evaluator.addRascalSearchPath((ISourceLocation) path); 
+          }
+          
+          ClassLoader cl = new SourceLocationClassLoader(pcfg.getClassloaders(), ShellEvaluatorFactory.class.getClassLoader());
+          evaluator.addClassLoader(cl);
+      }
+      catch (IOException e) {
+          System.err.println(e);
       }
   }
   
