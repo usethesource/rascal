6bf8bc197d91e6990ce81b25980c9daf8fd71776
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2019-10-20 17:41:58 +0200

    fixed Rascal test setup in pom file

--- test/RunTests.java
+++ test/RunTests.java
@@ -1,9 +1,7 @@
-package examples;
 
 import org.junit.runner.RunWith;
 import org.rascalmpl.test.infrastructure.RascalJUnitTestPrefix;
 import org.rascalmpl.test.infrastructure.RascalJUnitTestRunner;
-import org.rascalmpl.test.infrastructure.RecursiveTest;
 
 @RunWith(RascalJUnitTestRunner.class)
 @RascalJUnitTestPrefix("lang::rascalcore::check::tests")
