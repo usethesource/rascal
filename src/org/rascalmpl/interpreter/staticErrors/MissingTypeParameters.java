cbe1d489d6f04ada358a0eec43f3b867f02e926a
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2019-10-21 16:09:46 +0200

    fixed header

--- src/org/rascalmpl/interpreter/staticErrors/MissingTypeParameters.java
+++ src/org/rascalmpl/interpreter/staticErrors/MissingTypeParameters.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009-2013 CWI
+ * Copyright (c) 2009-2019 NWO-I CWI
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,8 +8,6 @@
  * Contributors:
 
  *   * Jurgen J. Vinju - Jurgen.Vinju@cwi.nl - CWI
- *   * Paul Klint - Paul.Klint@cwi.nl - CWI
- *   * Arnold Lankamp - Arnold.Lankamp@cwi.nl
 *******************************************************************************/
 package org.rascalmpl.interpreter.staticErrors;
 
