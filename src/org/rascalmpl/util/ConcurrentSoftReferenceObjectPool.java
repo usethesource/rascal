c3066d104ee204340cb99d4d583d3854f6aa748d
Davy Landman <davy.landman@swat.engineering>
2019-12-04 15:04:35 +0100

    Removed copy of inlined code

--- src/org/rascalmpl/util/ConcurrentSoftReferenceObjectPool.java
+++ src/org/rascalmpl/util/ConcurrentSoftReferenceObjectPool.java
@@ -116,23 +116,6 @@ public class ConcurrentSoftReferenceObjectPool<T> {
 	    }
 	}
 	
-	/**
-	 * Creates an executor service with a fixed pool size, that will time 
-	 * out after a certain period of inactivity.
-	 * 
-	 * @param poolSize The core- and maximum pool size
-	 * @param keepAliveTime The keep alive time
-	 * @param timeUnit The time unit
-	 * @return The executor service
-	 */
-	public static ExecutorService createFixedTimeoutExecutorService(
-	    int poolSize, long keepAliveTime, TimeUnit timeUnit) {
-	    ThreadPoolExecutor e = new ThreadPoolExecutor(poolSize, poolSize,
-	            keepAliveTime, timeUnit, new LinkedBlockingQueue<Runnable>());
-	    e.allowCoreThreadTimeOut(true);
-	    return e;
-	}
-	
 	public boolean healthCheck() {
 	    for (int i = 0; i < 10; i++) {
 	        // it could be that we are just in the middle of an off by one window, so loop a few times to make sure that's not the case
