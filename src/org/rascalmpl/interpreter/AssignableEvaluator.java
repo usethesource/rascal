c9e6bf28fa80afc55e52ed3b592336d2ff2f4062
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2020-02-20 16:46:13 +0100

    fixes #1193

--- src/org/rascalmpl/interpreter/AssignableEvaluator.java
+++ src/org/rascalmpl/interpreter/AssignableEvaluator.java
@@ -28,6 +28,7 @@ import org.rascalmpl.interpreter.staticErrors.UnexpectedType;
 import org.rascalmpl.interpreter.staticErrors.UninitializedVariable;
 
 import io.usethesource.vallang.IValue;
+import io.usethesource.vallang.exceptions.UnexpectedTypeException;
 import io.usethesource.vallang.type.TypeFactory;
 
 
@@ -98,6 +99,10 @@ public class AssignableEvaluator {
 	 * Given an old result and a right-hand side Result, compute a new result.
 	 */
 	public Result<IValue> newResult(Result<IValue> oldValue, Result<IValue> rhsValue) {
+	    if (rhsValue.getType().isBottom()) {
+	        throw new UnexpectedType(oldValue.getType(), tf.voidType(), getCurrentAST());
+	    }
+	    
 		Result<IValue> newValue;
 		if(oldValue != null) {
 			switch(this.__getOperator()){
