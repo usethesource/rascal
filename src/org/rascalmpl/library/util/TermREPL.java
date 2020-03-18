f1f039813b88c5c523bcab1d8f71e60eea4784a5
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2020-03-18 11:25:55 +0100

    generalized TermREPL interface to deal with any kind of webserver response on a command, such that repls can also return visualizations

--- src/org/rascalmpl/library/util/TermREPL.java
+++ src/org/rascalmpl/library/util/TermREPL.java
@@ -1,12 +1,14 @@
 package org.rascalmpl.library.util;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.io.Writer;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -15,10 +17,15 @@ import org.rascalmpl.interpreter.Evaluator;
 import org.rascalmpl.interpreter.IEvaluatorContext;
 import org.rascalmpl.interpreter.result.ICallableValue;
 import org.rascalmpl.interpreter.utils.ReadEvalPrintDialogMessages;
+import org.rascalmpl.library.lang.json.io.JsonValueWriter;
 import org.rascalmpl.repl.BaseREPL;
 import org.rascalmpl.repl.CompletionResult;
 import org.rascalmpl.repl.ILanguageProtocol;
+import org.rascalmpl.uri.URIResolverRegistry;
 
+import com.google.gson.stream.JsonWriter;
+
+import io.usethesource.vallang.IBool;
 import io.usethesource.vallang.IConstructor;
 import io.usethesource.vallang.IInteger;
 import io.usethesource.vallang.IList;
@@ -27,6 +34,7 @@ import io.usethesource.vallang.IString;
 import io.usethesource.vallang.ITuple;
 import io.usethesource.vallang.IValue;
 import io.usethesource.vallang.IValueFactory;
+import io.usethesource.vallang.IWithKeywordParameters;
 import io.usethesource.vallang.type.Type;
 import io.usethesource.vallang.type.TypeFactory;
 import jline.TerminalFactory;
@@ -39,13 +47,12 @@ public class TermREPL {
         this.vf = vf;
     }
 
-    public void startREPL(IConstructor repl, IEvaluatorContext ctx) {
+    public void startREPL(IConstructor repl, IString title, IString welcome, IString prompt,
+        ISourceLocation history, IValue handler, IValue completor, IValue stacktrace, IEvaluatorContext ctx) {
         try {
-            lang = new TheREPL(vf, repl, ctx.getInput(), ctx.getStdErr(), ctx.getStdOut());
-            // TODO: this used to get a repl from the IEvaluatorContext but that was wrong. Need to fix later.
-            ISourceLocation history = repl.has("history") ? (ISourceLocation) repl.get("history") : null;
+            lang = new TheREPL(vf, title, welcome, prompt, history, handler, completor, stacktrace, ctx.getInput(), ctx.getStdErr(), ctx.getStdOut());
             new BaseREPL(lang, null, ctx.getInput(), System.out, true, true, history , TerminalFactory.get(), null).run();
-        } catch (IOException | URISyntaxException e) {
+        } catch (Throwable e) {
             e.printStackTrace(ctx.getStdErr());
         }
     }
@@ -59,23 +66,24 @@ public class TermREPL {
         private final ICallableValue handler;
         private final ICallableValue completor;
         private final IValueFactory vf;
+        private final ICallableValue stacktrace;
         
-        public TheREPL(IValueFactory vf, IConstructor repl, InputStream input, Writer stderr, Writer stdout) throws IOException, URISyntaxException {
+        public TheREPL(IValueFactory vf, IString title, IString welcome, IString prompt, ISourceLocation history,
+            IValue handler, IValue completor, IValue stacktrace, InputStream input, PrintWriter stderr, PrintWriter stdout) {
             this.vf = vf;
             this.input = input;
             this.stderr = new PrintWriter(stderr);
             this.stdout = new PrintWriter(stdout);
-            this.handler = (ICallableValue)repl.get("handler");
-            this.completor = (ICallableValue)repl.get("completor");
-            
-            if (repl.has("prompt")) {
-                this.currentPrompt = ((IString)repl.get("prompt")).getValue();
-            }
+            this.handler = (ICallableValue) handler;
+            this.completor = (ICallableValue) completor;
+            this.stacktrace = (ICallableValue) stacktrace;
+            this.currentPrompt = prompt.getValue();
         }
-        
+
         @Override
         public void cancelRunningCommandRequested() {
             handler.getEval().interrupt();
+            handler.getEval().__setInterrupt(false);
         }
         
         @Override
@@ -85,14 +93,13 @@ public class TermREPL {
         
         @Override
         public void stop() {
-            cancelRunningCommandRequested();
+            handler.getEval().interrupt();
         }
         
         @Override
         public void stackTraceRequested() {
-            // TODO: add this to the REPL callbacks. It should be a DSL stacktrace. 
+            stacktrace.call(new Type[0], new IValue[0], null);
         }
-
 
         @Override
         public void initialize(InputStream input, Writer stdout, Writer stderr) {
@@ -117,12 +124,70 @@ public class TermREPL {
                 return;
             } 
             else {
-                IConstructor result = (IConstructor)call(handler, new Type[] { tf.stringType() }, new IValue[] { vf.string(line) });
-                if (result.has("result") && result.get("result") != null) {
-                    String str = ((IString)result.get("result")).getValue() + "\n";
-                    output.put("text/plain", new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8)));
+                IConstructor response = (IConstructor)call(handler, new Type[] { tf.stringType() }, new IValue[] { vf.string(line) });
+
+                try {
+                    switch (response.getName()) {
+                        case "response":
+                            handlePlainTextResponse(output, response);
+                            break;
+                        case "fileResponse":
+                            handleFileResponse(output, response);
+                            break;
+                        case "jsonResponse":
+                            handleJSONResponse(output, response);
+                    }
+                }
+                catch (IOException e) {
+                    output.put("text/plain", new ByteArrayInputStream(e.getMessage().getBytes()));
                 }
             }
+        }
+
+        private void handleJSONResponse(Map<String, InputStream> output, IConstructor response) throws IOException {
+            IValue data = response.get("val");
+            IWithKeywordParameters<? extends IConstructor> kws = response.asWithKeywordParameters();
+            
+            IValue dtf = kws.getParameter("dateTimeFormat");
+            IValue ics = kws.getParameter("implicitConstructors");
+            IValue ipn = kws.getParameter("implicitNodes");
+            IValue dai = kws.getParameter("dateTimeAsInt");
+            
+            JsonValueWriter writer = new JsonValueWriter()
+                .setCalendarFormat(dtf != null ? ((IString) dtf).getValue() : "yyyy-MM-dd\'T\'HH:mm:ss\'Z\'")
+                .setConstructorsAsObjects(ics != null ? ((IBool) ics).getValue() : true)
+                .setNodesAsObjects(ipn != null ? ((IBool) ipn).getValue() : true)
+                .setDatesAsInt(dai != null ? ((IBool) dai).getValue() : true);
+
+              final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+              
+              JsonWriter out = new JsonWriter(new OutputStreamWriter(baos, Charset.forName("UTF8")));
+              
+              writer.write(out, data);
+              out.flush();
+              out.close();
+              
+              output.put("application/json",  new ByteArrayInputStream(baos.toByteArray()));
+        }
+
+        private void handleFileResponse(Map<String, InputStream> output, IConstructor response)
+            throws UnsupportedEncodingException {
+            IString fileMimetype = (IString) response.get("mimeType");
+            ISourceLocation file = (ISourceLocation) response.get("file");
+            try {
+                output.put(fileMimetype.getValue(), URIResolverRegistry.getInstance().getInputStream(file));
+            }
+            catch (IOException e) {
+                output.put("text/plain", new ByteArrayInputStream(e.getMessage().getBytes("UTF8")));
+            }
+        }
+
+        private void handlePlainTextResponse(Map<String, InputStream> output, IConstructor response)
+            throws UnsupportedEncodingException {
+            IString content = (IString) response.get("content");
+            IString contentMimetype = (IString) response.get("mimeType");
+                  
+            output.put(contentMimetype.getValue(), new ByteArrayInputStream(content.getValue().getBytes("UTF8")));
         }
 
         @Override
@@ -174,13 +239,11 @@ public class TermREPL {
         
         @Override
         public void handleReset(Map<String, InputStream> output, Map<String, String> metadata) throws InterruptedException {
-            // TODO: add a rascal callback for this?
             handleInput("", output, metadata);
         }
 
         @Override
         public boolean isStatementComplete(String command) {
-            // TODO Auto-generated method stub
             return true;
         }
     }
