e6b1922c51eed6865acfaa6a9a4e0d568a093afc
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2020-07-29 11:56:25 +0200

    added test modules

--- test/org/rascalmpl/test/parallel/AllSuiteParallel.java
+++ test/org/rascalmpl/test/parallel/AllSuiteParallel.java
@@ -6,8 +6,10 @@ import org.rascalmpl.test.infrastructure.RecursiveRascalParallelTest;
 
 @RunWith(RascalJUnitParallelRecursiveTestRunner.class)
 @RecursiveRascalParallelTest({
-    "lang::rascal::tests::basic", 
-    "lang::rascal::tests::extends", 
+    "lang::rascal::tests::basic",
+    "lang::rascal::tests::concrete",
+    "lang::rascal::tests::extends",
+    "lang::rascal::tests::demo",
     "lang::rascal::tests::functionality", 
     "lang::rascal::tests::imports",
     "lang::rascal::tests::library"
