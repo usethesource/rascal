be8148d0f91fc7da8231e36ec37548df349cffb7
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2020-07-30 10:29:54 +0200

    removed IEvaluatorContext from Monitor library

--- src/org/rascalmpl/library/util/Monitor.java
+++ src/org/rascalmpl/library/util/Monitor.java
@@ -11,6 +11,7 @@
 *******************************************************************************/
 package org.rascalmpl.library.util;
 
+import org.rascalmpl.debug.IRascalMonitor;
 import org.rascalmpl.interpreter.IEvaluatorContext;
 import io.usethesource.vallang.IBool;
 import io.usethesource.vallang.IInteger;
@@ -20,42 +21,44 @@ import io.usethesource.vallang.IValueFactory;
 
 public class Monitor {
 	private final IValueFactory vf;
+    private final IRascalMonitor monitor;
 
-	public Monitor(IValueFactory vf) {
+	public Monitor(IValueFactory vf, IRascalMonitor monitor) {
 		this.vf = vf;
+		this.monitor = monitor;
 	}
 
-	public void startJob(IString name, IEvaluatorContext ctx) {
-		ctx.getEvaluator().startJob(name.getValue());
+	public void startJob(IString name) {
+		monitor.startJob(name.getValue());
 	}
 
-	public void startJob(IString name, IInteger totalWork, IEvaluatorContext ctx) {
-		ctx.getEvaluator().startJob(name.getValue(), totalWork.intValue());
+	public void startJob(IString name, IInteger totalWork) {
+		monitor.startJob(name.getValue(), totalWork.intValue());
 	}
 
 	public void startJob(IString name, IInteger workShare, IInteger totalWork,
 			IEvaluatorContext ctx) {
-		ctx.getEvaluator().startJob(name.getValue(), workShare.intValue(),
+		monitor.startJob(name.getValue(), workShare.intValue(),
 				totalWork.intValue());
 	}
 
-	public void event(IString name, IEvaluatorContext ctx) {
-		ctx.getEvaluator().event(name.getValue());
+	public void event(IString name) {
+		monitor.event(name.getValue());
 	}
 
-	public void event(IString name, IInteger inc, IEvaluatorContext ctx) {
-		ctx.getEvaluator().event(name.getValue(), inc.intValue());
+	public void event(IString name, IInteger inc) {
+		monitor.event(name.getValue(), inc.intValue());
 	}
 
-	public void event(IInteger inc, IEvaluatorContext ctx) {
-		ctx.getEvaluator().event(inc.intValue());
+	public void event(IInteger inc) {
+		monitor.event(inc.intValue());
 	}
 
-	public IValue endJob(IBool succeeded, IEvaluatorContext ctx) {
-		return vf.integer(ctx.getEvaluator().endJob(succeeded.getValue()));
+	public IValue endJob(IBool succeeded) {
+		return vf.integer(monitor.endJob(succeeded.getValue()));
 	}
 
-	public void todo(IInteger work, IEvaluatorContext ctx) {
-		ctx.getEvaluator().todo(work.intValue());
+	public void todo(IInteger work) {
+		monitor.todo(work.intValue());
 	}
 }
