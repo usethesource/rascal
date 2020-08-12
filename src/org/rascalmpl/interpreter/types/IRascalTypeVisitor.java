3b4b2a44f6555091f75eb03e51c3e45a10f88b4a
paulklint <Paul.Klint@cwi.nl>
2020-08-12 22:24:41 +0200

    Removed last traces of OverloadedFunctionType

--- src/org/rascalmpl/interpreter/types/IRascalTypeVisitor.java
+++ src/org/rascalmpl/interpreter/types/IRascalTypeVisitor.java
@@ -4,8 +4,6 @@ import io.usethesource.vallang.type.ITypeVisitor;
 
 public interface IRascalTypeVisitor<T,E extends Throwable> extends ITypeVisitor<T, E> {
   T visitFunction(RascalType type) throws E;
-  T visitOverloadedFunction(RascalType type) throws E;
   T visitReified(RascalType type) throws E;
   T visitNonTerminal(RascalType type) throws E;
-//  T visitFailureType(RascalType type) throws E;
 }
