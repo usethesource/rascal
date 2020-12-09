7dded5659f9fe654955b345f716e8556aabe65bf
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2020-12-09 17:58:53 +0100

    exporting more of the Shell API to help vscode extension

--- src/org/rascalmpl/shell/REPLRunner.java
+++ src/org/rascalmpl/shell/REPLRunner.java
@@ -16,10 +16,7 @@ import org.rascalmpl.uri.URIUtil;
 
 import jline.Terminal;
 
-public class REPLRunner extends BaseREPL  implements ShellRunner {
-    private static final boolean useSystemBrowser = "true".equals(System.getProperty("rascal.useSystemBrowser"))
-                                                    && Desktop.isDesktopSupported();
-
+public class REPLRunner extends BaseREPL implements ShellRunner {
     private static File getHistoryFile() throws IOException {
         File home = new File(System.getProperty("user.home"));
         File rascal = new File(home, ".rascal");
@@ -33,47 +30,54 @@ public class REPLRunner extends BaseREPL  implements ShellRunner {
         return historyFile;
     }
 
-  public REPLRunner(InputStream stdin, OutputStream stderr, OutputStream stdout, Terminal term)  throws IOException, URISyntaxException{
-    super(makeInterpreter(stdin, stderr, stdout, true, term.isAnsiSupported(), false, getHistoryFile(), term), null, stdin, stderr, stdout, true, term.isAnsiSupported(), getHistoryFile(), term, null);
-  }
-  
-  public REPLRunner(ILanguageProtocol language)  throws IOException, URISyntaxException{
-      super(language, null, null, null, null, true, true, new File(""), null, null);
-  }
+    public REPLRunner(InputStream stdin, OutputStream stderr, OutputStream stdout, Terminal term)
+        throws IOException, URISyntaxException {
+        super(makeInterpreter(stdin, stderr, stdout, true, term.isAnsiSupported(), false, getHistoryFile(), term), null,
+            stdin, stderr, stdout, true, term.isAnsiSupported(), getHistoryFile(), term, null);
+    }
 
-  private static ILanguageProtocol makeInterpreter(InputStream stdin, OutputStream stderr, OutputStream stdout, boolean prettyPrompt, boolean allowColors, boolean htmlOutput, File persistentHistory, Terminal terminal) throws IOException, URISyntaxException {
-    RascalInterpreterREPL repl = new RascalInterpreterREPL(prettyPrompt, allowColors, htmlOutput, getHistoryFile()) {
-        @Override
-        protected Evaluator constructEvaluator(InputStream input, OutputStream stdout, OutputStream stderr) {
-            return ShellEvaluatorFactory.getDefaultEvaluator(input, stdout, stderr);
-        }
+    public REPLRunner(ILanguageProtocol language) throws IOException, URISyntaxException {
+        super(language, null, null, null, null, true, true, new File(""), null, null);
+    }
 
-        @Override
-        public void handleInput(String line, Map<String, InputStream> output, Map<String, String> metadata)
-            throws InterruptedException {
-            super.handleInput(line, output, metadata);
-            
-            try {
-                // Note that Desktop.isDesktopSupported can not be factored into a class constant because
-                // it may throw exceptions on headless machines which are ignored below.
-                if ("true".equals(System.getProperty("rascal.useSystemBrowser")) && Desktop.isDesktopSupported()) {
-                    for (String mimetype : output.keySet()) {
-                        if (!mimetype.contains("html") && !mimetype.startsWith("image/")) {
-                            continue;
-                        }
+    private static ILanguageProtocol makeInterpreter(InputStream stdin, OutputStream stderr, OutputStream stdout,
+        boolean prettyPrompt, boolean allowColors, boolean htmlOutput, File persistentHistory, Terminal terminal)
+        throws IOException, URISyntaxException {
+        RascalInterpreterREPL repl =
+            new RascalInterpreterREPL(prettyPrompt, allowColors, htmlOutput, getHistoryFile()) {
+                @Override
+                protected Evaluator constructEvaluator(InputStream input, OutputStream stdout, OutputStream stderr) {
+                    return ShellEvaluatorFactory.getDefaultEvaluator(input, stdout, stderr);
+                }
 
-                        try {
-                            Desktop.getDesktop().browse(URIUtil.assumeCorrect(metadata.get("url")));
-                        }
-                        catch (IOException e) {
-                            getErrorWriter().println("failed to display content: " + e.getMessage());
+                @Override
+                public void handleInput(String line, Map<String, InputStream> output, Map<String, String> metadata)
+                    throws InterruptedException {
+                    super.handleInput(line, output, metadata);
+
+                    try {
+                        // Note that Desktop.isDesktopSupported can not be factored into a class constant because
+                        // it may throw exceptions on headless machines which are ignored below.
+                        if ("true".equals(System.getProperty("rascal.useSystemBrowser"))
+                            && Desktop.isDesktopSupported()) {
+                            for (String mimetype : output.keySet()) {
+                                if (!mimetype.contains("html") && !mimetype.startsWith("image/")) {
+                                    continue;
+                                }
+
+                                try {
+                                    Desktop.getDesktop().browse(URIUtil.assumeCorrect(metadata.get("url")));
+                                }
+                                catch (IOException e) {
+                                    getErrorWriter().println("failed to display content: " + e.getMessage());
+                                }
+                            }
                         }
                     }
+                    catch (Throwable e) {
+                        // we fail silently in order to support headless machines
+                    }
                 }
-            } catch(Throwable e) {
-                // we fail silently in order to support headless machines
-            }
-        }
     };
     
     repl.setMeasureCommandTime(false);
