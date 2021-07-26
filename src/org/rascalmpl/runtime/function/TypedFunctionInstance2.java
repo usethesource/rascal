50e446b0d5ec49adc1a32689d5a5fcb3d94adeaa
paulklint <Paul.Klint@cwi.nl>
2021-07-27 00:12:29 +0200

    Added type check to function instances

--- src/org/rascalmpl/runtime/function/TypedFunctionInstance2.java
+++ src/org/rascalmpl/runtime/function/TypedFunctionInstance2.java
@@ -2,20 +2,36 @@ package org.rascalmpl.core.library.lang.rascalcore.compile.runtime.function;
 
 import java.util.Map;
 
+import org.rascalmpl.exceptions.RuntimeExceptionFactory;
+
 import io.usethesource.vallang.IValue;
+import io.usethesource.vallang.impl.persistent.ValueFactory;
 import io.usethesource.vallang.type.Type;
+import io.usethesource.vallang.type.TypeFactory;
 
 public class TypedFunctionInstance2<R extends IValue,A,B> extends TypedFunctionInstance {
 	
 	private final TypedFunction2<R,A,B> function;
+	private final Type type_arg_0;
+	private final Type type_arg_1;
 
 	public TypedFunctionInstance2(TypedFunction2<R,A,B> function, Type ftype){
 		super(ftype);
 		this.function = function;
+		if(ftype.isFunction()) {
+			type_arg_0 = type.getFieldType(0);
+			type_arg_1 = type.getFieldType(1);
+		} else {
+			type_arg_0 = type_arg_1 = TypeFactory.getInstance().valueType();
+		}
 	}
 	
 	public R typedCall(A a, B b) {
-		return function.typedCall(a, b);
+		if(((IValue) a).getType().comparable(type_arg_0) &&
+		   ((IValue) b).getType().comparable(type_arg_1)){
+			return function.typedCall(a, b);
+		}
+		throw RuntimeExceptionFactory.callFailed(ValueFactory.getInstance().list((IValue)a, (IValue)b));
 	}
 	
 	@SuppressWarnings("unchecked")
