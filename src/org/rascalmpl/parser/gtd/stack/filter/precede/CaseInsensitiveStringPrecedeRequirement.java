bc8639d10d5223b0df9d3e0428cc646e9a243a4f
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2021-09-06 13:48:58 +0200

    generalizes the fixes of #968 #1526 and fixes #609 of commit a2190e4f89b69e96cf97b8765ffe6d4c3560e60c by also adding support for >> 'if', !>> 'if', 'if' << and 'if' !<<, so the case-insensitive versions of precede and follow requirements and restrictions, including basic tests

--- src/org/rascalmpl/parser/gtd/stack/filter/precede/CaseInsensitiveStringPrecedeRequirement.java
+++ src/org/rascalmpl/parser/gtd/stack/filter/precede/CaseInsensitiveStringPrecedeRequirement.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009-2021 CWI
+ * Copyright (c) 2009-2021 NWO-I CWI
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,51 +10,50 @@
  *   * Arnold Lankamp - Arnold.Lankamp@cwi.nl
  *   * Jurgen Vinju - Jurgen.Vinju@cwi.nl
 *******************************************************************************/
-package org.rascalmpl.parser.gtd.stack.filter.follow;
+package org.rascalmpl.parser.gtd.stack.filter.precede;
 
 import java.util.Arrays;
 
 import org.rascalmpl.parser.gtd.location.PositionStore;
-import org.rascalmpl.parser.gtd.stack.filter.ICompletionFilter;
+import org.rascalmpl.parser.gtd.stack.filter.IEnterFilter;
 
 /**
- * A filter that restricts the indicated substring from being followed by the
- * string associated with this filter. the match is case-insensitive
+ * A filter that requires the indicated substring to be preceded by the string
+ * associated with this filter, with a case-insensitive match
  */
-public class CaseInsensitiveStringFollowRestriction implements ICompletionFilter{
+public class CaseInsensitiveStringPrecedeRequirement implements IEnterFilter{
 	private final int[][] string;
 	
-	public CaseInsensitiveStringFollowRestriction(int[][] string) {
+	public CaseInsensitiveStringPrecedeRequirement(int[][] string){
 		super();
 		
 		this.string = string;
 		assert Arrays.stream(string).noneMatch(letter -> letter.length != 2);
 	}
 	
-	public boolean isFiltered(int[] input, int start, int end, PositionStore positionStore) {
-		if ((end + string.length - 1) >= input.length) {
-			return false;
+	public boolean isFiltered(int[] input, int start, PositionStore positionStore){
+		int startLocation = start - string.length;
+		if (startLocation < 0) {
+			return true;
 		}
-			
+		
 		for (int i = string.length - 1; i >= 0; --i){
-			if (input[end + i] != string[i][0]
-			  && input[end + i] != string[i][1]) {
-				return false;
+			if (input[startLocation + i] != string[i][0] && input[startLocation + i] != string[i][1]) {
+				return true;
 			}
 		}
 		
-		return true;
+		return false;
 	}
 	
-	public boolean isEqual(ICompletionFilter otherCompletionFilter) {
-		if (!(otherCompletionFilter instanceof CaseInsensitiveStringFollowRestriction)) {
+	public boolean isEqual(IEnterFilter otherEnterFilter){
+		if (!(otherEnterFilter instanceof CaseInsensitiveStringPrecedeRequirement)) {
 			return false;
 		}
 		
-		CaseInsensitiveStringFollowRestriction otherStringFollowFilter = (CaseInsensitiveStringFollowRestriction) otherCompletionFilter;
+		CaseInsensitiveStringPrecedeRequirement otherStringPrecedeFilter = (CaseInsensitiveStringPrecedeRequirement) otherEnterFilter;
 		
-		int[][] otherString = otherStringFollowFilter.string;
-		
+		int[][] otherString = otherStringPrecedeFilter.string;
 		if (string.length != otherString.length) {
 			return false;
 		}
