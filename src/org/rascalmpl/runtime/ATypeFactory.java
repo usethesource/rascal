f1ed4e4967540c2382d75df56a43a5d69c888d97
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2021-09-16 09:56:38 +0200

    removed unused imports

--- src/org/rascalmpl/runtime/ATypeFactory.java
+++ src/org/rascalmpl/runtime/ATypeFactory.java
@@ -8,10 +8,7 @@ import org.rascalmpl.values.ValueFactoryFactory;
 
 import io.usethesource.vallang.IBool;
 import io.usethesource.vallang.IConstructor;
-import io.usethesource.vallang.IInteger;
 import io.usethesource.vallang.IList;
-import io.usethesource.vallang.ISet;
-import io.usethesource.vallang.ISourceLocation;
 import io.usethesource.vallang.IString;
 import io.usethesource.vallang.IValue;
 import io.usethesource.vallang.IValueFactory;
