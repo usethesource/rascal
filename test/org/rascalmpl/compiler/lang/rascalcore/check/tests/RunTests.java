8a12115566746bf601372027f1685b45a9451c69
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2022-04-11 15:06:48 +0200

    moved RunTests to test folder

--- test/org/rascalmpl/compiler/lang/rascalcore/check/tests/RunTests.java
+++ test/org/rascalmpl/compiler/lang/rascalcore/check/tests/RunTests.java
@@ -3,7 +3,7 @@ package org.rascalmpl.core.library.lang.rascalcore.check.tests;
 import org.junit.runner.RunWith;
 import org.rascalmpl.test.infrastructure.RascalJUnitTestPrefix;
 import org.rascalmpl.test.infrastructure.RascalJUnitTestRunner;
-
+ 
 @RunWith(RascalJUnitTestRunner.class)
 @RascalJUnitTestPrefix("lang::rascalcore::check::tests")
 public class RunTests {
