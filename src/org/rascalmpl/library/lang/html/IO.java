6fd753a6ff9063dd8d0a5e883f4b310ab0eb172f
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2022-10-18 17:28:24 +0200

    added yielding of HTMLElement as XHTML output, both string and file

--- src/org/rascalmpl/library/lang/html/IO.java
+++ src/org/rascalmpl/library/lang/html/IO.java
@@ -12,24 +12,44 @@
 package org.rascalmpl.library.lang.html;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.Reader;
 import java.io.StringReader;
+import java.io.StringWriter;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Stack;
+import java.util.Map.Entry;
 
 import javax.swing.text.MutableAttributeSet;
 import javax.swing.text.html.HTML.Tag;
 import javax.swing.text.html.HTMLEditorKit.ParserCallback;
 import javax.swing.text.html.parser.ParserDelegator;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.rascalmpl.exceptions.RuntimeExceptionFactory;
 import org.rascalmpl.uri.URIResolverRegistry;
+import org.w3c.dom.Document;
+import org.w3c.dom.DocumentFragment;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.html.HTMLDOMImplementation;
+import org.w3c.dom.html.HTMLDocument;
 
 import io.usethesource.vallang.IConstructor;
+import io.usethesource.vallang.IList;
 import io.usethesource.vallang.ISourceLocation;
 import io.usethesource.vallang.IString;
 import io.usethesource.vallang.IValue;
@@ -73,7 +93,97 @@ public class IO {
             throw RuntimeExceptionFactory.io(factory.string(e.getMessage()));
         }
     }
+
+    /**
+     * Produces a HTML string output from an HTMLElement AST.
+     * 
+     * Why go through all the trouble of building a DOM? The only reason is compliance.
+     * Escapes, encodings, etc. all are maintained by these classes from org.w3c.dom.
+     */
+    public IString writeHTMLString(IConstructor cons) {
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.newDocument();
+
+            Element elem = doc.createElement("html");
+            createDocument(doc, cons, elem);
+
+            TransformerFactory tFactory = TransformerFactory.newInstance();
+            Transformer transformer = tFactory.newTransformer();
+            DOMSource source = new DOMSource(elem);
+            StringWriter writer = new StringWriter();
+            StreamResult result = new StreamResult(writer);
+            transformer.transform(source, result);
+
+            return factory.string(writer.toString());
+        }
+        catch (ParserConfigurationException | TransformerException e) {
+            throw RuntimeExceptionFactory.io(e.getMessage());
+        }
+    }
+
+     /**
+     * Produces a HTML string output from an HTMLElement AST.
+     * 
+     * Why go through all the trouble of building a DOM? The only reason is compliance.
+     * Escapes, encodings, etc. all are maintained by these classes from org.w3c.dom.
+     */
+    public void writeHTMLFile(ISourceLocation file, IConstructor cons) {
+        try (OutputStream out = URIResolverRegistry.getInstance().getOutputStream(file, false)) {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.newDocument();
+            
+            Element elem = doc.createElement("html");
+            createDocument(doc, cons, elem);
+
+            TransformerFactory tFactory = TransformerFactory.newInstance();
+            Transformer transformer = tFactory.newTransformer();
+            DOMSource source = new DOMSource(elem);
+            StreamResult result = new StreamResult(out);
+            transformer.transform(source, result);
+
+            return;
+        }
+        catch (ParserConfigurationException | TransformerException e) {
+            throw RuntimeExceptionFactory.io(e.getMessage());
+        }
+        catch (IOException e) {
+            throw RuntimeExceptionFactory.io(e.getMessage());
+        }
+    }
     
+    private void createDocument(Document doc, IConstructor cons, Node elem) {
+        if (cons.getConstructorType().getArity() == 0) {
+            // nullary node does not require recursion
+            elem.appendChild(createElement(doc, cons));
+        }
+        else if (cons.getName().equals("text")) {
+            // text nodes are flattened into the element, no recursion required
+            // TODO: what if there are multiple text contents?
+            elem.setTextContent(((IString) cons.get(0)).getValue());
+        }
+        else {
+            // normal elements require recursion
+            Element e = createElement(doc, cons);
+            elem.appendChild(e);
+
+            for (IValue child : (IList) cons.get(0)) {
+                createDocument(doc, (IConstructor) child, e);
+            }
+        }
+    }
+
+    private Element createElement(Document doc, IConstructor cons) {
+        Element elem = doc.createElement(cons.getName());
+        Map<String, IValue> params = cons.asWithKeywordParameters().getParameters();
+        for (Entry<String,IValue> entry : params.entrySet()) {
+            elem.setAttribute(entry.getKey(), ((IString) entry.getValue()).getValue());
+        }
+        return elem;
+    }
+
     private class Constructor extends ParserCallback {
         private Stack<java.util.List<IConstructor>> stack = new Stack<>();
         private Stack<java.util.Map<java.lang.String,IValue>> attributes = new Stack<>();
