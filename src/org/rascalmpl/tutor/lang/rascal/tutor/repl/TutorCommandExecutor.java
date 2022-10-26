1f9f3e00c854165765fa999407e820b0aaa3bf14
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2022-10-26 14:44:27 +0200

    organized imports

--- src/org/rascalmpl/tutor/lang/rascal/tutor/repl/TutorCommandExecutor.java
+++ src/org/rascalmpl/tutor/lang/rascal/tutor/repl/TutorCommandExecutor.java
@@ -18,8 +18,6 @@ import java.util.Map;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
@@ -29,7 +27,6 @@ import org.rascalmpl.library.Prelude;
 import org.rascalmpl.library.util.PathConfig;
 import org.rascalmpl.repl.RascalInterpreterREPL;
 import org.rascalmpl.shell.ShellEvaluatorFactory;
-import org.rascalmpl.uri.classloaders.SourceLocationClassLoader;
 
 import io.usethesource.vallang.IList;
 import io.usethesource.vallang.ISourceLocation;
@@ -124,7 +121,7 @@ public class TutorCommandExecutor {
                 try {
                     browser.get(metadata.get("url"));
                     browser.manage().window().maximize();
-                    
+
                     String screenshot = browser.findElement(By.tagName("body"))
                         .getScreenshotAs(OutputType.BASE64);
 
