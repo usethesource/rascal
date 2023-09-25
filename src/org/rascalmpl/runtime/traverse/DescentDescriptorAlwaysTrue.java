d9e2197bf78f3d13af33300a830e4a58e5e9a47c
paulklint <Paul.Klint@cwi.nl>
2023-09-25 14:11:07 +0200

    Differentiate DescendantDescriptor
    - Factored out IDescendantDescriptor
    - Added DescendantDescriptorAlwaysTrue for the degenerated case
      that every node has to be visited
    - Simplified DescendantDescriptor

---
+++ src/org/rascalmpl/runtime/traverse/DescentDescriptorAlwaysTrue.java
@@ -0,0 +1,34 @@
+package org.rascalmpl.core.library.lang.rascalcore.compile.runtime.traverse;
+
+import org.rascalmpl.values.parsetrees.ITree;
+
+import io.usethesource.vallang.IBool;
+import io.usethesource.vallang.IValue;
+
+public class DescentDescriptorAlwaysTrue implements IDescendantDescriptor {
+	private final boolean concreteMatch;
+	
+	DescentDescriptorAlwaysTrue(IBool concreteMatch){
+		this.concreteMatch = concreteMatch.getValue();
+	}
+	@Override
+	public boolean isConcreteMatch() {
+		return concreteMatch;
+	}
+
+	@Override
+	public boolean isAllwaysTrue() {
+		return true;
+	}
+
+	@Override
+	public IBool shouldDescentInAbstractValue(IValue subject) {
+		return TRUE;
+	}
+
+	@Override
+	public IBool shouldDescentInConcreteValue(ITree subject) {
+		return TRUE;
+	}
+
+}
