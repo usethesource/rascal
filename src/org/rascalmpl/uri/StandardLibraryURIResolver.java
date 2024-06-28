39671d9a5e19141aeda8e2c53e8c133f9b084653
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2024-06-28 13:27:44 +0200

    added more comments

--- src/org/rascalmpl/uri/StandardLibraryURIResolver.java
+++ src/org/rascalmpl/uri/StandardLibraryURIResolver.java
@@ -11,9 +11,17 @@ import io.usethesource.vallang.ISourceLocation;
  * that should be on the source path of the interpreter, which is contained in the same
  * jar as the current interpreter is from.
  * 
- * The std:/// scheme is mainly used by the interpreter, but it is also the location
+ * The std:/// scheme is mainly used by the interpreter, to load library modules; but it is also the location
  * of distributed sources of the standard library for use in the debugger. The references
  * that the type-checker produces for UI feature in the IDE also depend on this scheme.
+ * 
+ * This is accomplished by:
+ * 1. rewriting all the references to the source locations of the library in 
+ *   project://rascal/src/org/rascalmpl/library/...` to `std:///...`. This is done by the "packager"
+ * 2. copying all the .rsc files of the library to the jar in the right location (mvn resources plugin)
+ * 3. resolving std:/// to the same location inside of the jar
+ * 4. **not** having more than one standard library in the classpath of the JVM, or more than one
+ *    standard library in the libs path of a PathConfig. 
  */
 public class StandardLibraryURIResolver implements ILogicalSourceLocationResolver {
     private static final ISourceLocation currentRascalJar = 
