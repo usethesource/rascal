4290a52945cde35a5b26f3e03b79188aacb482f8
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2024-07-16 19:13:33 +0200

    removed unused code

--- src/org/rascalmpl/tutor/lang/rascal/tutor/repl/TutorCommandExecutor.java
+++ src/org/rascalmpl/tutor/lang/rascal/tutor/repl/TutorCommandExecutor.java
@@ -9,25 +9,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
-import java.time.Duration;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriverLogLevel;
-import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.rascalmpl.ideservices.IDEServices;
 import org.rascalmpl.interpreter.Evaluator;
 import org.rascalmpl.interpreter.env.GlobalEnvironment;
@@ -51,14 +42,11 @@ import io.usethesource.vallang.IValue;
 import io.usethesource.vallang.IValueFactory;
 
 public class TutorCommandExecutor {
-    private static final String BROWSER_BINARY = System.getProperty("webdriver.chrome.browser");
-    private static final String DRIVER_BINARY = System.getProperty("webdriver.chrome.driver");
+    private static final String SCREENSHOTTER_CONFIG = "org/rascalmpl/tutor/screenshotter.config";
     private final RascalInterpreterREPL repl;
     private final ByteArrayOutputStream shellStandardOutput;
     private final ByteArrayOutputStream shellErrorOutput;
-    private final ChromeDriverService service;
-    private final RemoteWebDriver driver; 
-
+    private final ITutorScreenshotFeature screenshot;
 
     public TutorCommandExecutor(PathConfig pcfg) throws IOException, URISyntaxException{
         shellStandardOutput = new ByteArrayOutputStream();
@@ -102,67 +90,23 @@ public class TutorCommandExecutor {
         repl.initialize(shellInputNotUsed, shellStandardOutput, shellErrorOutput, services);
         repl.setMeasureCommandTime(false); 
  
-        String driver = DRIVER_BINARY;
-        String browser = BROWSER_BINARY;
-
-        if (driver == null) {
-            driver = inferChromeDriverBinaryLocation();
-        }
-
-        if (browser == null) {
-            browser = inferChromeBrowserBinaryLocation();
-        }
-
-        if (driver != null && browser != null) {
-            this.service = new ChromeDriverService.Builder()         
-                .usingDriverExecutable(new File(driver))         
-                .usingAnyFreePort()         
-                .build();    
-
-            this.service.start();
-            this.driver = getBrowser(service, browser);
-        }
-        else {
-            this.service = null;
-            this.driver = null;
-            printInfoMessage();
-        }
+        this.screenshot = loadScreenShotter();
     }
 
-    private void printInfoMessage() {
-        System.err.println("INFO: tutor screenshot feature is currently disabled. To enable:");
-        System.err.println("\t* add the folder holding `chromedriver` to your PATH;");
-        System.err.println("\t* add the foldering holding `chrome` or `Google Chrome for Testing` to your PATH;");
-        System.err.println("\t* or use: `-Dwebdriver.chrome.browser=/path/to/chrome -Dwebdriver.chrome.driver/path/to/chromedriver`");
-        System.err.println("INFO: chrome and the chromedriver need to be aligned exactly. See https://googlechromelabs.github.io/chrome-for-testing/");
-    }
-
-    private String inferChromeBrowserBinaryLocation() throws IOException {
-        ISourceLocation pathBrowser = URIUtil.correctLocation("PATH", "", "Google Chrome for Testing");
-
-        if (URIResolverRegistry.getInstance().exists(pathBrowser)) {
-            System.err.println("driver exists: " + pathBrowser);
-            return URIResolverRegistry.getInstance().logicalToPhysical(pathBrowser).getPath();
-        }
-
-        pathBrowser = URIUtil.correctLocation("PATH", "", "chrome");
-        if (URIResolverRegistry.getInstance().exists(pathBrowser)) {
-            System.err.println("driver exists: " + pathBrowser);
-            return URIResolverRegistry.getInstance().logicalToPhysical(pathBrowser).getPath();
-        }
-
-        return null;
-    }
-
-    private String inferChromeDriverBinaryLocation() throws IOException {
-        ISourceLocation pathDriver = URIUtil.correctLocation("PATH", "", "chromedriver");
-
-        if (URIResolverRegistry.getInstance().exists(pathDriver)) {
-            System.err.println("driver exists: " + pathDriver);
-            return URIResolverRegistry.getInstance().logicalToPhysical(pathDriver).getPath();
-        }
-
-        return null;
+    private ITutorScreenshotFeature loadScreenShotter() {
+        try {
+            Enumeration<URL> resources = getClass().getClassLoader().getResources(SCREENSHOTTER_CONFIG);
+			
+            if (!resources.hasMoreElements()) {
+                return null;
+            }
+        
+            var content = new String(Prelude.consumeInputStream(resources.nextElement().openStream()), "UTF8");
+            return (ITutorScreenshotFeature) getClass().getClassLoader().loadClass(content.trim()).getDeclaredConstructor().newInstance();
+		}
+		catch (IOException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException | ClassNotFoundException e) {
+			throw new Error("WARNING: Could not load screenshot feature from " + SCREENSHOTTER_CONFIG, e);
+		}
     }
 
     private static ISourceLocation inferProjectRoot(ISourceLocation member) {
@@ -182,16 +126,6 @@ public class TutorCommandExecutor {
         }
 
         return current;
-    }
-    
-    @Override
-    protected void finalize() throws Throwable {
-        if (driver != null) {
-            driver.quit();
-        }
-        if (service != null) {
-            service.stop();
-        }
     }
 
     private String javaCompilerPathAsString(IList javaCompilerPath) {
@@ -251,34 +185,15 @@ public class TutorCommandExecutor {
                 result.put(mimeType, uuencode(content));
             }
             
-            if (metadata.get("url") != null && driver != null) {
+            if (metadata.get("url") != null && screenshot != null) {
                 try {
                     // load the page
-                    driver.get(metadata.get("url"));
-                    driver.manage().window().maximize();
-                    
-                    // wait for page to render completely
-                    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-                    wait.until(webDriver -> "complete".equals(
-                            ((JavascriptExecutor) webDriver).executeScript("return document.readyState")
-                    ));
+                    String pngImage = screenshot.takeScreenshotAsBase64PNG(metadata.get("url"));
 
-                    // also wait for the body to appear completely.
-                    WebElement body = wait.until(webDriver -> driver.findElement(By.tagName("body")));
-
-                    String previousScreenshot;
-                    String currentscreenshot = "";
-                    int max = 20;
-
-                    // keep taking shots until all visual elements have stopped moving 
-                    do {
-                        TimeUnit.SECONDS.sleep(1);
-                        previousScreenshot = currentscreenshot;
-                        currentscreenshot = body.getScreenshotAs(OutputType.BASE64);    
-                    } while (previousScreenshot.equals(currentscreenshot) && max-- > 0);
-
-                    // store the screenshot as an output
-                    result.put("application/rascal+screenshot", currentscreenshot);
+                    if (!pngImage.isEmpty()) {
+                        result.put("application/rascal+screenshot", pngImage);
+                    }
+        
                 }
                 catch (Throwable e) {
                     shellErrorOutput.write(e.getMessage().getBytes("UTF-8"));
@@ -290,24 +205,6 @@ public class TutorCommandExecutor {
         result.put("application/rascal+stderr", getErrorOutput());
 
         return result;
-    }
-
-    private static RemoteWebDriver getBrowser(ChromeDriverService service, String BROWSER_BINARY) {
-        ChromeOptions options = new ChromeOptions()
-            .setHeadless(true)
-            .setBinary(BROWSER_BINARY)
-            .addArguments("--headless", "--disable-gpu", "--window-size=1900,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage")
-            .addArguments("--user-data-dir=/tmp/rascal-config/google-chrome")
-            .setLogLevel(ChromeDriverLogLevel.OFF)
-            ;
-        
-        RemoteWebDriver driver = new RemoteWebDriver(service.getUrl(), options);
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(60));
-        driver.manage().window().maximize();
-        
-        return driver;
     }
 
     public String uuencode(InputStream content) throws IOException {
