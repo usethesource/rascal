d714323ee8ae6c470e3d3053193ab47bc11f1335
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2024-07-16 19:12:21 +0200

    screenshot feature is not injected dynamically

---
+++ src/org/rascalmpl/tutor/lang/rascal/tutor/repl/ITutorScreenshotFeature.java
@@ -0,0 +1,30 @@
+package org.rascalmpl.tutor.lang.rascal.tutor.repl;
+
+/**
+ * A interface to be implemented by a depending project. The 
+ * screenshot feature is injected into the tutor command executor 
+ * by dynamic loading of the class mentioned in this resource:
+ * org/rascalmpl/tutor/screenshotter.config
+ * 
+ * The goal is to not have dependencies on large projects
+ * like selenium and Chrome in the core of the rascal project.
+ * 
+ * The tutor will work fine without a screenshotter, except that
+ * screenshots will not be included with the documentation. It is
+ * advisable to run the tutor using the rascal-maven-plugin, which
+ * makes sure that the screenshot feature is properly injected.
+ */
+public interface ITutorScreenshotFeature {
+
+    /**
+     * The URL string is expected to be syntactically correct.
+     * Typically it is localhost:<port>, to point at the right
+     * server that is currently visualizizing something from the
+     * REPL or the IDEServices.
+     * 
+     * @param  url localhost url
+     * @return a base64 encoded PNG snapshot.
+     */
+    String takeScreenshotAsBase64PNG(String url);
+
+}
