aecc1fa2f4b24f3f00ad8b8241919d1b8e5c21c9
Pieter Olivier <pieter.olivier@swat.engineering>
2024-08-28 11:17:40 +0200

    INtroduced StackNodeVisitor

---
+++ src/org/rascalmpl/parser/gtd/stack/StackNodeVisitorAdapter.java
@@ -0,0 +1,69 @@
+package org.rascalmpl.parser.gtd.stack;
+
+public class StackNodeVisitorAdapter<P> implements StackNodeVisitor<P> {
+
+    @Override
+    public void visit(AlternativeStackNode<P> node) {
+        // Do nothing fallback
+    }
+
+    @Override
+    public void visit(CaseInsensitiveLiteralStackNode<P> node) {
+        // Do nothing fallback
+    }
+
+    @Override
+    public void visit(CharStackNode<P> node) {
+        // Do nothing fallback
+    }
+
+    @Override
+    public void visit(EmptyStackNode<P> node) {
+        // Do nothing fallback
+    }
+
+    @Override
+    public void visit(EpsilonStackNode<P> node) {
+        // Do nothing fallback
+    }
+
+    @Override
+    public void visit(ListStackNode<P> node) {
+        // Do nothing fallback
+    }
+
+    @Override
+    public void visit(LiteralStackNode<P> node) {
+        // Do nothing fallback
+    }
+
+    @Override
+    public void visit(MultiCharacterStackNode<P> node) {
+        // Do nothing fallback
+    }
+
+    @Override
+    public void visit(NonTerminalStackNode<P> node) {
+        // Do nothing fallback
+    }
+
+    @Override
+    public void visit(OptionalStackNode<P> node) {
+        // Do nothing fallback
+    }
+
+    @Override
+    public void visit(SeparatedListStackNode<P> node) {
+        // Do nothing fallback
+    }
+
+    @Override
+    public void visit(SequenceStackNode<P> node) {
+        // Do nothing fallback
+    }
+
+    @Override
+    public void visit(SkippingStackNode<P> node) {
+        // Do nothing fallback
+    }
+}
