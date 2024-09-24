usethesource/rascal

commit 7d1fdbe40fca6bc7c768e42147d1eddf85fe5a7e

minor improvements to import graph

src/org/rascalmpl/library/vis/Graphs.rsc
@@ -336,7 +336,7 @@ data CytoLayout(CytoLayoutName name = dagre(), bool animate=false)
         CytoLayoutName name = CytoLayoutName::breadthfirst(),
         num spacingFactor= 1,
         list[str] roots = [],
-        bool circle=false,
+        bool circle=faculse,
         bool grid=!circle,
         bool directed=false
     )

src/org/rascalmpl/library/lang/rascal/vis/ImportGraph.rsc
@@ -46,8 +46,8 @@ void importGraph(PathConfig pcfg, bool hideExternals=true) {
     // let's start with a simple graph and elaborate on details in later versions
     g = { <from, "I", to> | <from, to> <- m.imports, hideExternals ==> to notin m.external}
       + { <from, "E", to> | <from, to> <- m.extends, hideExternals ==> to notin m.external}
-      + { <"_", "_", to>  |  to <- top(m.imports + m.extends) } // pull up the top modules
-      + { <from, "x", "x">  | from <- bottom(m.imports + m.extends), hideExternals ==> from notin m.external} // pull the bottom modules down.
+    //   + { <"_", "_", to>  |  to <- top(m.imports + m.extends) } // pull up the top modules
+    //   + { <from, "x", "x">  | from <- bottom(m.imports + m.extends), hideExternals ==> from notin m.external} // pull the bottom modules down.
       ;
 
     loc modLinker(str name) {
@@ -59,7 +59,7 @@ void importGraph(PathConfig pcfg, bool hideExternals=true) {
 
     default loc modLinker(value _) = |nothing:///|;
 
-    showInteractiveContent(graph(g, \layout=defaultDagreLayout(), nodeLinker=modLinker), title="Rascal Import/Extend Graph");
+    showInteractiveContent(graph(g, \layout=defaultDagreLayout(), nodeLinker=modLinker), title="Rascal Import/Extend Graph", edgeStyle=defaultEdgeStyle()[\curve-style=taxi()]);
 }
 
 @synopsis{Container for everything we need to know about the modules in a project to visualize it.}

commit c0384a08be67b1a4497863160d322e59804b081c

Fixed typo in false boolean
This fixes: 7d1fdbe40fca6bc7c768e42147d1eddf85fe5a7e

src/org/rascalmpl/library/vis/Graphs.rsc
@@ -336,7 +336,7 @@ data CytoLayout(CytoLayoutName name = dagre(), bool animate=false)
         CytoLayoutName name = CytoLayoutName::breadthfirst(),
         num spacingFactor= 1,
         list[str] roots = [],
-        bool circle=faculse,
+        bool circle=false,
         bool grid=!circle,
         bool directed=false
     )