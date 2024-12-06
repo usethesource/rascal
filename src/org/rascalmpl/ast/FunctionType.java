7ac40dc42e5c6fe00a8680eeccc66bec18aa9084
Sung-Shik Jongmans <sung-shik.jongmans@swat.engineering>
2024-12-06 12:33:19 +0100

    Regenerate parser and AST using module `Boostrap`

--- src/org/rascalmpl/ast/FunctionType.java
+++ src/org/rascalmpl/ast/FunctionType.java
@@ -19,6 +19,7 @@ package org.rascalmpl.ast;
 import io.usethesource.vallang.IConstructor;
 import io.usethesource.vallang.ISourceLocation;
 
+@SuppressWarnings(value = {"unused"})
 public abstract class FunctionType extends AbstractAST {
   public FunctionType(ISourceLocation src, IConstructor node) {
     super(src /* we forget node on purpose */);
--- src/org/rascalmpl/ast/Mapping_Expression.java
+++ src/org/rascalmpl/ast/Mapping_Expression.java
@@ -19,6 +19,7 @@ package org.rascalmpl.ast;
 import io.usethesource.vallang.IConstructor;
 import io.usethesource.vallang.ISourceLocation;
 
+@SuppressWarnings(value = {"unused"})
 public abstract class Mapping_Expression extends AbstractAST {
   public Mapping_Expression(ISourceLocation src, IConstructor node) {
     super(src /* we forget node on purpose */);
