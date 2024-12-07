a2a3e5e66eff03b93070ad1e219a8cf042241a0c
Pieter Olivier <pieter.olivier@swat.engineering>
2024-12-07 11:06:17 +0100

    Implementend memoization of skipped nodes

--- src/org/rascalmpl/parser/gtd/result/out/SkippedNodeFlattener.java
+++ src/org/rascalmpl/parser/gtd/result/out/SkippedNodeFlattener.java
@@ -11,6 +11,10 @@
 *******************************************************************************/
 package org.rascalmpl.parser.gtd.result.out;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 import org.rascalmpl.parser.gtd.location.PositionStore;
 import org.rascalmpl.parser.gtd.result.SkippedNode;
 
@@ -18,20 +22,55 @@ import org.rascalmpl.parser.gtd.result.SkippedNode;
  * A converter for result nodes that contain skipped characters for error recovery
  */
 public class SkippedNodeFlattener<T, P>{
+	private static class MemoKey {
+		private int offset;
+		private int length;
+
+		public MemoKey(SkippedNode node) {
+			this.offset = node.getOffset();
+			this.length = node.getLength();
+		}
+
+		@Override
+		public boolean equals(Object peer) {
+			MemoKey peerKey = (MemoKey) peer;
+			return offset == peerKey.offset && length == peerKey.length;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(offset, length);
+		}
+	}
+
+	private Map<MemoKey, T> memoTable;
+
 	public SkippedNodeFlattener(){
 		super();
+		
+		memoTable = new HashMap<>();
 	}
 	
 	public T convertToUPTR(INodeConstructorFactory<T, P> nodeConstructorFactory, SkippedNode node, PositionStore positionStore){
-		T result = nodeConstructorFactory.createSkippedNode(node.getSkippedChars());
+		MemoKey key = new MemoKey(node);
+
+		T result = memoTable.get(key);
+
+		if (result != null) {
+			return result;
+		}
+
+		result = nodeConstructorFactory.createSkippedNode(node.getSkippedChars());
 
 		// Add source location
 		if (node.getInputUri() != null) {
-		int startOffset = node.getOffset();
-		int endOffset = startOffset + node.getLength();
+			int startOffset = node.getOffset();
+			int endOffset = startOffset + node.getLength();
 			P sourceLocation = nodeConstructorFactory.createPositionInformation(node.getInputUri(), startOffset, endOffset, positionStore);
-		result = nodeConstructorFactory.addPositionInformation(result, sourceLocation);
+			result = nodeConstructorFactory.addPositionInformation(result, sourceLocation);
 		}
+
+		memoTable.put(key, result);
 
 		return result;
 	}
