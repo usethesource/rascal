a5c7a6d5e29e05618221fd44d3a092af26c331f7
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2025-03-09 15:32:20 +0100

    copying typepal sources (and nothing else) from typepal dependency to org/rascalmpl/typepal such that checker, compiler and tutor can load its sources

--- src/org/rascalmpl/shell/RascalCheck.java
+++ src/org/rascalmpl/shell/RascalCheck.java
@@ -2,6 +2,6 @@ package org.rascalmpl.shell;
 
 public class RascalCheck extends AbstractCommandlineTool {
     public static void main(String[] args) {
-        main("lang::rascalcore::check::Checker", new String[] {"org/rascalmpl/compile"}, args);
+        main("lang::rascalcore::check::Checker", new String[] {"org/rascalmpl/compiler","org/rascalmpl/typepal"}, args);
     }
 }
