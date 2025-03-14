6fb37f40e136b1a069db4b00ee12fa90c5fdfe2e
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2025-03-14 11:32:31 +0100

    added junit test runner

--- src/org/rascalmpl/compiler/lang/rascalcore/java/JavaRunner.java
+++ src/org/rascalmpl/compiler/lang/rascalcore/java/JavaRunner.java
@@ -2,11 +2,21 @@ package org.rascalmpl.compiler.lang.rascalcore.java;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.URISyntaxException;
 
+import org.junit.runner.Description;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunListener;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.JUnit4;
+import org.junit.runners.model.InitializationError;
 import org.rascalmpl.exceptions.RuntimeExceptionFactory;
+import org.rascalmpl.library.Messages;
 import org.rascalmpl.uri.classloaders.SourceLocationClassLoader;
 
 import io.usethesource.vallang.IList;
+import io.usethesource.vallang.IListWriter;
+import io.usethesource.vallang.ISourceLocation;
 import io.usethesource.vallang.IString;
 import io.usethesource.vallang.IValueFactory;
 
@@ -15,6 +25,34 @@ public class JavaRunner {
 
     public JavaRunner(IValueFactory vf) {
         this.vf = vf;
+    }
+
+    /**
+     * This is to translate JUnit test results to Rascal messages
+     */
+    private final class TestRunListener extends RunListener {
+        private final Class<?> clz;
+        private final IListWriter messages;
+
+        private TestRunListener(Class<?> clz, IListWriter messages) {
+            this.clz = clz;
+            this.messages = messages;
+        }
+
+        @Override
+        public void testFinished(Description description) throws Exception {
+            messages.append(Messages.info(description.getDisplayName() + " succeeded", locForClass(clz)));
+        }
+
+        @Override
+        public void testFailure(Failure failure) throws Exception {
+            messages.append(Messages.error(failure.getDescription().getDisplayName() + " failed", locForClass(clz)));
+        }
+
+        @Override
+        public void testIgnored(Description description) throws Exception {
+            messages.append(Messages.info(description.getDisplayName() + " ignored", locForClass(clz)));
+        }
     }
     
     public void runJavaMain(IString qname, IList args, IList classpath) {
@@ -44,6 +82,44 @@ public class JavaRunner {
         }
         catch (InvocationTargetException e) {
             throw RuntimeExceptionFactory.javaException(e, null, null);
+        }
+    }
+
+    private ISourceLocation locForClass(Class<?> clz) {
+        try {
+            return vf.sourceLocation(clz.getProtectionDomain().getCodeSource().getLocation().toURI());
+        }
+        catch (URISyntaxException e) {
+           throw RuntimeExceptionFactory.illegalArgument(vf.string(e.getMessage()));
+        }
+    }
+
+    public IList runJUnitTestClass(IString qname, IList classpath) {
+        var loader = new SourceLocationClassLoader(classpath, getClass().getClassLoader());
+
+        try {
+            Class<?> clz = loader.loadClass(qname.getValue());
+            IListWriter messages = vf.listWriter();
+            
+            RunListener listener = new TestRunListener(clz, messages);
+            RunNotifier notifier = new RunNotifier();
+            notifier.addFirstListener(listener);
+            
+            new JUnit4(clz).run(notifier);
+
+            return messages.done();
+        }
+        catch (ClassNotFoundException e) {
+            throw RuntimeExceptionFactory.illegalArgument(vf.string("Class not found: " + e.getMessage()));
+        }
+        catch (SecurityException e) {
+            throw RuntimeExceptionFactory.illegalArgument(vf.string("Security problem: " + e.getMessage()));
+        }
+        catch (IllegalArgumentException e) {
+            throw RuntimeExceptionFactory.illegalArgument(vf.string(e.getMessage()));
+        }
+        catch (InitializationError e) {
+            throw RuntimeExceptionFactory.illegalArgument(vf.string("JUnit initialization error: " + e.getMessage()));
         }
     }
 }
