b2f7973c44f882fbba36b31193b0cea9d8b804bf
Davy Landman <davy.landman@gmail.com>
2025-03-28 12:04:16 +0100

    Updated the download-test-jar script

--- src/org/rascalmpl/compiler/lang/rascalcore/check/tests/download-test-jars.sh
+++ src/org/rascalmpl/compiler/lang/rascalcore/check/tests/download-test-jars.sh
@@ -7,10 +7,9 @@ function download() {
 }
 
 # these should be synced with `TestConfigs.rsc`
-download "org.rascalmpl" "rascal" "0.40.17" 
+download "org.rascalmpl" "rascal" "0.41.0-RC15" 
 download "org.rascalmpl" "typepal" "0.14.8" 
 download "org.rascalmpl" "typepal" "0.14.1" 
-download "org.rascalmpl" "rascal-core" "0.12.17" 
 download "org.rascalmpl" "drambiguity" "0.1.2" 
 download "org.rascalmpl" "flybytes" "0.1.5" 
 download "org.rascalmpl" "salix-core" "0.2.7" 
