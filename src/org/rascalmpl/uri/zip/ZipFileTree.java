75ddf3330989d551b5c34fbe31093a3f4b6cd1e3
Davy Landman <davy.landman@swat.engineering>
2025-03-31 18:04:04 +0200

    Rewrote the FileTree by a more complete mapping of the file tree, to be able to deal with specific errors that the tests found

--- src/org/rascalmpl/uri/zip/ZipFileTree.java
+++ src/org/rascalmpl/uri/zip/ZipFileTree.java
@@ -1,38 +1,46 @@
 /*******************************************************************************
- * Copyright (c) 2015-2021 CWI All rights reserved. This program and the accompanying materials are
- * made available under the terms of the Eclipse Public License v1.0 which accompanies this
- * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2009-2025 CWI
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors: * Davy Landman - Davy.Landman@cwi.nl - CWI * Jurgen Vinju - Jurgen.Vinju@cwi.nl -
- * CWI
+ * Contributors: 
+ *     * Davy Landman - Davy.Landman@cwi.nl - CWI
+ *     * Jurgen Vinju - Jurgen.Vinju@cwi.nl - CWI
  *******************************************************************************/
 package org.rascalmpl.uri.zip;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
-import org.rascalmpl.uri.FileTree;
+public class ZipFileTree extends CompressedFSTree {
+    public ZipFileTree(File jar) {
+        super(IndexedFSEntry.forFile(jar), openZip(jar));
+    }
 
-public class ZipFileTree extends FileTree {
-  public ZipFileTree(File jar) {
-    super();
-    totalSize = 0;
-    try (ZipFile jarFile = new ZipFile(jar)) {
-      for (Enumeration<? extends ZipEntry> e = jarFile.entries(); e.hasMoreElements();) {
-        ZipEntry je = e.nextElement();
-        if (je.isDirectory()) {
-          continue;
-        }
-        String name = je.getName();
-        totalSize += 8 + (name.length() * 2);
-        fs.put(name, new FSEntry(je.getTime()));
-      }
+    private static EntryEnumerator openZip(File zipFile) {
+        return () -> {
+            var zip = new ZipFile(zipFile);
+            var actual = zip.entries();
+            return new EntryEnumerator.CloseableIterator() {
+                @Override
+                public void close() throws IOException {
+                    zip.close();
+                }
+
+                @Override
+                public boolean hasNext() {
+                    return actual.hasMoreElements();
+                }
+
+                @Override
+                public ZipEntry next() {
+                    return actual.nextElement();
+                }
+            };
+        };
     }
-    catch (IOException e1) {
-      throwMe = e1;
-    }
-  }
 }
