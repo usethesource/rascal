1cc0c9d0894f4674811cbe2694c3bf5fd6698e41
Davy Landman <davy.landman@swat.engineering>
2025-04-02 18:25:34 +0200

    Some more test around the test framework

--- test/org/rascalmpl/uri/fs/FileSystemTreeTest.java
+++ test/org/rascalmpl/uri/fs/FileSystemTreeTest.java
@@ -1,6 +1,7 @@
 package org.rascalmpl.uri.fs;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertThrows;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -10,41 +11,59 @@ import org.junit.Test;
 
 public class FileSystemTreeTest {
     private FileSystemTree<FSEntry> target;
+    private long currentTime = 0;
 
     @Before
     public void initTarget() throws IOException {
         target = new FileSystemTree<FSEntry>(new FSEntry(0, 0));
+        currentTime = 0;
     }
+
+    private void addFile(String name) throws IOException {
+        addFile(name, currentTime, currentTime);
+        currentTime++;
+    }
+    private void addFile(String name, long created, long lastModified) throws IOException {
+        target.addFile(name, new FSEntry(created, lastModified), FSEntry::new);
+    }
+    private void addDirectory(String name) throws IOException {
+        addDirectory(name, currentTime, currentTime);
+        currentTime++;
+    }
+    private void addDirectory(String name, long created, long lastModified) throws IOException {
+        target.addDirectory(name, new FSEntry(created, lastModified), FSEntry::new);
+    }
+
 
     @Test
     public void emptyDirectoryListing() throws IOException {
         assertArrayEquals(new String[0], target.directChildren("/"));
-        target.addDirectory("a", new FSEntry(1, 1), FSEntry::new);
+        addDirectory("a");
         assertArrayEquals(new String[0], target.directChildren("/a"));
     }
 
     @Test
     public void afterAddingFileItShouldShow() throws IOException {
-        target.addFile("f1.txt", new FSEntry(2, 2), FSEntry::new);
+        addFile("f1.txt");
         assertArrayEquals(new String[] { "f1.txt" }, target.directChildren("/"));
-        target.addFile("a/f2.txt", new FSEntry(2, 2), FSEntry::new);
+        addFile("a/f2.txt");
         assertArrayEquals(new String[] { "f2.txt" }, target.directChildren("a"));
     }
 
     @Test
     public void prefixSlashShouldNotMatter() throws IOException {
-        target.addFile("a", new FSEntry(0, 0), FSEntry::new);
+        addFile("a");
         assertArrayEquals("no slash", new String[] { "a" }, target.directChildren(""));
         assertArrayEquals("single slash", new String[] { "a" }, target.directChildren("/"));
         assertArrayEquals("triple slash", new String[] { "a" }, target.directChildren("///"));
 
-        target.addFile("b/f1.txt", new FSEntry(2, 2), FSEntry::new);
+        addFile("b/f1.txt");
         assertArrayEquals(new String[] { "f1.txt" }, target.directChildren("///b"));
     }
 
     @Test
     public void postfixSlashShouldNotMatter() throws IOException {
-        target.addFile( "a/f1.txt", new FSEntry(2, 2), FSEntry::new);
+        addFile("a/f1.txt");
         assertArrayEquals("single slash should be fine", new String[] { "f1.txt" }, target.directChildren( "a/"));
         assertArrayEquals("triple slash should be fine", new String[] { "f1.txt" }, target.directChildren( "a///"));
         assertArrayEquals("prefix and double slash should be fine", new String[] { "f1.txt" }, target.directChildren("///a///"));
@@ -52,20 +71,33 @@ public class FileSystemTreeTest {
 
     @Test
     public void makeIntermediateDirectories() throws IOException {
-        target.addFile("a/b/c/d.txt", new FSEntry(2, 2), FSEntry::new);
-        target.addFile("/a/b/c/d2.txt", new FSEntry(3, 3), FSEntry::new);
+        addFile("a/b/c/d.txt");
+        addFile("/a/b/c/d2.txt");
         assertArrayEquals(new String[] { "c" }, target.directChildren("a/b"));
         assertArrayEquals(new String[] { "d.txt", "d2.txt" }, sorted(target.directChildren("a/b/c")));
     }
 
     @Test
     public void makeNewDirectories() throws IOException {
-        target.addDirectory("/b/c", new FSEntry(2, 2), FSEntry::new);
+        addDirectory("/b/c");
         assertArrayEquals(new String[] { "b" }, target.directChildren("/"));
         assertArrayEquals(new String[] { "c" }, target.directChildren("/b"));
-        target.addDirectory("d/e/", new FSEntry(2, 2), FSEntry::new);
+        addDirectory("d/e/");
         assertArrayEquals(new String[] { "b", "d" }, sorted(target.directChildren("/")));
         assertArrayEquals(new String[] { "e" }, target.directChildren("/d"));
+    }
+
+
+    @Test
+    public void delete() throws IOException {
+        addFile("a/b/c.txt");
+        assertThrows(IOException.class, () -> target.remove("a"));
+        assertThrows(IOException.class, () -> target.remove("a/b"));
+        target.remove("a/b/c.txt");
+        assertArrayEquals(new String[]{ "b"}, target.directChildren("a"));
+        assertArrayEquals(new String[0], target.directChildren("a/b"));
+        target.remove("a/b");
+        target.remove("a");
     }
 
     private static String[] sorted(String [] input) {
