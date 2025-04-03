1606772c4e49ebc8964de7fd6fb3ecc10e7bb25b
paulklint <Paul.Klint@cwi.nl>
2025-04-03 14:42:46 +0200

    Removed print

--- src/org/rascalmpl/runtime/RascalRuntimeValueFactory.java
+++ src/org/rascalmpl/runtime/RascalRuntimeValueFactory.java
@@ -118,11 +118,6 @@ public class RascalRuntimeValueFactory extends RascalValueFactory {
 
     private Class<IGTD<IConstructor, ITree, ISourceLocation>> generateParser(IMap grammar) {
         try {
-        	System.err.println("generateParser: " + grammar.hashCode());
-//        	for(IValue key : grammar) {
-//        		System.err.println(key + ":\n\t" + grammar.get(key));
-//        	}
-        	
             return getParserGenerator().getNewParser(rex, URIUtil.rootLocation("parser-generator"), "$GENERATED_PARSER$" + Math.abs(grammar.hashCode()), grammar);
         } 
         catch (ExceptionInInitializerError e) {
