7a0dda7946bdbbca42c075d4b43e947d067a4b08
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2025-05-30 14:49:23 +0200

    removed dead code

--- src/org/rascalmpl/runtime/$RascalModule.java
+++ src/org/rascalmpl/runtime/$RascalModule.java
@@ -47,7 +47,6 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.rascalmpl.ast.SyntaxRoleModifier;
 import org.rascalmpl.debug.IRascalMonitor;
 import org.rascalmpl.exceptions.JavaMethodLink;
 import org.rascalmpl.exceptions.RuntimeExceptionFactory;
@@ -254,41 +253,6 @@ public abstract class $RascalModule /*extends ATypeFactory*/ {
         }
     }
     
-    private static void $usage(String module, String error, Type kwargs) {
-        PrintWriter $ERR = new PrintWriter(System.err);
-        
-        if (!error.isEmpty() && !error.equals("help")) {
-            $ERR.println(error);
-        }
-        
-        $ERR.println("Usage: ");
-        $ERR.println("java -cp ... " + module + " <options>");
-
-        if (kwargs.getArity() > 0) {
-            $ERR.println(" [options]\n\nOptions:\n");
-
-            for (String param : kwargs.getFieldNames()) {
-                $ERR.print("\t-");
-                $ERR.print(param);
-                if (kwargs.getFieldType(param).isSubtypeOf(TypeFactory.getInstance().boolType())) {
-                    $ERR.println("\t[arg]: one of nothing (true), \'1\', \'0\', \'true\' or \'false\';");
-                }
-                else {
-                    $ERR.println("\t[arg]: " + kwargs.getFieldType(param) + " argument;");
-                }
-            }
-        }
-        else {
-            $ERR.println('\n');
-        }
-        
-        $ERR.flush();
-        
-        if (!error.equals("help")) {
-            throw new IllegalArgumentException();
-        }
-    }
-
     protected static Map<String, IValue> $parseCommandlineParameters(String module, String[] commandline, Type kwTypes) {
 		// reusing the same commandline parameter parser that the interpreter uses, based on the keyword parameter types
 		// of the function type of the `main` function.
