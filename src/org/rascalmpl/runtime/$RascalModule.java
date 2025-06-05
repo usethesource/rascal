3ef5b84a8892a3a112a83a2b5abf80f889458cf3
paulklint <Paul.Klint@cwi.nl>
2025-06-05 09:28:10 +0200

    Various improvements

--- src/org/rascalmpl/runtime/$RascalModule.java
+++ src/org/rascalmpl/runtime/$RascalModule.java
@@ -119,7 +119,7 @@ public abstract class $RascalModule {
     
     /*package*/  final IRascalMonitor $MONITOR;
 
-    protected final RascalExecutionContext rex;
+    protected final RascalExecutionContext $rex;
 	
     //protected final IValueFactory $RVF;
     public final TypeFactory $TF;
@@ -127,13 +127,13 @@ public abstract class $RascalModule {
     public final TypeStore $TS;
 	protected final Traverse $TRAVERSE;
 	
-	public final IBool Rascal_TRUE;
-	public final IBool Rascal_FALSE;
+	private final IBool Rascal_TRUE;
+	private final IBool Rascal_FALSE;
 
 	protected final FailReturnFromVoidException $failReturnFromVoidException;
     
     public $RascalModule(RascalExecutionContext rex){
-    	this.rex = rex;
+    	this.$rex = rex;
     	$IN = rex.getInReader();
     	$OUTWRITER = rex.getOutWriter();
     	$ERRWRITER = rex.getErrWriter();
@@ -425,41 +425,30 @@ public abstract class $RascalModule {
 	}
 	
 	public io.usethesource.vallang.type.Type $sort(String adtName){
-		Type adtType = $TF.abstractDataType($TS, adtName);
-		$TS.declareAbstractDataType(adtType);
-		return new NonTerminalType($RVF.constructor(RascalValueFactory.Symbol_Sort, $RVF.string(adtName)));
+		return $RTF.nonTerminalType($RVF.constructor(RascalValueFactory.Symbol_Sort, $RVF.string(adtName)));
 	}
 	
 	public io.usethesource.vallang.type.Type $lex(String adtName){
-		Type adtType = $TF.abstractDataType($TS, adtName);
-		$TS.declareAbstractDataType(adtType);
-		return new NonTerminalType($RVF.constructor(RascalValueFactory.Symbol_Lex, $RVF.string(adtName)));
+		return $RTF.nonTerminalType($RVF.constructor(RascalValueFactory.Symbol_Lex, $RVF.string(adtName)));
 	}
 	
 	public io.usethesource.vallang.type.Type $layouts(String adtName){
-		Type adtType = $TF.abstractDataType($TS, adtName);
-		$TS.declareAbstractDataType(adtType);
-		return new NonTerminalType($RVF.constructor(RascalValueFactory.Symbol_Layouts, $RVF.string(adtName)));
+		return $RTF.nonTerminalType($RVF.constructor(RascalValueFactory.Symbol_Layouts, $RVF.string(adtName)));
 	}
 	public io.usethesource.vallang.type.Type $keywords(String adtName){
-		Type adtType = $TF.abstractDataType($TS, adtName);
-		$TS.declareAbstractDataType(adtType);
-		return new NonTerminalType($RVF.constructor(RascalValueFactory.Symbol_Keywords, $RVF.string(adtName)));
+		return $RTF.nonTerminalType($RVF.constructor(RascalValueFactory.Symbol_Keywords, $RVF.string(adtName)));
 	}
 	
 	public io.usethesource.vallang.type.Type $parameterizedAdt(String adtName, Type[] tparams){
-		Type adtType = $TF.abstractDataType($TS, adtName, tparams);
-		return adtType;
+		return $TF.abstractDataType($TS, adtName, tparams);
 	}
 
 	public io.usethesource.vallang.type.Type $parameterizedSort(String adtName, Type[] tparams, IList vparams){
-		$TF.abstractDataType($TS, adtName, tparams);
-		return new NonTerminalType($RVF.constructor(RascalValueFactory.Symbol_ParameterizedSort, $RVF.string(adtName), vparams));
+		return $RTF.nonTerminalType($RVF.constructor(RascalValueFactory.Symbol_ParameterizedSort, $RVF.string(adtName), vparams));
 	}
 	
 	public io.usethesource.vallang.type.Type $parameterizedLex(String adtName, Type[] tparams, IList vparams){
-		$TF.abstractDataType($TS, adtName, tparams);
-		return new NonTerminalType($RVF.constructor(RascalValueFactory.Symbol_ParameterizedLex, $RVF.string(adtName), vparams));
+		return $RTF.nonTerminalType($RVF.constructor(RascalValueFactory.Symbol_ParameterizedLex, $RVF.string(adtName), vparams));
 	}
 	
 	 public IList readBinaryConstantsFile(Class<?> c, String path, int expected_length, String expected_md5Hash) {
@@ -3855,7 +3844,7 @@ public abstract class $RascalModule {
 	// Private methods for Slice Operator
 	
 	// ---- add ---------------------------------------------------------------
-	// The following package private functions $add, $product, $multiply, $divide, $intersect are never
+	// The following package private functions $add, $product, $product, $divide, $intersect are never
 	// called from generated code, but are used by the SliceOperator that needs these generic versions.
 
 	final IValue $add(final IValue lhs, final IValue rhs) {
@@ -4225,8 +4214,6 @@ public abstract class $RascalModule {
 				return $RVF.string(v.toString());
 			}
 		}
-		
-		
 }
 
 enum SliceOperator {
