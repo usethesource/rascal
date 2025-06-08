5c328ff94425a990e7248b105d5513a8c95462f6
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2025-06-08 14:11:28 +0200

    getting the main function to work

--- src/org/rascalmpl/shell/RascalPackage.java
+++ src/org/rascalmpl/shell/RascalPackage.java
@@ -1,7 +1,12 @@
 package org.rascalmpl.shell;
 
+import java.io.PrintWriter;
+
+import org.jline.terminal.Terminal;
+import org.rascalmpl.debug.IRascalMonitor;
+
 public class RascalPackage extends AbstractCommandlineTool {
-    public static void main(String[] args) {
-        main("lang::rascalcore::package::Packager", new String[] {"org/rascalmpl/compiler"}, args);
+    public static void main(String[] args, Terminal term, IRascalMonitor monitor, PrintWriter err, PrintWriter out) {
+        main("lang::rascalcore::package::Packager", new String[] {"org/rascalmpl/compiler"}, args, term, monitor, err, out);
     }
 }
