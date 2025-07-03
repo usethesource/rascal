9b75da884338f9666799a284cae35e2cf23579a4
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2025-07-03 16:09:32 +0200

    make sure the default BasicIDEServices can also factor out the project root while printing errors

--- src/org/rascalmpl/shell/ShellEvaluatorFactory.java
+++ src/org/rascalmpl/shell/ShellEvaluatorFactory.java
@@ -83,7 +83,7 @@ public class ShellEvaluatorFactory {
             stdout.println("PathConfig messages:");
             
             if (monitor instanceof IDEServices) {
-                ((IDEServices) monitor).registerDiagnostics(pcfg.getMessages());
+                ((IDEServices) monitor).registerDiagnostics(pcfg.getMessages(), projectRoot);
             } 
             else {
                 Messages.write(pcfg.getMessages(), pcfg.getProjectRoot(), stdout);
