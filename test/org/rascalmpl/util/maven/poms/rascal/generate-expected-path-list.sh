72b6a9130c16ed12846a9c7a06e9ae68014fe475
Pieter Olivier <pieter.olivier@swat.engineering>
2025-08-29 11:15:43 +0200

    Made check robust w.r.t. platform path separators

--- test/org/rascalmpl/util/maven/poms/rascal/generate-expected-path-list.sh
+++ test/org/rascalmpl/util/maven/poms/rascal/generate-expected-path-list.sh
@@ -3,5 +3,5 @@ CLASSFILE=`mktemp`
 mvn dependency:build-classpath -DincludeScope=compile -Dmdep.outputFile=$CLASSFILE
 REPO=`mvn help:evaluate -Dexpression=settings.localRepository -q -DforceStdout`
 PREFIX=$((${#REPO} + 2))
-tr ';' '\n' < $CLASSFILE | cut -c ${PREFIX}- | sort > expected-path-list.txt
+tr ';' '\n' < $CLASSFILE | cut -c ${PREFIX}- | tr '\\' '/' | sort > expected-path-list.txt
 rm $CLASSFILE
