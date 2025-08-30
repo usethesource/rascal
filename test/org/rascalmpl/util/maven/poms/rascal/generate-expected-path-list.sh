685928d8ababe7c29aef15129938eeec60e4c794
Pieter Olivier <pieter.olivier@swat.engineering>
2025-08-30 12:15:14 +0200

    Added some explanatory command on the use of the generate-expected-path-list script

--- test/org/rascalmpl/util/maven/poms/rascal/generate-expected-path-list.sh
+++ test/org/rascalmpl/util/maven/poms/rascal/generate-expected-path-list.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+# This script was used to generate "expected-path-list.txt"
+# When run in this directory it uses the "pom.xml" in this directory which is a frozen rascal pom to have a solid anchor.
+# We will only need to run this script again if we ever decide to change the "anchor" pom.
 CLASSFILE=`mktemp`
-mvn dependency:build-classpath -DincludeScope=compile -Dmdep.outputFile=$CLASSFILE
-REPO=`mvn help:evaluate -Dexpression=settings.localRepository -q -DforceStdout`
+# Use REPODEf if you want to do a run with an alternate (for instance empty) local repo 
+#REPODEF="-Dmaven.repo.local=D:\\tmp\\repo2"
+mvn --quiet dependency:build-classpath ${REPODEF} -DincludeScope=compile -Dmdep.outputFile=$CLASSFILE
+REPO=`mvn help:evaluate $REPODEF -Dexpression=settings.localRepository -q -DforceStdout`
 PREFIX=$((${#REPO} + 2))
 tr ';' '\n' < $CLASSFILE | cut -c ${PREFIX}- | tr '\\' '/' | sort > expected-path-list.txt
 rm $CLASSFILE
