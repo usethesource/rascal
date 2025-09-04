c6009244fe611dcc2a2b7339e496d438e73b8b70
Rodin Aarssen <rodin.aarssen@swat.engineering>
2025-09-04 15:50:22 +0200

    Moved the line and column base shifts out of ColumnMaps

--- src/org/rascalmpl/util/locations/LineColumnOffsetMap.java
+++ src/org/rascalmpl/util/locations/LineColumnOffsetMap.java
@@ -32,8 +32,6 @@ package org.rascalmpl.util.locations;
  * vallang uses UTF-32-bit codepoints, while lsp uses UTF-16, so in cases where a codepoint wouldn't fit inside 16bit char, it takes up two chars. Implementations of this class translate these efficiently.
  */
 public interface LineColumnOffsetMap {
-    int translateLine(int line);
-    int translateInverseLine(int line);
     int translateColumn(int line, int column, boolean isEnd);
     int translateInverseColumn(int line, int column, boolean isEnd);
 }
