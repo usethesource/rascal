c5fd3989846b88b85ef25350d71350f180828f5e
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2025-09-22 20:20:00 +0200

    tried to fix string stream by reusing apache.commons.io.ReaderInputStream

--- src/org/rascalmpl/repl/http/REPLContentServer.java
+++ src/org/rascalmpl/repl/http/REPLContentServer.java
@@ -44,6 +44,7 @@ import java.util.Map.Entry;
 import java.util.concurrent.CancellationException;
 import java.util.function.Function;
 
+import org.apache.commons.io.input.ReaderInputStream;
 import org.rascalmpl.library.lang.json.internal.JsonValueWriter;
 import org.rascalmpl.uri.URIResolverRegistry;
 import org.rascalmpl.values.ValueFactoryFactory;
@@ -235,63 +236,23 @@ public class REPLContentServer extends NanoHTTPD {
                     break;
             }
         }
-        var fixedContentType = new ContentType(mimeType.getValue()).tryUTF8();
-        Response response = newChunkedResponse(status, fixedContentType.getContentTypeHeader(), toInputStream(data, Charset.forName(fixedContentType.getEncoding())));
-        addHeaders(response, header);
-        return response;
+        
+        try {
+            var fixedContentType = new ContentType(mimeType.getValue()).tryUTF8();
+            Response response = newChunkedResponse(status, fixedContentType.getContentTypeHeader(), toInputStream(data, Charset.forName(fixedContentType.getEncoding())));
+            addHeaders(response, header);
+            return response;
+        }
+        catch (IOException e) {
+            return newFixedLengthResponse(Status.INTERNAL_ERROR, MIME_HTML, e.getMessage());
+        }
     }
 
-    private static InputStream toInputStream(IString data, Charset encoding) {
-        return new InputStream() {
-            boolean finished = false;
-            final Reader source = data.asReader();
-            final CharBuffer toSend = CharBuffer.allocate(1024).flip();
-            final CharsetEncoder enc = encoding.newEncoder();
-
-            @Override
-            public int read(byte[] b, int off, int len) throws IOException {
-                if (finished) {
-                    return -1;
-                }
-                var target = ByteBuffer.wrap(b, off, len);
-                while (target.remaining() > 0) {
-                    if (!toSend.hasRemaining()) {
-                        if (!fillBuffer()) {
-                            break;
-                        }
-                    }
-                    enc.encode(toSend, target, finished);
-                }
-                // todo figure out how much is written!
-                int read = target.position() - off;
-                return read == 0 ? -1 : read;
-            }
-
-            @Override
-            public int read() throws IOException {
-                var result = new byte[1];
-                int read = read(result, 0, 1);
-                if (read == -1) {
-                    return -1;
-                }
-                return result[0] & 0xFF;
-            }
-
-            private boolean fillBuffer() throws IOException {
-                assert !toSend.hasRemaining();
-                toSend.clear();
-                int read = source.read(toSend);
-                toSend.flip();
-                if (read == -1) {
-                    finished = true;
-                    return false;
-                }
-                return true;
-            }
-            
-        };
-
-
+    private static InputStream toInputStream(IString data, Charset encoding) throws IOException {
+        return new ReaderInputStream.Builder()
+            .setReader(data.asReader())
+            .setCharset(encoding)
+            .get();
     }
 
     private static void addHeaders(Response response, IMap header) {
