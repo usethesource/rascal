1d6acfb1793d26e1a68b13a1e3357c7b6bb872c7
Davy Landman <davy.landman@swat.engineering>
2025-11-17 10:57:50 +0100

    Replaced base64 code in the library and added java doc to the class

--- src/org/rascalmpl/util/StreamingBase64.java
+++ src/org/rascalmpl/util/StreamingBase64.java
@@ -17,8 +17,10 @@ import org.apache.commons.codec.binary.Base64OutputStream;
 import io.usethesource.vallang.io.binary.util.ByteBufferInputStream;
 
 
+/**
+ * Streaming translations of base64 contents
+ */
 public class StreamingBase64 {
-
 
     private static InputStream stringToLatinBytes(String source) {
         return stringToLatinBytes2(CharBuffer.wrap(source), s -> s);
@@ -26,7 +28,7 @@ public class StreamingBase64 {
 
     private static InputStream stringToLatinBytes(Reader source) {
         var buffer = new char[1024];
-        return stringToLatinBytes2(CharBuffer.wrap(buffer).flip(), cb -> {
+        return stringToLatinBytes2(CharBuffer.wrap(buffer).flip() /*empty*/, cb -> {
             try {
                 int read = source.read(buffer);
                 if (read == -1) {
@@ -69,10 +71,13 @@ public class StreamingBase64 {
 
 
     // we know this is only used for base64 chars, aka ascii/latin, so we're fine casting the byte to a char
-    private static OutputStream latinBytesTo(Writer target) {
+    private static OutputStream latinBytesTo(Writer target, boolean padding) {
         return new OutputStream() {
             @Override
             public void write(int b) throws IOException {
+                if (!padding && b == '=') {
+                    return;
+                }
                 target.write(b);
             }
             @Override
@@ -81,27 +86,39 @@ public class StreamingBase64 {
                 // to also our own char array and pass that
                 // it's the fastest path
                 char[] copy = new char[len];
+                int p = 0;
                 for (int i = off; i < off + len; i++) {
-                    copy[i - off] = (char)b[i];
+                    char c = (char)b[i];
+                    if (!padding && c == '=') {
+                        continue;
+                    }
+                    copy[p++] = c;
                 }
-                target.write(copy, 0, len);
+                target.write(copy, 0, p);
             }
         };
     }
 
     // we know this is only used for base64 chars, aka ascii/latin, so we're fine casting the byte to a char
-    private static OutputStream latinBytesTo(StringBuilder target) {
+    private static OutputStream latinBytesTo(StringBuilder target, boolean padding) {
         return new OutputStream() {
             @Override
             public void write(int b) throws IOException {
+                if (!padding && b == '=') {
+                    return;
+                }
                 target.append((char)((byte)0 & 0xFF));
             }
 
             @Override
             public void write(byte[] b, int off, int len) throws IOException {
                 for (int i = off; i < off + len; i++) {
+                    char c = (char)b[i];
+                    if (!padding && c == '=') {
+                        continue;
+                    }
                     // fast path as this appends to internal array
-                    target.append((char)b[i]);
+                    target.append(c);
                 }
             }
         };
@@ -109,26 +126,128 @@ public class StreamingBase64 {
 
 
 
+    /**
+     * Do a streaming base64 decode, reading base64 string while the inputstream is consumed
+     */
     public static InputStream decode(Reader source) {
         return new Base64InputStream(stringToLatinBytes(source), false);
     }
 
+    /**
+     * Do a streaming base64 decode, reading base64 string while the inputstream is consumed
+     */
     public static InputStream decode(String source) {
         return new Base64InputStream(stringToLatinBytes(source), false);
     }
 
+    /**
+     * Do a streaming base64 decode, reading base64 string (encoded as bytes) while the inputstream is consumed
+     */
     public static InputStream decode(byte[] source) {
         return new Base64InputStream(new ByteArrayInputStream(source), false);
     }
 
-
-    public static OutputStream encode(Writer target) {
-        return new Base64OutputStream(latinBytesTo(target), true, 1000, new byte[0]);
-    } 
-
-    public static OutputStream encode(StringBuilder target) {
-        return new Base64OutputStream(latinBytesTo(target), true, 1000, new byte[0]);
+    /** utility function to write the decoded bytes to a outputstream */
+    public static void decode(Reader source, OutputStream target) throws IOException {
+        copy(decode(source), target);
     }
 
-    
+    /** utility function to write the decoded bytes to a outputstream */
+    public static void decode(String source, OutputStream target) throws IOException {
+        copy(decode(source), target);
+    }
+
+    /** utility function to write the decoded bytes to a outputstream */
+    public static void decode(byte[] source, OutputStream target) throws IOException {
+        copy(decode(source), target);
+    }
+
+    private static void copy(InputStream decoder, OutputStream target) throws IOException {
+        try (var source = decoder) {
+            var buffer = new byte[3 * 1024]; // 4:3 ratio between base64 string and bytes produced.
+            int read = 0;
+            while ((read = source.read(buffer)) != -1) {
+                target.write(buffer, 0, read);
+            }
+        }
+    }
+
+
+
+    /**
+     * Create an OutputStream that on writing bytes, encodes them to the target writer
+     */
+    public static OutputStream encode(Writer target) {
+        return encode(target, true);
+    }
+
+    /**
+     * Create an OutputStream that on writing bytes, encodes them to the target writer, and you can disable the optional `=` padding characters
+     */
+    public static OutputStream encode(Writer target, boolean padding) {
+        return new Base64OutputStream(latinBytesTo(target, padding), true, 1000, new byte[0]);
+    } 
+
+    /**
+     * Create an OutputStream that on writing bytes, encodes them to the StringBuilder
+     */
+    public static OutputStream encode(StringBuilder target) {
+        return encode(target, true);
+    }
+
+    /**
+     * Create an OutputStream that on writing bytes, encodes them to the StringBuilder, and you can disable the optional `=` padding characters
+     */
+    public static OutputStream encode(StringBuilder target, boolean padding) {
+        return new Base64OutputStream(latinBytesTo(target, padding), true, 1000, new byte[0]);
+    }
+
+    /**
+     * Utility function that encodes a stream of bytes directly to a target
+     */
+    public static void encode(InputStream source, StringBuilder target, boolean padding) throws IOException {
+        copy(source, (buffer, len) -> {
+            for (int i = 0; i < len; i++) {
+                char c = (char)buffer[i];
+                if (!padding && c == '=') {
+                    continue;
+                }
+                // fast path as this appends to internal array
+                target.append(c);
+            }
+        });
+    }
+        
+    /**
+     * Utility function that encodes a stream of bytes directly to a target
+     */
+    public static void encode(InputStream source, Writer target, boolean padding) throws IOException {
+        copy(source, (buffer, len) -> {
+            var copy = new char[len];
+            var written = 0;
+            for (int i = 0; i < len; i++) {
+                char c = (char)buffer[i];
+                if (!padding && c == '=') {
+                    continue;
+                }
+                copy[written++] = c;
+            }
+            target.write(copy, 0, written);
+        });
+    }
+
+    @FunctionalInterface
+    private static interface CharWriter {
+        void write(byte[] c, int len) throws IOException;
+    }
+
+    private static void copy(InputStream source, CharWriter target) throws IOException {
+        try (var encoder = new Base64InputStream(source, true, 1000, new byte[0])) {
+            var buffer = new byte[4 * 1024]; // 3 source bytes generate 4 characters (encoded as bytes), so this aligns to buffer sizes
+            int read;
+            while ((read = encoder.read(buffer)) != -1) {
+                target.write(buffer, read);
+            }
+        }
+    }
 }
