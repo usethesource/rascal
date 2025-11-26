91f5a965f74ecbf4035e6ac8c459a992e893c641
Jean-Baptiste Doderlein <jean.baptiste.doderlein@gmail.com>
2025-11-26 11:26:56 +0100

    Undo change of forced push commit

--- src/org/rascalmpl/compiler/lang/rascalcore/check/tests/download-test-jars.sh
+++ src/org/rascalmpl/compiler/lang/rascalcore/check/tests/download-test-jars.sh
@@ -8,12 +8,10 @@ function download() {
 
 # these should be synced with `TestConfigs.rsc`
 download "org.rascalmpl" "rascal" "0.41.0-RC15" 
-download "org.rascalmpl" "rascal" "0.41.0-RC67"
 download "org.rascalmpl" "typepal" "0.14.8" 
 download "org.rascalmpl" "typepal" "0.14.1" 
 download "org.rascalmpl" "drambiguity" "0.1.2" 
 download "org.rascalmpl" "flybytes" "0.1.5" 
 download "org.rascalmpl" "salix-core" "0.2.7" 
 download "org.rascalmpl" "salix-contrib" "0.2.7" 
-download "org.rascalmpl" "rascal-lsp" "2.21.2" 
-download "org.rascalmpl" "java-air" "1.0.0-RC2"
+download "org.rascalmpl" "rascal-lsp" "2.21.2"
