7ff9b5fe5ec7c16c36e3557402018ff20832ebc9
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2025-12-02 12:19:48 +0100

    removed some old comments

--- src/org/rascalmpl/library/Prelude.java
+++ src/org/rascalmpl/library/Prelude.java
@@ -2626,7 +2626,6 @@ public class Prelude {
 			throw RuntimeExceptionFactory.illegalArgument(tree, "Missing lexical constructor");
 		}
 		
-		//Set implementation added here by Jurgen at 19/07/12 16:45
 		if (TreeAdapter.isList(tree)) {
 			if (type.isList() || splicing || isUntypedNodeType(type)) {
 				// if in node space, we also make a list; 
@@ -2655,7 +2654,6 @@ public class Prelude {
 				throw new Backtrack(RuntimeExceptionFactory.illegalArgument(tree, "Cannot match list with " + type));
 			}
 		}
-		//Changes end here
 		
 		if (TreeAdapter.isOpt(tree) && type.isBool()) {
 			IList args = TreeAdapter.getArgs(tree);
@@ -2726,21 +2724,7 @@ public class Prelude {
 			}
 			args = aw.done();
 			int length = args.length();
-			IMap comments = cw.done();
-			
-//			// this could be optimized.
-//			i = 0;
-//			int length = args.length();
-//			while (i < length) {
-//				if (TreeAdapter.isEmpty((IConstructor) args.get(i))) {
-//					length--;
-//					args = args.delete(i);
-//				}
-//				else {
-//					i++;
-//				}
-//			}
-			
+			IMap comments = cw.done();			
 			
 			java.lang.String constructorName = unescapedConsName(tree);			
 			
@@ -2748,8 +2732,7 @@ public class Prelude {
 				if (length == 1) {
 					// jump over injection
 					return implode(store, type, (ITree) args.get(0), splicing);
-				}
-				
+				}		
 				
 				// make a tuple if we're in node space
 				if (isUntypedNodeType(type)) {
