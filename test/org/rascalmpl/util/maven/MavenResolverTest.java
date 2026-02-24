1005b460ae7432f81650436eb6136a4ba6612f40
Toine Hartman <toine.hartman@swat.engineering>
2026-02-24 15:23:40 +0100

    Fix parent of sibling resolver.

--- test/org/rascalmpl/util/maven/MavenResolverTest.java
+++ test/org/rascalmpl/util/maven/MavenResolverTest.java
@@ -30,7 +30,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assume.assumeThat;
 
 import java.io.IOException;
 import java.nio.file.Files;
