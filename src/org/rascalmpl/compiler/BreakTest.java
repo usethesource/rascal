f7d96a9071b3ddb50321f8058998fd023affba12
Davy Landman <davy.landman@swat.engineering>
2026-03-17 17:41:42 +0100

    Working on breaking more tests

--- src/org/rascalmpl/compiler/BreakTest.java
+++ src/org/rascalmpl/compiler/BreakTest.java
@@ -4,24 +4,35 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 import org.rascalmpl.debug.IRascalMonitor;
+import org.rascalmpl.interpreter.Evaluator;
+import org.rascalmpl.interpreter.result.AbstractFunction;
 import org.rascalmpl.shell.RascalShell;
 import org.rascalmpl.shell.ShellEvaluatorFactory;
 import org.rascalmpl.test.infrastructure.RascalJUnitTestRunner;
 import org.rascalmpl.uri.URIResolverRegistry;
 
+import io.usethesource.vallang.IBool;
 import io.usethesource.vallang.ISourceLocation;
 
 public class BreakTest {
 
-    private static final String BREAKING_MODULE = "lang::rascalcore::check::tests::ChangeScenarioTests";
-    private static final String BREAKING_TEST = "fixedErrorsDisappear2";
+    // private static final String BREAKING_MODULE = "lang::rascalcore::check::tests::ChangeScenarioTests";
+    // private static final String BREAKING_TEST = "fixedErrorsDisappear2";
+    private static final String BREAKING_MODULE = "lang::rascalcore::check::tests::BinaryDependencyTests";
+    private static final String BREAKING_TEST = "notCompatibleAfterRemovingConstructor";
+
+    // notCompatibleAfterChangingFunctionArgument
+    // notCompatibleAfterChangingFunctionArgument
 
     private static final int PARALLEL_RUNS = 8; // set to 1 to avoid any multi-threading interactions, but it might take 20 rounds or something
     private static final int TRIES = 1000 / PARALLEL_RUNS;
@@ -75,19 +86,38 @@ public class BreakTest {
             evaluator.doImport(monitor, BREAKING_MODULE);
             try {
                 monitor.job(name, TRIES, (jobname, step) -> {
+                    String currentTest = "";
+                    var tests = evaluator.getHeap().getModule(BREAKING_MODULE).getTests();
                     try {
+                        tests = tests.stream()
+                            .filter(t -> !t.hasTag("ignore"))
+                            .collect(Collectors.toList());
+
+                        monitor.jobTodo(jobname, tests.size() * TRIES);
+
                         for (int i = 0; i < TRIES; i++) {
                             if (failed.get()) {
                                 return false;
                             }
+
+                            Collections.shuffle(tests);
+
                             monitor.jobStep(jobname, "Running: try " + (i + 1));
-                            evaluator.call(BREAKING_TEST);
+                            for (var t: tests) {
+                                currentTest = t.getName();
+                                monitor.jobStep(jobname, "Running: try " + (i + 1) + " => " + currentTest);
+                                var result = t.call();
+                                if (!((IBool)result).getValue()) {
+                                    throw new RuntimeException("Test " + currentTest +" returned false");
+                                }
+                            }
                         }
 
                     } catch (Throwable e ) {
                         failed.set(true);
                         iFailed.set(true);
-                        err.println("❌ test fail ");
+                        err.println("tests" + tests.stream().map(AbstractFunction::getName).toArray());
+                        err.println("❌ test fail :" + currentTest);
                         err.println(e);
                     }
                     return null;
@@ -104,6 +134,82 @@ public class BreakTest {
                 }
             }
 
+        }
+
+        if (iFailed.get()) {
+            errorPrinter.println("❌❌❌ Test run failed: " + name);
+            errorPrinter.println("Job output:");
+            errorPrinter.println(output.toString());
+            return true;
+        }
+        return false;
+    }
+
+    static boolean crashTestFreshEval(IRascalMonitor monitor, PrintWriter errorPrinter, String name, AtomicBoolean failed) {
+        var output = new StringWriter();
+        var iFailed = new AtomicBoolean(false);
+        try (var err = new PrintWriter(output, true); var out= new PrintWriter(output, false)) {
+            var projectRoot = RascalJUnitTestRunner.inferProjectRootFromClass(BreakTest.class);
+            monitor.job(name, TRIES * 2, (jobname, step) -> {
+                String currentTest = "";
+                var tests = Collections.<AbstractFunction>emptyList();
+                    try {
+                        for (int i = 0; i < TRIES; i++) {
+                            if (failed.get()) {
+                                return false;
+                            }
+                            monitor.jobStep(jobname, "Try " + (i + 1));
+                            var evaluator = ShellEvaluatorFactory.getDefaultEvaluatorForLocation(projectRoot, Reader.nullReader(), err, out, monitor, "$test-"+name+"$");
+                            try {
+
+                                evaluator.getConfiguration().setErrors(true);
+                                // make sure we're writing to the outputs
+                                evaluator.overwritePrintWriter(out, err);
+
+                                evaluator.doImport(monitor, BREAKING_MODULE);
+
+                                tests = evaluator.getHeap().getModule(BREAKING_MODULE).getTests()
+                                    .stream()
+                                    .filter(t -> !t.hasTag("ignore"))
+                                    .collect(Collectors.toList());
+
+                                if (i == 0) {
+                                    monitor.jobTodo(jobname, tests.size() * TRIES);
+                                }
+
+                                Collections.shuffle(tests);
+
+                                monitor.jobStep(jobname, "Running: try " + (i + 1));
+                                for (var t: tests) {
+                                    currentTest = t.getName();
+                                    monitor.jobStep(jobname, "Running: try " + (i + 1) + " => " + currentTest);
+                                    var result = t.call();
+                                    if (!((IBool)result).getValue()) {
+                                        throw new RuntimeException("Test " + currentTest +" returned false");
+                                    }
+                                }
+                            } finally {
+                                // clean up memory
+                                var memoryModule = evaluator.getHeap().getModule("lang::rascalcore::check::TestShared");
+                                var testRoot = memoryModule.getFrameVariable("testRoot");
+                                try {
+                                    URIResolverRegistry.getInstance().remove((ISourceLocation)testRoot.getValue(), true);
+                                }
+                                catch (Throwable e) {
+                                    err.println("Failure to cleanup the cache");
+                                }
+                            }
+                        }
+
+                } catch (Throwable e ) {
+                    failed.set(true);
+                    iFailed.set(true);
+                    err.println("tests: " + Arrays.deepToString(tests.stream().map(AbstractFunction::getName).toArray()));
+                    err.println("❌ test fail :" + currentTest);
+                    err.println(e);
+                }
+                return null;
+            });
         }
 
         if (iFailed.get()) {
