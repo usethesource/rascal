2b67b3676fa47ade02aad5e5fd8ecf0885cbf903
Rodin Aarssen <rodin.aarssen@swat.engineering>
2026-03-24 09:42:38 +0100

    Added IRemoteResolverRegistryClient interface

---
+++ src/org/rascalmpl/uri/vfs/IRemoteResolverRegistryClient.java
@@ -0,0 +1,11 @@
+package org.rascalmpl.uri.vfs;
+
+import org.eclipse.lsp4j.jsonrpc.services.JsonNotification;
+import org.rascalmpl.uri.ISourceLocationWatcher.ISourceLocationChangeType;
+
+import io.usethesource.vallang.ISourceLocation;
+
+public interface IRemoteResolverRegistryClient {
+    @JsonNotification("rascal/vfs/watcher/sourceLocationChanged")
+    void sourceLocationChanged(ISourceLocation root, ISourceLocationChangeType type, String watchId);
+}
