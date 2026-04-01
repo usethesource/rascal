9c29f7737b85dfd447270731984bc8229ba2a3b2
Rodin Aarssen <rodin.aarssen@swat.engineering>
2026-04-01 12:47:17 +0200

    Using @JsonSegment for cleaner jsonrpc interface defintions

--- src/org/rascalmpl/uri/vfs/IRemoteResolverRegistryClient.java
+++ src/org/rascalmpl/uri/vfs/IRemoteResolverRegistryClient.java
@@ -1,9 +1,11 @@
 package org.rascalmpl.uri.vfs;
 
 import org.eclipse.lsp4j.jsonrpc.services.JsonNotification;
+import org.eclipse.lsp4j.jsonrpc.services.JsonSegment;
 import org.rascalmpl.uri.remote.jsonrpc.ISourceLocationChanged;
 
+@JsonSegment("rascal/vfs/watcher")
 public interface IRemoteResolverRegistryClient {
-    @JsonNotification("rascal/vfs/watcher/sourceLocationChanged")
+    @JsonNotification
     void sourceLocationChanged(ISourceLocationChanged changed);
 }
