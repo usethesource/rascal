0ff434ff8b93bf6b636ee541cbe20da23d6ae01b
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2026-04-16 13:52:57 +0200

    fixed final issues, adding testsuite/module level statistics for use in the IDE and CI setups

--- src/org/rascalmpl/test/infrastructure/JUnitXMLReportListener.java
+++ src/org/rascalmpl/test/infrastructure/JUnitXMLReportListener.java
@@ -6,6 +6,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import javax.xml.stream.XMLOutputFactory;
@@ -19,31 +21,80 @@ import org.rascalmpl.uri.URIUtil;
 import io.usethesource.vallang.ISourceLocation;
 
 public class JUnitXMLReportListener implements ITestResultListener {
-    private XMLStreamWriter out;
     private final ISourceLocation folder;
     private final Map<String, ISourceLocation> modules;
     private String current = null;
+    private int tests = 0;
+    private int errors = 0;
+    private int failures = 0;
+    private int ignored = 0;
+    private long timestamp = 0L;
+    private List<Report> reports = new LinkedList<>();
 
     public JUnitXMLReportListener(ISourceLocation outputFolder, Map<String, ISourceLocation> modules) {
         this.folder = URIUtil.getChildLocation(outputFolder, "surefire-reports");
         this.modules = modules;
     }
 
-    @Override
-    public void start(String context, int count) {
-        try {
-            current = context;  
-            var xmlFile = targetXML(context);
+    private class Report {
+        public boolean ignored;
+        public boolean successful;
+        public String test;
+        public ISourceLocation loc;
+        public String message;
+        public Throwable exception;
 
-            out = XMLOutputFactory.newDefaultFactory()
-                .createXMLStreamWriter(URIResolverRegistry.getInstance().getOutputStream(xmlFile, false));
-            out.writeStartDocument();
-            out.writeStartElement("testsuite");    
+        public Report(boolean ignored, boolean successful, String test, ISourceLocation loc, String message, Throwable exception) {
+            this.ignored = ignored;
+            this.successful = successful;
+            this.test = test;
+            this.loc = loc;
+            this.message = message;
+            this.exception = exception;
         }
-        catch (IOException | XMLStreamException e) {
-            System.err.println("unexpected failure during test reporting");
-            throw new RuntimeException(e);   
+
+        public void write(XMLStreamWriter out) throws XMLStreamException {
+            out.writeStartElement("testcase");
+            out.writeAttribute("classname", current);
+            out.writeAttribute("name", test);
+            out.writeAttribute("file", modules.get(current).toString());
+            
+            if (ignored) {
+                out.writeEmptyElement("skipped");
+                out.writeStartElement("system-out");
+                out.writeCharacters("location: " + loc);
+                out.writeEndElement(); // system-out
+            }
+            else {
+                if (!successful) {
+                    out.writeStartElement(exception != null ? "error" : "failure" );
+                    out.writeAttribute("message", message);
+                    out.writeEndElement(); // error or failure
+                }
+
+                out.writeStartElement("system-out");
+                out.writeCharacters("location: " + loc);
+                if (exception != null) {
+                    StringWriter sw = new StringWriter();
+                    exception.printStackTrace(new PrintWriter(sw));
+                    out.writeCharacters("stacktrace:\n" + sw.toString());
+                }
+                out.writeEndElement(); // system-out
+            }
+
+            out.writeEndElement(); // testcase
         }
+    }
+
+    @Override
+    public void start(String module, int count) {
+        tests = 0;
+        errors = 0;
+        failures = 0;
+        ignored = 0;
+        timestamp = System.currentTimeMillis();
+        reports = new LinkedList<>();
+        current = module;  
     }
 
     private ISourceLocation targetXML(String context) {
@@ -52,62 +103,52 @@ public class JUnitXMLReportListener implements ITestResultListener {
 
     @Override
     public void report(boolean successful, String test, ISourceLocation loc, String message, Throwable exception) {
-        try {
-            startTestCase(test);
-            if (!successful) {
-                out.writeStartElement("failure");
-                    out.writeAttribute("message", message);
-                    if (exception != null) {
-                        StringWriter sw = new StringWriter();
-                        exception.printStackTrace(new PrintWriter(sw));
-                        out.writeCharacters(sw.toString());
-                    }
-                out.writeEndElement();
-                out.writeStartElement("system-out");
-                    out.writeCharacters("location: " + loc);
-                out.writeEndElement();
-            }
-            out.writeEndElement();
+        tests++;
+        if (exception != null) {
+            errors += 1;
         }
-        catch (XMLStreamException e) {
-            System.err.println("unexpected error during test reporting");
-            throw new RuntimeException(e);
+        else {
+            failures += 1;
         }
-    }
-
-    private void startTestCase(String test) throws XMLStreamException {
-        out.writeStartElement("testcase");
-        out.writeAttribute("classname", current);
-        out.writeAttribute("name", test);
-        out.writeAttribute("file", modules.get(current).toString());
+        reports.add(new Report(false, successful, test, loc, message, exception));
     }
 
     @Override
     public void ignored(String test, ISourceLocation loc) {
-        try {
-            startTestCase(test);
-            out.writeEmptyElement("skipped");
-            out.writeStartElement("system-out");
-                out.writeCharacters("location: " + loc);
-            out.writeEndElement();
-
-            out.writeEndElement();
-        }
-        catch (XMLStreamException e) {
-            System.err.println("unexpected error during test reporting");
-            throw new RuntimeException(e);
-        }
+        tests++;
+        ignored++;
+        reports.add(new Report(true, false, test, loc, "", null));
     }
 
     @Override
     public void done() {
         try {
+            var xmlFile = targetXML(current);
+
+            var out = XMLOutputFactory.newDefaultFactory()
+                .createXMLStreamWriter(URIResolverRegistry.getInstance().getOutputStream(xmlFile, false));
+            out.writeStartDocument();
+            
+            out.writeStartElement("testsuite");
+
+            out.writeAttribute("time", Long.toString(System.currentTimeMillis() - timestamp));
+            out.writeAttribute("timestamp", Long.toString(timestamp));
+            out.writeAttribute("ignored", Integer.toString(ignored));
+            out.writeAttribute("errors", Integer.toString(errors));
+            out.writeAttribute("tests", Integer.toString(tests));
+            out.writeAttribute("failures", Integer.toString(failures));
+
+            for(Report r : reports) {
+                r.write(out);
+            }
+
             out.writeEndElement();
+
             out.writeEndDocument();
             out.flush();
             out.close();
         }
-        catch (XMLStreamException e) {
+        catch (XMLStreamException | IOException e) {
             System.err.println("unexpected error during test reporting");
             throw new RuntimeException(e);
         }
