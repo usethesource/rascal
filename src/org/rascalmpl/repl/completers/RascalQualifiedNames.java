371fec8d66014bf75f46937ad67633190ac6037b
Toine Hartman <toine.hartman@swat.engineering>
2026-08-19 14:19:02 +0200

    Revert "Get keywords from auto-complete from stdlib."
    This reverts commit 9775112e739b03fb292f01d5303d50031e267322.

--- src/org/rascalmpl/repl/completers/RascalQualifiedNames.java
+++ src/org/rascalmpl/repl/completers/RascalQualifiedNames.java
@@ -26,19 +26,17 @@
  */
 package org.rascalmpl.repl.completers;
 
-import java.io.PrintWriter;
+import java.io.IOException;
 import java.io.Reader;
-import java.io.Writer;
+import java.net.URISyntaxException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-import org.rascalmpl.debug.NullRascalMonitor;
-import org.rascalmpl.shell.ShellEvaluatorFactory;
-
-import io.usethesource.vallang.ISet;
-import io.usethesource.vallang.IString;
+import org.rascalmpl.uri.URIResolverRegistry;
+import org.rascalmpl.values.ValueFactoryFactory;
 
 /**
  * Make sure we generate escapes before rascal keywords, such that `lang::rascal::syntax` becomes `lang::rascal::\syntax`
@@ -64,24 +62,66 @@ public class RascalQualifiedNames {
 
     private static final Set<String> RASCAL_KEYWORDS = new HashSet<String>();
 
-    private static void assureKeywordsAreLoaded() {
+    private static void assureKeywordsAreScrapped() {
+        // TODO: replace this with the `util::Reflective::getRascalReservedIdentifiers`
+        // BUT! it doesn't contain all the keywords, it's missing the `BasicType` ones like `int` etc
         if (RASCAL_KEYWORDS.isEmpty()) {
             synchronized (RASCAL_KEYWORDS) {
                 if (!RASCAL_KEYWORDS.isEmpty()) {
                     return;
                 }
 
-                var monitor = new NullRascalMonitor();
-                var eval = ShellEvaluatorFactory.getBasicEvaluator(Reader.nullReader(), new PrintWriter(Writer.nullWriter()), new PrintWriter(Writer.nullWriter()), monitor);
-                eval.doImport(monitor, "util::Reflective");
-
-                ((ISet) eval.call("getRascalReservedIdentifiers"))
-                    .stream()
-                    .filter(IString.class::isInstance)
-                    .map(IString.class::cast)
-                    .map(IString::getValue)
-                    .forEach(RASCAL_KEYWORDS::add);
-
+                String rascalGrammar = "";
+                URIResolverRegistry reg = URIResolverRegistry.getInstance();
+                try (Reader grammarReader = reg.getCharacterReader(ValueFactoryFactory.getValueFactory().sourceLocation("std", "", "/lang/rascal/syntax/Rascal.rsc"))) {
+                    StringBuilder res = new StringBuilder();
+                    char[] chunk = new char[8 * 1024];
+                    int read;
+                    while ((read = grammarReader.read(chunk, 0, chunk.length)) != -1) {
+                        res.append(chunk, 0, read);
+                    }
+                    rascalGrammar = res.toString();
+                }
+                catch (IOException | URISyntaxException e) {
+                    e.printStackTrace();
+                }
+                if (!rascalGrammar.isEmpty()) {
+                    /*
+                     * keyword RascalKeywords
+                     * = "o"
+                     * | "syntax"
+                     * | "keyword"
+                     * | "lexical"
+                     * ...
+                     * ;
+                     */
+                    Pattern findKeywordSection = Pattern.compile("^\\s*keyword([^=]|\\s)*=(?<keywords>([^;]|\\s)*);", Pattern.MULTILINE);
+                    Matcher m = findKeywordSection.matcher(rascalGrammar);
+                    if (m.find()) {
+                        String keywords = "|" + m.group("keywords");
+                        Pattern keywordEntry = Pattern.compile("\\s*[|]\\s*[\"](?<keyword>[^\"]*)[\"]");
+                        m = keywordEntry.matcher(keywords);
+                        while (m.find()) {
+                            RASCAL_KEYWORDS.add(m.group("keyword"));
+                        }
+                    }
+                    /*
+                     * syntax BasicType
+                    = \value: "value" 
+                    | \loc: "loc" 
+                    | \node: "node" 
+                     */
+                    Pattern findBasicTypeSection = Pattern.compile("^\\s*syntax\\s*BasicType([^=]|\\s)*=(?<keywords>([^;]|\\s)*);", Pattern.MULTILINE);
+                    m = findBasicTypeSection.matcher(rascalGrammar);
+                    if (m.find()) {
+                        String keywords = "|" + m.group("keywords");
+                        Pattern keywordEntry = Pattern.compile("\\s*[|][^:]*:\\s*[\"](?<keyword>[^\"]*)[\"]");
+                        m = keywordEntry.matcher(keywords);
+                        while (m.find()) {
+                            RASCAL_KEYWORDS.add(m.group("keyword"));
+                        }
+                    }
+                }
                 if (RASCAL_KEYWORDS.isEmpty()) {
                     RASCAL_KEYWORDS.add("syntax");
                 }
@@ -90,7 +130,7 @@ public class RascalQualifiedNames {
     }
 
     private static String escapeKeyword(String s) {
-        assureKeywordsAreLoaded();
+        assureKeywordsAreScrapped();
         if (RASCAL_KEYWORDS.contains(s.trim())) {
             return "\\" + s;
         }
@@ -103,5 +143,5 @@ public class RascalQualifiedNames {
         }
         return s;
     }
-
+    
 }
